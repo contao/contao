79b31af3e415c28d97dc72973c31da7ae9a79e02
Leo Feyer <github@contao.org>
2012-01-10 15:46:04 +0100

    [Comments] The library is now at library/Contao
    Ported the framework to the "Contao" namespace. Not much refactored yet
    though.

--- comments-bundle/src/Resources/contao/config/autoload.php
+++ comments-bundle/src/Resources/contao/config/autoload.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -29,28 +29,13 @@
 
 
 /**
- * Add content element
+ * Register the module classes
  */
-$GLOBALS['TL_CTE']['includes']['comments'] = 'ContentComments';
-
-
-/**
- * Front end modules
- */
-$GLOBALS['FE_MOD']['application']['comments'] = 'ModuleComments';
-
-
-/**
- * Back end modules
- */
-array_insert($GLOBALS['BE_MOD']['content'], 5, array
+Autoloader::addClasses(array
 (
-	'comments' => array
-	(
-		'tables'     => array('tl_comments'),
-		'icon'       => 'system/modules/comments/html/icon.gif',
-		'stylesheet' => 'system/modules/comments/html/style.css'
-	)
+	'Comments'        => 'system/modules/comments/Comments.php',
+	'ContentComments' => 'system/modules/comments/ContentComments.php',
+	'ModuleComments'  => 'system/modules/comments/ModuleComments.php'
 ));
 
 ?>
