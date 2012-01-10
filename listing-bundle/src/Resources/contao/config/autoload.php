0856d06d11063e92e208fbca7ffa0c97766f25e5
Leo Feyer <github@contao.org>
2012-01-10 15:46:04 +0100

    [Listing] The library is now at library/Contao
    Ported the framework to the "Contao" namespace. Not much refactored yet
    though.

--- listing-bundle/src/Resources/contao/config/autoload.php
+++ listing-bundle/src/Resources/contao/config/autoload.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -29,8 +29,11 @@
 
 
 /**
- * Front end module
+ * Register the module classes
  */
-$GLOBALS['FE_MOD']['application']['listing'] = 'ModuleListing';
+Autoloader::addClasses(array
+(
+	'ModuleListing' => 'system/modules/listing/ModuleListing.php'
+));
 
 ?>
