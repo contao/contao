51a8b568033c9b10282e1575f86ae39f929b1618
Leo Feyer <github@contao.org>
2014-10-31 15:38:10 +0100

    [News] Add the project resources

---
+++ news-bundle/src/ContaoNewsBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\NewsBundle;
+
+use Contao\CoreBundle\HttpKernel\Bundle\ContaoBundle;
+
+/**
+ * Configures the Contao news bundle.
+ *
+ * @author Leo Feyer <https://contao.org>
+ */
+class ContaoNewsBundle extends ContaoBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicFolders()
+    {
+        return [
+            $this->getPath() . '/../contao/assets'
+        ];
+    }
+}
