1b965797d152ab70f6dd2abc0e91d64970f8d831
Leo Feyer <github@contao.org>
2015-01-20 15:46:51 +0100

    [Core] Send the same cache headers for cached and uncached pages (see contao/core#7455)

--- core-bundle/contao/classes/Frontend.php
+++ core-bundle/contao/classes/Frontend.php
@@ -828,16 +828,16 @@ abstract class Frontend extends \Controller
 		if ($expire !== null && (\Config::get('cacheMode') == 'both' || \Config::get('cacheMode') == 'browser'))
 		{
 			$response->headers->set('Cache-Control', 'public, max-age=' . ($expire - time()));
-			$response->headers->set('Expires', gmdate('D, d M Y H:i:s', $expire) . ' GMT');
-			$response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', time()) . ' GMT');
 			$response->headers->set('Pragma', 'public');
+			$response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', time()) . ' GMT');
+			$response->headers->set('Expires', gmdate('D, d M Y H:i:s', $expire) . ' GMT');
 		}
 		else
 		{
-			$response->headers->set('Cache-Control', ['no-cache', 'pre-check=0, post-check=0']);
+			$response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+			$response->headers->set('Pragma', 'no-cache');
 			$response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT');
 			$response->headers->set('Expires', 'Fri, 06 Jun 1975 15:10:00 GMT');
-			$response->headers->set('Pragma', 'no-cache');
 		}
 
 		return $response;
