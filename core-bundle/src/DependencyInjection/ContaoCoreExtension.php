2eeaabd19c63e104df3bd769cf62b53a39576520
Yanick Witschi <yanick.witschi@terminal42.ch>
2015-02-23 21:49:40 +0100

    [Core] Added foundation for configuration files in prepends

--- core-bundle/src/DependencyInjection/ContaoCoreExtension.php
+++ core-bundle/src/DependencyInjection/ContaoCoreExtension.php
@@ -12,16 +12,42 @@ namespace Contao\CoreBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Adds the bundle services to the container.
  *
  * @author Leo Feyer <https://github.com/leofeyer>
+ * @author Yanick Witschi <https://github.com/toflar>
  */
-class ContaoCoreExtension extends Extension
+class ContaoCoreExtension extends Extension implements PrependExtensionInterface
 {
+    /**
+     * @var array
+     */
+    protected $configFiles = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        foreach ($this->configFiles as $file) {
+            $parsedConfig = Yaml::parse(
+                file_get_contents(
+                    __DIR__ . '/../Resources/config/' . $file
+                )
+            );
+
+            foreach ($parsedConfig as $bundleName => $bundleConfig) {
+                $container->prependExtensionConfig($bundleName, $bundleConfig);
+            }
+        }
+    }
+
     /**
      * {@inheritdoc}
      */
