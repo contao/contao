89003e85f47d36b70325d13e0a5024476bb3b962
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-02-23 17:53:10 +0100

    [Core] Add deprecated notice when using Contao\Database

--- core-bundle/contao/library/Contao/Database.php
+++ core-bundle/contao/library/Contao/Database.php
@@ -71,9 +71,12 @@ abstract class Database
 	/**
 	 * Establish the database connection
 	 *
-	 * @param array $arrConfig A configuration array
+     * @param array $arrConfig A configuration array
 	 *
-	 * @throws \Exception If a connection cannot be established
+     * @throws \Exception If a connection cannot be established
+     *
+     * @deprecated Deprecated since Contao 4, to be removed in Contao 5.
+     *             Use the Doctrine connection from Symfony container.
 	 */
 	protected function __construct(array $arrConfig)
 	{
@@ -133,6 +136,9 @@ abstract class Database
 	 * @param array $arrCustom A configuration array
 	 *
 	 * @return \Database The Database object
+     *
+     * @deprecated Deprecated since Contao 4, to be removed in Contao 5.
+     *             Use the Doctrine connection from Symfony container.
 	 */
 	public static function getInstance(array $arrCustom=null)
 	{
