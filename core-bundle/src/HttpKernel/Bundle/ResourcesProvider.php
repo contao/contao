266f810abe5b222a08c897f0e6d8b5ff0d8511b4
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-03-04 23:01:18 +0100

    [Core] Added missing docBlocks

--- core-bundle/src/HttpKernel/Bundle/ResourcesProvider.php
+++ core-bundle/src/HttpKernel/Bundle/ResourcesProvider.php
@@ -10,24 +10,44 @@
 
 namespace Contao\CoreBundle\HttpKernel\Bundle;
 
+/**
+ * Provides information about Contao resources and public folders.
+ *
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
 class ResourcesProvider
 {
     private $contaoResources = [];
     private $publicFolders = [];
     private $rootDir;
 
-
+    /**
+     * Constructor.
+     *
+     * @param string $kernelRootDir The kernel root directory
+     */
     public function __construct($kernelRootDir)
     {
         // We need relative paths starting from "TL_ROOT"
         $this->rootDir = dirname($kernelRootDir) . '/';
     }
 
+    /**
+     * Adds resource path of a bundle
+     *
+     * @param string $bundleName The bundle name
+     * @param string $path       The resources path
+     */
     public function addResourcesPath($bundleName, $path)
     {
         $this->contaoResources[$bundleName] = $path;
     }
 
+    /**
+     * Adds public folders
+     *
+     * @param array $paths The public folders
+     */
     public function addPublicFolders(array $paths)
     {
         foreach ($paths as $path) {
@@ -39,16 +59,31 @@ class ResourcesProvider
         }
     }
 
+    /**
+     * Returns all bundle names
+     *
+     * @return array The bundle names
+     */
     public function getBundleNames()
     {
         return array_keys($this->contaoResources);
     }
 
+    /**
+     * Returns all Contao resources paths
+     *
+     * @return array The resources paths
+     */
     public function getResourcesPaths()
     {
         return $this->contaoResources;
     }
 
+    /**
+     * Returns the public folders
+     *
+     * @return array The public folders
+     */
     public function getPublicFolders()
     {
         return $this->publicFolders;
