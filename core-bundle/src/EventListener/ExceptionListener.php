d1438ffd12d8610e72d40018305c3c1819a1b6e6
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-03-19 19:02:22 +0100

    [Core] Introduce new exception class DieNicelyException and handle it.

--- core-bundle/src/EventListener/ExceptionListener.php
+++ core-bundle/src/EventListener/ExceptionListener.php
@@ -10,6 +10,7 @@
 
 namespace Contao\CoreBundle\EventListener;
 
+use Contao\CoreBundle\Exception\DieNicelyException;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
 /**
@@ -26,5 +27,67 @@ class ExceptionListener
      */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
+        $exception = $event->getException();
+        do {
+            if ($exception instanceof DieNicelyException) {
+                $event->setResponse($this->handleDieNicelyException($exception));
+                return;
+            }
+        } while (null !== ($exception = $exception->getPrevious()));
+
+        // If nothing worked out, show the good old Contao screen.
+        $event->setResponse(
+            $this->handleDieNicelyException(
+                new DieNicelyException(
+                    'be_error',
+                    'An error occurred while executing this script!',
+                    500,
+                    array(),
+                    0,
+                    $event->getException()
+                )
+            )
+        );
+    }
+
+    /**
+     * Handle the die nicely exceptions.
+     *
+     * @param DieNicelyException $exception The exception
+     *
+     * @return Response The created response.
+     */
+    protected function handleDieNicelyException(DieNicelyException $exception)
+    {
+        $template = $exception->getTemplate();
+        if ($response = $this->tryReadTemplate(sprintf('%s/templates/%s.html5', TL_ROOT, $template))) {
+           return $response;
+        } elseif ($response = $this->tryReadTemplate(
+            sprintf('%s/../../contao/templates/backend/%s.html5', __DIR__, $template)
+        )) {
+            return $response;
+        }
+
+        // Show the message passed in the exception on the screen when template could not be located.
+        return new Response($exception->getMessage(), 500, array('Content-type' => ' text/html; charset=utf-8'));
+    }
+
+    /**
+     * Try to read a template.
+     *
+     * @param string $template The file path
+     *
+     * @return null|Response
+     */
+    private function tryReadTemplate($template)
+    {
+        if (file_exists($template)) {
+            ob_start();
+            include $template;
+
+            return new Response(ob_get_clean(), 500, array('Content-type' => ' text/html; charset=utf-8'));
+        }
+
+        return null;
     }
 }
