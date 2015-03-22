cf724b769bddbb130cf7c385abd404b4cf40d223
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-03-22 21:05:37 +0100

    [Core] Use the contao_root route for redirecting to the root page

--- core-bundle/src/Resources/contao/controllers/BackendPreview.php
+++ core-bundle/src/Resources/contao/controllers/BackendPreview.php
@@ -72,7 +72,7 @@ class BackendPreview extends \Backend
 		}
 		else
 		{
-			$objTemplate->url = $kernel->getContainer()->get('router')->generate('contao_frontend', array('alias' => ''), UrlGeneratorInterface::ABSOLUTE_URL);
+			$objTemplate->url = $kernel->getContainer()->get('router')->generate('contao_root', [], UrlGeneratorInterface::ABSOLUTE_URL);
 		}
 
 		// Switch to a particular member (see #6546)
