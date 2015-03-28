53624b65f104640e3e2a23aef992564b526d2c78
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-03-28 09:38:55 +0100

    [Core] Introduce redirect response exception.

---
+++ core-bundle/src/Exception/RedirectResponseException.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CoreBundle\Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Exception to send a redirect response and exit the execution in the Contao workflow.
+ *
+ * @author Christian Schiffler <https://github.com/discordier>
+ */
+class RedirectResponseException extends ResponseException
+{
+    /**
+     * Factory method creating an Response instance internally.
+     *
+     * Example:
+     *
+     *     throw RedirectResponseException::create('https://example.org/target.html');
+     *
+     * @param string $location The target URL
+     * @param int    $status   The response status code (defaults to 303)
+     * @param array  $headers  An array of response headers
+     *
+     * @return RedirectResponseException
+     */
+    public static function create($location, $status = 303, $headers = array())
+    {
+        return new static(new Response('', $status, array_merge(['Location' => $location], $headers)));
+    }
+
+    /**
+     * Factory method creating an ajax Response instance internally.
+     *
+     * Example:
+     *
+     *     throw RedirectResponseException::createAjax('https://example.org/target.html');
+     *
+     * @param string $location The target URL
+     * @param int    $status   The response status code (defaults to 204)
+     * @param array  $headers  An array of response headers
+     *
+     * @return RedirectResponseException
+     */
+    public static function createAjax($location, $status = 204, $headers = array())
+    {
+        return new static(new Response('', $status, array_merge(['X-Ajax-Location' => $location], $headers)));
+    }
+}
