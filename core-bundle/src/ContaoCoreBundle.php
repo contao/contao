18ade5708bf238c9357a288983503d2f856e9e96
Andreas Schempp <andreas.schempp@terminal42.ch>
2015-04-09 18:41:01 +0200

    [Core] Restore the SetApplicationPass

--- core-bundle/src/ContaoCoreBundle.php
+++ core-bundle/src/ContaoCoreBundle.php
@@ -13,6 +13,7 @@ namespace Contao\CoreBundle;
 use Contao\CoreBundle\DependencyInjection\Compiler\AddPackagesPass;
 use Contao\CoreBundle\DependencyInjection\Compiler\OrderProfilerPass;
 use Contao\CoreBundle\DependencyInjection\Compiler\AddResourcesPathsPass;
+use Contao\CoreBundle\DependencyInjection\Compiler\SetApplicationPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Contao\CoreBundle\DependencyInjection\ContaoCoreExtension;
@@ -57,6 +58,6 @@ class ContaoCoreBundle extends Bundle
         );
 
         $container->addCompilerPass(new AddResourcesPathsPass());
-        $container->addCompilerPass(new OrderProfilerPass(), PassConfig::TYPE_OPTIMIZE);
+        $container->addCompilerPass(new SetApplicationPass());
     }
 }
