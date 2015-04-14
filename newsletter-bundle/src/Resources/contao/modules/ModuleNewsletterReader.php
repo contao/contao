3f4919759a6bd0e69f7e3e3965f670a2b6093275
Leo Feyer <github@contao.org>
2015-04-14 16:01:44 +0200

    [Newsletter] Use the NotFoundHttpException class instead of rendering a page 404 object.

--- newsletter-bundle/src/Resources/contao/modules/ModuleNewsletterReader.php
+++ newsletter-bundle/src/Resources/contao/modules/ModuleNewsletterReader.php
@@ -10,6 +10,8 @@
 
 namespace Contao;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 /**
  * Front end module "newsletter reader".
@@ -99,9 +101,7 @@ class ModuleNewsletterReader extends \Module
 
 		if (null === $objNewsletter)
 		{
-			/** @var \PageError404 $objHandler */
-			$objHandler = new $GLOBALS['TL_PTY']['error_404']();
-			$objHandler->generate($objPage->id);
+			throw new NotFoundHttpException('Page not found');
 		}
 
 		// Overwrite the page title (see #2853 and #4955)
