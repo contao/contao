014251066876ab0cf30facd7a97abdc930499e3d
Leo Feyer <github@contao.org>
2015-04-14 16:03:10 +0200

    [Faq] Use the NotFoundHttpException class instead of rendering a page 404 object.

--- faq-bundle/src/Resources/contao/modules/ModuleFaqReader.php
+++ faq-bundle/src/Resources/contao/modules/ModuleFaqReader.php
@@ -10,6 +10,8 @@
 
 namespace Contao;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 /**
  * Class ModuleFaqReader
@@ -98,9 +100,7 @@ class ModuleFaqReader extends \Module
 
 		if (null === $objFaq)
 		{
-			/** @var \PageError404 $objHandler */
-			$objHandler = new $GLOBALS['TL_PTY']['error_404']();
-			$objHandler->generate($objPage->id);
+			throw new NotFoundHttpException('Page not found');
 		}
 
 		// Overwrite the page title and description (see #2853 and #4955)
