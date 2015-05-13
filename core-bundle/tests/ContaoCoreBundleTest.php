5fc2b82376b48a8fc30908c8763c56d44f28d9a7
Leo Feyer <github@contao.org>
2015-05-13 20:40:40 +0200

    [Core] Move the `ContaoCoreBundleTest` class to the root namespace.

--- core-bundle/tests/ContaoCoreBundleTest.php
+++ core-bundle/tests/ContaoCoreBundleTest.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0+
  */
 
-namespace Contao\CoreBundle\Test\Bundle;
+namespace Contao\CoreBundle\Test;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\CoreBundle\Test\TestCase;
