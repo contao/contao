4cc34bd20975a069263ba08bacae4581dd383444
Leo Feyer <github@contao.org>
2015-05-28 14:44:25 +0200

    [Core] Set REMOTE_ADDR and SCRIPT_NAME in the InsecureInstallationListenerTest class.

--- core-bundle/tests/EventListener/InsecureInstallationListenerTest.php
+++ core-bundle/tests/EventListener/InsecureInstallationListenerTest.php
@@ -106,8 +106,10 @@ class InsecureInstallationListenerTest extends TestCase
     {
         $request = new Request();
 
-        $request->server->set('REQUEST_URI', '/web/app_dev.php?do=test');
+        $request->server->set('SCRIPT_NAME', 'app_dev.php');
         $request->server->set('SCRIPT_FILENAME', $this->getRootDir() . '/web/app_dev.php');
+        $request->server->set('REMOTE_ADDR', '123.456.789.0');
+        $request->server->set('REQUEST_URI', '/web/app_dev.php?do=test');
 
         return $request;
     }
