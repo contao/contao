24d9e63acc9d7b8bf6b84cd7400e6ab2847ef071
Leo Feyer <github@contao.org>
2015-11-24 13:05:11 +0100

    [Core] Add tests for the event classes.

---
+++ core-bundle/tests/Event/PreviewUrlConvertEventTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CoreBundle\Test\Event;
+
+use Contao\CoreBundle\Event\PreviewUrlConvertEvent;
+
+/**
+ * Tests the PreviewUrlConvertEvent class.
+ *
+ * @author Leo Feyer <https://github.com/leofeyer>
+ */
+class PreviewUrlConvertEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the object instantiation.
+     */
+    public function testInstantiation()
+    {
+        $event = new PreviewUrlConvertEvent();
+
+        $this->assertInstanceOf('Contao\CoreBundle\Event\PreviewUrlConvertEvent', $event);
+    }
+
+    /**
+     * Tests the URL getter and setter.
+     */
+    public function testUrlGetterSetter()
+    {
+        $event = new PreviewUrlConvertEvent();
+
+        $this->assertNull($event->getUrl());
+
+        $event->setUrl('http://localhost');
+
+        $this->assertEquals('http://localhost', $event->getUrl());
+    }
+}
