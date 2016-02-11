200fee61f27fb64fc7abd310acbacf22967c77ac
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-02-11 14:30:21 +0100

    [Core] Enable sub-subpalettes in edit mode

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -1917,7 +1917,7 @@ class DC_Table extends \DataContainer implements \listable, \editable
 			// Render boxes
 			foreach ($boxes as $k=>$v)
 			{
-				$strAjax = '';
+				$strAjax = array();
 				$blnAjax = false;
 				$key = '';
 				$cls = '';
@@ -1939,6 +1939,7 @@ class DC_Table extends \DataContainer implements \listable, \editable
 				}
 
 				$return .= "\n\n" . '<fieldset' . ($key ? ' id="pal_'.$key.'"' : '') . ' class="' . $class . ($legend ? '' : ' nolegend') . '">' . $legend;
+				$thisId = '';
 
 				// Build rows of the current box
 				foreach ($v as $vv)
@@ -1947,10 +1948,23 @@ class DC_Table extends \DataContainer implements \listable, \editable
 					{
 						if ($blnAjax && \Environment::get('isAjaxRequest'))
 						{
-							return $strAjax . '<input type="hidden" name="FORM_FIELDS[]" value="'.specialchars($this->strPalette).'">';
+							if ($ajaxId == $thisId)
+							{
+								return $strAjax[$thisId] . '<input type="hidden" name="FORM_FIELDS[]" value="'.specialchars($this->strPalette).'">';
+							}
+
+							if (count($strAjax) > 1)
+							{
+								$current = $strAjax[$thisId];
+								unset($strAjax[$thisId]);
+								end($strAjax);
+								$thisId = key($strAjax);
+								$strAjax[$thisId] = $current;
+							}
 						}
 
 						$blnAjax = false;
+						$thisId  = '';
 						$return .= "\n" . '</div>';
 
 						continue;
@@ -2003,7 +2017,7 @@ class DC_Table extends \DataContainer implements \listable, \editable
 					$this->objActiveRecord->{$this->strField} = $this->varValue;
 
 					// Build the row and pass the current palette string (thanks to Tristan Lins)
-					$blnAjax ? $strAjax .= $this->row($this->strPalette) : $return .= $this->row($this->strPalette);
+					$blnAjax ? $strAjax[$thisId] .= $this->row($this->strPalette) : $return .= $this->row($this->strPalette);
 				}
 
 				$class = 'tl_box';
