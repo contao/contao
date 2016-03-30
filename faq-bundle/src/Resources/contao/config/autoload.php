40e7c228057772f631b46d5d777be97b26823434
Leo Feyer <github@contao.org>
2016-03-30 13:14:09 +0200

    [Faq] Use the Composer autoloader to load the Contao classes (see contao/core-bundle#437).

--- faq-bundle/src/Resources/contao/config/autoload.php
+++ faq-bundle/src/Resources/contao/config/autoload.php
@@ -8,20 +8,6 @@
  * @license LGPL-3.0+
  */
 
-// Classes
-ClassLoader::addClasses(array
-(
-	// Models
-	'Contao\FaqCategoryModel' => 'vendor/contao/faq-bundle/src/Resources/contao/models/FaqCategoryModel.php',
-	'Contao\FaqModel'         => 'vendor/contao/faq-bundle/src/Resources/contao/models/FaqModel.php',
-
-	// Modules
-	'Contao\ModuleFaq'        => 'vendor/contao/faq-bundle/src/Resources/contao/modules/ModuleFaq.php',
-	'Contao\ModuleFaqList'    => 'vendor/contao/faq-bundle/src/Resources/contao/modules/ModuleFaqList.php',
-	'Contao\ModuleFaqPage'    => 'vendor/contao/faq-bundle/src/Resources/contao/modules/ModuleFaqPage.php',
-	'Contao\ModuleFaqReader'  => 'vendor/contao/faq-bundle/src/Resources/contao/modules/ModuleFaqReader.php',
-));
-
 // Templates
 TemplateLoader::addFiles(array
 (
