5c20f6ba3f2c37646220cbbcd1582a2b534ee589
Leo Feyer <github@contao.org>
2016-05-11 11:26:06 +0200

    [News] Remove the back end icon.

--- news-bundle/src/Resources/contao/config/config.php
+++ news-bundle/src/Resources/contao/config/config.php
@@ -17,7 +17,6 @@ array_insert($GLOBALS['BE_MOD']['content'], 1, array
 	'news' => array
 	(
 		'tables'      => array('tl_news_archive', 'tl_news', 'tl_news_feed', 'tl_content'),
-		'icon'        => 'bundles/contaonews/icon.svg',
 		'table'       => array('TableWizard', 'importTable'),
 		'list'        => array('ListWizard', 'importList')
 	)
