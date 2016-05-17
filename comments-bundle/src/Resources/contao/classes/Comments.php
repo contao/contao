e3415be3bf7c26f74b55d2adc737763a344390cc
Leo Feyer <github@contao.org>
2016-05-17 17:09:49 +0200

    [Comments] Add the URI when throwing 403 and 404 exceptions (see #369).

--- comments-bundle/src/Resources/contao/classes/Comments.php
+++ comments-bundle/src/Resources/contao/classes/Comments.php
@@ -66,7 +66,7 @@ class Comments extends \Frontend
 			// Do not index or cache the page if the page number is outside the range
 			if ($page < 1 || $page > max(ceil($total/$objConfig->perPage), 1))
 			{
-				throw new PageNotFoundException('Page not found');
+				throw new PageNotFoundException('Page not found: ' . \Environment::get('uri'));
 			}
 
 			// Set limit and offset
