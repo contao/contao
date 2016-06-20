4dd53c244e0d76c3b8d54ac7089871d3a6ea9f1a
Leo Feyer <github@contao.org>
2016-06-20 15:01:32 +0200

    [Core] Use $framework->isInitialized() instead of class_exists('Contao\Sytstem').

--- core-bundle/src/EventListener/PrettyErrorScreenListener.php
+++ core-bundle/src/EventListener/PrettyErrorScreenListener.php
@@ -342,7 +342,7 @@ class PrettyErrorScreenListener
      */
     private function loadLanguageStrings()
     {
-        if (!class_exists('Contao\System')) {
+        if (!$this->framework->isInitialized()) {
             return null;
         }
 
