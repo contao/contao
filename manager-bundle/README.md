2e8155e48586603708892230ce646d2a04c72e5e
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-06-29 08:42:32 +0200

    [Manager] Bundle basics

---
+++ manager-bundle/README.md
@@ -0,0 +1,15 @@
+Contao 4 manager bundle
+=======================
+
+Contao is an Open Source PHP Content Management System for people who want a
+professional website that is easy to maintain. Visit the [project website][1]
+for more information.
+
+This bundle adds manager functionality to Contao core. It provides a 
+[Symfony][2] skeleton that allows full application management using the
+[Contao Manager][3] GUI.
+
+
+[1]: https://contao.org
+[2]: https://symfony.com
+[3]: https://github.com/contao/package-manager
---
+++ manager-bundle/src/DependencyInjection/ContaoManagerExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\ManagerBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+
+/**
+ * Adds the bundle services to the container.
+ *
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
+class ContaoManagerExtension extends ConfigurableExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+    }
+}
