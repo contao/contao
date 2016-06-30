3d0abb7a63dbdd96d4ade820c13b79ce902e7e42
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-06-30 20:02:16 +0200

    [Manager] Manager bundle is loading the Contao application defaults

--- manager-bundle/src/DependencyInjection/ContaoManagerExtension.php
+++ manager-bundle/src/DependencyInjection/ContaoManagerExtension.php
@@ -10,9 +10,11 @@
 
 namespace Contao\ManagerBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Contao\CoreBundle\DependencyInjection\PrependContaoExtensionInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -27,14 +29,38 @@ class ContaoManagerExtension extends Extension implements PrependExtensionInterf
      */
     public function prepend(ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('framework.yml');
+        $loader->load('twig.yml');
+        $loader->load('doctrine.yml');
+        $loader->load('swiftmailer.yml');
+        $loader->load('monolog.yml');
+
+        if (in_array('Lexik\\Bundle\\MaintenanceBundle\\LexikMaintenanceBundle', $container->getParameter('kernel.bundles'), true)) {
+            $loader->load('lexik_maintenance.yml');
+        }
+
+        if ('dev' === $container->getParameter('kernel.environment')) {
+            $loader->load('web_profiler.yml');
+        }
+
+        $coreLoader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../../core-bundle/src/Resources/config'));
+        $coreLoader->load('security.yml');
+
+        $this->prependBundles($container);
+    }
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+
+    private function prependBundles(ContainerBuilder $container)
+    {
         foreach ($container->getExtensions() as $extension) {
             if ($extension instanceof PrependContaoExtensionInterface && !$extension instanceof PrependExtensionInterface) {
                 $extension->prepend($container);
             }
         }
-    }
-
-    public function load(array $configs, ContainerBuilder $container)
-    {
     }
 }
