3464082caf1a6257b5cf670665bfeb1f78ae5060
Leo Feyer <github@contao.org>
2016-08-10 10:16:20 +0200

    [Core] Send the correct cache headers in Frontend::getResponseFromCache().

--- core-bundle/src/Resources/contao/classes/Frontend.php
+++ core-bundle/src/Resources/contao/classes/Frontend.php
@@ -861,15 +861,13 @@ abstract class Frontend extends \Controller
 		// Send the cache headers
 		if ($expire !== null && (\Config::get('cacheMode') == 'both' || \Config::get('cacheMode') == 'browser'))
 		{
-			$response->headers->set('Cache-Control', 'public, max-age=' . ($expire - time()));
-			$response->headers->set('Pragma', 'public');
+			$response->headers->set('Cache-Control', 'private, max-age=' . ($expire - time()));
 			$response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', time()) . ' GMT');
 			$response->headers->set('Expires', gmdate('D, d M Y H:i:s', $expire) . ' GMT');
 		}
 		else
 		{
 			$response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-			$response->headers->set('Pragma', 'no-cache');
 			$response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT');
 			$response->headers->set('Expires', 'Fri, 06 Jun 1975 15:10:00 GMT');
 		}
