5b098cefc17f3b1fa6ffc84f55a1a13945af8596
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-08-16 14:46:43 +0200

    [Calendar] Added bundle autoloading for Contao Manager

---
+++ calendar-bundle/src/ContaoManagerPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CalendarBundle;
+
+use Contao\ManagerBundle\Manager\Bundle\BundleConfig;
+use Contao\ManagerBundle\Manager\Bundle\BundlePluginInterface;
+use Contao\ManagerBundle\Manager\Bundle\IniParser;
+use Contao\ManagerBundle\Manager\Bundle\JsonParser;
+
+/**
+ * Plugin for the Contao Manager.
+ *
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
+class ContaoManagerPlugin implements BundlePluginInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function getAutoloadConfigs(JsonParser $jsonParser, IniParser $iniParser)
+    {
+        return [
+            BundleConfig::create('Contao\CalendarBundle\ContaoCalendarBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
+                ->setReplace(['calendar'])
+        ];
+    }
+}
