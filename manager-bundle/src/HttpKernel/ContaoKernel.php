86badcb5144a987a682e46b58c7a7ced81e11d1d
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-10-09 20:04:44 +0200

    [Manager] Managed edition only has a parameters.yml generated by install tool

--- manager-bundle/src/HttpKernel/ContaoKernel.php
+++ manager-bundle/src/HttpKernel/ContaoKernel.php
@@ -81,7 +81,9 @@ class ContaoKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+        if (file_exists($this->getRootDir() . '/config/parameters.yml')) {
+            $loader->load($this->getRootDir() . '/config/parameters.yml');
+        }
     }
 
     /**
