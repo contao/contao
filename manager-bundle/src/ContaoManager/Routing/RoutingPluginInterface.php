999f0202c8d0d80b1188bad4b885f7fe1baaba87
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-11-07 17:28:10 +0100

    [Manager] CS

--- manager-bundle/src/ContaoManager/Routing/RoutingPluginInterface.php
+++ manager-bundle/src/ContaoManager/Routing/RoutingPluginInterface.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
 namespace Contao\ManagerBundle\ContaoManager\Routing;
 
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollection;
 
+/**
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
 interface RoutingPluginInterface
 {
     /**
--- manager-bundle/src/HttpKernel/ContaoCache.php
+++ manager-bundle/src/HttpKernel/ContaoCache.php
@@ -1,9 +1,20 @@
 <?php
 
+/**
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
 namespace Contao\ManagerBundle\HttpKernel;
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
+/**
+ * @author Andreas Schempp <https://github.com/aschempp>
+ */
 class ContaoCache extends HttpCache
 {
 }
