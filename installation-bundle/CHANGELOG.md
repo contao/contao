1f7003ac88accde1a5d2f1564aa7640e79d8e56c
Leo Feyer <github@contao.org>
2016-11-17 16:28:24 +0100

    [Installation] Use the Filesystem to dump the parameters.yml file and initialize the dumper with a minimum configuration.

--- installation-bundle/CHANGELOG.md
+++ installation-bundle/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Contao installation bundle change log
 
+### 1.2.0 (2016-XX-XX)
+
+ * Use the Filesystem to dump the parameters.yml file and initialize the dumper with a minimum configuration.
+
 ### 1.2.0-RC1 (2016-10-31)
 
  * Show the console output if a post installation task cannot be completed (see #32).
--- installation-bundle/src/Config/ParameterDumper.php
+++ installation-bundle/src/Config/ParameterDumper.php
@@ -10,12 +10,14 @@
 
 namespace Contao\InstallationBundle\Config;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Yaml\Yaml;
 
 /**
- * Dumps the parameters into the paramters.yml file.
+ * Dumps the parameters into the parameters.yml file.
  *
  * @author Leo Feyer <https://github.com/leofeyer>
+ * @author Andreas Schempp <https://github.com/aschempp>
  */
 class ParameterDumper
 {
@@ -25,25 +27,49 @@ class ParameterDumper
     private $rootDir;
 
     /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
      * @var array
      */
-    private $parameters;
+    private $parameters = [
+        'parameters' => [
+            'database_host' => 'localhost',
+            'database_port' => 3306,
+            'database_user' => null,
+            'database_password' => null,
+            'database_name' => null,
+            'mailer_transport' => 'mail',
+            'mailer_host' => '127.0.0.1',
+            'mailer_user' => null,
+            'mailer_password' => null,
+            'mailer_port' => 25,
+            'mailer_encryption' => null,
+            'prepend_locale' => false,
+            'secret' => 'ThisTokenIsNotSoSecretChangeIt',
+        ],
+    ];
 
     /**
      * Constructor.
      *
-     * @param string $rootDir
+     * @param string     $rootDir
+     * @param Filesystem $filesystem
      */
-    public function __construct($rootDir)
+    public function __construct($rootDir, Filesystem $filesystem = null)
     {
         $this->rootDir = $rootDir;
-        $this->parameters = Yaml::parse(file_get_contents($rootDir.'/config/parameters.yml.dist'));
+        $this->filesystem = $filesystem ?: new Filesystem();
 
-        if (file_exists($rootDir.'/config/parameters.yml')) {
-            $this->parameters = array_merge(
-                $this->parameters,
-                Yaml::parse(file_get_contents($rootDir.'/config/parameters.yml'))
-            );
+        foreach (['config/parameters.yml.dist', 'config/parameters.yml'] as $file) {
+            if (file_exists($rootDir.'/'.$file)) {
+                $this->parameters = array_merge(
+                    $this->parameters,
+                    Yaml::parse(file_get_contents($rootDir.'/'.$file))
+                );
+            }
         }
     }
 
@@ -83,7 +109,7 @@ class ParameterDumper
             $this->parameters['parameters']['database_port'] = (int) $this->parameters['parameters']['database_port'];
         }
 
-        file_put_contents(
+        $this->filesystem->dumpFile(
             $this->rootDir.'/config/parameters.yml',
             "# This file has been auto-generated during installation\n".Yaml::dump($this->parameters)
         );
