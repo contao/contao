3fc40bdb00881cdbd099fbfcf4811253b37934db
Leo Feyer <github@contao.org>
2016-11-18 12:11:07 +0100

    [Calendar] Fix the coding style.

--- calendar-bundle/src/ContaoManager/Plugin.php
+++ calendar-bundle/src/ContaoManager/Plugin.php
@@ -22,14 +22,14 @@ use Contao\ManagerBundle\ContaoManager\Bundle\ParserInterface;
 class Plugin implements BundlePluginInterface
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function getBundles(ParserInterface $parser)
     {
         return [
             BundleConfig::create('Contao\CalendarBundle\ContaoCalendarBundle')
                 ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle'])
-                ->setReplace(['calendar'])
+                ->setReplace(['calendar']),
         ];
     }
 }
