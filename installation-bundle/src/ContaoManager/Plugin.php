ad3b28cc8dde8fab7c3dd603c52085812d98efdc
Leo Feyer <github@contao.org>
2016-11-18 13:55:36 +0100

    [Installation] Fix the coding style and add a @dev tag as long as there is no stable release.

--- installation-bundle/src/ContaoManager/Plugin.php
+++ installation-bundle/src/ContaoManager/Plugin.php
@@ -15,20 +15,20 @@ use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
- * Plugin for Contao Manager
+ * Plugin for the Contao Manager.
  *
  * @author Andreas Schempp <https://github.com/aschempp>
  */
 class Plugin implements RoutingPluginInterface
 {
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
     {
         return $resolver
-            ->resolve(__DIR__ . '/../Resources/config/routing.yml')
-            ->load(__DIR__ . '/../Resources/config/routing.yml')
+            ->resolve(__DIR__.'/../Resources/config/routing.yml')
+            ->load(__DIR__.'/../Resources/config/routing.yml')
         ;
     }
 }
