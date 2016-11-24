5d55b03eb65c18dac9ca5873059521c131501b76
Yanick Witschi <yanick.witschi@terminal42.ch>
2016-11-24 13:43:17 +0100

    [Manager] Added unit tests for the RouteLoader

---
+++ manager-bundle/tests/Routing/RouteLoaderTest.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace Contao\ManagerBundle\Test\HttpKernel;
+
+use Contao\ManagerBundle\ContaoManager\PluginLoader;
+use Contao\ManagerBundle\ContaoManager\Routing\RoutingPluginInterface;
+use Contao\ManagerBundle\Routing\RouteLoader;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Tests the RouteLoader class.
+ *
+ * @author Yanick Witschi <https://github.com/toflar>
+ */
+class RouteLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the object instantiation.
+     */
+    public function testInstantiation()
+    {
+        $loader = $this->getMock(LoaderInterface::class);
+        $kernel = $this->getMock(KernelInterface::class);
+        $pluginLoader = $this->getMockBuilder(PluginLoader::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $routeLoader = new RouteLoader(
+            $loader,
+            $pluginLoader,
+            $kernel
+        );
+        $this->assertInstanceOf('Contao\ManagerBundle\Routing\RouteLoader', $routeLoader);
+    }
+
+    public function testLoadFromPlugins()
+    {
+        $loaderResolver = $this->getMock(LoaderResolverInterface::class);
+        $loader = $this->getMock(LoaderInterface::class);
+        $loader
+            ->expects($this->exactly(2))
+            ->method('getResolver')
+            ->willReturn($loaderResolver);
+        $kernel = $this->getMock(KernelInterface::class);
+
+        $routeCollection1 = new RouteCollection();
+        $routeCollection1->add('foo', new Route('/foo/path'));
+        $routeCollection2 = new RouteCollection();
+        $routeCollection2->add('foo2', new Route('/foo2/path2'));
+
+        $plugin1 = $this->getMock(RoutingPluginInterface::class);
+        $plugin1
+            ->expects($this->once())
+            ->method('getRouteCollection')
+            ->willReturn($routeCollection1);
+        $plugin2 = $this->getMock(RoutingPluginInterface::class);
+        $plugin2
+            ->expects($this->once())
+            ->method('getRouteCollection')
+            ->willReturn($routeCollection2);
+
+        $pluginLoader = $this->getMockBuilder(PluginLoader::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        $pluginLoader
+            ->expects($this->once())
+            ->method('getInstancesOf')
+            ->with(PluginLoader::ROUTING_PLUGINS)
+            ->willReturn([$plugin1, $plugin2]);
+
+        $routeLoader = new RouteLoader(
+            $loader,
+            $pluginLoader,
+            $kernel
+        );
+
+        $collection = $routeLoader->loadFromPlugins();
+
+        $this->assertCount(2, $collection);
+        $this->assertNotNull($collection->get('foo'));
+        $this->assertNotNull($collection->get('foo2'));
+        $this->assertInstanceOf(Route::class, $collection->get('foo'));
+        $this->assertInstanceOf(Route::class, $collection->get('foo2'));
+    }
+}
