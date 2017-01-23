75cd9791c8f161820494c1558832d66270d61f2c
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-01-23 23:58:57 +0100

    [Manager] Fixed the ContaoKernel tests

--- manager-bundle/tests/HttpKernel/ContaoKernelTest.php
+++ manager-bundle/tests/HttpKernel/ContaoKernelTest.php
@@ -16,10 +16,8 @@ use Contao\ManagerPlugin\Bundle\BundleLoader;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Contao\ManagerPlugin\PluginLoader;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * Tests the ContaoKernel class.
@@ -156,59 +154,66 @@ class ContaoKernelTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Tests the registerContainerConfiguration() method loads the parameters.yml.
+     * Tests the registerContainerConfiguration() method.
+     *
+     * @dataProvider containerConfigurationProvider
      */
-    public function testRegisterContainerConfigurationLoadsParametersYml()
+    public function testRegisterContainerConfiguration($rootDir, $expectedResult)
     {
-        $container = new ContainerBuilder();
-        $loader = new YamlFileLoader($container, new FileLocator());
+        $files = [];
 
-        $this->kernel->setRootDir(__DIR__.'/../Fixtures/HttpKernel/WithParametersYml');
+        $loader = $this->getMock(LoaderInterface::class);
+        $loader
+            ->expects($this->atLeastOnce())
+            ->method('load')
+            ->willReturnCallback(
+                function ($resource) use (&$files) {
+                    if (is_string($resource)) {
+                        $files[] = basename($resource);
+                    } elseif (is_callable($resource)) {
+                        $container = new ContainerBuilder();
+                        $container->setParameter('kernel.environment', 'dev');
+                        call_user_func($resource, $container);
+                    }
+                }
+            )
+        ;
+
+        $this->kernel->setRootDir($rootDir);
         $this->kernel->registerContainerConfiguration($loader);
 
-        $this->assertEquals('localhost', $container->getParameter('database_host'));
+        $this->assertEquals($expectedResult, $files);
     }
 
     /**
-     * Tests the registerContainerConfiguration() method does nothing if parameters.yml is not available.
+     * Provides data for the testRegisterContainerConfiguration method.
+     *
+     * @return array
      */
-    public function testRegisterContainerConfigurationIgnoresMissingParametersYml()
+    public function containerConfigurationProvider()
     {
-        $container = new ContainerBuilder();
-        $loader = new YamlFileLoader($container, new FileLocator());
-
-        $this->kernel->setRootDir(sys_get_temp_dir());
-        $this->kernel->registerContainerConfiguration($loader);
-
-        $this->assertEmpty($container->getParameterBag()->all());
-    }
-
-    /**
-     * Tests the registerContainerConfiguration() method loads the config.yml.
-     */
-    public function testRegisterContainerConfigurationLoadsConfigYml()
-    {
-        $container = new ContainerBuilder();
-        $loader = new YamlFileLoader($container, new FileLocator());
-
-        $this->kernel->setRootDir(__DIR__.'/../Fixtures/HttpKernel/WithConfigYml');
-        $this->kernel->registerContainerConfiguration($loader);
-
-        $this->assertEquals('localhost', $container->getParameter('database_host'));
-    }
-
-    /**
-     * Tests the registerContainerConfiguration() method does nothing if config.yml is not available.
-     */
-    public function testRegisterContainerConfigurationIgnoresMissingConfigYml()
-    {
-        $container = new ContainerBuilder();
-        $loader = new YamlFileLoader($container, new FileLocator());
-
-        $this->kernel->setRootDir(sys_get_temp_dir());
-        $this->kernel->registerContainerConfiguration($loader);
-
-        $this->assertEmpty($container->getParameterBag()->all());
+        return [
+            [
+                __DIR__.'/../Fixtures/HttpKernel/WithParametersYml',
+                ['parameters.yml', 'parameters.yml']
+            ],
+            [
+                __DIR__.'/../Fixtures/HttpKernel/WithConfigDevYml',
+                ['config_dev.yml']
+            ],
+            [
+                __DIR__.'/../Fixtures/HttpKernel/WithConfigYml',
+                ['config.yml']
+            ],
+            [
+                __DIR__.'/../Fixtures/HttpKernel/WithConfigsYml',
+                ['config_dev.yml']
+            ],
+            [
+                sys_get_temp_dir(),
+                []
+            ],
+        ];
     }
 
     /**
