812f0a03cd71eaa0e2cae41fd2d4d1c2343b3e55
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-04-28 14:39:12 +0200

    [Manager] Extended reporting if the script handler fails (see #27).

--- manager-bundle/src/Composer/ScriptHandler.php
+++ manager-bundle/src/Composer/ScriptHandler.php
@@ -104,7 +104,7 @@ class ScriptHandler
         );
 
         if (!$process->isSuccessful()) {
-            throw new \RuntimeException(sprintf('An error occurred while executing the "%s" command.', $cmd));
+            throw new \RuntimeException(sprintf('An error occurred while executing the "%s" command: %s', $cmd, $process->getErrorOutput()));
         }
     }
 
