e65a8b1f9bc72d13a59eb59ba08030f2380fd424
Leo Feyer <github@contao.org>
2017-05-04 17:18:40 +0200

    [Faq] Improve the FAQ extension class tests.

--- faq-bundle/tests/DependencyInjection/ContaoFaqExtensionTest.php
+++ faq-bundle/tests/DependencyInjection/ContaoFaqExtensionTest.php
@@ -11,6 +11,8 @@
 namespace Contao\FaqBundle\Tests\DependencyInjection;
 
 use Contao\FaqBundle\DependencyInjection\ContaoFaqExtension;
+use Contao\FaqBundle\EventListener\InsertTagsListener;
+use Contao\FaqBundle\Menu\FaqPickerProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
@@ -22,6 +24,24 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 class ContaoFaqExtensionTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->container = new ContainerBuilder(new ParameterBag(['kernel.debug' => false]));
+
+        $extension = new ContaoFaqExtension();
+        $extension->load([], $this->container);
+    }
+
+    /**
      * Tests the object instantiation.
      */
     public function testInstantiation()
@@ -32,16 +52,40 @@ class ContaoFaqExtensionTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Tests adding the bundle services to the container.
+     * Tests the contao_faq.listener.insert_tags service.
      */
-    public function testLoad()
+    public function testInsertTagsListener()
     {
-        $container = new ContainerBuilder(new ParameterBag(['kernel.debug' => false]));
+        $this->assertTrue($this->container->has('contao_faq.listener.insert_tags'));
 
-        $extension = new ContaoFaqExtension();
-        $extension->load([], $container);
+        $definition = $this->container->getDefinition('contao_faq.listener.insert_tags');
 
-        $this->assertTrue($container->has('contao_faq.listener.insert_tags'));
-        $this->assertTrue($container->has('contao_faq.listener.faq_picker_provider'));
+        $this->assertEquals(InsertTagsListener::class, $definition->getClass());
+        $this->assertEquals('contao.framework', (string) $definition->getArgument(0));
+    }
+
+    /**
+     * Tests the contao_faq.listener.faq_picker_provider service.
+     */
+    public function testFaqPickerProvider()
+    {
+        $this->assertTrue($this->container->has('contao_faq.listener.faq_picker_provider'));
+
+        $definition = $this->container->getDefinition('contao_faq.listener.faq_picker_provider');
+
+        $this->assertEquals(FaqPickerProvider::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $this->assertEquals('router', (string) $definition->getArgument(0));
+        $this->assertEquals('request_stack', (string) $definition->getArgument(1));
+        $this->assertEquals('security.token_storage', (string) $definition->getArgument(2));
+
+        $methodCalls = $definition->getMethodCalls();
+
+        $this->assertEquals('setFramework', $methodCalls[0][0]);
+
+        $tags = $definition->getTags();
+
+        $this->assertArrayHasKey('contao.picker_menu_provider', $tags);
+        $this->assertEquals(64, $tags['contao.picker_menu_provider'][0]['priority']);
     }
 }
