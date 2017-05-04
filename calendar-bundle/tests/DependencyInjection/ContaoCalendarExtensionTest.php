9978de8adb401a72be083af1b3b7e3ccb9ccc096
Leo Feyer <github@contao.org>
2017-05-04 17:08:52 +0200

    [Calendar] Improve the calendar extension class tests.

--- calendar-bundle/tests/DependencyInjection/ContaoCalendarExtensionTest.php
+++ calendar-bundle/tests/DependencyInjection/ContaoCalendarExtensionTest.php
@@ -11,6 +11,11 @@
 namespace Contao\CalendarBundle\Tests\DependencyInjection;
 
 use Contao\CalendarBundle\DependencyInjection\ContaoCalendarExtension;
+use Contao\CalendarBundle\EventListener\GeneratePageListener;
+use Contao\CalendarBundle\EventListener\InsertTagsListener;
+use Contao\CalendarBundle\EventListener\PreviewUrlConvertListener;
+use Contao\CalendarBundle\EventListener\PreviewUrlCreateListener;
+use Contao\CalendarBundle\Menu\EventPickerProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
@@ -22,6 +27,24 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 class ContaoCalendarExtensionTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->container = new ContainerBuilder(new ParameterBag(['kernel.debug' => false]));
+
+        $extension = new ContaoCalendarExtension();
+        $extension->load([], $this->container);
+    }
+
+    /**
      * Tests the object instantiation.
      */
     public function testInstantiation()
@@ -32,19 +55,93 @@ class ContaoCalendarExtensionTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Tests adding the bundle services to the container.
+     * Tests the contao_calendar.listener.generate_page service.
      */
-    public function testLoad()
+    public function testGeneratePageListener()
     {
-        $container = new ContainerBuilder(new ParameterBag(['kernel.debug' => false]));
+        $this->assertTrue($this->container->has('contao_calendar.listener.generate_page'));
 
-        $extension = new ContaoCalendarExtension();
-        $extension->load([], $container);
+        $definition = $this->container->getDefinition('contao_calendar.listener.generate_page');
 
-        $this->assertTrue($container->has('contao_calendar.listener.generate_page'));
-        $this->assertTrue($container->has('contao_calendar.listener.insert_tags'));
-        $this->assertTrue($container->has('contao_calendar.listener.preview_url_create'));
-        $this->assertTrue($container->has('contao_calendar.listener.preview_url_convert'));
-        $this->assertTrue($container->has('contao_calendar.listener.event_picker_provider'));
+        $this->assertEquals(GeneratePageListener::class, $definition->getClass());
+        $this->assertEquals('contao.framework', (string) $definition->getArgument(0));
+    }
+
+    /**
+     * Tests the contao_calendar.listener.insert_tags service.
+     */
+    public function testInsertTagsListener()
+    {
+        $this->assertTrue($this->container->has('contao_calendar.listener.insert_tags'));
+
+        $definition = $this->container->getDefinition('contao_calendar.listener.insert_tags');
+
+        $this->assertEquals(InsertTagsListener::class, $definition->getClass());
+        $this->assertEquals('contao.framework', (string) $definition->getArgument(0));
+    }
+
+    /**
+     * Tests the contao_calendar.listener.preview_url_create service.
+     */
+    public function testPreviewUrlCreateListener()
+    {
+        $this->assertTrue($this->container->has('contao_calendar.listener.preview_url_create'));
+
+        $definition = $this->container->getDefinition('contao_calendar.listener.preview_url_create');
+
+        $this->assertEquals(PreviewUrlCreateListener::class, $definition->getClass());
+        $this->assertEquals('request_stack', (string) $definition->getArgument(0));
+        $this->assertEquals('contao.framework', (string) $definition->getArgument(1));
+
+        $tags = $definition->getTags();
+
+        $this->assertArrayHasKey('kernel.event_listener', $tags);
+        $this->assertEquals('contao.preview_url_create', $tags['kernel.event_listener'][0]['event']);
+        $this->assertEquals('onPreviewUrlCreate', $tags['kernel.event_listener'][0]['method']);
+    }
+
+    /**
+     * Tests the contao_calendar.listener.preview_url_convert service.
+     */
+    public function testPreviewUrlConvertListener()
+    {
+        $this->assertTrue($this->container->has('contao_calendar.listener.preview_url_convert'));
+
+        $definition = $this->container->getDefinition('contao_calendar.listener.preview_url_convert');
+
+        $this->assertEquals(PreviewUrlConvertListener::class, $definition->getClass());
+        $this->assertEquals('request_stack', (string) $definition->getArgument(0));
+        $this->assertEquals('contao.framework', (string) $definition->getArgument(1));
+
+        $tags = $definition->getTags();
+
+        $this->assertArrayHasKey('kernel.event_listener', $tags);
+        $this->assertEquals('contao.preview_url_convert', $tags['kernel.event_listener'][0]['event']);
+        $this->assertEquals('onPreviewUrlConvert', $tags['kernel.event_listener'][0]['method']);
+    }
+
+    /**
+     * Tests the contao_calendar.listener.event_picker_provider service.
+     */
+    public function testEventPickerProvider()
+    {
+        $this->assertTrue($this->container->has('contao_calendar.listener.event_picker_provider'));
+
+        $definition = $this->container->getDefinition('contao_calendar.listener.event_picker_provider');
+
+        $this->assertEquals(EventPickerProvider::class, $definition->getClass());
+        $this->assertFalse($definition->isPublic());
+        $this->assertEquals('router', (string) $definition->getArgument(0));
+        $this->assertEquals('request_stack', (string) $definition->getArgument(1));
+        $this->assertEquals('security.token_storage', (string) $definition->getArgument(2));
+
+        $methodCalls = $definition->getMethodCalls();
+
+        $this->assertEquals('setFramework', $methodCalls[0][0]);
+
+        $tags = $definition->getTags();
+
+        $this->assertArrayHasKey('contao.picker_menu_provider', $tags);
+        $this->assertEquals(96, $tags['contao.picker_menu_provider'][0]['priority']);
     }
 }
