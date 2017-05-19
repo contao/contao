9551c3523f77b23692be844fea49baadc03c5d89
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-05-19 09:19:41 +0200

    [Installation] Re-add the "sqlCompileCommand" hook (see #51).

--- installation-bundle/CHANGELOG.md
+++ installation-bundle/CHANGELOG.md
@@ -2,4 +2,5 @@
 
 ### DEV
 
+ * Re-add the "sqlCompileCommand" hook (see #51).
  * Purge the opcode caches after deleting the Symfony cache (see contao/contao-manager#80).
--- installation-bundle/src/Database/Installer.php
+++ installation-bundle/src/Database/Installer.php
@@ -158,6 +158,17 @@ class Installer
             }
         }
 
-        $this->commands = array_filter($return);
+        $return = array_filter($return);
+
+        // HOOK: allow third-party developers to modify the array (see #3281)
+        if (isset($GLOBALS['TL_HOOKS']['sqlCompileCommands']) && is_array($GLOBALS['TL_HOOKS']['sqlCompileCommands']))
+        {
+            foreach ($GLOBALS['TL_HOOKS']['sqlCompileCommands'] as $callback)
+            {
+                $return = \System::importStatic($callback[0])->{$callback[1]}($return);
+            }
+        }
+
+        $this->commands = $return;
     }
 }
