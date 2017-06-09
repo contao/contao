b49f4e4a53608a2e3bd173a9fba1f933b6ca7fd9
Leo Feyer <github@contao.org>
2017-06-09 16:53:25 +0200

    [Newsletter] Pass either $dc or null to the toggleVisibility() callbacks.

--- newsletter-bundle/src/Resources/contao/dca/tl_newsletter_recipients.php
+++ newsletter-bundle/src/Resources/contao/dca/tl_newsletter_recipients.php
@@ -473,11 +473,11 @@ class tl_newsletter_recipients extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}(($dc ?: $this));
+					$this->{$callback[0]}->{$callback[1]}($dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$callback(($dc ?: $this));
+					$callback($dc);
 				}
 			}
 		}
@@ -512,11 +512,11 @@ class tl_newsletter_recipients extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, ($dc ?: $this));
+					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$blnVisible = $callback($blnVisible, ($dc ?: $this));
+					$blnVisible = $callback($blnVisible, $dc);
 				}
 			}
 		}
@@ -541,11 +541,11 @@ class tl_newsletter_recipients extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}(($dc ?: $this));
+					$this->{$callback[0]}->{$callback[1]}($dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$callback(($dc ?: $this));
+					$callback($dc);
 				}
 			}
 		}
