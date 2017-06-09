0334d5a730ce6e6678458f9bad564ae7b0bd2b0f
Leo Feyer <github@contao.org>
2017-06-09 16:52:26 +0200

    [Calendar] Pass either $dc or null to the toggleVisibility() callbacks.

--- calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
+++ calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
@@ -1051,11 +1051,11 @@ class tl_calendar_events extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}(($dc ?: $this));
+					$this->{$callback[0]}->{$callback[1]}($dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$callback(($dc ?: $this));
+					$callback($dc);
 				}
 			}
 		}
@@ -1090,11 +1090,11 @@ class tl_calendar_events extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, ($dc ?: $this));
+					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$blnVisible = $callback($blnVisible, ($dc ?: $this));
+					$blnVisible = $callback($blnVisible, $dc);
 				}
 			}
 		}
@@ -1119,11 +1119,11 @@ class tl_calendar_events extends Backend
 				if (is_array($callback))
 				{
 					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}(($dc ?: $this));
+					$this->{$callback[0]}->{$callback[1]}($dc);
 				}
 				elseif (is_callable($callback))
 				{
-					$callback(($dc ?: $this));
+					$callback($dc);
 				}
 			}
 		}
