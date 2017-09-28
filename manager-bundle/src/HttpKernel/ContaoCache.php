56ca85549fe245666141280ff3b79af8b6e4afcf
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-09-28 20:57:53 +0200

    [Manager] Ignore "csrf_" prefixed cookies in the Contao cache (see #47).

--- manager-bundle/src/HttpKernel/ContaoCache.php
+++ manager-bundle/src/HttpKernel/ContaoCache.php
@@ -34,7 +34,7 @@ class ContaoCache extends HttpCache implements CacheInvalidation
     {
         parent::__construct($kernel, $cacheDir);
 
-        $this->addSubscriber(new HeaderReplaySubscriber());
+        $this->addSubscriber(new HeaderReplaySubscriber(['ignore_cookies' => ['/^csrf_./']]));
     }
 
     /**
--- manager-bundle/tests/HttpKernel/ContaoCacheTest.php
+++ manager-bundle/tests/HttpKernel/ContaoCacheTest.php
@@ -13,24 +13,72 @@ declare(strict_types=1);
 namespace Contao\ManagerBundle\Tests\HttpKernel;
 
 use Contao\ManagerBundle\HttpKernel\ContaoCache;
+use FOS\HttpCache\SymfonyCache\Events;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Terminal42\HeaderReplay\SymfonyCache\HeaderReplaySubscriber;
 
 class ContaoCacheTest extends TestCase
 {
-    public function testInstantiation(): void
+    /**
+     * @var ContaoCache
+     */
+    private $cache;
+
+    /**
+     * @var string
+     */
+    private $tmpdir;
+
+    protected function setUp(): void
     {
-        $tmpdir = sys_get_temp_dir().'/'.uniqid('BundleCacheClearerTest_', false);
+        parent::setUp();
+
+        $this->tmpdir = sys_get_temp_dir().'/'.uniqid('BundleCacheClearerTest_', false);
 
         $fs = new Filesystem();
-        $fs->mkdir($tmpdir);
+        $fs->mkdir($this->tmpdir);
 
-        $cache = new ContaoCache($this->createMock(KernelInterface::class), $tmpdir);
+        $this->cache = new ContaoCache($this->createMock(KernelInterface::class), $this->tmpdir);
+    }
 
-        $this->assertInstanceOf('Contao\ManagerBundle\HttpKernel\ContaoCache', $cache);
-        $this->assertInstanceOf('Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache', $cache);
+    protected function tearDown(): void
+    {
+        parent::tearDown();
 
-        $fs->remove($tmpdir);
+        $fs = new Filesystem();
+        $fs->remove($this->tmpdir);
+    }
+
+    public function testInstantiation(): void
+    {
+        $this->assertInstanceOf('Contao\ManagerBundle\HttpKernel\ContaoCache', $this->cache);
+        $this->assertInstanceOf('Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache', $this->cache);
+    }
+
+    public function testAddsTheEventSubscribers(): void
+    {
+        $dispatcher = $this->cache->getEventDispatcher();
+        $preHandleListeners = $dispatcher->getListeners(Events::PRE_HANDLE);
+        $headerReplayListener = $preHandleListeners[0][0];
+
+        $this->assertInstanceOf(HeaderReplaySubscriber::class, $headerReplayListener);
+
+        $reflection = new \ReflectionClass($headerReplayListener);
+        $optionsProperty = $reflection->getProperty('options');
+        $optionsProperty->setAccessible(true);
+        $options = $optionsProperty->getValue($headerReplayListener);
+
+        $this->assertSame(
+            [
+                'user_context_headers' => [
+                    'cookie',
+                    'authorization',
+                ],
+                'ignore_cookies' => ['/^csrf_./'],
+            ],
+            $options
+        );
     }
 }
