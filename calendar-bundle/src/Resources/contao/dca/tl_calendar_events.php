37f0d8f2ae65225329abaffb1ae1e5a869416c05
Martin Auswöger <martin@auswoeger.com>
2017-10-31 16:15:20 +0100

    [Calendar] Use the slug generator service to generate the event alias (see #14)
    Description
    -----------
    
    See contao/core-bundle#1016
    
    Commits
    -------
    
    ac1a2f9f Use the slug generator service for the event alias
    d9b5537c Handle the $objCalendar === null case
    30bf980a Fix a wrong nesting

--- calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
+++ calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
@@ -670,7 +670,20 @@ class tl_calendar_events extends Backend
 		if ($varValue == '')
 		{
 			$autoAlias = true;
-			$varValue = StringUtil::generateAlias($dc->activeRecord->title);
+			$slugOptions = [];
+
+			// Read the slug options from the associated page
+			if (($objCalendar = CalendarModel::findByPk($dc->activeRecord->pid)) !== null && ($objPage = PageModel::findWithDetails($objCalendar->jumpTo)) !== null)
+			{
+				$slugOptions['locale'] = $objPage->language;
+
+				if ($objPage->validAliasCharacters)
+				{
+					$slugOptions['validChars'] = $objPage->validAliasCharacters;
+				}
+			}
+
+			$varValue = System::getContainer()->get('contao.slug.generator')->generate(StringUtil::stripInsertTags($dc->activeRecord->title), $slugOptions);
 		}
 
 		$objAlias = $this->Database->prepare("SELECT id FROM tl_calendar_events WHERE alias=? AND id!=?")
