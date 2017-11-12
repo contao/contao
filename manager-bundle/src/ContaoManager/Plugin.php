aba785769f255afcf5199e589eef259f6e2b5122
Leo Feyer <github@contao.org>
2017-11-12 17:20:34 +0100

    [Manager] Enable container.dumper.inline_class_loader and container.autowiring.strict_mode.

--- manager-bundle/src/ContaoManager/Plugin.php
+++ manager-bundle/src/ContaoManager/Plugin.php
@@ -124,6 +124,9 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface, RoutingPlu
                 } else {
                     $loader->load('@ContaoManagerBundle/Resources/skeleton/app/config_prod.yml');
                 }
+
+                $container->setParameter('container.autowiring.strict_mode', true);
+                $container->setParameter('container.dumper.inline_class_loader', true);
             }
         );
     }
