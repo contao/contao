191e7105bcdd98be30e4c693bc08c2470aad08c8
Leo Feyer <github@contao.org>
2017-11-23 17:42:21 +0100

    [Core] Correctly set the picker options.

--- core-bundle/src/Resources/contao/dca/tl_layout.php
+++ core-bundle/src/Resources/contao/dca/tl_layout.php
@@ -244,7 +244,7 @@ $GLOBALS['TL_DCA']['tl_layout'] = array
 			'label'                   => &$GLOBALS['TL_LANG']['tl_layout']['external'],
 			'exclude'                 => true,
 			'inputType'               => 'fileTree',
-			'eval'                    => array('multiple'=>true, 'orderField'=>'orderExt', 'fieldType'=>'checkbox', 'filesOnly'=>true, 'extensions'=>'css,scss,less'),
+			'eval'                    => array('multiple'=>true, 'fieldType'=>'checkbox', 'filesOnly'=>true, 'extensions'=>'css,scss,less', 'orderField'=>'orderExt'),
 			'sql'                     => "blob NULL"
 		),
 		'orderExt' => array
