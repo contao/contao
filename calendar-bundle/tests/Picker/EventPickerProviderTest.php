edf2a2a1333a2b6366c98dd74bf0f1738e86c2a5
Leo Feyer <github@contao.org>
2017-12-12 21:10:38 +0100

    [Calendar] Clean up.

--- calendar-bundle/tests/Picker/EventPickerProviderTest.php
+++ calendar-bundle/tests/Picker/EventPickerProviderTest.php
@@ -29,7 +29,7 @@ class EventPickerProviderTest extends ContaoTestCase
     /**
      * @var EventPickerProvider
      */
-    protected $provider;
+    private $provider;
 
     /**
      * {@inheritdoc}
