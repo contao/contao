e61845bb9744d855e4abf2015048c9062870d31a
Leo Feyer <github@contao.org>
2018-01-02 13:30:09 +0100

    [Manager] Temporarily disable header replaying until issues have been resolved.

--- manager-bundle/tests/HttpKernel/ContaoCacheTest.php
+++ manager-bundle/tests/HttpKernel/ContaoCacheTest.php
@@ -14,9 +14,7 @@ namespace Contao\ManagerBundle\Tests\HttpKernel;
 
 use Contao\ManagerBundle\HttpKernel\ContaoCache;
 use Contao\TestCase\ContaoTestCase;
-use FOS\HttpCache\SymfonyCache\Events;
 use Symfony\Component\HttpKernel\KernelInterface;
-use Terminal42\HeaderReplay\SymfonyCache\HeaderReplaySubscriber;
 
 class ContaoCacheTest extends ContaoTestCase
 {
@@ -26,31 +24,5 @@ class ContaoCacheTest extends ContaoTestCase
 
         $this->assertInstanceOf('Contao\ManagerBundle\HttpKernel\ContaoCache', $cache);
         $this->assertInstanceOf('Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache', $cache);
-    }
-
-    public function testAddsTheEventSubscribers(): void
-    {
-        $cache = new ContaoCache($this->createMock(KernelInterface::class), $this->getTempDir());
-        $dispatcher = $cache->getEventDispatcher();
-        $preHandleListeners = $dispatcher->getListeners(Events::PRE_HANDLE);
-        $headerReplayListener = $preHandleListeners[0][0];
-
-        $this->assertInstanceOf(HeaderReplaySubscriber::class, $headerReplayListener);
-
-        $reflection = new \ReflectionClass($headerReplayListener);
-        $optionsProperty = $reflection->getProperty('options');
-        $optionsProperty->setAccessible(true);
-        $options = $optionsProperty->getValue($headerReplayListener);
-
-        $this->assertSame(
-            [
-                'user_context_headers' => [
-                    'cookie',
-                    'authorization',
-                ],
-                'ignore_cookies' => ['/^csrf_./'],
-            ],
-            $options
-        );
     }
 }
