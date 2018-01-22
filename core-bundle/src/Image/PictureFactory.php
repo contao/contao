bc74e5619e40213da7177a071b328ba9bfdf6fc7
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2018-01-22 12:45:54 +0100

    [Core] Check if third image size parameter is actually a string (see #1304)
    Description
    -----------
    
    If the third element of the `$size` parameter for `PictureFactory::create` is an actual integer (an image size ID) instead of a string, the following error will occur:
    ```
    Symfony\Component\Debug\Exception\FatalThrowableError:
    Type error: substr_count() expects parameter 1 to be string, integer given
    
      at vendor\contao\core-bundle\src\Image\PictureFactory.php:100
      at substr_count(5, '_')
         (vendor\contao\core-bundle\src\Image\PictureFactory.php:100)
      at Contao\CoreBundle\Image\PictureFactory->create('files/foo.jpg', array('', '', 5))
         (vendor\contao\core-bundle\src\Resources\contao\library\Contao\Controller.php:1527)
      at Contao\Controller::addImageToTemplate(object(FrontendTemplate), array('id' => '16', 'pid' => '1', 'tstamp' => '1515681465', 'headline' => 'Lorem ipsum', 'alias' => 'lorem-ipsum', 'author' => '2', 'date' => '1515492660', 'time' => '1515492660', 'subheadline' => 'Lorem ipsum dolor sit amet, consetetur sadipscing', 'teaser' => '…', 'addImage' => '1', 'overwriteMeta' => '', 'singleSRC' => 'files/foo.jpg', 'alt' => '', 'imageTitle' => '', 'size' => array('', '', 5), …), 0, null, object(FilesModel))
         (src\Intomedia\WebsiteBundle\EventListener\HookListener.php:46)
      at Intomedia\WebsiteBundle\EventListener\HookListener->onParseArticles(object(FrontendTemplate), array('id' => '16', 'pid' => '1', 'tstamp' => '1515681465', 'headline' => 'Lorem ipsum', 'alias' => 'lorem-ipsum', 'author' => '2', 'date' => '1515492660', 'time' => '1515492660', 'subheadline' => 'Lorem ipsum dolor sit amet, consetetur sadipscing', 'teaser' => '…', 'addImage' => '1', 'overwriteMeta' => '', 'singleSRC' => 'files/foo.jpg', 'alt' => '', 'imageTitle' => '', 'size' => array('', '', 5), …), object(NewsListModule))
         (vendor\contao\news-bundle\src\Resources\contao\modules\ModuleNews.php:204)
      at Contao\ModuleNews->parseArticle(object(NewsModel), false, ' featured odd', 2)
         (vendor\contao\news-bundle\src\Resources\contao\modules\ModuleNews.php:237)
      at Contao\ModuleNews->parseArticles(object(Collection))
         (vendor\contao\news-bundle\src\Resources\contao\modules\ModuleNewsList.php:150)
      at Contao\ModuleNewsList->compile()
         (vendor\contao\core-bundle\src\Resources\contao\modules\Module.php:227)
      at Contao\Module->generate()
         (vendor\contao\news-bundle\src\Resources\contao\modules\ModuleNewsList.php:65)
    …
         (web\app_dev.php:65)
    ```
    /cc @ausi
    
    Commits
    -------
    
    9cd09d0c check if third size parameter is actually a string

--- core-bundle/src/Image/PictureFactory.php
+++ core-bundle/src/Image/PictureFactory.php
@@ -102,7 +102,7 @@ class PictureFactory implements PictureFactoryInterface
             $image = $this->imageFactory->create($path);
         }
 
-        if (\is_array($size) && isset($size[2]) && 1 === substr_count($size[2], '_')) {
+        if (\is_array($size) && isset($size[2]) && \is_string($size[2]) && 1 === substr_count($size[2], '_')) {
             $image->setImportantPart($this->imageFactory->getImportantPartFromLegacyMode($image, $size[2]));
             $size[2] = ResizeConfigurationInterface::MODE_CROP;
         }
