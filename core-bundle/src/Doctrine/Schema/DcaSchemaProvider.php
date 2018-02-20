c8246750756a8437d4ccdb6cc358e0aec9966c7f
Leo Feyer <github@contao.org>
2018-02-21 00:26:28 +0100

    [Core] Adjust a regex (just for the sake of it).

--- core-bundle/src/Doctrine/Schema/DcaSchemaProvider.php
+++ core-bundle/src/Doctrine/Schema/DcaSchemaProvider.php
@@ -259,7 +259,7 @@ class DcaSchemaProvider
             case 'real':
             case 'numeric':
             case 'decimal':
-                if (preg_match('/[A-Za-z]+\((\d+)\,(\d+)\)/', $dbType, $match)) {
+                if (preg_match('/[a-z]+\((\d+)\,(\d+)\)/i', $dbType, $match)) {
                     $length = null;
                     $precision = $match[1];
                     $scale = $match[2];
