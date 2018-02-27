83ff4e573736ecbe6f8898bfd7ae4ecd76d022ef
Leo Feyer <github@contao.org>
2018-02-27 21:35:33 +0100

    [Core] Update the copyright notices.

--- core-bundle/src/Resources/contao/helper/exception.php
+++ core-bundle/src/Resources/contao/helper/exception.php
@@ -1,13 +1,12 @@
 <?php
 
-/**
- * Contao Open Source CMS
+/*
+ * This file is part of Contao.
  *
- * Copyright (c) 2005-2018 Leo Feyer
+ * (c) Leo Feyer
  *
- * @license LGPL-3.0+
+ * @license LGPL-3.0-or-later
  */
-
 
 /**
  * Class UnusedArgumentsException
