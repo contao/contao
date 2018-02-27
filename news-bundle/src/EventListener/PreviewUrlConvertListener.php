6424d02a04240c4e3b1db2dc5a7f6382adb70ede
Leo Feyer <github@contao.org>
2018-02-27 21:48:12 +0100

    [News] Update the copyright notices.

--- news-bundle/src/EventListener/PreviewUrlConvertListener.php
+++ news-bundle/src/EventListener/PreviewUrlConvertListener.php
@@ -3,9 +3,9 @@
 /*
  * This file is part of Contao.
  *
- * Copyright (c) 2005-2018 Leo Feyer
+ * (c) Leo Feyer
  *
- * @license LGPL-3.0+
+ * @license LGPL-3.0-or-later
  */
 
 namespace Contao\NewsBundle\EventListener;
