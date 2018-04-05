ba1ab6179aab471b508993ba95bbe05ea56095d4
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-04-05 18:01:33 +0200

    [Core] Add a translator insert tag (see #1400).
    Description
    -----------
    
    Contao 4.5 supports the Symfony translator, but there is no insert tag to use the Symfony translator. Here's a fix for that.
    
     - `{{trans::foo_bar}}` => Replace `foo_bar` with the default message domain (*messages*), mostly not useful
     - `{{trans::foo_bar::app}}` => Replace `foo_bar` from `app` message domain
     - `{{trans::foo_bar::app::param1:param2}}` => Replace `foo_bar` from `app` message domain with parameters `param1` and `param2`. The parameters are separated by single colons, like they are for the `label` insert tag.
    
    Commits
    -------
    
    9bc77b9d Added event listener for {{trans::}} insert tag
    e0590f71 Fixed empty parameters
    03694976 Switch domain and parameter position
    d1bd88f1 Added unit tests
    ac1a54f7 Fix the coding style.

---
+++ core-bundle/tests/EventListener/InsertTags/TranslationListenerTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\EventListener\InsertTags;
+
+use Contao\CoreBundle\EventListener\InsertTags\TranslationListener;
+use Contao\CoreBundle\Tests\TestCase;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class TranslationListenerTest extends TestCase
+{
+    public function testCanBeInstantiated(): void
+    {
+        $listener = new TranslationListener($this->createMock(TranslatorInterface::class));
+
+        $this->assertInstanceOf('Contao\CoreBundle\EventListener\InsertTags\TranslationListener', $listener);
+    }
+
+    /**
+     * @param string      $id
+     * @param string      $result
+     * @param string|null $domain
+     * @param array       $parameters
+     *
+     * @dataProvider insertTagsProvider
+     */
+    public function testReplacesInsertTagsWithTranslation(string $id, string $result, string $domain = null, array $parameters = []): void
+    {
+        $translator = $this->createMock(TranslatorInterface::class);
+
+        $translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($id, $parameters, $domain)
+            ->willReturn($result)
+        ;
+
+        $listener = new TranslationListener($translator);
+
+        if (null === $domain) {
+            $insertTag = sprintf('trans::%s', $id);
+        } elseif (empty($parameters)) {
+            $insertTag = sprintf('trans::%s::%s', $id, $domain);
+        } else {
+            $insertTag = sprintf('trans::%s::%s::%s', $id, $domain, implode(':', $parameters));
+        }
+
+        $this->assertSame($result, $listener->onReplaceInsertTags($insertTag));
+    }
+
+    /**
+     * @return array
+     */
+    public function insertTagsProvider(): array
+    {
+        return [
+            ['foo', 'bar'],
+            ['foo', 'baz', 'bar'],
+            ['foo', 'else', 'bar', ['baz', 'what']],
+        ];
+    }
+
+    public function testIgnoresOtherInsertTags(): void
+    {
+        $translator = $this->createMock(TranslatorInterface::class);
+
+        $translator
+            ->expects($this->never())
+            ->method('trans')
+        ;
+
+        $translator
+            ->expects($this->never())
+            ->method('transChoice')
+        ;
+
+        $listener = new TranslationListener($translator);
+
+        $this->assertFalse($listener->onReplaceInsertTags('env::pageTitle'));
+    }
+}
