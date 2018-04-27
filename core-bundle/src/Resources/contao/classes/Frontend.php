30a50aa4f30d161e0502c60910b356d401807935
Leo Feyer <github@contao.org>
2018-04-27 11:08:23 +0200

    [Core] Do not limit indexing to regular pages (see #1435).

--- core-bundle/src/Resources/contao/classes/Frontend.php
+++ core-bundle/src/Resources/contao/classes/Frontend.php
@@ -604,7 +604,7 @@ abstract class Frontend extends Controller
 		}
 
 		// Index page if searching is allowed and there is no back end user
-		if (\Config::get('enableSearch') && $objPage->type == 'regular' && !BE_USER_LOGGED_IN && !$objPage->noSearch)
+		if (\Config::get('enableSearch') && $objResponse->getStatusCode() == 200 && !BE_USER_LOGGED_IN && !$objPage->noSearch)
 		{
 			// Index protected pages if enabled
 			if (\Config::get('indexProtected') || (!FE_USER_LOGGED_IN && !$objPage->protected))
