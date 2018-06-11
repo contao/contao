e368966a3a2bfc6bb58245fb0f7115548bccedfb
Martin Auswöger <martin@auswoeger.com>
2018-06-11 16:51:14 +0200

    [Core] Add the gulp watch command (see #1547).
    Description
    -----------
    
    Makes it way easier to work with the CSS and JS files 😎
    
    Commits
    -------
    
    bf917244 Add the gulp watch command.

--- core-bundle/gulpfile.js
+++ core-bundle/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp'),
     csso = require('gulp-csso'),
     ignore = require('gulp-ignore'),
+    livereload = require('gulp-livereload'),
     rename = require('gulp-rename'),
     svgo = require('gulp-svgo'),
     uglify = require('gulp-uglify'),
@@ -16,7 +17,8 @@ gulp.task('minify-public', function (cb) {
             rename({
                 suffix: '.min'
             }),
-            gulp.dest('src/Resources/public')
+            gulp.dest('src/Resources/public'),
+            livereload()
         ],
         cb
     );
@@ -26,7 +28,8 @@ gulp.task('minify-theme-js', function (cb) {
     pump([
             gulp.src('src/Resources/contao/themes/flexible/src/*.js'),
             uglify(),
-            gulp.dest('src/Resources/contao/themes/flexible')
+            gulp.dest('src/Resources/contao/themes/flexible'),
+            livereload()
         ],
         cb
     );
@@ -39,7 +42,8 @@ gulp.task('minify-theme-css', function (cb) {
                 comments: false,
                 restructure: false
             }),
-            gulp.dest('src/Resources/contao/themes/flexible')
+            gulp.dest('src/Resources/contao/themes/flexible'),
+            livereload()
         ],
         cb
     );
@@ -49,10 +53,19 @@ gulp.task('minify-theme-icons', function (cb) {
     pump([
             gulp.src('src/Resources/contao/themes/flexible/icons/*.svg'),
             svgo(),
-            gulp.dest('src/Resources/contao/themes/flexible/icons')
+            gulp.dest('src/Resources/contao/themes/flexible/icons'),
+            livereload()
         ],
         cb
     );
+});
+
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(['src/Resources/public/*.js', '!src/Resources/public/*.min.js'], ['minify-public']);
+    gulp.watch('src/Resources/contao/themes/flexible/src/*.js', ['minify-theme-js']);
+    gulp.watch('src/Resources/contao/themes/flexible/src/*.css', ['minify-theme-css']);
+    gulp.watch('src/Resources/contao/themes/flexible/icons/*.svg', ['minify-theme-icons']);
 });
 
 gulp.task('default', ['minify-public', 'minify-theme-js', 'minify-theme-css', 'minify-theme-icons']);
