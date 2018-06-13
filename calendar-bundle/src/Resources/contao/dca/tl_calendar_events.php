04af0bb73f67ea2e61871de31e1189f43c9cc96e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2018-06-13 10:08:43 +0200

    [Calendar] Reduce memory consumption during sitemap generation (see #31).
    Description
    -----------
    
    See https://github.com/contao/news-bundle/pull/37
    
    Commits
    -------
    
    e5ada9e0 reduce memory consumption during sitemap generation (contao/core-bundle#1157)

--- calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
+++ calendar-bundle/src/Resources/contao/dca/tl_calendar_events.php
@@ -151,7 +151,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events'] = array
 		(
 			'foreignKey'              => 'tl_calendar.title',
 			'sql'                     => "int(10) unsigned NOT NULL default '0'",
-			'relation'                => array('type'=>'belongsTo', 'load'=>'eager')
+			'relation'                => array('type'=>'belongsTo', 'load'=>'lazy')
 		),
 		'tstamp' => array
 		(
@@ -194,7 +194,7 @@ $GLOBALS['TL_DCA']['tl_calendar_events'] = array
 			'foreignKey'              => 'tl_user.name',
 			'eval'                    => array('doNotCopy'=>true, 'chosen'=>true, 'mandatory'=>true, 'includeBlankOption'=>true, 'tl_class'=>'w50'),
 			'sql'                     => "int(10) unsigned NOT NULL default '0'",
-			'relation'                => array('type'=>'hasOne', 'load'=>'eager')
+			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
 		),
 		'addTime' => array
 		(
