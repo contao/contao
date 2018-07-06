3a22dc38441b6bb59d625f6b642ae7e188b68333
Leo Feyer <github@contao.org>
2018-07-06 16:51:23 +0200

    [Core] Do not load records eagerly by default (see contao/calendar-bundle#33).

--- core-bundle/src/Resources/contao/dca/tl_layout.php
+++ core-bundle/src/Resources/contao/dca/tl_layout.php
@@ -121,7 +121,7 @@ $GLOBALS['TL_DCA']['tl_layout'] = array
 		(
 			'foreignKey'              => 'tl_theme.name',
 			'sql'                     => "int(10) unsigned NOT NULL default '0'",
-			'relation'                => array('type'=>'belongsTo', 'load'=>'eager')
+			'relation'                => array('type'=>'belongsTo', 'load'=>'lazy')
 		),
 		'tstamp' => array
 		(
--- core-bundle/src/Resources/contao/dca/tl_module.php
+++ core-bundle/src/Resources/contao/dca/tl_module.php
@@ -302,7 +302,7 @@ $GLOBALS['TL_DCA']['tl_module'] = array
 			'foreignKey'              => 'tl_page.title',
 			'eval'                    => array('fieldType'=>'radio'),
 			'sql'                     => "int(10) unsigned NOT NULL default '0'",
-			'relation'                => array('type'=>'hasOne', 'load'=>'eager')
+			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
 		),
 		'redirectBack' => array
 		(
