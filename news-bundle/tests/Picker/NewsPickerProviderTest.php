48e321ea1d84ac52f7d34e16dbec307c5aac4835
Leo Feyer <github@contao.org>
2018-07-16 17:12:12 +0200

    [News] Handle the "source" attribute in the picker (see contao/core-bundle#1574)

--- news-bundle/tests/Picker/NewsPickerProviderTest.php
+++ news-bundle/tests/Picker/NewsPickerProviderTest.php
@@ -239,17 +239,23 @@ class NewsPickerProviderTest extends TestCase
      */
     public function testReturnsTheDcaAttributes()
     {
+        $extra = ['source' => 'tl_news.2'];
+
         $this->assertSame(
             [
                 'fieldType' => 'radio',
+                'preserveRecord' => 'tl_news.2',
                 'value' => '5',
             ],
-            $this->provider->getDcaAttributes(new PickerConfig('link', [], '{{news_url::5}}'))
+            $this->provider->getDcaAttributes(new PickerConfig('link', $extra, '{{news_url::5}}'))
         );
 
         $this->assertSame(
-            ['fieldType' => 'radio'],
-            $this->provider->getDcaAttributes(new PickerConfig('link', [], '{{link_url::5}}'))
+            [
+                'fieldType' => 'radio',
+                'preserveRecord' => 'tl_news.2',
+            ],
+            $this->provider->getDcaAttributes(new PickerConfig('link', $extra, '{{link_url::5}}'))
         );
     }
 
