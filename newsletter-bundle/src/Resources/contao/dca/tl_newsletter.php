22fb6437b09e08c022b507fcf0c5a9c2390fd9d9
Leo Feyer <github@contao.org>
2018-08-14 09:49:06 +0200

    [Newsletter] Revert the StringUtil::gererateSlug() changes (see contao/core-bundle#1671)

--- newsletter-bundle/src/Resources/contao/dca/tl_newsletter.php
+++ newsletter-bundle/src/Resources/contao/dca/tl_newsletter.php
@@ -481,7 +481,13 @@ class tl_newsletter extends Backend
 				}
 			}
 
-			$varValue = StringUtil::generateSlug($dc->activeRecord->subject, $slugOptions);
+			$varValue = System::getContainer()->get('contao.slug.generator')->generate(StringUtil::stripInsertTags($dc->activeRecord->subject), $slugOptions);
+
+			// Prefix numeric aliases (see #1598)
+			if (is_numeric($varValue))
+			{
+				$varValue = 'id-' . $varValue;
+			}
 		}
 
 		$objAlias = $this->Database->prepare("SELECT id FROM tl_newsletter WHERE alias=? AND id!=?")
