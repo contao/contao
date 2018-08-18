cdb2b0d86d39f31791293aa69200f22c10971e41
Martin Auswöger <martin@auswoeger.com>
2018-08-18 22:57:54 +0200

    Fix installation-bundle test namespace

--- installation-bundle/tests/Database/InstallerTest.php
+++ installation-bundle/tests/Database/InstallerTest.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\InstallationBundle\Test\Database;
+namespace Contao\InstallationBundle\Tests\Database;
 
 use Contao\CoreBundle\Doctrine\Schema\DcaSchemaProvider;
 use Contao\InstallationBundle\Database\Installer;
