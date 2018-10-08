55f46c6a15300e7eefe5eaa2855b58b106434515
Martin Auswöger <martin@auswoeger.com>
2018-10-08 15:07:51 +0200

    Disable the phar stream wrapper (see #105)
    Description
    -----------
    
    As of the newly discovered unserialization attack via the `phar://` stream wrapper ([Black Hat talk](https://www.blackhat.com/us-18/briefings/schedule/index.html#its-a-php-unserialization-vulnerability-jim-but-not-as-we-know-it-11078)), we should disable the phar stream wrapper for the whole application.
    
    It should be noted that this is **only a preventive measure** and shouldn’t be considered a “fix” for this kind of attacks. **We must never pass user controlled values directly to any file operation functions without validating them.**
    
    If anybody needs the stream wrapper for a specific use case in their project or extension, I think the following code snippet should be safe to use:
    
    ```php
    if ($pharDisabled = !\in_array('phar', stream_get_wrappers(), true)) {
    	stream_wrapper_restore('phar');
    }
    try {
    	// Your code using phar://...
    }
    finally {
    	if ($pharDisabled) {
    		stream_wrapper_unregister('phar');
    	}
    }
    ```
    
    Commits
    -------
    
    723a8681 Disable the phar stream wrapper
    40104ec1 Disable the stream wrapper in initialize.php too

--- manager-bundle/src/Resources/system/initialize.php
+++ manager-bundle/src/Resources/system/initialize.php
@@ -9,6 +9,11 @@ if (!defined('TL_SCRIPT')) {
     die('Your script is not compatible with Contao 4.');
 }
 
+// Disable the phar stream wrapper for security reasons (see #105)
+if (\in_array('phar', stream_get_wrappers(), true)) {
+    stream_wrapper_unregister('phar');
+}
+
 /** @var Composer\Autoload\ClassLoader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
