contao/contao

commit 5a284eb8883c1b8a5bbdabdc1ab48cc64a5a8a69

[Core] Add drag and drop file upload for file tree (see #1386).
Description
-----------

-

Commits
-------

bf325c27 Add drag and drop file upload for file tree
9d5637ec Fix the coding style.
101ae6d2 Use the old array notation.

core-bundle/src/Resources/contao/classes/FileUpload.php
@@ -252,7 +252,7 @@ class FileUpload extends \Backend
 	 *
 	 * @return string
 	 */
-	protected function getMaximumUploadSize()
+	public static function getMaximumUploadSize()
 	{
 		// Get the upload_max_filesize from the php.ini
 		$upload_max_filesize = ini_get('upload_max_filesize');

core-bundle/src/Resources/contao/drivers/DC_Folder.php
@@ -518,6 +518,29 @@ class DC_Folder extends \DataContainer implements \listable, \editable
 </form>';
 		}
 
+		if (!$GLOBALS['TL_DCA'][$this->strTable]['config']['closed'] && !$GLOBALS['TL_DCA'][$this->strTable]['config']['notCreatable'] && \Input::get('act') != 'select')
+		{
+			$GLOBALS['TL_CSS'][] = 'assets/dropzone/css/dropzone.min.css';
+			$GLOBALS['TL_JAVASCRIPT'][] = 'assets/dropzone/js/dropzone.min.js';
+
+			$strAccepted = implode(',', array_map(function ($a) { return '.' . $a; }, \StringUtil::trimsplit(',', strtolower(\Config::get('uploadTypes')))));
+			$intMaxSize = round(\FileUpload::getMaximumUploadSize() / 1024 / 1024);
+
+			$return .= '<script>'
+				.'Dropzone.autoDiscover = false;'
+				.'Backend.enableFileTreeUpload("tl_listing", '.json_encode(array(
+					'url' => html_entity_decode($this->addToUrl('act=move&mode=2&pid='.urlencode($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['root'][0] ?? \Config::get('uploadPath')))),
+					'paramName' => 'files',
+					'maxFilesize' => $intMaxSize,
+					'acceptedFiles' => $strAccepted,
+					'params' => array(
+						'FORM_SUBMIT' => 'tl_upload',
+						'action' => 'fileupload',
+					),
+				)).')</script>'
+			;
+		}
+
 		return $return;
 	}
 

core-bundle/src/Resources/contao/themes/flexible/src/main.css
@@ -611,6 +611,9 @@ table.tl_listing {
 	padding:2px 0;
 	margin:20px 15px 16px;
 }
+.tl_listing_container.tree_view {
+	position:relative;
+}
 #tl_buttons + .tl_listing_container {
 	margin-top:10px;
 }
@@ -885,6 +888,9 @@ table.tl_listing {
 	padding:5px 0;
 	border-bottom:1px solid #e9e9e9;
 	background:#f6f6f8;
+}
+.tl_folder.tl_folder_dropping {
+	background-color:#ccc !important;
 }
 .tl_listing .tl_left {
 	flex-grow:1;
@@ -1740,6 +1746,19 @@ fieldset.collapsed > legend {
 	min-height:auto !important;
 	border:3px dashed #ddd !important;
 	border-radius:2px;
+}
+.dropzone-filetree {
+	display:none;
+	position:absolute;
+	top:0;
+	left:0;
+	width:100%;
+	height:100%;
+	opacity:.8;
+	z-index:1;
+}
+.dropzone-filetree-enabled {
+	display:block;
 }
 .dz-message span {
 	font-size:1.3125rem;

core-bundle/src/Resources/public/core.js
@@ -2515,6 +2515,100 @@ var Backend =
 		;
 
 		window.addEvent('domready', init);
+	},
+
+	/**
+	 * Enable drag and drop file upload for the file tree
+	 *
+	 * @param {object} wrap    The DOM element
+	 * @param {object} options An optional options object
+	 */
+	enableFileTreeUpload: function(wrap, options) {
+		wrap = $(wrap);
+
+		var fallbackUrl = options.url,
+			dzElement = new Element('div', {
+				'class': 'dropzone dropzone-filetree',
+				html: '<span class="dropzone-previews"></span>'
+			}).inject(wrap, 'top'),
+			currentHover, currentHoverTime;
+
+		options.previewsContainer = dzElement.getElement('.dropzone-previews');
+		options.clickable = false;
+
+		var dz = new Dropzone(wrap, options);
+
+		dz.on('queuecomplete', function() {
+			window.location.reload();
+		});
+
+		dz.on('dragover', function(event) {
+			if (!event.dataTransfer || !event.dataTransfer.types || event.dataTransfer.types.indexOf('Files') === -1) {
+				return;
+			}
+
+			wrap.getElements('.tl_folder_dropping').removeClass('tl_folder_dropping');
+			var target = event.target && $(event.target);
+
+			if (target) {
+				var folder = target.match('.tl_folder') ? target : target.getParent('.tl_folder');
+
+				if (!folder) {
+					folder = target.getParent('.parent');
+					folder = folder && folder.getPrevious('.tl_folder');
+				}
+
+				if (folder) {
+					var link = folder.getElement('img[src$="/icons/new.svg"]');
+					link = link && link.getParent('a');
+				}
+			}
+
+			if (link && link.href) {
+				dz.options.url = ''+link.href;
+				folder.addClass('tl_folder_dropping');
+
+				if (currentHover !== folder) {
+					currentHover = folder;
+					currentHoverTime = new Date().getTime();
+
+					var expandLink = folder.getElement('img[src$="/icons/folPlus.svg"]');
+					expandLink = expandLink && expandLink.getParent('a');
+
+					if (expandLink) {
+						// Expand the folder after one second hover time
+						setTimeout(function() {
+							if (currentHover === folder && currentHoverTime + 900 < new Date().getTime()) {
+								var event = document.createEvent('HTMLEvents');
+								event.initEvent('click', true, true);
+								expandLink.dispatchEvent(event);
+								currentHover = undefined;
+								currentHoverTime = undefined;
+							}
+						}, 1000);
+					}
+				}
+			} else {
+				dz.options.url = fallbackUrl;
+				currentHover = undefined;
+				currentHoverTime = undefined;
+			}
+		});
+
+		dz.on('drop', function (event) {
+			if (!event.dataTransfer || !event.dataTransfer.types || event.dataTransfer.types.indexOf('Files') === -1) {
+				return;
+			}
+
+			dzElement.addClass('dropzone-filetree-enabled');
+			Backend.getScrollOffset();
+		});
+
+		dz.on('dragleave', function() {
+			wrap.getElements('.tl_folder_dropping').removeClass('tl_folder_dropping');
+			currentHover = undefined;
+			currentHoverTime = undefined;
+		});
 	}
 };
 

commit c03f5b5d1d17372e72b71e6abf9baa0d6d64a32d

Fix a BC break in the FileUpload class (see #188)
Description
-----------

Fixes BC break codefog/contao-haste#135 which was introduced in 5a284eb8883c1b8a5bbdabdc1ab48cc64a5a8a69.

@leofeyer should we deprecate the protected method `getMaximumUploadSize()` and use the static one everywhere instead?

Commits
-------

43add55a Fix BC break introduced in 5a284eb8883c1b8a5bbdabdc1ab48cc64a5a8a69
57ffcefb Deprecate FileUpload::getMaximumUploadSize()
ad82c693 Use Symfony to read the max filesize ini setting
3ed7b003 Use a use statement instead of the FQCN

core-bundle/src/Resources/contao/classes/DropZone.php
@@ -26,7 +26,7 @@ class DropZone extends FileUpload
 	public function generateMarkup()
 	{
 		// Maximum file size in MB
-		$intMaxSize = round($this->getMaximumUploadSize() / 1024 / 1024);
+		$intMaxSize = round(static::getMaxUploadSize() / 1024 / 1024);
 
 		// String of accepted file extensions
 		$strAccepted = implode(',', array_map(function ($a) { return '.' . $a; }, \StringUtil::trimsplit(',', strtolower(\Config::get('uploadTypes')))));
@@ -79,7 +79,7 @@ class DropZone extends FileUpload
 		if (isset($GLOBALS['TL_LANG']['tl_files']['fileupload'][1]))
 		{
 			$return .= '
-  <p class="tl_help tl_tip">' . sprintf($GLOBALS['TL_LANG']['tl_files']['fileupload'][1], \System::getReadableSize($this->getMaximumUploadSize()), \Config::get('gdMaxImgWidth') . 'x' . \Config::get('gdMaxImgHeight')) . '</p>';
+  <p class="tl_help tl_tip">' . sprintf($GLOBALS['TL_LANG']['tl_files']['fileupload'][1], \System::getReadableSize(static::getMaxUploadSize()), \Config::get('gdMaxImgWidth') . 'x' . \Config::get('gdMaxImgHeight')) . '</p>';
 		}
 
 		return $return;

core-bundle/src/Resources/contao/classes/FileUpload.php
@@ -10,6 +10,8 @@
 
 namespace Contao;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * Provide methods to handle file uploads in the back end.
  *
@@ -90,7 +92,7 @@ class FileUpload extends Backend
 			throw new \InvalidArgumentException('Invalid target path ' . $strTarget);
 		}
 
-		$maxlength_kb = $this->getMaximumUploadSize();
+		$maxlength_kb = static::getMaxUploadSize();
 		$maxlength_kb_readable = $this->getReadableSize($maxlength_kb);
 		$arrUploaded = array();
 		$arrFiles = $this->getFilesFromGlobal();
@@ -211,7 +213,7 @@ class FileUpload extends Backend
 		if (isset($GLOBALS['TL_LANG']['tl_files']['fileupload'][1]))
 		{
 			$return .= '
-  <p class="tl_help tl_tip">' . sprintf($GLOBALS['TL_LANG']['tl_files']['fileupload'][1], \System::getReadableSize($this->getMaximumUploadSize()), \Config::get('gdMaxImgWidth') . 'x' . \Config::get('gdMaxImgHeight')) . '</p>';
+  <p class="tl_help tl_tip">' . sprintf($GLOBALS['TL_LANG']['tl_files']['fileupload'][1], \System::getReadableSize(static::getMaxUploadSize()), \Config::get('gdMaxImgWidth') . 'x' . \Config::get('gdMaxImgHeight')) . '</p>';
 		}
 
 		return $return;
@@ -257,27 +259,25 @@ class FileUpload extends Backend
 	 * Return the maximum upload file size in bytes
 	 *
 	 * @return string
+	 *
+	 * @deprecated Deprecated since Contao 4.6, to be removed in Contao 5.0.
+	 *             Use FileUpload::getMaxUploadSize() instead.
 	 */
-	public static function getMaximumUploadSize()
+	protected function getMaximumUploadSize()
 	{
-		// Get the upload_max_filesize from the php.ini
-		$upload_max_filesize = ini_get('upload_max_filesize');
+		@trigger_error('Using FileUpload::getMaximumUploadSize() has been deprecated and will no longer work in Contao 5.0. Use FileUpload::getMaxUploadSize() instead.', E_USER_DEPRECATED);
 
-		// Convert the value to bytes
-		if (stripos($upload_max_filesize, 'K') !== false)
-		{
-			$upload_max_filesize = round(str_replace('K', '', $upload_max_filesize) * 1024);
-		}
-		elseif (stripos($upload_max_filesize, 'M') !== false)
-		{
-			$upload_max_filesize = round(str_replace('M', '', $upload_max_filesize) * 1024 * 1024);
-		}
-		elseif (stripos($upload_max_filesize, 'G') !== false)
-		{
-			$upload_max_filesize = round(str_replace('G', '', $upload_max_filesize) * 1024 * 1024 * 1024);
-		}
+		return static::getMaxUploadSize();
+	}
 
-		return min($upload_max_filesize, \Config::get('maxFileSize'));
+	/**
+	 * Return the maximum upload file size in bytes
+	 *
+	 * @return string
+	 */
+	public static function getMaxUploadSize()
+	{
+		return min(UploadedFile::getMaxFilesize(), \Config::get('maxFileSize'));
 	}
 
 	/**

core-bundle/src/Resources/contao/drivers/DC_Folder.php
@@ -520,7 +520,7 @@ class DC_Folder extends DataContainer implements \listable, \editable
 			$GLOBALS['TL_JAVASCRIPT'][] = 'assets/dropzone/js/dropzone.min.js';
 
 			$strAccepted = implode(',', array_map(function ($a) { return '.' . $a; }, \StringUtil::trimsplit(',', strtolower(\Config::get('uploadTypes')))));
-			$intMaxSize = round(\FileUpload::getMaximumUploadSize() / 1024 / 1024);
+			$intMaxSize = round(\FileUpload::getMaxUploadSize() / 1024 / 1024);
 
 			$return .= '<script>'
 				.'Dropzone.autoDiscover = false;'

core-bundle/src/Resources/contao/forms/FormFileUpload.php
@@ -321,24 +321,7 @@ class FormFileUpload extends Widget implements \uploadable
 			return $this->maxlength;
 		}
 
-		// Get the upload_max_filesize from the php.ini
-		$upload_max_filesize = ini_get('upload_max_filesize');
-
-		// Convert the value to bytes
-		if (stripos($upload_max_filesize, 'K') !== false)
-		{
-			$upload_max_filesize = round($upload_max_filesize * 1024);
-		}
-		elseif (stripos($upload_max_filesize, 'M') !== false)
-		{
-			$upload_max_filesize = round($upload_max_filesize * 1024 * 1024);
-		}
-		elseif (stripos($upload_max_filesize, 'G') !== false)
-		{
-			$upload_max_filesize = round($upload_max_filesize * 1024 * 1024 * 1024);
-		}
-
-		return min($upload_max_filesize, \Config::get('maxFileSize'));
+		return \FileUpload::getMaxUploadSize();
 	}
 }
 