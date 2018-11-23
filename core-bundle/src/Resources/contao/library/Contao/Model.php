edcc8462c1dcbdcc810b8efc4463c1752f23a1d5
Leo Feyer <github@contao.org>
2018-11-23 12:28:34 +0100

    Update Model.php

--- core-bundle/src/Resources/contao/library/Contao/Model.php
+++ core-bundle/src/Resources/contao/library/Contao/Model.php
@@ -110,7 +110,7 @@ abstract class Model
 	/**
 	 * Load the relations and optionally process a result set
 	 *
-	 * @param Result|array $objResult An optional database result or array to initialize the model
+	 * @param Result|array $objResult An optional database result or array
 	 */
 	public function __construct($objResult=null)
 	{
