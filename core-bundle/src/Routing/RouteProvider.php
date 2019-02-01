1751b20bcae8c56c9765fba82b4cc531fd8688e8
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-02-01 09:16:34 +0100

    Sort unpreferred languages by root page priority (see #309)
    Description
    -----------
    
    My case was this:
    
     - `folderUrl` is enabled
     - my browser prefers `de` as language
     - I have four pages, which are fetched in this order from the DB
       1. Alias `foo` with language `en`
       2. Alias `foo` with language `fr`
       3. Alias `foo` with language `zh`
       4. Alias `foo/bar` with language `en`
    
    Because none of the languages is in my browser preferences, the items are currently not re-sorted. So Page 1 is never compared to page 4, which means they are not sorted by alias.
    
    I have not yet created a unit test, it probably needs a new test method and can't be accomplished within the existing data providers.
    
    Commits
    -------
    
    4646d5b5 Sort unpreferred languages by root page priority
    6fcd4eea Added unit tests

--- core-bundle/src/Routing/RouteProvider.php
+++ core-bundle/src/Routing/RouteProvider.php
@@ -421,7 +421,7 @@ class RouteProvider implements RouteProviderInterface
                         return -1;
                     }
 
-                    return $pageB->rootIsFallback ? 1 : 0;
+                    return $pageB->rootIsFallback ? 1 : strnatcmp((string) $pageA->rootSorting, (string) $pageB->rootSorting);
                 }
 
                 if (null === $langA && null !== $langB) {
