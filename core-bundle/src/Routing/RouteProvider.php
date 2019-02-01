ff24fc3bbc11123ef87d545a33d01244c3440cbe
Leo Feyer <github@contao.org>
2019-02-01 15:52:54 +0100

    Use the spaceship operator to compare the root sorting values

--- core-bundle/src/Routing/RouteProvider.php
+++ core-bundle/src/Routing/RouteProvider.php
@@ -421,7 +421,7 @@ class RouteProvider implements RouteProviderInterface
                         return -1;
                     }
 
-                    return $pageB->rootIsFallback ? 1 : strnatcmp((string) $pageA->rootSorting, (string) $pageB->rootSorting);
+                    return $pageB->rootIsFallback ? 1 : $pageA->rootSorting <=> $pageB->rootSorting;
                 }
 
                 if (null === $langA && null !== $langB) {
