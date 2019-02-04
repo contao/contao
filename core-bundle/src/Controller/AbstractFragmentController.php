contao/contao

commit 2f9ccef9aebd9b74478c7ee3ba956c054293eb44

Extend from AbstractController instead of Controller (forward compatibility with Symfony 4.2)

core-bundle/src/Controller/AbstractFragmentController.php
@@ -17,10 +17,10 @@ use Contao\FrontendTemplate;
 use Contao\Model;
 use Contao\StringUtil;
 use Contao\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Container;
 
-abstract class AbstractFragmentController extends Controller implements FragmentOptionsAwareInterface
+abstract class AbstractFragmentController extends AbstractController implements FragmentOptionsAwareInterface
 {
     /**
      * @var array

core-bundle/src/Controller/BackendController.php
@@ -24,7 +24,7 @@ use Contao\BackendPopup;
 use Contao\BackendPreview;
 use Contao\BackendSwitch;
 use Contao\CoreBundle\Picker\PickerConfig;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,7 +34,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route(defaults={"_scope" = "backend", "_token_check" = true})
  */
-class BackendController extends Controller
+class BackendController extends AbstractController
 {
     /**
      * @Route("/contao", name="contao_backend")

core-bundle/src/Controller/FrontendController.php
@@ -18,7 +18,7 @@ use Contao\FrontendCron;
 use Contao\FrontendIndex;
 use Contao\FrontendShare;
 use Contao\PageError401;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
@@ -28,7 +28,7 @@ use Symfony\Component\Security\Core\Exception\LogoutException;
 /**
  * @Route(defaults={"_scope" = "frontend", "_token_check" = true})
  */
-class FrontendController extends Controller
+class FrontendController extends AbstractController
 {
     public function indexAction(): Response
     {

core-bundle/src/Controller/InitializeController.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Controller;
 
 use Contao\CoreBundle\Response\InitializeControllerResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +22,7 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @deprecated Deprecated in Contao 4.0, to be removed in Contao 5.0
  */
-class InitializeController extends Controller
+class InitializeController extends AbstractController
 {
     /**
      * Initializes the Contao framework.

core-bundle/src/Controller/InsertTagsController.php
@@ -14,7 +14,7 @@ namespace Contao\CoreBundle\Controller;
 
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Contao\InsertTags;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -25,7 +25,7 @@ use Symfony\Component\HttpFoundation\Response;
  * Symfony fragment URI signer. If you use it directly, make sure to add a
  * permission check, because insert tags can contain arbitrary data!
  */
-class InsertTagsController extends Controller
+class InsertTagsController extends AbstractController
 {
     /**
      * @var ContaoFrameworkInterface

commit 6bb3c8259a847d65bb642c9a1f502a487270ae8f

Define controller service dependencies (see #311)
Description
-----------

This fixes issues introduced in 2f9ccef9aebd9b74478c7ee3ba956c054293eb44

Commits
-------

56ee98a6 Define controller service dependencies
20b78ffe Run the PHP-CS-Fixer

core-bundle/src/Controller/AbstractFragmentController.php
@@ -13,10 +13,12 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Controller;
 
 use Contao\CoreBundle\Fragment\FragmentOptionsAwareInterface;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\FrontendTemplate;
 use Contao\Model;
 use Contao\StringUtil;
 use Contao\Template;
+use FOS\HttpCacheBundle\Http\SymfonyResponseTagger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Container;
 
@@ -30,6 +32,20 @@ abstract class AbstractFragmentController extends AbstractController implements 
     public function setFragmentOptions(array $options): void
     {
         $this->options = $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedServices(): array
+    {
+        return array_merge(
+            parent::getSubscribedServices(),
+            [
+                'contao.framework' => ContaoFramework::class,
+                'fos_http_cache.http.symfony_response_tagger' => '?'.SymfonyResponseTagger::class,
+            ]
+        );
     }
 
     /**

core-bundle/src/Controller/FrontendModule/AbstractFrontendModuleController.php
@@ -14,10 +14,12 @@ namespace Contao\CoreBundle\Controller\FrontendModule;
 
 use Contao\BackendTemplate;
 use Contao\CoreBundle\Controller\AbstractFragmentController;
+use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\ModuleModel;
 use Contao\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Translation\TranslatorInterface;
 
 abstract class AbstractFrontendModuleController extends AbstractFragmentController
 {
@@ -36,6 +38,20 @@ abstract class AbstractFrontendModuleController extends AbstractFragmentControll
         $this->tagResponse(['contao.db.tl_module.'.$model->id]);
 
         return $this->getResponse($template, $model, $request);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedServices(): array
+    {
+        return array_merge(
+            parent::getSubscribedServices(),
+            [
+                'translator' => TranslatorInterface::class,
+                'contao.routing.scope_matcher' => ScopeMatcher::class,
+            ]
+        );
     }
 
     protected function getBackendWildcard(ModuleModel $module): Response