6c6cab78c829d832094f407024bb24712f2aedba
Martin Auswöger <martin@auswoeger.com>
2019-03-16 14:17:13 +0100

    Add deferred-images command

---
+++ core-bundle/src/Command/DeferredImagesCommand.php
@@ -0,0 +1,234 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Command;
+
+use Contao\CoreBundle\Image\ImageFactoryInterface;
+use Contao\Image\DeferredImageInterface;
+use Contao\Image\DeferredImageStorageInterface;
+use Contao\Image\DeferredResizerInterface;
+use Contao\Image\ResizerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Terminal;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+/**
+ * Resize deferred images that have not been processed yet.
+ */
+class DeferredImagesCommand extends Command
+{
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var ImageFactoryInterface
+     */
+    private $imageFactory;
+
+    /**
+     * @var ResizerInterface
+     */
+    private $resizer;
+
+    /**
+     * @var string
+     */
+    private $targetDir;
+
+    /**
+     * @var DeferredImageStorageInterface
+     */
+    private $storage;
+
+    /**
+     * @var int
+     */
+    private $terminalWidth;
+
+    public function __construct(Filesystem $filesystem, ImageFactoryInterface $imageFactory, ResizerInterface $resizer, string $targetDir, DeferredImageStorageInterface $storage)
+    {
+        $this->filesystem = $filesystem;
+        $this->imageFactory = $imageFactory;
+        $this->resizer = $resizer;
+        $this->targetDir = $targetDir;
+        $this->storage = $storage;
+        $this->terminalWidth = (new Terminal())->getWidth();
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('contao:deferred-images')
+            ->setDefinition([
+                new InputOption('time-limit', 'l', InputArgument::OPTIONAL, 'Time limit in seconds', '0'),
+                new InputOption('concurrent', 'c', InputArgument::OPTIONAL, 'Run multiple processes concurrently', '1'),
+                new InputOption('throttle', 't', InputArgument::OPTIONAL, 'Pause between resizes to limit cpu utilization, 0.1 relates to 10% cpu usage', '1'),
+            ])
+            ->setDescription('Resizes deferred images that have not been processed yet.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (!$this->resizer instanceof DeferredResizerInterface) {
+            throw new \RuntimeException('Deferred resizer not available');
+        }
+
+        $timeLimit = (float) $input->getOption('time-limit');
+        $throttle = (float) $input->getOption('throttle');
+        $concurrent = (string) $input->getOption('concurrent');
+
+        if ($timeLimit < 0) {
+            throw new InvalidArgumentException(sprintf('Time-limit value "%s" is invalid.', $timeLimit));
+        }
+
+        if ($throttle < 0.001 || $throttle > 1) {
+            throw new InvalidArgumentException(sprintf('Throttle value "%s" is invalid.', $timeLimit));
+        }
+
+        if ((string) (int) $concurrent !== $concurrent || (int) $concurrent < 1) {
+            throw new InvalidArgumentException(sprintf('Concurrent value "%s" is invalid.', $concurrent));
+        }
+
+        if ((int) $concurrent > 1) {
+            return $this->executeConcurrent((int) $concurrent, $input, $output);
+        }
+
+        $startTime = microtime(true);
+
+        $maxDuration = 0;
+        $lastDuration = 0;
+
+        foreach ($this->storage->listPaths() as $path) {
+            $sleep = (1 - $throttle) / $throttle * $lastDuration;
+
+            if ($timeLimit && microtime(true) - $startTime + $maxDuration + $sleep > $timeLimit) {
+                $output->writeln("\n".'Time limit of '.$timeLimit.' seconds reached.');
+
+                return 0;
+            }
+
+            usleep((int) ($sleep * 1000000));
+
+            $maxDuration = max($maxDuration, $lastDuration = $this->resizeImage($path, $output));
+        }
+
+        $output->writeln("\n".'All images resized.');
+
+        return 0;
+    }
+
+    private function resizeImage(string $path, OutputInterface $output): float
+    {
+        $startTime = microtime(true);
+
+        if ($this->filesystem->exists($path)) {
+            return 0;
+        }
+
+        $output->write(str_pad($path, $this->terminalWidth - 14, '.').' ');
+
+        try {
+            $image = $this->imageFactory->create($this->targetDir.'/'.$path);
+            $resizer = $this->resizer;
+
+            if ($image instanceof DeferredImageInterface) {
+                $resizedImage = $resizer->resizeDeferredImage($image, false);
+
+                if (null === $resizedImage) {
+                    $output->write("\r".str_repeat(' ', $this->terminalWidth)."\r");
+                } else {
+                    $output->writeln(sprintf('done%7.3Fs', $duration = microtime(true) - $startTime));
+
+                    return $duration;
+                }
+            } else {
+                $output->write("\r".str_repeat(' ', $this->terminalWidth)."\r");
+            }
+        } catch (\Throwable $exception) {
+            $output->writeln('failed');
+            $output->writeln($exception->getMessage());
+        }
+
+        return 0;
+    }
+
+    private function executeConcurrent(int $count, InputInterface $input, OutputInterface $output): int
+    {
+        $phpFinder = new PhpExecutableFinder();
+
+        $output->writeln('Starting '.$count.' concurrent processes...');
+
+        if (false === ($phpPath = $phpFinder->find())) {
+            throw new \RuntimeException('The php executable could not be found.');
+        }
+
+        /** @var $processes Process[] */
+        $processes = [];
+
+        /** @var $outputs string[] */
+        $outputs = [];
+
+        for ($i = 0; $i < $count; ++$i) {
+            $process = new Process(array_merge([$phpPath], $_SERVER['argv'], ['--concurrent=1']));
+            $process->setTimeout(null);
+            $process->setEnv([
+                'LINES' => getenv('LINES'),
+                'COLUMNS' => getenv('COLUMNS') - 4,
+            ]);
+            $process->start();
+            $processes[] = $process;
+            $outputs[] = '';
+        }
+
+        do {
+            $isRunning = false;
+
+            foreach ($processes as $index => $process) {
+                if ($process->isRunning()) {
+                    $isRunning = true;
+                }
+                $outputs[$index] .= $process->getIncrementalOutput();
+                $rows = explode("\n", $outputs[$index]);
+                $outputs[$index] = array_pop($rows);
+                $output->write(array_map(function ($row) use ($index) {
+                    return sprintf('%02d: ', $index + 1).preg_replace('/^.*\r/s', '', $row)."\n";
+                }, $rows));
+            }
+
+            usleep(15000);
+        } while ($isRunning);
+
+        $output->write($outputs);
+
+        return max(...array_map(function (Process $process): int {
+            return (int) $process->getExitCode();
+        }, $processes));
+    }
+}
