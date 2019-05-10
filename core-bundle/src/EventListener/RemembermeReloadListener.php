4ee7417dfbbd3a3592dfbb77376a75d781293936
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-05-10 07:03:36 +0200

    CS

--- core-bundle/src/EventListener/RemembermeReloadListener.php
+++ core-bundle/src/EventListener/RemembermeReloadListener.php
@@ -1,5 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 namespace Contao\CoreBundle\EventListener;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -10,7 +20,7 @@ use Symfony\Component\Security\Http\RememberMe\RememberMeServicesInterface;
 
 class RemembermeReloadListener
 {
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelRequest(GetResponseEvent $event): void
     {
         $request = $event->getRequest();
 
