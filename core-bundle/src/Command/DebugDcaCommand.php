d73c01152b190de03c8494392ef254f7e80b540a
Andreas Schempp <andreas.schempp@terminal42.ch>
2019-05-14 15:39:14 +0200

    Added command to debug/dump a DCA

---
+++ core-bundle/src/Command/DebugDcaCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Command;
+
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\DcaLoader;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+/**
+ * Dumps debug information about a Contao DCA.
+ */
+class DebugDcaCommand extends Command
+{
+    /**
+     * @var ContaoFramework
+     */
+    private $framework;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(ContaoFramework $framework, Connection $connection)
+    {
+        $this->framework = $framework;
+        $this->connection = $connection;
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('debug:dca')
+            ->addArgument('table', InputArgument::REQUIRED, 'The DCA table name')
+            ->setDescription('Dumps the DCA configuration for a table.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $table = $input->getArgument('table');
+
+        $dcaLoader = $this->framework->createInstance(DcaLoader::class, [$table]);
+        $dcaLoader->load();
+
+        $cloner = new VarCloner();
+        $dumper = new CliDumper();
+
+        $dumper->dump($cloner->cloneVar($GLOBALS['TL_DCA'][$table]));
+
+        return 0;
+    }
+}
