e50b31a4e6692bb3f198285fd9eca1e06f0260ae
David Greminger <david.greminger@1up.io>
2019-07-07 13:08:32 +0200

    Remove unused

--- core-bundle/tests/EventListener/TwoFactorFrontendListenerTest.php
+++ core-bundle/tests/EventListener/TwoFactorFrontendListenerTest.php
@@ -25,7 +25,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Kernel;
-use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
