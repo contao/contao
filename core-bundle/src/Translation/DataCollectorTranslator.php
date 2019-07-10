2cf5847474b1bb4812eb296ce9da01697d0d19e1
Leo Feyer <github@contao.org>
2019-07-10 13:00:45 +0200

    Fix the coding style

--- core-bundle/src/Translation/DataCollectorTranslator.php
+++ core-bundle/src/Translation/DataCollectorTranslator.php
@@ -13,15 +13,13 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Translation;
 
 use Symfony\Component\Translation\DataCollectorTranslator as SymfonyDataCollectorTranslator;
-use Symfony\Component\Translation\MessageCatalogueInterface;
-use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Component\Translation\TranslatorInterface as LegacyTranslatorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DataCollectorTranslator extends SymfonyDataCollectorTranslator
 {
     /**
-     * @var TranslatorInterface&TranslatorBagInterface&LegacyTranslatorInterface
+     * @var TranslatorInterface|LegacyTranslatorInterface
      */
     private $translator;
 
@@ -37,8 +35,9 @@ class DataCollectorTranslator extends SymfonyDataCollectorTranslator
     /**
      * {@inheritdoc}
      *
-     * Gets the translation from Contao’s $GLOBALS['TL_LANG'] array if the message domain starts with
-     * "contao_". The locale parameter is ignored in this case.
+     * Gets the translation from Contao’s $GLOBALS['TL_LANG'] array if the
+     * message domain starts with "contao_". The locale parameter is ignored in
+     * this case.
      */
     public function trans($id, array $parameters = [], $domain = null, $locale = null): string
     {
@@ -62,34 +61,7 @@ class DataCollectorTranslator extends SymfonyDataCollectorTranslator
         return $this->translator->transChoice($id, $number, $parameters, $domain, $locale);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setLocale($locale): ?string
-    {
-        return $this->translator->setLocale($locale);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getLocale(): string
-    {
-        return $this->translator->getLocale();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getCatalogue($locale = null): MessageCatalogueInterface
-    {
-        return $this->translator->getCatalogue($locale);
-    }
-
-    /**
-     * @return array
-     */
-    public function getCollectedMessages()
+    public function getCollectedMessages(): array
     {
         if (method_exists($this->translator, 'getCollectedMessages')) {
             return array_merge($this->translator->getCollectedMessages(), $this->messages);
