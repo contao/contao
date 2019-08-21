9132fcbf130ee39373146626b6eb0f5d98e18a59
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-08-21 11:24:52 +0200

    Use $_SERVER instead of getenv() (see #652)
    Description
    -----------
    
    Ohterwise `.env` files are not going to be supported.
    
    Commits
    -------
    
    9b131310 Use $_SERVER instead of getenv()
    622120a8 Fix tests

--- manager-bundle/src/HttpKernel/ContaoCache.php
+++ manager-bundle/src/HttpKernel/ContaoCache.php
@@ -41,7 +41,7 @@ class ContaoCache extends HttpCache implements CacheInvalidation
 
         $this->isDebug = $kernel->isDebug();
 
-        $this->addSubscriber(new StripCookiesSubscriber(array_filter(explode(',', getenv('COOKIE_WHITELIST') ?: ''))));
+        $this->addSubscriber(new StripCookiesSubscriber(array_filter(explode(',', $_SERVER['COOKIE_WHITELIST'] ?? ''))));
         $this->addSubscriber(new PurgeListener());
         $this->addSubscriber(new PurgeTagsListener());
         $this->addSubscriber(new CleanupCacheTagsListener());
