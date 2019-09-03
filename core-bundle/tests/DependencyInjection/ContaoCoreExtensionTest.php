12c4204a5d7848120a8789bbef8c7e0852e1dab6
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2019-09-04 00:41:08 +0200

    adjust ContaoCoreExtensionTest

--- core-bundle/tests/DependencyInjection/ContaoCoreExtensionTest.php
+++ core-bundle/tests/DependencyInjection/ContaoCoreExtensionTest.php
@@ -35,6 +35,7 @@ use Contao\CoreBundle\Controller\ImagesController;
 use Contao\CoreBundle\Controller\InsertTagsController;
 use Contao\CoreBundle\Controller\RobotsTxtController;
 use Contao\CoreBundle\Cors\WebsiteRootsConfigProvider;
+use Contao\CoreBundle\Cron\Cron;
 use Contao\CoreBundle\Csrf\MemoryTokenStorage;
 use Contao\CoreBundle\DataCollector\ContaoDataCollector;
 use Contao\CoreBundle\DependencyInjection\ContaoCoreExtension;
@@ -324,7 +325,8 @@ class ContaoCoreExtensionTest extends TestCase
         $this->assertTrue($definition->isPrivate());
         $this->assertSame('contao.framework', (string) $definition->getArgument(0));
         $this->assertSame('database_connection', (string) $definition->getArgument(1));
-        $this->assertSame('%fragment.path%', (string) $definition->getArgument(2));
+        $this->assertSame(Cron::class, (string) $definition->getArgument(2));
+        $this->assertSame('%fragment.path%', (string) $definition->getArgument(3));
 
         $tags = $definition->getTags();
 
