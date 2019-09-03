d4e14ffc52c8bda265b0832ee2c5c7d3fa30d19f
Leo Feyer <github@contao.org>
2019-09-03 23:40:52 +0200

    Fix the coding style

--- core-bundle/src/Controller/RobotsTxtController.php
+++ core-bundle/src/Controller/RobotsTxtController.php
@@ -54,7 +54,10 @@ class RobotsTxtController
         $pageModel = $this->contaoFramework->getAdapter(PageModel::class);
 
         /** @var PageModel|null $rootPage */
-        $rootPage = $pageModel->findPublishedFallbackByHostname($request->server->get('HTTP_HOST'), ['fallbackToEmpty' => true]);
+        $rootPage = $pageModel->findPublishedFallbackByHostname(
+            $request->server->get('HTTP_HOST'),
+            ['fallbackToEmpty' => true]
+        );
 
         if (null === $rootPage) {
             return new Response('', Response::HTTP_NOT_FOUND);
