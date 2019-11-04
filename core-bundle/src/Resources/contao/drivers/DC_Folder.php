9ab14ee3bd6610036352907701516b44f29aff50
Leo Feyer <github@contao.org>
2019-11-04 10:11:47 +0100

    Hide the breadcrumb menu if the node is not within the given path (see #888)
    Description
    -----------
    
    Fixes #856
    
    @ausi I am not 100% sure if this is the best place to fix the issue. Can you check as well please?
    
    Commits
    -------
    
    c7d12ce5 Hide the breadcrumb menu if the node is not within the given path (see #856)
    492b74b0 Also initialize the picker if $_GET['act'] is set

--- core-bundle/src/Resources/contao/drivers/DC_Folder.php
+++ core-bundle/src/Resources/contao/drivers/DC_Folder.php
@@ -3003,6 +3003,14 @@ class DC_Folder extends \DataContainer implements \listable, \editable
 				throw new \RuntimeException('Invalid path ' . $strPath);
 			}
 
+			$strNode = System::getContainer()->get('session')->getBag('contao_backend')->get('tl_files_node');
+
+			// If the files node is not within the current path, remove it (see #856)
+			if ($strNode && ($i = array_search($strNode, $this->arrFilemounts)) !== false && strncmp($strNode . '/', $strPath . '/', strlen($strPath) + 1) !== 0)
+			{
+				unset($this->arrFilemounts[$i], $GLOBALS['TL_DCA']['tl_files']['list']['sorting']['breadcrumb']);
+			}
+
 			// Allow only those roots that are allowed in root nodes
 			if (!empty($this->arrFilemounts))
 			{
