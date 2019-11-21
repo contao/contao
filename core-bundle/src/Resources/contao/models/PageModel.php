deb0313f7893708043a2cc18553639b509bbc8e2
Leo Feyer <github@contao.org>
2019-11-21 15:11:21 +0100

    Do not add the preview script when rebuilding the search index (see #942)

--- core-bundle/src/Resources/contao/models/PageModel.php
+++ core-bundle/src/Resources/contao/models/PageModel.php
@@ -1057,6 +1057,12 @@ class PageModel extends Model
 
 		$this->loadDetails();
 
+		$context = System::getContainer()->get('router')->getContext();
+		$baseUrl = $context->getBaseUrl();
+
+		// Always generate the URL without the preview script (see #942)
+		$context->setBaseUrl('');
+
 		$objUrlGenerator = System::getContainer()->get('contao.routing.url_generator');
 
 		$strUrl = $objUrlGenerator->generate
@@ -1076,6 +1082,8 @@ class PageModel extends Model
 			$strUrl = substr($strUrl, \strlen(Environment::get('path')) + 1);
 		}
 
+		$context->setBaseUrl($baseUrl);
+
 		$strUrl = $this->applyLegacyLogic($strUrl, $strParams);
 
 		return $strUrl;
@@ -1092,6 +1100,12 @@ class PageModel extends Model
 	{
 		$this->loadDetails();
 
+		$context = System::getContainer()->get('router')->getContext();
+		$baseUrl = $context->getBaseUrl();
+
+		// Always generate the URL without the preview script (see #942)
+		$context->setBaseUrl('');
+
 		$objUrlGenerator = System::getContainer()->get('contao.routing.url_generator');
 
 		$strUrl = $objUrlGenerator->generate
@@ -1105,6 +1119,8 @@ class PageModel extends Model
 			),
 			UrlGeneratorInterface::ABSOLUTE_URL
 		);
+
+		$context->setBaseUrl($baseUrl);
 
 		$strUrl = $this->applyLegacyLogic($strUrl, $strParams);
 
@@ -1127,9 +1143,10 @@ class PageModel extends Model
 			return $this->getAbsoluteUrl($strParams);
 		}
 
-		$router = $container->get('router');
+		$context = $container->get('router')->getContext();
+		$baseUrl = $context->getBaseUrl();
 
-		$context = $router->getContext();
+		// Always generate the URL with the preview script
 		$context->setBaseUrl($previewScript);
 
 		$objUrlGenerator = $container->get('contao.routing.url_generator');
@@ -1146,7 +1163,7 @@ class PageModel extends Model
 			UrlGeneratorInterface::ABSOLUTE_URL
 		);
 
-		$context->setBaseUrl('');
+		$context->setBaseUrl($baseUrl);
 
 		$strUrl = $this->applyLegacyLogic($strUrl, $strParams);
 
