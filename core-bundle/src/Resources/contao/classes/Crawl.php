2f0d8b9f12a86cd59dbfeca770e79cc317591fb9
Yanick Witschi <yanick.witschi@terminal42.ch>
2019-12-05 11:01:46 +0100

    Implemented subscriber specific log files

--- core-bundle/src/Resources/contao/classes/Crawl.php
+++ core-bundle/src/Resources/contao/classes/Crawl.php
@@ -12,12 +12,14 @@ namespace Contao;
 
 use Contao\CoreBundle\Exception\ResponseException;
 use Contao\CoreBundle\Search\Escargot\Factory;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Contao\CoreBundle\Search\Escargot\Subscriber\SubscriberResult;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\GroupHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Terminal42\Escargot\Exception\InvalidJobIdException;
 
@@ -74,55 +76,63 @@ class Crawl extends Backend implements \executable
 			return $template->parse();
 		}
 
+		$activeSubscribers = $subscribersWidget->value;
 		$template->isRunning = true;
-		$template->activeSubscribers = $factory->getSubscribers($subscribersWidget->value);
+		$template->activeSubscribers = $factory->getSubscribers($activeSubscribers);
 
 		$jobId = \Input::get('jobId');
 		$queue = $factory->createLazyQueue();
+		$debugLogPath = sys_get_temp_dir() . '/contao-crawl/' . $jobId . '.log';
+		$resultCache = sys_get_temp_dir() . '/contao-crawl/' . $jobId . '.result-cache';
+
+		if ($downloadLog = \Input::get('downloadLog'))
+		{
+			if ('debug' === $downloadLog)
+			{
+				$filePath = $debugLogPath;
+				$fileName = 'crawl_debug.log';
+			}
+			else
+			{
+				$filePath = $this->getSubscriberLogFilePath($downloadLog, $jobId);
+				$fileName = 'crawl_' . $downloadLog . '.log';
+			}
+
+			$response = new BinaryFileResponse($filePath);
+			$response->setPrivate();
+			$response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
+			throw new ResponseException($response);
+		}
 
 		if (!$jobId)
 		{
 			$baseUris = $factory->getSearchUriCollection();
-			$escargot = $factory->create($baseUris, $queue, $subscribersWidget->value);
+			$escargot = $factory->create($baseUris, $queue, $activeSubscribers);
 			Controller::redirect(\Controller::addToUrl('&jobId=' . $escargot->getJobId()));
 		}
 
 		try
 		{
-			$escargot = $factory->createFromJobId($jobId, $queue, $subscribersWidget->value);
+			$escargot = $factory->createFromJobId($jobId, $queue, $activeSubscribers);
 		}
 		catch (InvalidJobIdException $e)
 		{
-			Controller::redirect(str_replace('&jobId='. $jobId, '', Environment::get('request')));
+			Controller::redirect(str_replace('&jobId=' . $jobId, '', Environment::get('request')));
 		}
 
 		$escargot = $escargot->withConcurrency((int) $concurrencyWidget->value);
 		$escargot = $escargot->withMaxRequests((int) $maxRequestsWidget->value);
 
-		$logPath = sys_get_temp_dir() . '/contao-crawl/' . $jobId . '.log';
-		$resultCache = sys_get_temp_dir() . '/contao-crawl/' . $jobId . '.result-cache';
-
-		if (\Input::get('download') === 'log')
-		{
-			$response = new BinaryFileResponse($logPath);
-			$response->setPrivate();
-			$response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'crawl.log');
-			throw new ResponseException($response);
-		}
-
 		try
 		{
-			$escargot = $factory->createFromJobId($jobId, $queue, $subscribersWidget->value);
+			$escargot = $factory->createFromJobId($jobId, $queue, $activeSubscribers);
 		}
 		catch (InvalidJobIdException $e)
 		{
-			Controller::redirect(str_replace('&jobId='. $jobId, '', Environment::get('request')));
+			Controller::redirect(str_replace('&jobId=' . $jobId, '', Environment::get('request')));
 		}
 
-		$streamHandler = new StreamHandler($logPath);
-		$streamHandler->setFormatter(new LineFormatter("[%context.source%] %message%\n"));
-		$logger = new Logger('file-logger');
-		$logger->pushHandler($streamHandler);
+		$logger = $this->createLogger($factory, $activeSubscribers, $jobId, $debugLogPath);
 
 		$escargot = $escargot->withConcurrency((int) $concurrencyWidget->value);
 		$escargot = $escargot->withMaxRequests((int) $maxRequestsWidget->value);
@@ -136,23 +146,25 @@ class Crawl extends Backend implements \executable
 			// Commit the result on the lazy queue
 			$queue->commit($jobId);
 
-			// Write the logs
-			$streamHandler->close();
-
 			// Save the results between requests
-			$results = [];
-			$existingResults = [];
-			if (file_exists($resultCache)) {
+			$results = array();
+			$existingResults = array();
+
+			if (file_exists($resultCache))
+			{
 				$existingResults = json_decode(file_get_contents($resultCache), true);
 			}
 
-			foreach ($factory->getSubscribers($subscribersWidget->value) as $subscriber)
+			foreach ($factory->getSubscribers($activeSubscribers) as $subscriber)
 			{
 				$previousResult = null;
-				if (isset($existingResults[$subscriber->getName()])) {
+
+				if (isset($existingResults[$subscriber->getName()]))
+				{
 					$previousResult = SubscriberResult::fromArray($existingResults[$subscriber->getName()]);
 				}
 				$results[$subscriber->getName()] = $subscriber->getResult($previousResult)->toArray();
+				$results[$subscriber->getName()]['hasLog'] = file_exists($this->getSubscriberLogFilePath($subscriber->getName(), $jobId));
 			}
 
 			file_put_contents($resultCache, json_encode($results));
@@ -167,20 +179,65 @@ class Crawl extends Backend implements \executable
 				$queue->deleteJobId($jobId);
 			}
 
-			$response = new JsonResponse([
+			$response = new JsonResponse(array(
 				'pending' => $pending,
 				'total' => $all,
 				'finished' => $finished,
 				'results' => $results,
-				'hasLog' => file_exists($logPath),
-			]);
+				'hasDebugLog' => file_exists($debugLogPath),
+				'test' => $debugLogPath,
+			));
 
 			throw new ResponseException($response);
 		}
 
-		$template->logHref = \Controller::addToUrl('&jobId=' . $escargot->getJobId() . '&download=log');
+		$template->debugLogHref = \Controller::addToUrl('&jobId=' . $escargot->getJobId() . '&downloadLog=debug');
+
+		$subscriberLogHrefs = array();
+
+		foreach ($factory->getSubscribers($activeSubscribers) as $subscriber)
+		{
+			$subscriberLogHrefs[$subscriber->getName()] = \Controller::addToUrl('&jobId=' . $escargot->getJobId() . '&downloadLog=' . $subscriber->getName());
+		}
+
+		$template->subscriberLogHrefs = $subscriberLogHrefs;
 
 		return $template->parse();
+	}
+
+	/**
+	 * Creates a logger that logs everything on debug level in a general debug
+	 * log file and everything above info level into a subscriber specific
+	 * log file.
+	 */
+	private function createLogger(Factory $factory, array $activeSubscribers, string $jobId, string $debugLogPath): LoggerInterface
+	{
+		$handlers = array();
+
+		// Create the general debug handler
+		$debugHandler = new StreamHandler($debugLogPath, Logger::DEBUG);
+		$debugHandler->setFormatter(new LineFormatter("[%context.source%] %message%\n"));
+		$handlers[] = $debugHandler;
+
+		// Create the subscriber specific info handlers
+		foreach ($factory->getSubscribers($activeSubscribers) as $subscriber)
+		{
+			$subscriberHandler = new StreamHandler($this->getSubscriberLogFilePath($subscriber->getName(), $jobId), Logger::INFO);
+			$subscriberHandler->setFormatter(new LineFormatter("%message%\n"));
+			$handlers[] = $subscriberHandler;
+		}
+
+		$groupHandler = new GroupHandler($handlers);
+
+		$logger = new Logger('crawl-logger');
+		$logger->pushHandler($groupHandler);
+
+		return $logger;
+	}
+
+	private function getSubscriberLogFilePath(string $subscriberName, string $jobId): string
+	{
+		return sys_get_temp_dir() . '/contao-crawl/' . $jobId . '_' . $subscriberName . '.log';
 	}
 
 	private function generateSubscribersWidget(array $subscriberNames): Widget
@@ -194,26 +251,30 @@ class Crawl extends Backend implements \executable
 		$widget->multiple = true;
 		$widget->setInputCallback($this->getInputCallback($name));
 
-		$options = [];
+		$options = array();
 
-		foreach ($subscriberNames as $subscriberName) {
-			$options[] = [
+		foreach ($subscriberNames as $subscriberName)
+		{
+			$options[] = array(
 				'value' => $subscriberName,
 				'label' => $GLOBALS['TL_LANG']['tl_maintenance']['crawl']['subscriberNames'][$subscriberName],
 				'default' => false,
-			];
+			);
 		}
 
-		if (1 === \count($options)) {
+		if (1 === \count($options))
+		{
 			$options[0]['default'] = true;
 		}
 
 		$widget->options = $options;
 
-		if ($this->isActive()) {
+		if ($this->isActive())
+		{
 			$widget->validate();
 
-			if ($widget->hasErrors()) {
+			if ($widget->hasErrors())
+			{
 				$this->valid = false;
 			}
 		}
@@ -232,10 +293,12 @@ class Crawl extends Backend implements \executable
 		$widget->value = 10;
 		$widget->setInputCallback($this->getInputCallback($name));
 
-		if ($this->isActive()) {
+		if ($this->isActive())
+		{
 			$widget->validate();
 
-			if ($widget->hasErrors()) {
+			if ($widget->hasErrors())
+			{
 				$this->valid = false;
 			}
 		}
@@ -254,10 +317,12 @@ class Crawl extends Backend implements \executable
 		$widget->value = 20;
 		$widget->setInputCallback($this->getInputCallback($name));
 
-		if ($this->isActive()) {
+		if ($this->isActive())
+		{
 			$widget->validate();
 
-			if ($widget->hasErrors()) {
+			if ($widget->hasErrors())
+			{
 				$this->valid = false;
 			}
 		}
@@ -267,7 +332,8 @@ class Crawl extends Backend implements \executable
 
 	private function getInputCallback(string $name): \Closure
 	{
-		return function () use ($name) {
+		return static function () use ($name)
+		{
 			return \Input::get($name);
 		};
 	}
