1b97ad3caf17602fd7ba9acc28cb8781740f70ec
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-01-13 18:27:37 +0000

    add TODO comment

--- core-bundle/src/Cron/LegacyCron.php
+++ core-bundle/src/Cron/LegacyCron.php
@@ -84,6 +84,7 @@ class LegacyCron implements ServiceAnnotationInterface
         $system->loadLanguageFile('default');
 
         foreach ($GLOBALS['TL_CRON'][$interval] as $name => $cron) {
+            // TODO: migrate our own cronjobs to new framework
             @trigger_error('Using $GLOBALS[\'TL_CRON\'] has been deprecated and will be removed in Contao 5.0. Use the "contao.cronjob" service tag instead.', E_USER_DEPRECATED);
 
             $system->importStatic($cron[0])->{$cron[1]}();
