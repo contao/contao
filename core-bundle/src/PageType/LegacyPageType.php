ed0226056d23ce647c3de9f96b4e6fafa7a7a6fe
David Molineus <david.molineus@netzmacht.de>
2020-01-17 13:32:16 +0100

    Add page types for existing page types

---
+++ core-bundle/src/PageType/LegacyPageType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\PageType;
+
+/**
+ * Class LegacyPageType is responsible to wrap an existing page type configured in $GLOBALS['TL_PTY']
+ */
+class LegacyPageType extends AbstractPageType
+{
+    /** @var string */
+    protected $name;
+
+    public function __construct(string $name)
+    {
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
