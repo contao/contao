06def8944ef17dbb6f69c6417b4744c3ff9b7fd4
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-01-27 09:50:57 +0100

    Fix determination of ajax url

--- core-bundle/src/Resources/public/mootao.js
+++ core-bundle/src/Resources/public/mootao.js
@@ -35,8 +35,8 @@ Request.Contao = new Class(
 	},
 
 	initialize: function(options) {
-		if (options && !options.url) {
-			this.options.url = location.href;
+		if (options && !options.url && options.field && options.field.form && options.field.form.action) {
+			this.options.url = options.field.form.action;
 		}
 		this.parent(options);
 	},
