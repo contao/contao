d034dac392021bddda6c530796d4e2877cdb8d71
David Greminger <david.greminger@1up.io>
2020-02-07 10:06:40 +0100

    Change message

--- core-bundle/src/Security/Authentication/AuthenticationSuccessHandler.php
+++ core-bundle/src/Security/Authentication/AuthenticationSuccessHandler.php
@@ -175,7 +175,7 @@ class AuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterf
         $targetPath = $request->request->get('_target_path');
 
         if (null === $targetPath) {
-            throw new BadRequestHttpException('Missing form field "_target_path". Did you maybe adjust any login template?');
+            throw new BadRequestHttpException('Missing form field "_target_path". You probably need to adjust your custom login template.');
         }
 
         return base64_decode($targetPath, true);
