959fe7fc8f859c32a7211541ed91a31a0bddc9e5
Leo Feyer <github@contao.org>
2020-02-21 10:47:30 +0100

    Correctly save new template folders (see #1355)

--- core-bundle/src/Resources/contao/drivers/DC_Folder.php
+++ core-bundle/src/Resources/contao/drivers/DC_Folder.php
@@ -2293,6 +2293,7 @@ class DC_Folder extends DataContainer implements \listable, \editable
 			}
 
 			$this->varValue = $varValue;
+			$this->intId = $this->strPath . '/' . $varValue . $this->strExtension;
 		}
 		elseif ($this->blnIsDbAssisted && $this->objActiveRecord !== null)
 		{
