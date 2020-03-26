f2cb3f1181fb4106e289518e435a1ae5181beb4f
Leo Feyer <github@contao.org>
2020-03-26 17:05:03 +0100

    Add a 5 seconds timeout to the CAPTCHA widget

--- core-bundle/src/Resources/contao/forms/FormCaptcha.php
+++ core-bundle/src/Resources/contao/forms/FormCaptcha.php
@@ -64,8 +64,6 @@ class FormCaptcha extends \Widget
 
 		$this->arrAttributes['maxlength'] = 2;
 		$this->strCaptchaKey = 'c' . md5(uniqid(mt_rand(), true));
-		$this->arrAttributes['required'] = true;
-		$this->arrConfiguration['mandatory'] = true;
 	}
 
 	/**
