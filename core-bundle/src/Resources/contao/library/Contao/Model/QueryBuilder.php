c40baba62d9dd8780a5b2da2463f91cbb1d7a722
Leo Feyer <github@contao.org>
2020-05-07 14:45:57 +0200

    Deprecate the "group" option in the query builder (see #1704)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #1680
    | Docs PR or issue | -
    
    <!--
    Bugfixes should be based on the 4.4 or 4.9 branch and features on the master
    branch. Select the correct branch in the "base:" drop-down menu above.
    
    Replace this notice with a short README for your feature/bugfix. This will help
    people to understand your PR and can be used as a start for the documentation.
    -->
    
    Commits
    -------
    
    82929aa8 Deprecate the "group" option in the query builder

--- core-bundle/src/Resources/contao/library/Contao/Model/QueryBuilder.php
+++ core-bundle/src/Resources/contao/library/Contao/Model/QueryBuilder.php
@@ -72,6 +72,7 @@ class QueryBuilder
 		// Group by
 		if ($arrOptions['group'] !== null)
 		{
+			@trigger_error('Using the "group" option has been deprecated and will no longer work in Contao 5.0. See https://github.com/contao/contao/issues/1680', E_USER_DEPRECATED);
 			$strQuery .= " GROUP BY " . $arrOptions['group'];
 		}
 
