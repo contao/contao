f5b212e9317d4dfc74613723322c5ee324bbb763
Leo Feyer <github@contao.org>
2020-06-11 16:08:30 +0200

    Move the listener to the manager bundle and add a unit test

--- manager-bundle/src/EventListener/DoctrineListener.php
+++ manager-bundle/src/EventListener/DoctrineListener.php
@@ -8,7 +8,7 @@
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\InstallationBundle\EventListener;
+namespace Contao\ManagerBundle\EventListener;
 
 use Doctrine\DBAL\Event\SchemaAlterTableRenameColumnEventArgs;
 
---
+++ manager-bundle/tests/EventListener/DoctrineListenerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerBundle\Tests\EventListener;
+
+use Contao\ManagerBundle\EventListener\DoctrineListener;
+use Doctrine\DBAL\Event\SchemaAlterTableRenameColumnEventArgs;
+use Doctrine\DBAL\Platforms\MySQL57Platform;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\TableDiff;
+use Doctrine\DBAL\Types\IntegerType;
+use PHPUnit\Framework\TestCase;
+
+class DoctrineListenerTest extends TestCase
+{
+    public function testConvertsRenameToDropAndAdd()
+    {
+        $column = new Column('bar', new IntegerType());
+
+        $tableDiff = new TableDiff('tl_member');
+        $tableDiff->renamedColumns['foo'] = new Column('foo', new IntegerType());
+
+        $args = new SchemaAlterTableRenameColumnEventArgs('foo', $column, $tableDiff, new MySQL57Platform());
+
+        $this->assertEmpty($args->getSql());
+
+        $listener = new DoctrineListener();
+        $listener->onSchemaAlterTableRenameColumn($args);
+
+        $this->assertSame(
+            [
+                'ALTER TABLE tl_member DROP `foo`',
+                'ALTER TABLE tl_member ADD bar INT NOT NULL',
+            ],
+            $args->getSql()
+        );
+    }
+}
