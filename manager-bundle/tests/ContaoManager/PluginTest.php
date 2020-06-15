71a5553a7fc69ad5db53c009f5a2d54ff992f08e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-06-15 22:15:30 +0100

    use MAILER_DSN with MAILER_URL fallback

--- manager-bundle/tests/ContaoManager/PluginTest.php
+++ manager-bundle/tests/ContaoManager/PluginTest.php
@@ -530,7 +530,7 @@ class PluginTest extends ContaoTestCase
 
         $bag = $container->getParameterBag()->all();
 
-        $this->assertSame($expected, $bag['env(MAILER_URL)']);
+        $this->assertSame($expected, $bag['env(MAILER_DSN)']);
     }
 
     public function getMailerParameters(): \Generator
