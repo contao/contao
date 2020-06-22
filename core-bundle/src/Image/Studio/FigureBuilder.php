91569ddfdd02fc5b5fb998b7afbbef0d14c61265
M. Vondano <m-vo@users.noreply.github.com>
2020-06-22 18:58:20 +0200

    ignore sniffer false positive "unused private elements" (used through cloned instance)

--- core-bundle/src/Image/Studio/FigureBuilder.php
+++ core-bundle/src/Image/Studio/FigureBuilder.php
@@ -54,6 +54,8 @@ class FigureBuilder
     /**
      * User defined size configuration.
      *
+     * @phpcsSuppress SlevomatCodingStandard.Classes.UnusedPrivateElements
+     *
      * @var mixed|null
      */
     private $sizeConfiguration;
