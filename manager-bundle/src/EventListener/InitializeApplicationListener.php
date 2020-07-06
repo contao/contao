aeecd7d3c0c4abbf6b8135c1474af84d548b5eb5
M. Vondano <m-vo@users.noreply.github.com>
2020-07-06 12:20:45 +0200

    collapse expression into one line

--- manager-bundle/src/EventListener/InitializeApplicationListener.php
+++ manager-bundle/src/EventListener/InitializeApplicationListener.php
@@ -43,11 +43,6 @@ class InitializeApplicationListener
             return;
         }
 
-        $filesystem->copy(
-                __DIR__.'/../Resources/skeleton/system/initialize.php',
-                $targetPath,
-                true
-            )
-        ;
+        $filesystem->copy(__DIR__.'/../Resources/skeleton/system/initialize.php', $targetPath, true);
     }
 }
