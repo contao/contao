2072440d039fb23642f88fbab7c8be877f2c5913
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2020-07-17 12:15:23 +0200

    Add user commands (see #1463)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | -
    | Docs PR or issue | contao/docs#450
    
    * Command `contao:user:list` will show a table of user accounts in the command line output. Takes the flag `--admin` to show only admins and the flag `--fields` to specify the header fields of the table you want to show.
    Returns error code 1 if no accounts were found.
    The Contao Manager can utilize this command to show, if the installation lacks an admin account.
    * Command `contao:user:create` will create a back end user account. Provide all user data as arguments (e.g. `--usename j.doe --name="Joh Doe"`) or type then when asked in the interactive mode.
    
    Commits
    -------
    
    69068997 Draft user commands
    7ebb6d99 Merge branch 'master' into feature/user-commands
    fb8d8b35 Merge branch 'master' into feature/user-commands
    176d77bf Merge branch 'feature/user-commands' of github.com:richardhj/contao into feature/user-commands
    bad1f7aa Apply some feedback
    986239cd Add json format option
    2d4b891c Disable some tests
    3c6c6a6e Do not use query builder
    01efc2a8 Fix tests for UserListCommand
    672341dc Fix tests for UserCreateCommand
    0b6b25be Fix minor issues
    2cc636b1 Merge branch 'master' into feature/user-commands
    bbe37e89 Throw error message
    63707805 Merge branch 'master' into feature/user-commands
    2331cdac Lazy command loading
    07218ad3 Run the CS fixer
    caa968ee Update core-bundle/src/Command/UserListCommand.php
    
    Co-authored-by: Leo Feyer <github@contao.org>
    a2ca6ee1 Remove outdated code
    5d6dac5c Fix a minor CS issue
    eacec9bf Merge remote-tracking branch 'origin/master' into feature/user-commands
    bc3f4e48 Fix checkmark on windows platforms
    e31919ff Increase test coverage
    e4ea80b1 Do not handle empty result as error
    2e5abd95 Merge branch 'master' into feature/user-commands
    99d27124 Use Utf8::strlen()

---
+++ core-bundle/tests/Command/UserListCommandTest.php
@@ -0,0 +1,154 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Command;
+
+use Contao\CoreBundle\Command\UserListCommand;
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\Model\Collection;
+use Contao\UserModel;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class UserListCommandTest extends TestCase
+{
+    public function testDefinition(): void
+    {
+        $command = $this->getCommand();
+
+        $this->assertNotEmpty($command->getDescription());
+
+        $definition = $command->getDefinition();
+
+        $this->assertTrue($definition->hasOption('column'));
+        $this->assertTrue($definition->hasOption('admins'));
+    }
+
+    public function testTakesAdminsFlagAsArgument(): void
+    {
+        $command = $this->getCommand();
+
+        $input = [
+            '--admins' => true,
+        ];
+
+        $code = (new CommandTester($command))->execute($input);
+
+        $this->assertSame(0, $code);
+    }
+
+    public function testReturnsValidJson(): void
+    {
+        $command = $this->getCommand();
+
+        $input = [
+            '--format' => 'json',
+        ];
+
+        $commandTester = new CommandTester($command);
+
+        $code = $commandTester->execute($input);
+        $output = $commandTester->getDisplay();
+
+        $this->assertSame(0, $code);
+        $this->assertNotNull(json_decode($output, true));
+    }
+
+    public function testReturnsValidJsonWithSubset(): void
+    {
+        $command = $this->getCommand();
+
+        $input = [
+            '--format' => 'json',
+            '--column' => ['name', 'username'],
+        ];
+
+        $commandTester = new CommandTester($command);
+
+        $code = $commandTester->execute($input);
+        $output = $commandTester->getDisplay();
+
+        $this->assertSame(0, $code);
+        $this->assertNotNull(json_decode($output, true));
+    }
+
+    public function testTakesColumnAsArgument(): void
+    {
+        $command = $this->getCommand();
+
+        $input = [
+            '--column' => ['username', 'name'],
+        ];
+
+        $code = (new CommandTester($command))->execute($input);
+
+        $this->assertSame(0, $code);
+    }
+
+    private function getCommand(bool $noResult = false): UserListCommand
+    {
+        $collection = new Collection([$this->mockAdminUser(), $this->mockContaoUser()], 'tl_user');
+
+        $userModelAdapter = $this->mockAdapter(['findBy', 'findAll']);
+        $userModelAdapter
+            ->method('findAll')
+            ->willReturn($noResult ? null : $collection, null)
+        ;
+
+        $collection = new Collection([$this->mockAdminUser()], 'tl_user');
+
+        $userModelAdapter
+            ->method('findBy')
+            ->with('admin', '1')
+            ->willReturn($noResult ? null : $collection, null)
+        ;
+
+        $command = new UserListCommand($this->mockContaoFramework([UserModel::class => $userModelAdapter]));
+        $command->setApplication(new Application());
+
+        return $command;
+    }
+
+    private function mockContaoUser(): UserModel
+    {
+        /** @var UserModel&MockObject $userModel */
+        $userModel = $this->mockClassWithProperties(UserModel::class);
+        $userModel->id = '2';
+        $userModel->username = 'j.doe';
+        $userModel->name = 'John Doe';
+
+        $userModel
+            ->method('row')
+            ->willReturn((array) $userModel)
+        ;
+
+        return $userModel;
+    }
+
+    private function mockAdminUser(): UserModel
+    {
+        /** @var UserModel&MockObject $userModel */
+        $userModel = $this->mockClassWithProperties(UserModel::class);
+        $userModel->id = '1';
+        $userModel->username = 'j.doe';
+        $userModel->name = 'John Doe';
+        $userModel->admin = '1';
+
+        $userModel
+            ->method('row')
+            ->willReturn((array) $userModel)
+        ;
+
+        return $userModel;
+    }
+}
