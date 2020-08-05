d0ec9de119f3b2e1f16d916d7a2d11c353744a0c
Martin Auswöger <martin@auswoeger.com>
2020-08-05 12:25:49 +0200

    Fix order of CSRF and private response listeners (see #2074)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #2067
    
    The problem in #2067 was the order of the CSRF and make-response-private listeners: First, the make-response-private listener kept the response public, then the CSRF listener added a cookie-header to remove the CSRF cookie. This “cookie deletion” was then stored in the HTTP cache and every subsequent access to this resource always resulted in the CSRF cookie to get removed.
    
    I’m not sure what the “correct” priorities for them are, but I think both listeners should come very late. /cc @Toflar 
    
    ### ToDo
    
    - [x] Unit test to make sure the relative order is kept in the future.
    
    Commits
    -------
    
    852a1b27 Fix order of CSRF and private response listeners
    5a52486b Fix tests
    de8ab65e Test order between CSRF and response private listener
    877d7573 Coding style
    918db6a5 Add priority comments
    e61a9d6e Fix phpstan
    81a8c501 Update priorities to -832 and -896

--- core-bundle/tests/DependencyInjection/ContaoCoreExtensionTest.php
+++ core-bundle/tests/DependencyInjection/ContaoCoreExtensionTest.php
@@ -487,7 +487,7 @@ class ContaoCoreExtensionTest extends TestCase
         $this->assertSame(
             [
                 'kernel.request' => ['onKernelRequest', 36],
-                'kernel.response' => 'onKernelResponse',
+                'kernel.response' => ['onKernelResponse', -832],
             ],
             CsrfTokenCookieSubscriber::getSubscribedEvents()
         );
@@ -718,7 +718,9 @@ class ContaoCoreExtensionTest extends TestCase
         $this->assertSame(
             [
                 'kernel.event_listener' => [
-                    [],
+                    [
+                        'priority' => -896,
+                    ],
                 ],
             ],
             $tags
@@ -732,6 +734,18 @@ class ContaoCoreExtensionTest extends TestCase
 
         // Ensure that the listener is registered after the MergeHeaderListener
         $this->assertTrue($priority < $mergeHeadersListenerPriority);
+
+        $clearSessionDataListenerDefinition = $this->container->getDefinition('contao.listener.clear_session_data');
+        $clearSessionDataListenerTags = $clearSessionDataListenerDefinition->getTags();
+        $clearSessionDataListenerPriority = $clearSessionDataListenerTags['kernel.event_listener'][0]['priority'] ?? 0;
+
+        // Ensure that the listener is registered after the ClearSessionDataListener
+        $this->assertTrue($priority < $clearSessionDataListenerPriority);
+
+        $csrfCookieListenerPriority = CsrfTokenCookieSubscriber::getSubscribedEvents()['kernel.response'][1] ?? 0;
+
+        // Ensure that the listener is registered after the CsrfTokenCookieSubscriber
+        $this->assertTrue($priority < (int) $csrfCookieListenerPriority);
     }
 
     public function testRegistersTheMergeHttpHeadersListener(): void
