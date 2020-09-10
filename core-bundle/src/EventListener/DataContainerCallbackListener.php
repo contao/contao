contao/contao

commit 336a295458f2d936db19da88dcddfa4549cdcbbf

Add Psalm with level 7 (see #1483)
Description
-----------

| Q                | A
| -----------------| ---
| Fixed issues     | n/a
| Docs PR or issue | n/a

Use Psalm aside of PHPStan.
Psalm starts with the most permissive level 8, this PR fixes errors until level 7.

Commits
-------

c6198f20 Swap PHPStan with Psalm, fixed errorLevel 7
8c188f05 Re-enable PHPStan
e630ebff Fix CS
dda153e3 Keep @phpstan and @psalm separate so we can pass additional arguments

core-bundle/src/EventListener/DataContainerCallbackListener.php
@@ -71,10 +71,7 @@ class DataContainerCallbackListener
         return $dcaRef;
     }
 
-    /**
-     * @param array|callable|null $dcaRef
-     */
-    private function updateSingleton(&$dcaRef, array $callbacks): void
+    private function updateSingleton(?array &$dcaRef, array $callbacks): void
     {
         krsort($callbacks, SORT_NUMERIC);
 

.github/workflows/ci.yml
@@ -65,7 +65,9 @@ jobs:
                   vendor/bin/ecs check *-bundle/src/Resources/contao/templates --config vendor/contao/easy-coding-standard/config/template.yaml --no-progress-bar --ansi
 
             - name: Analyze the code
-              run: vendor/bin/phpstan analyse core-bundle/src core-bundle/tests --level=4 --no-progress
+              run: |
+                  vendor/bin/phpstan analyse core-bundle/src core-bundle/tests --level=4 --no-progress
+                  vendor/bin/psalm --threads=4 --no-progress
 
     tests:
         name: PHP ${{ matrix.php }}

core-bundle/src/Security/Authentication/RememberMe/ExpiringTokenBasedRememberMeServices.php
@@ -147,7 +147,7 @@ class ExpiringTokenBasedRememberMeServices extends AbstractRememberMeServices
     /**
      * @param array<RememberMe> $rows
      */
-    private function findValidToken(array $rows, string $cookieValue): RememberMe
+    private function findValidToken(array $rows, string $cookieValue): ?RememberMe
     {
         $lastException = null;
 
@@ -167,7 +167,11 @@ class ExpiringTokenBasedRememberMeServices extends AbstractRememberMeServices
             }
         }
 
-        throw $lastException;
+        if (null !== $lastException) {
+            throw $lastException;
+        }
+
+        return null;
     }
 
     private function createRememberMeCookie(Request $request, string $series, string $cookieValue): Cookie

monorepo.yml
@@ -12,6 +12,10 @@ composer:
         'contao/monorepo-tools': 'dev-master'
         'contao/phpstan': '^0.12'
         'phpstan/extension-installer': '^1.0'
+        'psalm/plugin-phpunit': '^0.9'
+        'seferov/symfony-psalm-plugin': '^1.0'
+        'vimeo/psalm': '^3.9'
+        'weirdan/doctrine-psalm-plugin': '^0.10'
 
 repositories:
     core-bundle:

psalm.xml
@@ -1,0 +1,31 @@
+<?xml version="1.0"?>
+<psalm
+    errorLevel="7"
+    resolveFromConfigFile="true"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns="https://getpsalm.org/schema/config"
+    xsi:schemaLocation="https://getpsalm.org/schema/config vendor/vimeo/psalm/config.xsd"
+>
+    <projectFiles>
+        <directory name="core-bundle/src"/>
+        <directory name="core-bundle/tests"/>
+        <ignoreFiles>
+            <directory name="vendor"/>
+            <directory name="core-bundle/src/Resources"/>
+            <directory name="core-bundle/tests/Fixtures"/>
+            <directory name="core-bundle/tests/Functional/app"/>
+        </ignoreFiles>
+    </projectFiles>
+    <plugins>
+        <pluginClass class="Psalm\PhpUnitPlugin\Plugin"/>
+        <pluginClass class="Weirdan\DoctrinePsalmPlugin\Plugin"/>
+        <pluginClass class="Seferov\SymfonyPsalmPlugin\Plugin"/>
+    </plugins>
+    <issueHandlers>
+        <UndefinedConstant>
+            <errorLevel type="suppress">
+                <file name="core-bundle/src/Controller/InitializeController.php" />
+            </errorLevel>
+        </UndefinedConstant>
+    </issueHandlers>
+</psalm>

commit 2bc1fb5f4e964af4f0526cbb2c21ad56ead81477

Fix wrong typehint (see #2270)
Description
-----------

`$dcaRef` can be of type `array|callable|null` (see line 61 above, https://github.com/contao/contao/pull/2270/files#diff-26ec4b3e6c5b2ff5898a7c6caa86c1c6L61), so the type hint is wrong and can lead to an error: 
```
Argument 1 passed to Contao\CoreBundle\EventListener\DataContainerCallbackListener::updateSingleton() 
must be of the type array or null, object given, called in 
/vendor/contao/core-bundle/src/EventListener/DataContainerCallbackListener.php on line 53
```

PS: this bug was introduced by myself in https://github.com/contao/contao/pull/1483 🙃

Commits
-------

25cab748 Remove typehint as it is wrong

core-bundle/src/EventListener/DataContainerCallbackListener.php
@@ -71,7 +71,10 @@ class DataContainerCallbackListener
         return $dcaRef;
     }
 
-    private function updateSingleton(?array &$dcaRef, array $callbacks): void
+    /**
+     * @param array|callable|null $dcaRef
+     */
+    private function updateSingleton(&$dcaRef, array $callbacks): void
     {
         krsort($callbacks, SORT_NUMERIC);
 
@@ -81,7 +84,10 @@ class DataContainerCallbackListener
         }
     }
 
-    private function addCallbacks(?array &$dcaRef, array $callbacks): void
+    /**
+     * @param array|callable|null $dcaRef
+     */
+    private function addCallbacks(&$dcaRef, array $callbacks): void
     {
         if (null === $dcaRef) {
             $dcaRef = [];