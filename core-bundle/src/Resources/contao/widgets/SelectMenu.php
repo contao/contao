ef55fdac66cc94e78c9131ce20183bb623bfba20
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-09-10 16:02:00 +0200

    Correctly generate name for hidden field in select menu (see #2275)
    Description
    -----------
    
    fixes #2271
    
    Commits
    -------
    
    343ad98a Correctly generate name for hidden field in select menu
    
    fixes #2271
    1cf9a4c1 Also adjust the form_select template
    39aceee6 CS

--- core-bundle/src/Resources/contao/widgets/SelectMenu.php
+++ core-bundle/src/Resources/contao/widgets/SelectMenu.php
@@ -155,7 +155,7 @@ class SelectMenu extends \Widget
 
 		return sprintf(
 			'%s<select name="%s" id="ctrl_%s" class="%s%s"%s onfocus="Backend.getScrollOffset()">%s</select>%s',
-			($this->multiple ? '<input type="hidden" name="' . rtrim($this->strName, '[]') . '" value="">' : ''),
+			($this->multiple ? '<input type="hidden" name="' . (substr($this->strName, -2) == '[]' ? substr($this->strName, 0, -2) : $this->strName) . '" value="">' : ''),
 			$this->strName,
 			$this->strId,
 			$strClass,
