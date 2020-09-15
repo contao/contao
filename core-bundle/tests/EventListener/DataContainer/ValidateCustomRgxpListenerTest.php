6a5086375f8cb81e0ed18eb81787d6fe116a1d3d
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-09-15 09:27:10 +0200

    add unit tests

---
+++ core-bundle/tests/EventListener/DataContainer/ValidateCustomRgxpListenerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\EventListener\DataContainer;
+
+use Contao\CoreBundle\EventListener\DataContainer\ValidateCustomRgxpListener;
+use Contao\CoreBundle\Tests\TestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ValidateCustomRgxpListenerTest extends TestCase
+{
+    public function testThrowsExceptionIfInvalidRegex(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('ERR.invalidCustomRgxp');
+
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator
+            ->expects($this->once())
+            ->method('trans')
+            ->willReturnArgument(0)
+        ;
+
+        $listener = new ValidateCustomRgxpListener($translator);
+        $listener('foo');
+    }
+
+    public function testDoesNotThrowsExceptionIfValidRegex(): void
+    {
+        $translator = $this->createMock(TranslatorInterface::class);
+        $translator
+            ->expects($this->never())
+            ->method('trans')
+            ->willReturnArgument(0)
+        ;
+
+        $listener = new ValidateCustomRgxpListener($translator);
+        
+        $this->assertEquals('/foo/i', $listener('/foo/i'));
+    }
+}
