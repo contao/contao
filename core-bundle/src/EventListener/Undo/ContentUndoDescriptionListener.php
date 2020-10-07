5e813a0554b30095bf8085715bd41601bfb6ffa6
Benedict Massolle <bm@presentprogressive.de>
2020-10-07 14:45:46 +0200

    Content Element Undo Listener wip

--- core-bundle/src/EventListener/Undo/ContentUndoDescriptionListener.php
+++ core-bundle/src/EventListener/Undo/ContentUndoDescriptionListener.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 /*
  * This file is part of Contao.
@@ -20,81 +18,165 @@ class ContentUndoDescriptionListener
 {
     public function __invoke(UndoDescriptionEvent $event): void
     {
-        if ('tl_content' !== $event->getTable()) {
+        if (!$this->supports($event)) {
             return;
         }
 
-        $descriptor = null;
-        $data = $event->getData();
-        $method = $this->getMethodFromType($data['type']);
-
-        if (method_exists($this, $method)) {
-            $descriptor = $this->{$method}($data);
-        }
-
-        $event->setDescription($descriptor);
+        $description = $this->getDescriptionForType($event->getData());
+        $event->setDescription($description);
     }
 
     private function getMethodFromType(string $type): string
     {
-        return 'get'.ucwords($type).'Descriptor';
+        return 'get' . ucwords($type) . 'Description';
     }
 
-    private function getHeadlineDescriptor(array $data): string
+    private function getDescriptionForType(array $data): ?string
+    {
+        $method = $this->getMethodFromType($data['type']);
+        return method_exists($this, $method) ? $this->{$method}($data) : null;
+    }
+
+    private function getHeadlineDescription(array $data): string
     {
         $headline = StringUtil::deserialize($data['headline']);
-
-        return $headline[1];
+        return $headline['value'];
     }
 
-    private function getTextDescriptor(array $data): string
+    private function getTextDescription(array $data): string
     {
         $text = $data['text'];
-
         return StringUtil::substrHtml($text, 100);
     }
 
-    private function getHtmlDescriptor(array $data): string
+    private function getHtmlDescription(array $data): string
     {
-        return $data['html'];
+        $html = htmlspecialchars($data['html']);
+        return StringUtil::substr($html, 100);
     }
 
-    private function getListDescriptor(array $data): string
+    private function getListDescription(array $data): string
     {
         $items = StringUtil::deserialize($data['listitems']);
-
         return implode(', ', array_values($items));
     }
 
-    private function getImageDescriptor(array $data): string
+    private function getTableDescription(array $data): string
     {
-        $uuid = StringUtil::binToUuid($data['singleSRC']);
-        $image = FilesModel::findByUuid($uuid);
-
-        return null !== $image ? $image->name.'.'.$image->extension : $uuid;
+        $table = StringUtil::deserialize($data['tableitems']);
+        return implode(', ', $table[0]);
     }
 
-    private function getGalleryDescriptor(array $data): string
+    private function getAccordionStartDescription(array $data): string
     {
-        $uuids = array_map(
-            static function ($uuid) {
-                return \StringUtil::binToUuid($uuid);
-            },
-            StringUtil::deserialize($data['multiSRC'])
-        );
-
-        $files = FilesModel::findMultipleByUuids($uuids);
-
-        if (null === $files) {
-            return implode(', ', $uuids);
+        if (!empty($data['mooHeadline'])) {
+            return $data['mooHeadline'];
         }
 
-        $fileNames = array_reduce($files->fetchAll(), static function ($acc, $file) {
-            array_push($acc, $file['name']);
+        return 'ID ' . $data['id'];
+    }
 
-            return $acc;
-        }, []);
+    private function getAccordionStopDescription(array $data): string
+    {
+        return 'ID ' . $data['id'];
+    }
 
-        return implode(', ', $fileNames);
+    private function getAccordionSingleDescription(array $data): string
+    {
+        if (!empty($data['mooHeadline'])) {
+            return $data['mooHeadline'];
+        }
+
+        return htmlspecialchars($data['text']);
+    }
+
+    private function getSliderStartDescription(array $data): string
+    {
+        if (!empty($data['headline'])) {
+            return $data['headline'];
+        }
+
+        return 'ID ' . $data['id'];
+    }
+
+    private function getSliderStopDescription(array $data): string
+    {
+        if (!empty($data['headline'])) {
+            return $data['headline'];
+        }
+
+        return 'ID ' . $data['id'];
+    }
+
+    private function getCodeDescription(array $data): string
+    {
+        return htmlspecialchars($data['code']);
+    }
+
+    private function getMarkdownDescription(array $data): string
+    {
+        return htmlspecialchars($data['markdown']);
+    }
+
+    private function getHyperlinkDescription(array $data): string
+    {
+        return $data['url'];
+    }
+
+    private function getToplinkDescription(array $data): string
+    {
+        return !empty($data['linkTitle']) ? $data['linkTitle'] : 'ID ' . $data['id'];
+    }
+
+//    private function getImageDescription(array $data): string
+//    {
+//        $uuid = StringUtil::binToUuid($data['singleSRC']);
+//        $image = FilesModel::findByUuid($uuid);
+//
+//        return null !== $image ? $image->name . '.' . $image->extension : $uuid;
+//    }
+
+//    private function getGalleryDescription(array $data): string
+//    {
+//        $uuids = array_map(
+//            static function ($uuid) {
+//                return \StringUtil::binToUuid($uuid);
+//            },
+//            StringUtil::deserialize($data['multiSRC'])
+//        );
+//
+//        $files = FilesModel::findMultipleByUuids($uuids);
+//
+//        if (null === $files) {
+//            return implode(', ', $uuids);
+//        }
+//
+//        $fileNames = array_reduce($files->fetchAll(), static function ($acc, $file) {
+//            array_push($acc, $file['name']);
+//
+//            return $acc;
+//        }, []);
+//
+//        return implode(', ', $fileNames);
+//    }
+
+    public function getYoutubeDescription(array $data): string
+    {
+        return $data['youtube'];
+    }
+
+    public function getVimeoDescription(array $data): string
+    {
+        return $data['vimeo'];
+    }
+
+
+    /**
+     * @param UndoDescriptionEvent $event
+     * @return bool
+     */
+    private function supports(UndoDescriptionEvent $event)
+    {
+        return ('tl_content' === $event->getTable());
     }
 }
---
+++ core-bundle/tests/EventListener/Undo/ContentUndoDescriptionListenerTest.php
@@ -0,0 +1,260 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\EventListener\Undo;
+
+use Contao\CoreBundle\Event\UndoDescriptionEvent;
+use Contao\CoreBundle\EventListener\Undo\ContentUndoDescriptionListener;
+use Contao\CoreBundle\EventListener\Undo\UndoDescriptionListener;
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\StringUtil;
+
+class ContentUndoDescriptionListenerTest extends TestCase
+{
+    public function testDoesNotHandleOtherThanContentTable(): void
+    {
+        $listener = new ContentUndoDescriptionListener();
+
+        $event = new UndoDescriptionEvent('tl_foo', [], []);
+        $listener($event);
+
+        $this->assertSame($event->getDescription(), null);
+    }
+
+    /**
+     * @dataProvider rowAndOptionsProvider
+     */
+    public function testGetDescriptionForRow(array $data, array $options, ?string $expected): void
+    {
+        $listener = new ContentUndoDescriptionListener();
+
+        $event = new UndoDescriptionEvent('tl_content', $data, $options);
+        $listener($event);
+
+        $this->assertSame($event->getDescription(), $expected);
+    }
+
+    public function rowAndOptionsProvider(): \Generator
+    {
+        yield 'Not a supported content element' => [
+            [
+                'type' => 'myCustomContentElement'
+            ],
+            [],
+            null
+        ];
+
+        yield 'Headline' => [
+            [
+                'type' => 'headline',
+                'headline' => serialize(['unit' => 'h1', 'value' => 'This is a headline'])
+            ],
+            [],
+            'This is a headline',
+        ];
+
+        yield 'Text' => [
+            [
+                'type' => 'text',
+                'text' => '<p>Contao is a powerful open source CMS.</p>'
+            ],
+            [],
+            'Contao is a powerful open source CMS.',
+        ];
+
+        yield 'HTML' => [
+            [
+                'type' => 'html',
+                'html' => '<p>Contao is a powerful open source CMS.</p>'
+            ],
+            [],
+            htmlspecialchars('<p>Contao is a powerful open source CMS.</p>')
+        ];
+
+        yield 'List' => [
+            [
+                'type' => 'list',
+                'listitems' => serialize(['Foo', 'Bar', 'Baz'])
+            ],
+            [],
+            'Foo, Bar, Baz'
+        ];
+
+        yield 'Table' => [
+            [
+                'type' => 'table',
+                'tableitems' => serialize([['TH 1', 'TH 2', 'TH 3'], ['TD 1', 'TD 1', 'TD 1']])
+            ],
+            [],
+            'TH 1, TH 2, TH 3'
+        ];
+
+        yield 'AccordionStart with mooHeadline' => [
+            [
+                'type' => 'accordionStart',
+                'mooHeadline' => 'An accordion section'
+            ],
+            [],
+            'An accordion section'
+        ];
+
+        yield 'AccordionStart without mooHeadline' => [
+            [
+                'id' => 42,
+                'type' => 'accordionStart',
+                'mooHeadline' => ''
+            ],
+            [],
+            'ID 42'
+        ];
+
+        yield 'AccordionStop' => [
+            [
+                'id' => 42,
+                'type' => 'accordionStop'
+            ],
+            [],
+            'ID 42'
+        ];
+
+
+        yield 'AccordionSingle with headline' => [
+            [
+                'id' => 42,
+                'type' => 'accordionSingle',
+                'mooHeadline' => 'This is a section headline',
+                'text' => '<p>This is the section text.</p>'
+            ],
+            [],
+            'This is a section headline'
+        ];
+
+        yield 'AccordionSingle without headline' => [
+            [
+                'id' => 42,
+                'type' => 'accordionSingle',
+                'mooHeadline' => '',
+                'text' => '<p>This is the section text.</p>'
+            ],
+            [],
+            htmlspecialchars('<p>This is the section text.</p>')
+        ];
+
+        yield 'SliderStart' => [
+            [
+                'id' => 42,
+                'type' => 'sliderStart'
+            ],
+            [],
+            'ID 42'
+        ];
+
+        yield 'SliderStop' => [
+            [
+                'id' => 42,
+                'type' => 'sliderStop'
+            ],
+            [],
+            'ID 42'
+        ];
+
+        $codeSnippet = <<<CODE_SNIPPET
+<?php declare(strict_types=1);
+// Comment
+echo 'This is a code snippet';
+CODE_SNIPPET;
+
+        yield 'Code' => [
+            [
+                'type' => 'code',
+                'code' => $codeSnippet
+            ],
+            [],
+            htmlspecialchars($codeSnippet)
+        ];
+
+        $markdownSnippet = <<<MARKDOWN_SNIPPET
+# This is a headline
+This is a markdown paragraph.
+MARKDOWN_SNIPPET;
+
+        yield 'Markdown' => [
+            [
+                'type' => 'markdown',
+                'markdown' => $markdownSnippet
+            ],
+            [],
+            $markdownSnippet
+        ];
+
+        yield 'Hyperlink' => [
+            [
+                'type' => 'hyperlink',
+                'url' => 'https://contao.org'
+            ],
+            [],
+            'https://contao.org'
+        ];
+
+        yield 'Toplink with linkTitle' => [
+            [
+                'id' => 42,
+                'type' => 'toplink',
+                'linkTitle' => 'Nach oben'
+            ],
+            [],
+            'Nach oben'
+        ];
+
+        yield 'Toplink without linkTitle' => [
+            [
+                'id' => 42,
+                'type' => 'toplink',
+                'linkTitle' => ''
+            ],
+            [],
+            'ID 42'
+        ];
+
+        // TODO: Image
+        // TODO: Gallery
+        // TODO: Player
+
+        yield 'Youtube' => [
+            [
+                'id' => 42,
+                'type' => 'youtube',
+                'youtube' => 'https://www.youtube.com/watch?v=REboWtl5gmE'
+            ],
+            [],
+            'https://www.youtube.com/watch?v=REboWtl5gmE'
+        ];
+
+        yield 'Vimeo' => [
+            [
+                'id' => 42,
+                'type' => 'vimeo',
+                'vimeo' => 'https://vimeo.com/275028611'
+            ],
+            [],
+            'https://vimeo.com/275028611'
+        ];
+
+        // TODO: Download
+        // TODO: Downloads
+        // TODO: Alias
+        // TODO: Article
+        // TODO: Teaser
+        // TODO: Form
+        // TODO: Module
+    }
+}
