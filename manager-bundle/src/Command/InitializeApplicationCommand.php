8abcc0e75a8456c07d29676ca47065464667e3b4
M. Vondano <m-vo@users.noreply.github.com>
2020-10-22 21:47:09 +0200

    add command to initialize application

---
+++ manager-bundle/src/Command/InitializeApplicationCommand.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+use Webmozart\PathUtil\Path;
+
+/**
+ * @internal
+ */
+class InitializeApplicationCommand extends Command
+{
+    protected static $defaultName = 'contao:initialize-application';
+
+    /**
+     * @var string
+     */
+    private $projectDir;
+
+    /**
+     * @var string
+     */
+    private $webDir;
+
+    public function __construct(string $projectDir, string $webDir)
+    {
+        $this->projectDir = $projectDir;
+        $this->webDir = $webDir;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHidden(true)
+            ->setDescription('Executes all tasks to initialize a Contao Managed Edition. Add this command to your composer "post-install-cmd" and "post-update-cmd" scripts.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $this->purgeProdCache();
+
+        $commands = [
+            ['contao:install-web-dir', '--env=prod'],
+            ['cache:clear', '--no-warmup', '--env=prod'],
+            ['cache:clear', '--no-warmup', '--env=dev'],
+            ['cache:warmup', '--env=prod'],
+            ['assets:install', $this->webDir, '--symlink', '--relative', '--env=prod'],
+            ['contao:install', $this->webDir, '--env=prod'],
+            ['contao:symlinks', $this->webDir, '--env=prod'],
+        ];
+
+        $commandFlags = array_filter([
+            $output->isDecorated() ? '--ansi' : '--no-ansi',
+            $this->getVerbosityFlag($output),
+        ]);
+
+        foreach ($commands as $command) {
+            $this->executeCommand(array_merge($command, $commandFlags), $output);
+        }
+    }
+
+    /**
+     * Removes the prod cache directory.
+     */
+    private function purgeProdCache(): void
+    {
+        $filesystem = new Filesystem();
+
+        $cacheDir = Path::join($this->projectDir, 'var/cache/prod');
+
+        try {
+            if (!$filesystem->exists($cacheDir)) {
+                return;
+            }
+
+            $filesystem->remove($cacheDir);
+        } catch (\Exception $e) {
+            // ignore
+        }
+    }
+
+    /**
+     * Executes a console command in its own process and streams the output.
+     */
+    private function executeCommand(array $command, OutputInterface $output): void
+    {
+        $process = new Process(
+            array_merge(
+                [Path::join($this->projectDir, 'vendor/bin', 'contao-console')],
+                $command
+            )
+        );
+
+        // Increase the timeout according to terminal42/background-process (see #54)
+        $process->setTimeout(500);
+
+        $process->run(
+            static function (string $type, string $buffer) use ($output): void {
+                $output->write($buffer);
+            }
+        );
+
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred while executing the "%s" command: %s', implode(' ', $command), $process->getErrorOutput()));
+        }
+    }
+
+    private function getVerbosityFlag(OutputInterface $output): string
+    {
+        switch ($output->getVerbosity()) {
+            case OutputInterface::VERBOSITY_DEBUG:
+                return '-vvv';
+
+            case OutputInterface::VERBOSITY_VERY_VERBOSE:
+                return '-vv';
+
+            case OutputInterface::VERBOSITY_VERBOSE:
+                return '-v';
+
+            default:
+                return '';
+        }
+    }
+}
