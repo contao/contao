7db26501214c7581c53c8a7c16adf2c5c63c2670
M. Vondano <m-vo@users.noreply.github.com>
2020-10-22 23:30:45 +0200

    add unit tests

---
+++ manager-bundle/tests/Command/InitializeApplicationCommandTest.php
@@ -0,0 +1,220 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerBundle\Tests\Command;
+
+use Contao\ManagerBundle\Command\InitializeApplicationCommand;
+use Contao\ManagerBundle\Process\ProcessFactory;
+use Contao\TestCase\ContaoTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+
+class InitializeApplicationCommandTest extends ContaoTestCase
+{
+    public function testIsHidden(): void
+    {
+        $command = new InitializeApplicationCommand('project/dir', 'web');
+
+        $this->assertTrue($command->isHidden());
+    }
+
+    public function testPurgesProdCacheDirectory(): void
+    {
+        $filesystem = $this->createMock(Filesystem::class);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with('project/dir/var/cache/prod')
+            ->willReturn(true)
+        ;
+
+        $filesystem
+            ->expects($this->once())
+            ->method('remove')
+            ->with('project/dir/var/cache/prod')
+        ;
+
+        $command = new InitializeApplicationCommand(
+            'project/dir', 'web', $filesystem, $this->getProcessFactoryMock()
+        );
+
+        (new CommandTester($command))->execute([]);
+    }
+
+    public function testDoesNotPurgeProdCacheDirectoryIfItDoesntExist(): void
+    {
+        $filesystem = $this->createMock(Filesystem::class);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with('project/dir/var/cache/prod')
+            ->willReturn(false)
+        ;
+
+        $filesystem
+            ->expects($this->never())
+            ->method('remove')
+        ;
+
+        $command = new InitializeApplicationCommand(
+            'project/dir', 'web', $filesystem, $this->getProcessFactoryMock()
+        );
+
+        (new CommandTester($command))->execute([]);
+    }
+
+    /**
+     * @dataProvider provideCommands
+     */
+    public function testExecutesCommands(array $options, array $flags): void
+    {
+        $filesystem = $this->createMock(Filesystem::class);
+        $filesystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with('project/dir/var/cache/prod')
+            ->willReturn(false)
+        ;
+
+        $process = $this->createMock(Process::class);
+        $process
+            ->method('isSuccessful')
+            ->willReturn(true)
+        ;
+
+        $commandArguments = [
+            [array_merge(['project/dir/vendor/bin/contao-console', 'contao:install-web-dir', '--env=prod'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'cache:clear', '--no-warmup', '--env=prod'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'cache:clear', '--no-warmup', '--env=dev'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'cache:warmup', '--env=prod'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'assets:install', 'web', '--symlink', '--relative', '--env=prod'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'contao:install', 'web', '--env=prod'], $flags)],
+            [array_merge(['project/dir/vendor/bin/contao-console', 'contao:symlinks', 'web', '--env=prod'], $flags)],
+        ];
+
+        $processFactory = $this->createMock(ProcessFactory::class);
+        $processFactory
+            ->expects($this->exactly(7))
+            ->method('create')
+            ->willReturn($process)
+            ->withConsecutive(...$commandArguments)
+        ;
+
+        $command = new InitializeApplicationCommand(
+            'project/dir', 'web', $filesystem, $processFactory
+        );
+
+        (new CommandTester($command))->execute([], $options);
+    }
+
+    public function provideCommands(): \Generator
+    {
+        yield 'no arguments' => [
+            [],
+            ['--no-ansi'],
+        ];
+
+        yield 'ansi' => [
+            ['decorated' => true],
+            ['--ansi'],
+        ];
+
+        yield 'normal' => [
+            ['verbosity' => OutputInterface::VERBOSITY_NORMAL],
+            ['--no-ansi'],
+        ];
+
+        yield 'verbose' => [
+            ['verbosity' => OutputInterface::VERBOSITY_VERBOSE],
+            ['--no-ansi', '-v'],
+        ];
+
+        yield 'very verbose' => [
+            ['verbosity' => OutputInterface::VERBOSITY_VERY_VERBOSE],
+            ['--no-ansi', '-vv'],
+        ];
+
+        yield 'debug' => [
+            ['verbosity' => OutputInterface::VERBOSITY_DEBUG],
+            ['--no-ansi', '-vvv'],
+        ];
+
+        yield 'ansi and verbose' => [
+            ['decorated' => true, 'verbosity' => OutputInterface::VERBOSITY_VERBOSE],
+            ['--ansi', '-v'],
+        ];
+    }
+
+    public function testThrowsIfCommandFails(): void
+    {
+        $filesystem = $this->createMock(Filesystem::class);
+        $filesystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with('project/dir/var/cache/prod')
+            ->willReturn(false)
+        ;
+
+        $process = $this->createMock(Process::class);
+
+        $process
+            ->method('isSuccessful')
+            ->willReturn(false)
+        ;
+
+        $process
+            ->method('getErrorOutput')
+            ->willReturn('<error>')
+        ;
+
+        $processFactory = $this->createMock(ProcessFactory::class);
+        $processFactory
+            ->expects($this->once())
+            ->method('create')
+            ->willReturn($process)
+        ;
+
+        $command = new InitializeApplicationCommand(
+            'project/dir', 'web', $filesystem, $processFactory
+        );
+
+        $commandTester = (new CommandTester($command));
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessageMatches('/An error occurred while executing the ".+" command: <error>/');
+
+        $commandTester->execute([]);
+    }
+
+    private function getProcessFactoryMock(): Process
+    {
+        $process = $this->createMock(Process::class);
+        $process
+            ->method('isSuccessful')
+            ->willReturn(true)
+        ;
+
+        /** @var Process&MockObject $processFactory */
+        $processFactory = $this->createMock(ProcessFactory::class);
+        $processFactory
+            ->method('create')
+            ->willReturn($process)
+        ;
+
+        return $processFactory;
+    }
+}
---
+++ manager-bundle/tests/Process/ProcessFactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\ManagerBundle\Tests\Process;
+
+use Contao\ManagerBundle\Process\ProcessFactory;
+use Contao\TestCase\ContaoTestCase;
+
+class ProcessFactoryTest extends ContaoTestCase
+{
+    public function testCreatesCommand(): void
+    {
+        $process = (new ProcessFactory())->create(
+            ['my', 'command'],
+            'cwd',
+            ['env'],
+            'input',
+            100
+        );
+
+        $this->assertSame("'my' 'command'", $process->getCommandLine());
+        $this->assertSame('cwd', $process->getWorkingDirectory());
+        $this->assertSame(['env'], $process->getEnv());
+        $this->assertSame('input', $process->getInput());
+        $this->assertSame(100, $process->getTimeout());
+    }
+}
