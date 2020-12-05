1d6f41716e1cf42d144a4b9c4e27081d865e3122
Martin Auswöger <martin@auswoeger.com>
2020-12-05 14:47:41 +0100

    Move migrations to core bundle

--- core-bundle/src/Migration/Version305/Version350Update.php
+++ core-bundle/src/Migration/Version305/Version350Update.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\InstallationBundle\Database;
+namespace Contao\CoreBundle\Migration\Version305;
 
 use Contao\CoreBundle\Migration\AbstractMigration;
 use Contao\CoreBundle\Migration\MigrationResult;
