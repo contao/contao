4df690a8d99add38543d0d062425e1837525e2ee
fritzmg <fmg@inspiredminds.at>
2020-12-10 13:47:36 +0000

    update AddNativeTransportFactoryPassTest for prefer-lowest

--- core-bundle/tests/DependencyInjection/Compiler/AddNativeTransportFactoryPassTest.php
+++ core-bundle/tests/DependencyInjection/Compiler/AddNativeTransportFactoryPassTest.php
@@ -16,22 +16,32 @@ use Contao\CoreBundle\DependencyInjection\Compiler\AddNativeTransportFactoryPass
 use Contao\CoreBundle\Tests\TestCase;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Mailer\Transport\NativeTransportFactory;
 
 class AddNativeTransportFactoryPassTest extends TestCase
 {
-    public function testAddsTheNativeTransportFactory(): void
+    public function testAddsTheNativeTransportFactoryIfSupported(): void
     {
         $container = new ContainerBuilder();
 
         $pass = new AddNativeTransportFactoryPass();
         $pass->process($container);
 
-        $this->assertTrue($container->hasDefinition('mailer.transport_factory.native'));
+        if ($this->supportsNativeMailer()) {
+            $this->assertTrue($container->hasDefinition('mailer.transport_factory.native'));
 
-        /** @var ChildDefinition $definition */
-        $definition = $container->getDefinition('mailer.transport_factory.native');
+            /** @var ChildDefinition $definition */
+            $definition = $container->getDefinition('mailer.transport_factory.native');
 
-        $this->assertTrue($definition->hasTag('mailer.transport_factory'));
-        $this->assertSame('mailer.transport_factory.abstract', $definition->getParent());
+            $this->assertTrue($definition->hasTag('mailer.transport_factory'));
+            $this->assertSame('mailer.transport_factory.abstract', $definition->getParent());
+        } else {
+            $this->assertFalse($container->hasDefinition('mailer.transport_factory.native'));
+        }
+    }
+
+    private function supportsNativeMailer(): bool
+    {
+        return class_exists(NativeTransportFactory::class);
     }
 }
