bc33c0b9c881a7baad52fffda5facd5f40becc15
Leo Feyer <github@contao.org>
2020-12-23 12:25:08 +0100

    Use the bracketed syntax in the Symfony polyfill

--- core-bundle/src/Resources/functions/symfony-polyfill.php
+++ core-bundle/src/Resources/functions/symfony-polyfill.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace Symfony\Component\HttpFoundation;
-
-// Forward compatibility with symfony/http-foundation >=5.0
-if (!class_exists(InputBag::class)) {
-    class InputBag extends ParameterBag
-    {
-        public function all()
+namespace Symfony\Component\HttpFoundation {
+    // Forward compatibility with symfony/http-foundation >=5.0
+    if (!class_exists(InputBag::class)) {
+        class InputBag extends ParameterBag
         {
-            $key = \func_num_args() > 0 ? func_get_arg(0) : null;
+            public function all()
+            {
+                $key = \func_num_args() > 0 ? func_get_arg(0) : null;
 
-            if ($key) {
-                return $this->get($key);
+                if ($key) {
+                    return $this->get($key);
+                }
+
+                return parent::all();
             }
-
-            return parent::all();
         }
     }
 }
 
-namespace Symfony\Component\Translation;
+namespace Symfony\Component\Translation {
+    use Symfony\Contracts\Translation\TranslatorInterface as BaseTranslatorInterface;
 
-use Symfony\Contracts\Translation\TranslatorInterface as BaseTranslatorInterface;
-
-// Backwards compatibility with symfony/translation <5.0
-if (!interface_exists(TranslatorInterface::class)) {
-    interface TranslatorInterface extends BaseTranslatorInterface
-    {
+    // Backwards compatibility with symfony/translation <5.0
+    if (!interface_exists(TranslatorInterface::class)) {
+        interface TranslatorInterface extends BaseTranslatorInterface
+        {
+        }
     }
 }
