3eae075ab0929382e5bb790415de5d6f0e524a96
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2021-01-11 10:42:25 +0000

    Use token checker instead of FE_USER_LOGGED_IN constant (see #2628)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #2576
    | Docs PR or issue | -
    
    This replaces the usage of the `FE_USER_LOGGED_IN` constant with the `contao.security.token_checker` service as discussed.
    
    Commits
    -------
    
    d0cb8229 replace usage of FE_USER_LOGGED_IN constant
    d989c9f0 update README
    8d33308c CS

--- core-bundle/src/Resources/contao/library/Contao/Controller.php
+++ core-bundle/src/Resources/contao/library/Contao/Controller.php
@@ -738,10 +738,12 @@ abstract class Controller extends System
 		// Only apply the restrictions in the front end
 		if (TL_MODE == 'FE')
 		{
+			$blnFeUserLoggedIn = System::getContainer()->get('contao.security.token_checker')->hasFrontendUser();
+
 			// Protected element
 			if ($objElement->protected)
 			{
-				if (!FE_USER_LOGGED_IN)
+				if (!$blnFeUserLoggedIn)
 				{
 					$blnReturn = false;
 				}
@@ -766,7 +768,7 @@ abstract class Controller extends System
 			}
 
 			// Show to guests only
-			elseif ($objElement->guests && FE_USER_LOGGED_IN)
+			elseif ($objElement->guests && $blnFeUserLoggedIn)
 			{
 				$blnReturn = false;
 			}
