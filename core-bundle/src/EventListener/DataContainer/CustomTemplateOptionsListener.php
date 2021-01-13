a6ae4bb8a415e857c4be04f3608524372292c4ce
fritzmg <fmg@inspiredminds.at>
2021-01-13 19:22:51 +0000

    improvements based on code review

--- core-bundle/src/EventListener/DataContainer/CustomTemplateOptionsListener.php
+++ core-bundle/src/EventListener/DataContainer/CustomTemplateOptionsListener.php
@@ -12,11 +12,9 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\EventListener\DataContainer;
 
-use Contao\ContentElement;
 use Contao\Controller;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\DataContainer;
-use Contao\Module;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class CustomTemplateOptionsListener
@@ -46,34 +44,26 @@ class CustomTemplateOptionsListener
      */
     private $proxyClass;
 
-    public function __construct(ContaoFramework $framework, RequestStack $requestStack, array $customTemplates, string $prefix, string $proxyClass)
+    public function __construct(ContaoFramework $framework, RequestStack $requestStack, array $customTemplates, string $prefix, string $proxyClass = null)
     {
         /** @var Controller $controller */
         $controller = $framework->getAdapter(Controller::class);
 
         $this->controller = $controller;
         $this->requestStack = $requestStack;
-        $this->prefix = $prefix;
         $this->customTemplates = $customTemplates;
+        $this->prefix = $prefix;
         $this->proxyClass = $proxyClass;
     }
 
     public function __invoke(DataContainer $dc)
     {
         if ($this->isOverrideAll()) {
-            return $this->getOverrideAllTemplates($this->prefix);
+            // Add a blank option that allows us to reset all custom templates to the default one
+            return array_merge(['' => '-'], $this->controller->getTemplateGroup($this->prefix));
         }
 
-        $defaultTemplate = $this->customTemplates[$dc->activeRecord->type] ?? null;
-
-        // Extract default template from legacy class
-        if (null === $defaultTemplate) {
-            $class = $this->getLegacyClass($dc);
-
-            if (!empty($class) && $class !== $this->proxyClass) {
-                $defaultTemplate = $this->getTemplateFromObject(new $class($dc->activeRecord));
-            }
-        }
+        $defaultTemplate = $this->customTemplates[$dc->activeRecord->type] ?? $this->getLegacyDefaultTemplate($dc);
 
         if (null === $defaultTemplate) {
             $defaultTemplate = $this->prefix.$dc->activeRecord->type;
@@ -82,44 +72,22 @@ class CustomTemplateOptionsListener
         return $this->getTemplateGroup($defaultTemplate);
     }
 
-    private function getLegacyClass(DataContainer $dc): ?string
-    {
-        switch ($dc->table) {
-            case 'tl_content': return ContentElement::findClass($dc->activeRecord->type);
-
-            case 'tl_module': return Module::findClass($dc->activeRecord->type);
-        }
-
-        return null;
-    }
-
-    private function getTemplateGroup(string $template): array
-    {
-        return $this->controller->getTemplateGroup($template.'_', [], $template);
-    }
-
-    private function getOverrideAllTemplates(string $prefix): array
-    {
-        // Add a blank option that allows us to reset all custom templates to the default one
-        return array_merge(['' => '-'], $this->controller->getTemplateGroup($prefix));
-    }
-
-    private function isOverrideAll(): bool
-    {
-        $request = $this->requestStack->getCurrentRequest();
-
-        if (null === $request || !$request->query->has('act')) {
-            return false;
-        }
-
-        return 'overrideAll' === $request->query->get('act');
-    }
-
     /**
-     * Uses the reflection API to return the default template name from the given object.
+     * Uses the reflection API to return the default template from a legacy class.
      */
-    private function getTemplateFromObject($object): ?string
+    private function getLegacyDefaultTemplate(DataContainer $dc): ?string
     {
+        if (null === $this->proxyClass || !method_exists($this->proxyClass, 'findClass')) {
+            return null;
+        }
+
+        $class = $this->proxyClass::findClass($dc->activeRecord->type);
+
+        if (empty($class) || $class === $this->proxyClass) {
+            return null;
+        }
+
+        $object = new $class($dc->activeRecord);
         $reflection = new \ReflectionClass($object);
 
         try {
@@ -132,5 +100,21 @@ class CustomTemplateOptionsListener
         $property->setAccessible(true);
 
         return $property->getValue($object) ?: null;
+    }
+
+    private function getTemplateGroup(string $template): array
+    {
+        return $this->controller->getTemplateGroup($template.'_', [], $template);
+    }
+
+    private function isOverrideAll(): bool
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (null === $request || !$request->query->has('act')) {
+            return false;
+        }
+
+        return 'overrideAll' === $request->query->get('act');
     }
 }
