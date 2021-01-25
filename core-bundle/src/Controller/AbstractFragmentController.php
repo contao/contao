0e921de6bc8ed7c770f9fb8a66f3ffee2be09408
Leo Feyer <github@contao.org>
2021-01-25 14:09:10 +0100

    Fix a BC break in the AbstractFragmentController (see #2688)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #2652
    | Docs PR or issue | -
    
    Commits
    -------
    
    4b2b3756 Fix a BC break in the AbstractFragmentController
    f77e4e3b CS

--- core-bundle/src/Controller/AbstractFragmentController.php
+++ core-bundle/src/Controller/AbstractFragmentController.php
@@ -33,7 +33,10 @@ abstract class AbstractFragmentController extends AbstractController implements 
         $this->options = $options;
     }
 
-    public static function getSubscribedServices(): array
+    /**
+     * @return array<string>
+     */
+    public static function getSubscribedServices()
     {
         $services = parent::getSubscribedServices();
 
