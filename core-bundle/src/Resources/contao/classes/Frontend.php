4f6919e0870c31c084c6d35e30e3e48174ba9fd1
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-02-13 17:09:21 +0100

    CS

--- core-bundle/src/Resources/contao/classes/Frontend.php
+++ core-bundle/src/Resources/contao/classes/Frontend.php
@@ -347,7 +347,7 @@ abstract class Frontend extends Controller
 			}
 			catch (RoutingExceptionInterface $exception)
 			{
-				$logger->log(LogLevel::ERROR, $strError, ['contao' => new ContaoContext(__METHOD__, 'ERROR')]);
+				$logger->log(LogLevel::ERROR, $strError, array('contao' => new ContaoContext(__METHOD__, 'ERROR')));
 
 				throw new NoRootPageFoundException('No root page found', 0, $exception);
 			}
