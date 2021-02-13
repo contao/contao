f5daea46a184d22181da563a941dd064565ae9ea
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2021-02-14 00:49:34 +0100

    Make /contao route prefix configurable

--- core-bundle/src/DependencyInjection/ContaoCoreExtension.php
+++ core-bundle/src/DependencyInjection/ContaoCoreExtension.php
@@ -83,6 +83,7 @@ class ContaoCoreExtension extends Extension
         $container->setParameter('contao.security.two_factor.enforce_backend', $config['security']['two_factor']['enforce_backend']);
         $container->setParameter('contao.localconfig', $config['localconfig'] ?? []);
         $container->setParameter('contao.backend', $config['backend']);
+        $container->setParameter('contao.backend.route_prefix', $config['backend']['route_prefix']);
 
         $this->handleSearchConfig($config, $container);
         $this->handleCrawlConfig($config, $container);
