c5fb866b2c5182944c43397514623e29eba11e89
M. Vondano <m-vo@users.noreply.github.com>
2021-03-12 00:33:26 +0100

    deprecate sqlCompileCommands hook

--- installation-bundle/src/Database/Installer.php
+++ installation-bundle/src/Database/Installer.php
@@ -213,6 +213,8 @@ class Installer
 
         // HOOK: allow third-party developers to modify the array (see #3281)
         if (isset($GLOBALS['TL_HOOKS']['sqlCompileCommands']) && \is_array($GLOBALS['TL_HOOKS']['sqlCompileCommands'])) {
+            @trigger_error('Using the "sqlCompileCommands" hook has been deprecated and will no longer work in Contao 5.0.', E_USER_DEPRECATED);
+
             foreach ($GLOBALS['TL_HOOKS']['sqlCompileCommands'] as $callback) {
                 $return = System::importStatic($callback[0])->{$callback[1]}($return);
             }
