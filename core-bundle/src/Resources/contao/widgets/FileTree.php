c1c923b6e9948bb87cdda6dc1fd323c9be55520f
Alexej Kossmann <a.kossmann@heimrich-hannot.de>
2021-03-16 08:42:35 +0100

    added submitOnChange to FileTree Widget

--- core-bundle/src/Resources/contao/widgets/FileTree.php
+++ core-bundle/src/Resources/contao/widgets/FileTree.php
@@ -385,7 +385,7 @@ class FileTree extends Widget
               onSuccess: function(txt, json) {
                 $("ctrl_' . $this->strId . '").getParent("div").set("html", json.content);
                 json.javascript && Browser.exec(json.javascript);
-                $("ctrl_' . $this->strId . '").fireEvent("change");
+                $("ctrl_' . $this->strId . '").fireEvent("change");' . ($this->arrConfiguration['submitOnChange']) ? ' Backend.autoSubmit("' . $this->strTable . '");' : '' . '
               }
             }).post({"action":"reloadFiletree", "name":"' . $this->strName . '", "value":value.join("\t"), "REQUEST_TOKEN":"' . REQUEST_TOKEN . '"});
           }
