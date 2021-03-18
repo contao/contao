d0b6eaf08069916f4127c880ff1646072e812010
Markus Nestmann <38976632+MarkejN@users.noreply.github.com>
2021-03-18 12:00:02 +0100

    Fix code style

--- core-bundle/src/EventListener/HttpCache/StripCookiesSubscriber.php
+++ core-bundle/src/EventListener/HttpCache/StripCookiesSubscriber.php
@@ -64,7 +64,7 @@ class StripCookiesSubscriber implements EventSubscriberInterface
 
         // Osano Cookie Consent
         'cookieconsent_status',
-        
+
         // Cookiebot Cookie Consent
         'CookieConsent',
     ];
