2cfcd8b123ccf3f3e7ce72a029687ab254a00fbd
Alexej Kossmann <a.kossmann@heimrich-hannot.de>
2021-03-19 18:00:43 +0100

    added onchange attribute to fileTree input

--- core-bundle/src/Resources/contao/widgets/FileTree.php
+++ core-bundle/src/Resources/contao/widgets/FileTree.php
@@ -348,7 +348,7 @@ class FileTree extends Widget
 		$strSet = implode(',', array_map('StringUtil::binToUuid', $arrSet));
 		$strOrder = $blnHasOrder ? implode(',', array_map('StringUtil::binToUuid', $this->{$this->orderField})) : '';
 
-		$return = '<input type="hidden" name="' . $this->strName . '" id="ctrl_' . $this->strId . '" value="' . $strSet . '">' . ($blnHasOrder ? '
+		$return = '<input type="hidden" name="' . $this->strName . '" id="ctrl_' . $this->strId . '" value="' . $strSet . '" onchange="' . $this->onchange . '">' . ($blnHasOrder ? '
   <input type="hidden" name="' . $this->strOrderName . '" id="ctrl_' . $this->strOrderId . '" value="' . $strOrder . '">' : '') . '
   <div class="selector_container">' . (($blnHasOrder && \count($arrValues) > 1) ? '
     <p class="sort_hint">' . $GLOBALS['TL_LANG']['MSC']['dragItemsHint'] . '</p>' : '') . '
@@ -385,7 +385,7 @@ class FileTree extends Widget
               onSuccess: function(txt, json) {
                 $("ctrl_' . $this->strId . '").getParent("div").set("html", json.content);
                 json.javascript && Browser.exec(json.javascript);
-                $("ctrl_' . $this->strId . '").fireEvent("change");' . ($this->arrConfiguration['submitOnChange']) ? ' Backend.autoSubmit("' . $this->strTable . '");' : '' . '
+                $("ctrl_' . $this->strId . '").fireEvent("change");' . ($this->arrConfiguration['submitOnChange'] ? ' Backend.autoSubmit("' . $this->strTable . '");' : '') . '
               }
             }).post({"action":"reloadFiletree", "name":"' . $this->strName . '", "value":value.join("\t"), "REQUEST_TOKEN":"' . REQUEST_TOKEN . '"});
           }
