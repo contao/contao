9cbb7bbc79c374ab3ea93522a4752f993738f9f9
M. Vondano <m-vo@users.noreply.github.com>
2021-05-04 16:11:23 +0200

    add NodeVisitor that can alter escape filter strategies

---
+++ core-bundle/src/Twig/Interop/ContaoEscaperNodeVisitor.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Twig\Interop;
+
+use Twig\Environment;
+use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Expression\FilterExpression;
+use Twig\Node\ModuleNode;
+use Twig\Node\Node;
+use Twig\NodeVisitor\AbstractNodeVisitor;
+
+/**
+ * This NodeVisitor alters all "escape('html')" filter expressions into
+ * "escape('contao_html')" filter expressions if the template they belong
+ * to is amongst the configured affected templates.
+ */
+class ContaoEscaperNodeVisitor extends AbstractNodeVisitor
+{
+    /**
+     * Evaluate affected templates on the fly so that they can be added after
+     * building the container.
+     *
+     * @var \Closure():array<string>
+     */
+    private $affectedTemplates;
+
+    /**
+     * @var array|null
+     */
+    private $escaperFilterNodes;
+
+    public function __construct(\Closure $affectedTemplates)
+    {
+        $this->affectedTemplates = $affectedTemplates;
+    }
+
+    /**
+     * Make sure to run after @see \Twig\NodeVisitor\EscaperNodeVisitor.
+     */
+    public function getPriority(): int
+    {
+        return 1;
+    }
+
+    protected function doEnterNode(Node $node, Environment $env): Node
+    {
+        if (
+            $node instanceof ModuleNode &&
+            \in_array($node->getTemplateName(), ($this->affectedTemplates)(), true)
+        ) {
+            $this->escaperFilterNodes = [];
+        } elseif (null !== $this->escaperFilterNodes && $this->isEscaperFilterExpressionWithHtmlStrategy($node)) {
+            $this->escaperFilterNodes[] = $node;
+        }
+
+        return $node;
+    }
+
+    protected function doLeaveNode(Node $node, Environment $env)
+    {
+        if ($node instanceof ModuleNode && null !== $this->escaperFilterNodes) {
+            foreach ($this->escaperFilterNodes as $escaperFilterNode) {
+                $this->setContaoEscaperArguments($escaperFilterNode);
+            }
+
+            $this->escaperFilterNodes = null;
+        }
+
+        return $node;
+    }
+
+    private function isEscaperFilterExpressionWithHtmlStrategy(Node $node): bool
+    {
+        if (!$node instanceof FilterExpression) {
+            return false;
+        }
+
+        $getConstantValue = static function (Node $node): ?string {
+            if (!$node instanceof ConstantExpression) {
+                return null;
+            }
+
+            return $node->getAttribute('value');
+        };
+
+        return 'escape' === $getConstantValue($node->getNode('filter')) &&
+            'html' === $getConstantValue($node->getNode('arguments')->getNode(0));
+    }
+
+    private function setContaoEscaperArguments(FilterExpression $node): void
+    {
+        $line = $node->getTemplateLine();
+
+        $arguments = new Node([
+            new ConstantExpression('contao_html', $line),
+            new ConstantExpression(null, $line),
+            new ConstantExpression(true, $line),
+        ]);
+
+        $node->setNode('arguments', $arguments);
+    }
+}
---
+++ core-bundle/tests/Twig/Interop/ContaoEscaperNodeVisitorTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Twig\Interop;
+
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\CoreBundle\Twig\Extension\InteropExtension;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+
+class ContaoEscaperNodeVisitorTest extends TestCase
+{
+    public function testEscapesEntities(): void
+    {
+        $output = $this->getEnvironment()->render(
+            'modern.html.twig',
+            [
+                'headline' => '&amp; is the HTML entity for &',
+                'content' => 'This is <i>raw HTML</i>.',
+            ]
+        );
+
+        $this->assertSame(
+            '<h1>&amp;amp; is the HTML entity for &amp;</h1><p>This is <i>raw HTML</i>.</p>',
+            $output
+        );
+    }
+
+    public function testDoesNotDoubleEncode(): void
+    {
+        $output = $this->getEnvironment()->render(
+            'legacy.html.twig',
+            [
+                'headline' => '&amp; will look like &',
+                'content' => 'This is <i>raw HTML</i>.',
+            ]
+        );
+
+        $this->assertSame(
+            '<h1>&amp; will look like &amp;</h1><p>This is <i>raw HTML</i>.</p>',
+            $output
+        );
+    }
+
+    protected function getEnvironment(): Environment
+    {
+        $templateContent = '<h1>{{ headline }}</h1><p>{{ content|raw }}</p>';
+
+        $loader = new ArrayLoader([
+            'modern.html.twig' => $templateContent,
+            'legacy.html.twig' => $templateContent,
+        ]);
+
+        $environment = new Environment($loader);
+
+        $interopExtension = new InteropExtension($environment);
+        $interopExtension->registerTemplateForInputEncoding('legacy.html.twig');
+
+        $environment->addExtension($interopExtension);
+
+        return $environment;
+    }
+}
