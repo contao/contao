dc698b296bcc065a0e6c154db197884d58a6b800
Cliff Parnitzky <github@cliff-parnitzky.de>
2021-05-04 18:49:23 +0200

    Update the corrsponding test

--- manager-bundle/tests/EventListener/BackendMenuListenerTest.php
+++ manager-bundle/tests/EventListener/BackendMenuListenerTest.php
@@ -220,7 +220,7 @@ class BackendMenuListenerTest extends ContaoTestCase
 
         $this->assertSame('contao_manager', $manager->getLabel());
         $this->assertSame('/contao-manager.phar.php', $manager->getUri());
-        $this->assertSame(['class' => 'navigation contao_manager'], $manager->getLinkAttributes());
+        $this->assertSame(['class' => 'navigation contao_manager', 'title' => 'contao_manager_title'], $manager->getLinkAttributes());
         $this->assertSame(['translation_domain' => 'ContaoManagerBundle'], $manager->getExtras());
     }
 
