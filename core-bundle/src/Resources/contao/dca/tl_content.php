806b72325bd83452ea29f674923eca7c3e08c8f7
M. Vondano <m-vo@users.noreply.github.com>
2021-06-22 13:45:50 +0200

    Allow empty content element translation key (see #3090)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | -
    | Docs PR or issue | -
    
    Fixes `Warning: Undefined array key "foo"` if a new `foo` content element controller was created but no translation added yet.
    
    The existing check (elvis) only checks for falsy values but not if the array key actually exists.
    
    Commits
    -------
    
    85df0173 do not crash if a language key is missing
    f97349c5 fall back to cte type

--- core-bundle/src/Resources/contao/dca/tl_content.php
+++ core-bundle/src/Resources/contao/dca/tl_content.php
@@ -1241,7 +1241,7 @@ class tl_content extends Backend
 	public function addCteType($arrRow)
 	{
 		$key = $arrRow['invisible'] ? 'unpublished' : 'published';
-		$type = $GLOBALS['TL_LANG']['CTE'][$arrRow['type']][0] ?: '&nbsp;';
+		$type = $GLOBALS['TL_LANG']['CTE'][$arrRow['type']][0] ?? $arrRow['type'];
 		$class = 'limit_height';
 
 		// Remove the class if it is a wrapper element
