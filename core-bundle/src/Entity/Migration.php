7fea103521e1bf863d294f8983d9e0c611b342ea
fritzmg <fmg@inspiredminds.at>
2021-06-22 16:39:13 +0100

    fix copy & paste error

--- core-bundle/src/Entity/Migration.php
+++ core-bundle/src/Entity/Migration.php
@@ -22,7 +22,7 @@ use Doctrine\ORM\Mapping\GeneratedValue;
  *         @ORM\Index(name="name", columns={"name"})
  *     }
  * )
- * @ORM\Entity(repositoryClass="Contao\CoreBundle\Repository\CronJobRepository")
+ * @ORM\Entity(repositoryClass="Contao\CoreBundle\Repository\MigrationRepository")
  */
 class Migration
 {
