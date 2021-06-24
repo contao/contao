1e696a7fdda9cdec3b0271652141db40c54480e2
Leo Feyer <github@contao.org>
2021-06-24 10:30:03 +0200

    Update the CI tools

--- core-bundle/src/EventListener/CsrfTokenCookieSubscriber.php
+++ core-bundle/src/EventListener/CsrfTokenCookieSubscriber.php
@@ -147,14 +147,16 @@ class CsrfTokenCookieSubscriber implements EventSubscriberInterface
 
         $content = str_replace($tokens, '', $content, $replacedCount);
 
-        if ($replacedCount > 0) {
-            $response->setContent($content);
-
-            // Remove the Content-Length header now that we have changed the
-            // content length (see #2416). Do not add the header or adjust an
-            // existing one (see symfony/symfony#1846).
-            $response->headers->remove('Content-Length');
+        if ($replacedCount <= 0) {
+            return;
         }
+
+        $response->setContent($content);
+
+        // Remove the Content-Length header now that we have changed the
+        // content length (see #2416). Do not add the header or adjust an
+        // existing one (see symfony/symfony#1846).
+        $response->headers->remove('Content-Length');
     }
 
     private function removeCookies(Request $request, Response $response): void
--- core-bundle/src/Resources/contao/classes/BackendUser.php
+++ core-bundle/src/Resources/contao/classes/BackendUser.php
@@ -35,43 +35,36 @@ class BackendUser extends User
 {
 	/**
 	 * Edit page flag
-	 * @var string
 	 */
 	const CAN_EDIT_PAGE = 1;
 
 	/**
 	 * Edit page hierarchy flag
-	 * @var string
 	 */
 	const CAN_EDIT_PAGE_HIERARCHY = 2;
 
 	/**
 	 * Delete page flag
-	 * @var string
 	 */
 	const CAN_DELETE_PAGE = 3;
 
 	/**
 	 * Edit articles flag
-	 * @var string
 	 */
 	const CAN_EDIT_ARTICLES = 4;
 
 	/**
 	 * Edit article hierarchy flag
-	 * @var string
 	 */
 	const CAN_EDIT_ARTICLE_HIERARCHY = 5;
 
 	/**
 	 * Delete articles flag
-	 * @var string
 	 */
 	const CAN_DELETE_ARTICLES = 6;
 
 	/**
 	 * Symfony Security session key
-	 * @var string
 	 * @deprecated Deprecated since Contao 4.8, to be removed in Contao 5.0
 	 */
 	const SECURITY_SESSION_KEY = '_security_contao_backend';
