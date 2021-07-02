79a8dcff1c6805b15ff3820cf4f6c6c854a83ddd
Leo Feyer <github@contao.org>
2021-07-02 15:18:25 +0200

    Filter "guests only" pages to restore backwards compatibility

--- core-bundle/src/Resources/contao/modules/ModuleQuicklink.php
+++ core-bundle/src/Resources/contao/modules/ModuleQuicklink.php
@@ -94,6 +94,13 @@ class ModuleQuicklink extends Module
 		{
 			$objSubpage->loadDetails();
 
+			// Hide the page if it is not protected and only visible to guests (backwards compatibility)
+			if ($objSubpage->guests && !$objSubpage->protected && !$user)
+			{
+				trigger_deprecation('contao/core-bundle', '4.12', 'Using the "show to guests only" feature has been deprecated an will no longer work in Contao 5.0. Use the "protect page" function instead.');
+				continue;
+			}
+
 			// PageModel->groups is an array after calling loadDetails()
 			if (!$objSubpage->protected || $this->showProtected || (!$user && \in_array(-1, $objSubpage->groups)) || ($user && $user->isMemberOf($objSubpage->groups)))
 			{
