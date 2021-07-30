4d02f589abbddcbabb576ae4b0c6fd0ae0508cfd
fritzmg <fmg@inspiredminds.at>
2021-07-30 14:43:37 +0100

    fix AddSessionBagsPass

--- core-bundle/src/DependencyInjection/Compiler/AddSessionBagsPass.php
+++ core-bundle/src/DependencyInjection/Compiler/AddSessionBagsPass.php
@@ -23,7 +23,7 @@ class AddSessionBagsPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        if (!$container->hasDefinition('session')) {
+        if (!$container->has('session')) {
             return;
         }
 
