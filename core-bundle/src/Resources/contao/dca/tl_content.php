be126332fb925819c973ac1c0cbd8d0076aaafc4
Martin Auswöger <martin@auswoeger.com>
2021-08-02 15:28:10 +0200

    Fix a func_get_arg() value error (see #3243)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #3238
    
    Commits
    -------
    
    199e4219 Fix func_get_arg ValueError

--- core-bundle/src/Resources/contao/dca/tl_content.php
+++ core-bundle/src/Resources/contao/dca/tl_content.php
@@ -1973,7 +1973,7 @@ class tl_content extends Backend
 	{
 		if (Input::get('cid'))
 		{
-			$this->toggleVisibility(Input::get('cid'), (Input::get('state') == 1), (@func_get_arg(12) ?: null));
+			$this->toggleVisibility(Input::get('cid'), (Input::get('state') == 1), (func_num_args() <= 12 ? null : func_get_arg(12)));
 			$this->redirect($this->getReferer());
 		}
 
