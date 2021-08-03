0e200d00cd199bef2f235212b2a3a8df53a17ae6
David Greminger <david.greminger@1up.io>
2021-08-03 20:44:51 +0200

    Remove leftovers

--- core-bundle/src/Security/Authentication/Provider/AuthenticationProvider.php
+++ core-bundle/src/Security/Authentication/Provider/AuthenticationProvider.php
@@ -47,12 +47,9 @@ class AuthenticationProvider extends DaoAuthenticationProvider
 
     /**
      * @internal Do not inherit from this class; decorate the "contao.security.authentication_provider" service instead
-     *
-     * @todo Replace EncoderFactoryInterface with Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface
      */
     public function __construct(UserProviderInterface $userProvider, UserCheckerInterface $userChecker, string $providerKey, PasswordHasherFactoryInterface $passwordHasherFactory, ContaoFramework $framework, AuthenticationProviderInterface $twoFactorAuthenticationProvider, AuthenticationHandlerInterface $twoFactorAuthenticationHandler, AuthenticationContextFactoryInterface $authenticationContextFactory, RequestStack $requestStack, TrustedDeviceManagerInterface $trustedDeviceManager)
     {
-        /** @phpstan-ignore-next-line */
         parent::__construct($userProvider, $userChecker, $providerKey, $passwordHasherFactory, false);
 
         $this->userChecker = $userChecker;
