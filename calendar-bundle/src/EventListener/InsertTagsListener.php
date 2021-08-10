dc643ebba13fb2c0e6abb99f00ed0ed55ab2fb3f
Martin Auswöger <martin@auswoeger.com>
2021-08-10 15:52:54 +0200

    Add blank argument to news, events and faq bundle

--- calendar-bundle/src/EventListener/InsertTagsListener.php
+++ calendar-bundle/src/EventListener/InsertTagsListener.php
@@ -91,17 +91,19 @@ class InsertTagsListener
         switch ($insertTag) {
             case 'event':
                 return sprintf(
-                    '<a href="%s" title="%s">%s</a>',
+                    '<a href="%s" title="%s"%s>%s</a>',
                     $events->generateEventUrl($model, \in_array('absolute', $arguments, true)) ?: './',
                     StringUtil::specialchars($model->title),
+                    \in_array('blank', $arguments, true) ? ' target="_blank" rel="noreferrer noopener"' : '',
                     $model->title
                 );
 
             case 'event_open':
                 return sprintf(
-                    '<a href="%s" title="%s">',
+                    '<a href="%s" title="%s"%s>',
                     $events->generateEventUrl($model, \in_array('absolute', $arguments, true)) ?: './',
-                    StringUtil::specialchars($model->title)
+                    StringUtil::specialchars($model->title),
+                    \in_array('blank', $arguments, true) ? ' target="_blank" rel="noreferrer noopener"' : ''
                 );
 
             case 'event_url':
--- faq-bundle/src/EventListener/InsertTagsListener.php
+++ faq-bundle/src/EventListener/InsertTagsListener.php
@@ -66,7 +66,7 @@ class InsertTagsListener
             return '';
         }
 
-        return $this->generateReplacement($faq, $key, $url);
+        return $this->generateReplacement($faq, $key, $url, \in_array('blank', \array_slice($elements, 2), true));
     }
 
     /**
@@ -92,22 +92,24 @@ class InsertTagsListener
     /**
      * @return string|false
      */
-    private function generateReplacement(FaqModel $faq, string $key, string $url)
+    private function generateReplacement(FaqModel $faq, string $key, string $url, bool $blank)
     {
         switch ($key) {
             case 'faq':
                 return sprintf(
-                    '<a href="%s" title="%s">%s</a>',
+                    '<a href="%s" title="%s"%s>%s</a>',
                     $url ?: './',
                     StringUtil::specialchars($faq->question),
+                    $blank ? ' target="_blank" rel="noreferrer noopener"' : '',
                     $faq->question
                 );
 
             case 'faq_open':
                 return sprintf(
-                    '<a href="%s" title="%s">',
+                    '<a href="%s" title="%s"%s>',
                     $url ?: './',
-                    StringUtil::specialchars($faq->question)
+                    StringUtil::specialchars($faq->question),
+                    $blank ? ' target="_blank" rel="noreferrer noopener"' : ''
                 );
 
             case 'faq_url':
