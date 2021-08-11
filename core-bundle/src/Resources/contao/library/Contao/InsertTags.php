c28913c4a9a691592323d0210aeeb08216ee5364
Martin Auswöger <martin@auswoeger.com>
2021-08-11 17:45:30 +0200

    Fix iflng and ifnlng insert tags

--- core-bundle/src/Resources/contao/library/Contao/InsertTags.php
+++ core-bundle/src/Resources/contao/library/Contao/InsertTags.php
@@ -136,7 +136,7 @@ class InsertTags extends Controller
 			// Skip certain elements if the output will be cached
 			if ($blnCache)
 			{
-				if ($elements[0] == 'date' || $elements[0] == 'ua' || $elements[0] == 'post' || $elements[1] == 'back' || $elements[1] == 'referer' || \in_array('uncached', $flags) || strncmp($elements[0], 'cache_', 6) === 0)
+				if ($elements[0] == 'date' || $elements[0] == 'ua' || $elements[0] == 'post' || ($elements[1] ?? null) == 'back' || ($elements[1] ?? null) == 'referer' || \in_array('uncached', $flags) || strncmp($elements[0], 'cache_', 6) === 0)
 				{
 					/** @var FragmentHandler $fragmentHandler */
 					$fragmentHandler = $container->get('fragment.handler');
@@ -626,71 +626,41 @@ class InsertTags extends Controller
 
 				// Conditional tags (if)
 				case 'iflng':
-					if (!empty($elements[1]))
+					if (!empty($elements[1]) && !$this->languageMatches($elements[1]))
 					{
-						$langs = StringUtil::trimsplit(',', $elements[1]);
-
-						// Check if there are wildcards (see #8313)
-						foreach ($langs as $k=>$v)
+						// Skip everything until the next iflng tag
+						for (; $_rit<$_cnt; $_rit+=2)
 						{
-							if (substr($v, -1) == '*')
+							if (!empty($tags[$_rit+3]) && \in_array(substr($tags[$_rit+3], 0, 6), array('iflng', 'iflng:', 'iflng|')))
 							{
-								$langs[$k] = substr($v, 0, -1);
-
-								if (\strlen($objPage->language) > 2 && 0 === strncmp($objPage->language, $langs[$k], 2))
-								{
-									$langs[] = $objPage->language;
-								}
-							}
-						}
-
-						if (!\in_array($objPage->language, $langs))
-						{
-							for (; $_rit<$_cnt; $_rit+=2)
-							{
-								if ($tags[$_rit+1] == 'iflng' || $tags[$_rit+1] == 'iflng|urlattr' || $tags[$_rit+1] == 'iflng|attr' || $tags[$_rit+1] == 'iflng::' . $objPage->language)
-								{
-									break;
-								}
+								$tags[$_rit+2] = '';
+								break;
 							}
 						}
 					}
+
+					// iflng does not output anything and the cache must not be used
 					unset($arrCache[$strTag]);
-					break;
+					continue 2;
 
 				// Conditional tags (if not)
 				case 'ifnlng':
-					if (!empty($elements[1]))
+					if (!empty($elements[1]) && $this->languageMatches($elements[1]))
 					{
-						$langs = StringUtil::trimsplit(',', $elements[1]);
-
-						// Check if there are wildcards (see #8313)
-						foreach ($langs as $k=>$v)
+						// Skip everything until the next ifnlng tag
+						for (; $_rit<$_cnt; $_rit+=2)
 						{
-							if (substr($v, -1) == '*')
+							if (!empty($tags[$_rit+3]) && \in_array(substr($tags[$_rit+3], 0, 7), array('ifnlng', 'ifnlng:', 'ifnlng|')))
 							{
-								$langs[$k] = substr($v, 0, -1);
-
-								if (\strlen($objPage->language) > 2 && 0 === strncmp($objPage->language, $langs[$k], 2))
-								{
-									$langs[] = $objPage->language;
-								}
-							}
-						}
-
-						if (\in_array($objPage->language, $langs))
-						{
-							for (; $_rit<$_cnt; $_rit+=2)
-							{
-								if ($tags[$_rit+1] == 'ifnlng' || $tags[$_rit+1] == 'ifnlng|urlattr' || $tags[$_rit+1] == 'ifnlng|attr')
-								{
-									break;
-								}
+								$tags[$_rit+2] = '';
+								break;
 							}
 						}
 					}
+
+					// ifnlng does not output anything and the cache must not be used
 					unset($arrCache[$strTag]);
-					break;
+					continue 2;
 
 				// Environment
 				case 'env':
@@ -1311,6 +1281,28 @@ class InsertTags extends Controller
 		$attributesResult .= substr($attributes, $offset);
 
 		return $attributesResult;
+	}
+
+	private function languageMatches(string $language): bool
+	{
+		global $objPage;
+
+		foreach (StringUtil::trimsplit(',', $language) as $lang)
+		{
+			if ($objPage->language === $lang)
+			{
+				return true;
+			}
+
+			if (
+				substr($lang, -1) === '*'
+				&& 0 === strncmp($objPage->language, $lang, \strlen($lang) - 1)
+			) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
 
