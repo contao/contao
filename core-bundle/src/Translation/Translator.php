79d8e3ee116da39c7fd6d839a81969dfd08daeef
fritzmg <fmg@inspiredminds.at>
2021-08-23 17:52:13 +0100

    remove deprecation notice from translator

--- core-bundle/src/Translation/Translator.php
+++ core-bundle/src/Translation/Translator.php
@@ -58,10 +58,6 @@ class Translator implements TranslatorInterface, TranslatorBagInterface, LocaleA
             $locale = $this->translator->getLocale();
         }
 
-        if ('MSC.textDirection' === $id && 'contao_default' === $domain) {
-            @trigger_error('Using "MSC.textDirection" to retrieve the locale\'s text direction will no longer work in Contao 5.0. Use the PHP intl library instead.', E_USER_DEPRECATED);
-        }
-
         $this->framework->initialize();
         $this->loadLanguageFile(substr($domain, 7), $locale);
 
