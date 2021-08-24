435ac064539c226034e0730477107f7998e0710b
Martin Auswöger <martin@auswoeger.com>
2021-08-24 10:44:27 +0200

    Fix dynamic debug mode for templates (see #3353)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #3351
    
    Commits
    -------
    
    74dd0d6e Fix dynamic debug mode for templates

--- core-bundle/src/Resources/contao/dca/tl_templates.php
+++ core-bundle/src/Resources/contao/dca/tl_templates.php
@@ -504,9 +504,6 @@ class tl_templates extends Backend
 		$objTemplate->title = StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['showDifferences']);
 		$objTemplate->charset = System::getContainer()->getParameter('kernel.charset');
 
-		System::getContainer()->setParameter('kernel.debug', false);
-		$GLOBALS['TL_CONFIG']['debugMode'] = false;
-
 		throw new ResponseException($objTemplate->getResponse());
 	}
 
