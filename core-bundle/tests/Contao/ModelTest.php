cfbf8f815ded75cde75ac0eaff6ca58cd8383745
Severin Glöckle <gloeckle@pixlscript.de>
2021-08-28 16:07:01 +0200

    show table name in model relation error

---
+++ core-bundle/tests/Contao/ModelTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Contao\CoreBundle\Tests\Contao;
+
+
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\System;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @group legacy
+ */
+class ModelTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $container = $this->getContainerWithContaoConfiguration();
+        $container->setParameter('contao.resources_paths', $this->getTempDir());
+
+        (new Filesystem())->mkdir($this->getTempDir() . '/languages/en');
+
+        System::setContainer($container);
+    }
+
+    public function testThrowsExceptionForNonExistantRelations()
+    {
+        $this->expectException(\Exception::class);
+
+        // ContentModel could be any other Model class extending from \Contao\Model
+        $model = new \Contao\ContentModel();
+        $model->getRelated('foo');
+    }
+
+    /**
+     * @dataProvider brokenRelationsProvider
+     */
+    public function testIncludesTableAndRelationNameForNonExistantRelationsInException(string $table, string $relation)
+    {
+        // We expect to find the table name and the relation in the exception,
+        // e.g. tl_foo.bar for the relation "bar" of table "tl_foo".
+        $expectedExceptionContent = $table . '.' . $relation;
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageMatches('/' . preg_quote($expectedExceptionContent) . '/');
+
+        // Sadly, we cannot use a mock of \Contao\Model for abstract testing
+        // since PHPUnit mocks cannot call or mock static methods (in this case,
+        // static::getTable() within the Model), so use a real model
+        // class and dynamically set the table name via reflection.
+
+        // ContentModel could be any other Model class extending from \Contao\Model
+        $mock       = new \Contao\ContentModel();
+        $reflection = new \ReflectionClass($mock);
+        $reflection->setStaticPropertyValue('strTable', $table);
+
+        $mock->getRelated($relation);
+    }
+
+    public function brokenRelationsProvider(): \Generator
+    {
+        yield 'tl_foo.bar' => [
+            'tl_foo',
+            'bar'
+        ];
+
+        yield 'tl_bar.foo' => [
+            'tl_bar',
+            'foo'
+        ];
+    }
+}
