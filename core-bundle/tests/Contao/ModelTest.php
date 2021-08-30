6f02dadd22ded08d2cd64569e485cf7d368df09c
Severin Glöckle <gloeckle@pixlscript.de>
2021-08-30 16:09:19 +0200

    fix coding standard

--- core-bundle/tests/Contao/ModelTest.php
+++ core-bundle/tests/Contao/ModelTest.php
@@ -1,7 +1,16 @@
 <?php
 
-namespace Contao\CoreBundle\Tests\Contao;
+declare(strict_types=1);
 
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Contao;
 
 use Contao\CoreBundle\Tests\TestCase;
 use Contao\System;
@@ -12,19 +21,19 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class ModelTest extends TestCase
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
         $container = $this->getContainerWithContaoConfiguration();
         $container->setParameter('contao.resources_paths', $this->getTempDir());
 
-        (new Filesystem())->mkdir($this->getTempDir() . '/languages/en');
+        (new Filesystem())->mkdir($this->getTempDir().'/languages/en');
 
         System::setContainer($container);
     }
 
-    public function testThrowsExceptionForNonExistantRelations()
+    public function testThrowsExceptionForNonExistantRelations(): void
     {
         $this->expectException(\Exception::class);
 
@@ -36,13 +45,13 @@ class ModelTest extends TestCase
     /**
      * @dataProvider brokenRelationsProvider
      */
-    public function testIncludesTableAndRelationNameForNonExistantRelationsInException(string $table, string $relation)
+    public function testIncludesTableAndRelationNameForNonExistantRelationsInException(string $table, string $relation): void
     {
         // We expect to find the table name and the relation in the exception,
         // e.g. tl_foo.bar for the relation "bar" of table "tl_foo".
-        $expectedExceptionContent = $table . '.' . $relation;
+        $expectedExceptionContent = $table.'.'.$relation;
         $this->expectException(\Exception::class);
-        $this->expectExceptionMessageMatches('/' . preg_quote($expectedExceptionContent) . '/');
+        $this->expectExceptionMessageMatches('/'.preg_quote($expectedExceptionContent).'/');
 
         // Sadly, we cannot use a mock of \Contao\Model for abstract testing
         // since PHPUnit mocks cannot call or mock static methods (in this case,
@@ -50,7 +59,7 @@ class ModelTest extends TestCase
         // class and dynamically set the table name via reflection.
 
         // ContentModel could be any other Model class extending from \Contao\Model
-        $mock       = new \Contao\ContentModel();
+        $mock = new \Contao\ContentModel();
         $reflection = new \ReflectionClass($mock);
         $reflection->setStaticPropertyValue('strTable', $table);
 
@@ -61,12 +70,12 @@ class ModelTest extends TestCase
     {
         yield 'tl_foo.bar' => [
             'tl_foo',
-            'bar'
+            'bar',
         ];
 
         yield 'tl_bar.foo' => [
             'tl_bar',
-            'foo'
+            'foo',
         ];
     }
 }
