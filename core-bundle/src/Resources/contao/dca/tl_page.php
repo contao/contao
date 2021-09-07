4207c6c383e43dd2ec673d0d7913fc9659de486e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2021-09-07 08:58:04 +0100

    Handle `%` characters in the SERP preview (see #3422)
    Description
    -----------
    
    If you use a `%` character in your website root title, the following warning will occur when trying to insert a new page or editing a page within that website root:
    
    ```
    ErrorException:
    Warning: sprintf(): Too few arguments
    
      at vendor\contao\contao\core-bundle\src\Resources\contao\widgets\SerpPreview.php:73
      at Contao\SerpPreview->generate()
         (vendor\contao\contao\core-bundle\src\Resources\contao\library\Contao\Widget.php:651)
      at Contao\Widget->generateWithError(true)
         (vendor\contao\contao\core-bundle\src\Resources\contao\templates\backend\be_widget.html5:3)
      at include('vendor/contao/contao/core-bundle/src/Resources/contao/templates/backend/be_widget.html5')
         (vendor\contao\contao\core-bundle\src\Resources\contao\library\Contao\TemplateInheritance.php:100)
      at Contao\Widget->inherit()
         (vendor\contao\contao\core-bundle\src\Resources\contao\library\Contao\Widget.php:598)
      at Contao\Widget->parse()
         (vendor\contao\contao\core-bundle\src\Resources\contao\classes\DataContainer.php:668)
      at Contao\DataContainer->row('{title_legend},title,alias,type;{meta_legend},pageTitle,robots,description,serpPreview;{protected_legend:hide},protected;{layout_legend:hide},includeLayout;{cache_legend:hide},includeCache;{chmod_legend:hide},includeChmod;{expert_legend:hide},cssClass,sitemap,hide,noSearch,guests,requireItem;{tabnav_legend:hide},tabindex,accesskey;{publish_legend},published,start,stop')
         (vendor\contao\contao\core-bundle\src\Resources\contao\drivers\DC_Table.php:1985)
      at Contao\DC_Table->edit()
         (vendor\contao\contao\core-bundle\src\Resources\contao\classes\Backend.php:644)
      at Contao\Backend->getBackendModule('page', null)
         (vendor\contao\contao\core-bundle\src\Resources\contao\controllers\BackendMain.php:167)
      at Contao\BackendMain->run()
         (vendor\contao\contao\core-bundle\src\Controller\BackendController.php:48)
      at Contao\CoreBundle\Controller\BackendController->mainAction()
         (vendor\symfony\http-kernel\HttpKernel.php:158)
      at Symfony\Component\HttpKernel\HttpKernel->handleRaw(object(Request), 1)
         (vendor\symfony\http-kernel\HttpKernel.php:80)
      at Symfony\Component\HttpKernel\HttpKernel->handle(object(Request), 1, true)
         (vendor\symfony\http-kernel\Kernel.php:201)
      at Symfony\Component\HttpKernel\Kernel->handle(object(Request))
         (web\index.php:31)
      at require('web/index.php')
         (web\app.php:4)         
    ```
    
    This PR fixes this by escaping any `%` character with another `%` character for `sprintf`.
    
    Commits
    -------
    
    9b006ed4 fix SerpPreview when using %
    252edff9 use preg_replace
    787b1350 add global flag
    583c3733 fix title_tag_callback urls
    63a105cc cs fix and replace %% in JS

--- core-bundle/src/Resources/contao/dca/tl_page.php
+++ core-bundle/src/Resources/contao/dca/tl_page.php
@@ -1018,7 +1018,16 @@ class tl_page extends Contao\Backend
 		// Set the global page object so we can replace the insert tags
 		$objPage = $model;
 
-		return self::replaceInsertTags(str_replace('{{page::pageTitle}}', '%s', $layout->titleTag ?: '{{page::pageTitle}} - {{page::rootPageTitle}}'));
+		return implode(
+			'%s',
+			array_map(
+				static function ($strVal)
+				{
+					return str_replace('%', '%%', self::replaceInsertTags($strVal));
+				},
+				explode('{{page::pageTitle}}', $layout->titleTag ?: '{{page::pageTitle}} - {{page::rootPageTitle}}', 2)
+			)
+		);
 	}
 
 	/**
