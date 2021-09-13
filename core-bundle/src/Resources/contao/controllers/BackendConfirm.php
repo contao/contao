58952ed1075520724647a6f56fdd69ebdeab2c4c
Leo Feyer <github@contao.org>
2021-09-13 13:18:06 +0200

    Add a label for "edit multiple" to the deep link confirmation screen (see #3462)
    Description
    -----------
    
    | Q                | A
    | -----------------| ---
    | Fixed issues     | Fixes #3377
    | Docs PR or issue | -
    
    Commits
    -------
    
    b4dbd589 Add a label for "edit multiple" to the deep link confirmation screen

--- core-bundle/src/Resources/contao/controllers/BackendConfirm.php
+++ core-bundle/src/Resources/contao/controllers/BackendConfirm.php
@@ -129,7 +129,11 @@ class BackendConfirm extends Backend
 				$arrInfo['act'] = $GLOBALS['TL_LANG'][$arrInfo['table']][$arrInfo['mode']][0];
 			}
 		}
-		else
+		elseif ($arrInfo['act'] == 'select' && isset($GLOBALS['TL_LANG']['MSC']['all']))
+		{
+			$arrInfo['act'] = \is_array($GLOBALS['TL_LANG']['MSC']['all']) ? $GLOBALS['TL_LANG']['MSC']['all'][0] : $GLOBALS['TL_LANG']['MSC']['all'];
+		}
+		elseif (!empty($GLOBALS['TL_LANG'][$arrInfo['table']][$arrInfo['act']]))
 		{
 			$arrInfo['act'] = \is_array($GLOBALS['TL_LANG'][$arrInfo['table']][$arrInfo['act']]) ? $GLOBALS['TL_LANG'][$arrInfo['table']][$arrInfo['act']][0] : $GLOBALS['TL_LANG'][$arrInfo['table']][$arrInfo['act']];
 		}
