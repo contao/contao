f050bdcbb91020086e0608d0254b25bebb30897d
Leo Feyer <github@contao.org>
2021-09-13 11:05:45 +0200

    Fix the position of the pagination menu in "edit multiple" mode

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -348,12 +348,6 @@ class DC_Table extends DataContainer implements \listable, \editable
 
 			$return .= $this->panel();
 			$return .= ($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['mode'] == 4) ? $this->parentView() : $this->listView();
-
-			// Add another panel at the end of the page
-			if (strpos($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['panelLayout'], 'limit') !== false)
-			{
-				$return .= $this->paginationMenu();
-			}
 		}
 
 		return $return;
@@ -4597,7 +4591,13 @@ class DC_Table extends DataContainer implements \listable, \editable
 </div>' : '') . '
 </div>';
 
-		// Close form
+		// Add another panel at the end of the page
+		if (strpos($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['panelLayout'], 'limit') !== false)
+		{
+			$return .= $this->paginationMenu();
+		}
+
+		// Close the form
 		if (Input::get('act') == 'select')
 		{
 			// Submit buttons
@@ -5065,6 +5065,12 @@ class DC_Table extends DataContainer implements \listable, \editable
 <label for="tl_radio_reset" class="tl_radio_label">' . $GLOBALS['TL_LANG']['MSC']['resetSelected'] . '</label> <input type="radio" name="picker" id="tl_radio_reset" value="" class="tl_tree_radio">
 </div>' : '') . '
 </div>';
+
+			// Add another panel at the end of the page
+			if (strpos($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['panelLayout'], 'limit') !== false)
+			{
+				$return .= $this->paginationMenu();
+			}
 
 			// Close the form
 			if (Input::get('act') == 'select')
