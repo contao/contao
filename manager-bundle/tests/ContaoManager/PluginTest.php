d3dbf5c54dc327541b504bd84cfa73dc4f9f1543
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-10-12 09:09:03 +0200

    CS

--- manager-bundle/tests/ContaoManager/PluginTest.php
+++ manager-bundle/tests/ContaoManager/PluginTest.php
@@ -57,7 +57,7 @@ class PluginTest extends ContaoTestCase
         unset($_SERVER['DATABASE_URL'], $_SERVER['APP_SECRET'], $_ENV['DATABASE_URL']);
     }
 
-    public function testDependsOnCoreBundlePlugin()
+    public function testDependsOnCoreBundlePlugin(): void
     {
         $plugin = new Plugin();
 
