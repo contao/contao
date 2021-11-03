dce423aefa892f60ed320aeb2d56e0a515be1f04
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-11-03 14:38:44 +0100

    CS

--- core-bundle/src/EventListener/CommandSchedulerListener.php
+++ core-bundle/src/EventListener/CommandSchedulerListener.php
@@ -67,6 +67,7 @@ class CommandSchedulerListener implements ServiceSubscriberInterface
 
     /**
      * Lazy-load services to prevent issues with MySQL server_version.
+     *
      * @see https://github.com/contao/contao/pull/3623
      *
      * @return string[]
