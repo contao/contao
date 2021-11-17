619b670fa6a05c9da27b0a2fab28185751dfd368
Leo Feyer <github@contao.org>
2021-11-17 11:08:07 +0100

    Do not use FQCN service IDs for non-autowiring services

--- core-bundle/src/DependencyInjection/ContaoCoreExtension.php
+++ core-bundle/src/DependencyInjection/ContaoCoreExtension.php
@@ -55,11 +55,7 @@ class ContaoCoreExtension extends Extension
         $configuration = new Configuration($projectDir);
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
-
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('commands.yml');
         $loader->load('controller.yml');
         $loader->load('listener.yml');
