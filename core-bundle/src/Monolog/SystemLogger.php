f2a9fea245db240910bd0df70395af8701985f80
Severin Glöckle <gloeckle@pixlscript.de>
2021-11-18 20:44:28 +0100

    add additional utility methods

--- core-bundle/src/Monolog/SystemLogger.php
+++ core-bundle/src/Monolog/SystemLogger.php
@@ -24,34 +24,65 @@ final class SystemLogger
         $this->logger = $logger;
     }
 
-    public function info(string $message, string $method = null): void
+    public function access(string $message, string $method = null): void
     {
-        $this->logWithContaoContext(LogLevel::INFO, $message, ContaoContext::GENERAL, $method);
+        $this->log(ContaoContext::ACCESS, $message, $method ?? $this->getCallerMethod());
+    }
+
+    public function configuration(string $message, string $method = null): void
+    {
+        $this->log(ContaoContext::CONFIGURATION, $message, $method ?? $this->getCallerMethod());
+    }
+
+    public function cron(string $message, string $method = null): void
+    {
+        $this->log(ContaoContext::CRON, $message, $method ?? $this->getCallerMethod());
+    }
+
+    public function email(string $message, string $method = null): void
+    {
+        $this->log(ContaoContext::EMAIL, $message, $method ?? $this->getCallerMethod());
     }
 
     public function error(string $message, string $method = null): void
     {
-        $this->logWithContaoContext(LogLevel::ERROR, $message, ContaoContext::ERROR, $method);
+        $this->log(ContaoContext::ERROR, $message, $method ?? $this->getCallerMethod(), LogLevel::ERROR);
     }
 
-    public function log(string $action, string $message, string $method = null): void
+    public function files(string $message, string $method = null): void
     {
-        $this->logWithContaoContext(LogLevel::INFO, $message, $action, $method);
+        $this->log(ContaoContext::FILES, $message, $method ?? $this->getCallerMethod());
     }
 
-    private function logWithContaoContext(string $level, string $message, string $action = ContaoContext::GENERAL, string $method = null): void
+    public function forms(string $message, string $method = null): void
     {
-        $method = $method ?? $this->getCallerMethod();
+        $this->log(ContaoContext::FORMS, $message, $method ?? $this->getCallerMethod());
+    }
 
-        $context = new ContaoContext($method, $action);
+    public function general(string $message, string $method = null): void
+    {
+        $this->info($message, $method ?? $this->getCallerMethod());
+    }
 
-        $this->logger->log($level, $message, ['contao' => $context]);
+    public function info(string $message, string $method = null): void
+    {
+        $this->log(ContaoContext::GENERAL, $message, $method ?? $this->getCallerMethod());
+    }
+
+    public function log(string $action, string $message, string $method = null, string $level = null): void
+    {
+        $this->logWithContaoContext($level ?? LogLevel::INFO, $message, $action, $method ?? $this->getCallerMethod());
+    }
+
+    private function logWithContaoContext(string $level, string $message, string $action, string $method): void
+    {
+        $this->logger->log($level, $message, ['contao' => new ContaoContext($method, $action)]);
     }
 
     private function getCallerMethod(): string
     {
-        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 4);
-        $caller = $trace[3] ?? null;
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 3);
+        $caller = $trace[2] ?? null;
 
         return $caller ? $caller['class'].'::'.$caller['function'] : '';
     }
--- core-bundle/tests/Monolog/SystemLoggerTest.php
+++ core-bundle/tests/Monolog/SystemLoggerTest.php
@@ -21,64 +21,147 @@ use Psr\Log\LogLevel;
 
 class SystemLoggerTest extends TestCase
 {
-    public function testSetsContaoContextForInfo(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForAccess(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext(__METHOD__, ContaoContext::GENERAL);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, 'Generic log message', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::ACCESS);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
 
-        (new SystemLogger($logger))->info('Generic log message');
+        (new SystemLogger($logger))->access($message, $method);
     }
 
-    public function testSetsContaoContextForError(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForConfiguration(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext(__METHOD__, ContaoContext::ERROR);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::ERROR, 'Error message', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::CONFIGURATION);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
 
-        (new SystemLogger($logger))->error('Error message');
+        (new SystemLogger($logger))->configuration($message, $method);
     }
 
-    public function testSetsContaoContextForLogWithCustomAction(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForCron(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext(__METHOD__, ContaoContext::CRON);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, 'Message with custom action CRON', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::CRON);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
 
-        (new SystemLogger($logger))->log(ContaoContext::CRON, 'Message with custom action CRON');
+        (new SystemLogger($logger))->cron($message, $method);
     }
 
-    public function testAllowsCustomMethodNameForInfo(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForEmail(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext('MyClass::myMethod', ContaoContext::GENERAL);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, 'Generic log message', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::EMAIL);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
 
-        (new SystemLogger($logger))->info('Generic log message', 'MyClass::myMethod');
+        (new SystemLogger($logger))->email($message, $method);
     }
 
-    public function testAllowsCustomMethodNameForError(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForError(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext('MyClass::myMethod', ContaoContext::ERROR);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::ERROR, 'Error message', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::ERROR);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::ERROR, $message, $expectedContext);
 
-        (new SystemLogger($logger))->error('Error message', 'MyClass::myMethod');
+        (new SystemLogger($logger))->error($message, $method);
     }
 
-    public function testAllowsCustomMethodNameForLog(): void
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForFiles(string $message, string $method = null): void
     {
         $logger = $this->createMock(LoggerInterface::class);
 
-        $expectedContext = new ContaoContext('MyClass::myMethod', ContaoContext::CRON);
-        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, 'Message with custom action CRON', $expectedContext);
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::FILES);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
 
-        (new SystemLogger($logger))->log(ContaoContext::CRON, 'Message with custom action CRON', 'MyClass::myMethod');
+        (new SystemLogger($logger))->files($message, $method);
+    }
+
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForForms(string $message, string $method = null): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::FORMS);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
+
+        (new SystemLogger($logger))->forms($message, $method);
+    }
+
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForGeneral(string $message, string $method = null): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::GENERAL);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
+
+        (new SystemLogger($logger))->general($message, $method);
+    }
+
+    /**
+     * @dataProvider logMessageProvider
+     */
+    public function testSetsContaoContextForInfo(string $message, string $method = null): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $expectedContext = new ContaoContext($method ?? __METHOD__, ContaoContext::GENERAL);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
+
+        (new SystemLogger($logger))->info($message, $method);
+    }
+
+    /**
+     * @dataProvider logMessageWithActionProvider
+     */
+    public function testSetsContaoContextForLogWithCustomAction(string $action, string $message, string $method = null): void
+    {
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $expectedContext = new ContaoContext($method ?? __METHOD__, $action);
+        $this->assertLogActionWithContaoContext($logger, LogLevel::INFO, $message, $expectedContext);
+
+        (new SystemLogger($logger))->log($action, $message, $method);
+    }
+
+    public function logMessageProvider(): \Generator
+    {
+        yield 'without method' => ['Log message', null];
+        yield 'with custom method' => ['Log message', 'MyClass::myMethod'];
+    }
+
+    public function logMessageWithActionProvider(): \Generator
+    {
+        yield 'action from ContaoContext' => [ContaoContext::ACCESS, 'Log message', null];
+        yield 'custom action' => ['FooAction', 'Log message', null];
+        yield 'custom action and custom method' => ['FooAction', 'Log message', 'MyClass::myMethod'];
     }
 
     private function assertLogActionWithContaoContext(MockObject $logger, string $level, string $message, ContaoContext $expectedContext): void
