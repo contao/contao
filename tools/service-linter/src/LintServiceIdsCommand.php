c3f7f6ac5f60a662e5daedaa7b9bf03cd09a3caa
Leo Feyer <github@contao.org>
2021-11-23 11:12:26 +0100

    Use a separate namespace for tools

--- tools/service-linter/src/LintServiceIdsCommand.php
+++ tools/service-linter/src/LintServiceIdsCommand.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\ServiceIdLinter;
+namespace Contao\Tools\ServiceIdLinter;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
