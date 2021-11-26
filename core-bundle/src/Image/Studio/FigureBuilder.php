2e46f7a8ef9084c1ecd417917cf7717745d76c99
Leo Feyer <github@contao.org>
2021-11-26 15:21:18 +0100

    Adjust the FigureBuilder::setLinkAttribute() method

--- core-bundle/src/Image/Studio/FigureBuilder.php
+++ core-bundle/src/Image/Studio/FigureBuilder.php
@@ -351,10 +351,8 @@ class FigureBuilder
      *
      * Set the value to null to remove it. If you want to explicitly remove an
      * auto-generated value from the results, set the $forceRemove flag to true.
-     *
-     * @param string|int|bool $forceRemove
      */
-    public function setLinkAttribute(string $attribute, ?string $value, $forceRemove = false): self
+    public function setLinkAttribute(string $attribute, ?string $value, bool $forceRemove = false): self
     {
         if (null !== $value || $forceRemove) {
             $this->additionalLinkAttributes[$attribute] = $value;
