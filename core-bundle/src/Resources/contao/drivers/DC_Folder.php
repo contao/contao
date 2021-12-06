5ee6bc5cd93e658bb4eb88b7a73bd607e7c364cd
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2021-12-06 17:51:45 +0100

    Add Chosen to select menus in the backend DCA filters (see #3660)
    Description
    -----------
    
    This pull request adds the Chosen plugin to select menus (to both, filter dropdowns and the search dropdown) in the backend DCA filters. Screenshot:
    
    <img width="729" alt="Zrzut ekranu 2021-11-10 o 11 03 43" src="https://user-images.githubusercontent.com/193483/141093334-35203ae1-db5b-42af-b435-3af1712480b3.png">
    
    One thing to consider is that it is possible to hit the "Enter" key and submit the form with the current regular select menus. This is not yet the case with the "Chosen" plugin, but we might want to add that.
    
    Feature proposal per @Toflar request.
    
    Commits
    -------
    
    8ce0ae64 Add Chosen to select menus in the backend DCA filters
    b4ab9268 Merge branch '4.x' into feature/dca-filters-chosen
    867c8817 Add Chosen to select menus to sorting and limit dropdowns
    15a8c412 Add Chosen to search menu in DC_Folder

--- core-bundle/src/Resources/contao/drivers/DC_Folder.php
+++ core-bundle/src/Resources/contao/drivers/DC_Folder.php
@@ -2992,7 +2992,7 @@ class DC_Folder extends DataContainer implements ListableDataContainerInterface,
 		return '
     <div class="tl_search tl_subpanel">
       <strong>' . $GLOBALS['TL_LANG']['MSC']['search'] . ':</strong>
-      <select name="tl_field" class="tl_select' . ($active ? ' active' : '') . '">
+      <select name="tl_field" class="tl_select tl_chosen' . ($active ? ' active' : '') . '">
         <option value="name">' . ($GLOBALS['TL_DCA'][$this->strTable]['fields']['name']['label'][0] ?: (\is_array($GLOBALS['TL_LANG']['MSC']['name'] ?? null) ? $GLOBALS['TL_LANG']['MSC']['name'][0] : ($GLOBALS['TL_LANG']['MSC']['name'] ?? null))) . '</option>
       </select>
       <span>=</span>
