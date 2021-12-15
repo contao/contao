16720607ef7c38172538e4ef38d788754fb0ca97
David Greminger <david.greminger@1up.io>
2021-12-15 22:26:53 +0100

    Fix CS

--- core-bundle/src/Resources/contao/dca/tl_module.php
+++ core-bundle/src/Resources/contao/dca/tl_module.php
@@ -11,7 +11,6 @@
 use Contao\Backend;
 use Contao\BackendUser;
 use Contao\Controller;
-use Contao\CoreBundle\EventListener\DataContainer\RootPageDependentModuleListener;
 use Contao\CoreBundle\Exception\AccessDeniedException;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\DataContainer;
