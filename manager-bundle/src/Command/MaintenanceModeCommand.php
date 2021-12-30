155a85ce6774f1a7a25dd9c56567438e67113ab3
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-12-30 10:23:31 +0100

    Improve the maintenance mode command

--- manager-bundle/src/Command/MaintenanceModeCommand.php
+++ manager-bundle/src/Command/MaintenanceModeCommand.php
@@ -43,25 +43,59 @@ class MaintenanceModeCommand extends Command
     {
         $this
             ->setName('contao:maintenance-mode')
-            ->addArgument('state', InputArgument::REQUIRED, 'Use "enable" to enable and "disable" to disable the maintenance mode. If the state is already the desired one, nothing happens. You can also use "on" and "off".')
+            ->setAliases(['lexik:maintenance:lock', 'lexik:maintenance:unlock'])
+            ->addArgument('state', InputArgument::OPTIONAL, 'Use "enable" to enable and "disable" to disable the maintenance mode. If the state is already the desired one, nothing happens. You can also use "on" and "off".')
             ->addOption('template', 't', InputOption::VALUE_REQUIRED, 'Allows to take a different Twig template name when enabling the maintenance mode.', '@ContaoCore/Error/service_unavailable.html.twig')
             ->addOption('templateVars', null, InputOption::VALUE_OPTIONAL, 'Add custom template variables to the Twig template when enabling the maintenance mode (provide as JSON).', '{}')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format (txt, json)', 'txt')
             ->setDescription('Changes the state of the system maintenance mode.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $shouldEnable = \in_array($input->getArgument('state'), ['enable', 'on'], true);
+        $state = $input->getArgument('state');
+
+        if ('lexik:maintenance:lock' === $input->getFirstArgument()) {
+            $state = 'enable';
+        } elseif ('lexik:maintenance:unlock' === $input->getFirstArgument()) {
+            $state = 'disable';
+        }
+
         $io = new SymfonyStyle($input, $output);
 
-        if ($shouldEnable) {
+        if (\in_array($state, ['enable', 'on'], true)) {
             $this->enable($input->getOption('template'), $input->getOption('templateVars'));
             $io->success('Maintenance mode enabled.');
-        } else {
+
+            return 0;
+        }
+
+        if (\in_array($state, ['disable', 'off'], true)) {
             $this->disable();
             $io->success('Maintenance mode disabled.');
+
+            return 0;
         }
+
+        $isEnabled = $this->filesystem->exists($this->maintenanceFilePath);
+
+        if ('json' === $input->getOption('format')) {
+            $output->writeln(json_encode([
+                'enabled' => $isEnabled,
+                'maintenanceFilePath' => $this->maintenanceFilePath,
+            ], JSON_THROW_ON_ERROR));
+
+            return 0;
+        }
+
+        if ($isEnabled) {
+            $io->note('Maintenance mode is enabled.');
+
+            return 1;
+        }
+
+        $io->info('Maintenance mode is disabled.');
 
         return 0;
     }
