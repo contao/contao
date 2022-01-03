f6b8d50ce7dacf34fee865cf4f192b455c130d27
David Greminger <david.greminger@1up.io>
2022-01-03 17:53:26 +0100

    Remove obsolete registration

--- core-bundle/src/Resources/contao/config/config.php
+++ core-bundle/src/Resources/contao/config/config.php
@@ -114,7 +114,6 @@ use Contao\Picker;
 use Contao\PurgeData;
 use Contao\RadioButton;
 use Contao\RadioTable;
-use Contao\RootPageDependentModule;
 use Contao\SectionWizard;
 use Contao\SelectMenu;
 use Contao\SerpPreview;
@@ -333,35 +332,34 @@ $GLOBALS['TL_CTE'] = array
 // Back end form fields
 $GLOBALS['BE_FFL'] = array
 (
-	'text'                    => TextField::class,
-	'password'                => Password::class,
-	'textStore'               => TextStore::class,
-	'textarea'                => TextArea::class,
-	'select'                  => SelectMenu::class,
-	'checkbox'                => CheckBox::class,
-	'checkboxWizard'          => CheckBoxWizard::class,
-	'radio'                   => RadioButton::class,
-	'radioTable'              => RadioTable::class,
-	'inputUnit'               => InputUnit::class,
-	'trbl'                    => TrblField::class,
-	'chmod'                   => ChmodTable::class,
-	'picker'                  => Picker::class,
-	'pageTree'                => PageTree::class,
-	'pageSelector'            => PageSelector::class,
-	'fileTree'                => FileTree::class,
-	'fileSelector'            => FileSelector::class,
-	'fileUpload'              => Upload::class,
-	'tableWizard'             => TableWizard::class,
-	'listWizard'              => ListWizard::class,
-	'optionWizard'            => OptionWizard::class,
-	'moduleWizard'            => ModuleWizard::class,
-	'keyValueWizard'          => KeyValueWizard::class,
-	'imageSize'               => ImageSize::class,
-	'timePeriod'              => TimePeriod::class,
-	'metaWizard'              => MetaWizard::class,
-	'sectionWizard'           => SectionWizard::class,
-	'serpPreview'             => SerpPreview::class,
-	'rootPageDependentModule' => RootPageDependentModule::class
+	'text'           => TextField::class,
+	'password'       => Password::class,
+	'textStore'      => TextStore::class,
+	'textarea'       => TextArea::class,
+	'select'         => SelectMenu::class,
+	'checkbox'       => CheckBox::class,
+	'checkboxWizard' => CheckBoxWizard::class,
+	'radio'          => RadioButton::class,
+	'radioTable'     => RadioTable::class,
+	'inputUnit'      => InputUnit::class,
+	'trbl'           => TrblField::class,
+	'chmod'          => ChmodTable::class,
+	'picker'         => Picker::class,
+	'pageTree'       => PageTree::class,
+	'pageSelector'   => PageSelector::class,
+	'fileTree'       => FileTree::class,
+	'fileSelector'   => FileSelector::class,
+	'fileUpload'     => Upload::class,
+	'tableWizard'    => TableWizard::class,
+	'listWizard'     => ListWizard::class,
+	'optionWizard'   => OptionWizard::class,
+	'moduleWizard'   => ModuleWizard::class,
+	'keyValueWizard' => KeyValueWizard::class,
+	'imageSize'      => ImageSize::class,
+	'timePeriod'     => TimePeriod::class,
+	'metaWizard'     => MetaWizard::class,
+	'sectionWizard'  => SectionWizard::class,
+	'serpPreview'    => SerpPreview::class
 );
 
 // Front end form fields
