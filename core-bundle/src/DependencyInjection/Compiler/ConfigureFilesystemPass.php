33156654b73f03ce2668c2ddafeef428f7913379
M. Vondano <m-vo@users.noreply.github.com>
2022-01-05 11:59:53 +0100

    use a single config instance in the compiler pass

--- core-bundle/src/DependencyInjection/Compiler/ConfigureFilesystemPass.php
+++ core-bundle/src/DependencyInjection/Compiler/ConfigureFilesystemPass.php
@@ -16,15 +16,31 @@ use Contao\CoreBundle\DependencyInjection\Filesystem\ConfigureFilesystemInterfac
 use Contao\CoreBundle\DependencyInjection\Filesystem\FilesystemConfig;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class ConfigureFilesystemPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        foreach ($container->getExtensions() as $extension) {
-            if ($extension instanceof ConfigureFilesystemInterface) {
-                $extension->configureFilesystem(new FilesystemConfig($container));
-            }
+        if (empty($extensions = $this->getExtensionsThatConfigureTheFilesystem($container))) {
+            return;
         }
+
+        $config = new FilesystemConfig($container);
+
+        foreach ($extensions as $extension) {
+            $extension->configureFilesystem($config);
+        }
+    }
+
+    /**
+     * @return array<ConfigureFilesystemInterface>
+     */
+    private function getExtensionsThatConfigureTheFilesystem(ContainerBuilder $container): array
+    {
+        return array_filter(
+            $container->getExtensions(),
+            static fn (ExtensionInterface $extension): bool => $extension instanceof ConfigureFilesystemInterface
+        );
     }
 }
