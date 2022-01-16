ad9dbb73cba25a9576274e0d4a1eadbe43797e19
Severin Glöckle <gloeckle@pixlscript.de>
2022-01-16 15:35:13 +0100

    implement ResetInterface for LayoutOptionsListener

--- core-bundle/src/EventListener/DataContainer/LayoutOptionsListener.php
+++ core-bundle/src/EventListener/DataContainer/LayoutOptionsListener.php
@@ -14,16 +14,17 @@ namespace Contao\CoreBundle\EventListener\DataContainer;
 
 use Contao\CoreBundle\ServiceAnnotation\Callback;
 use Doctrine\DBAL\Connection;
+use Symfony\Contracts\Service\ResetInterface;
 
 /**
  * @Callback(table="tl_page", target="fields.layout.options")
  * @Callback(table="tl_page", target="fields.subpagesLayout.options")
  */
-class LayoutOptionsListener
+class LayoutOptionsListener implements ResetInterface
 {
     private Connection $connection;
 
-    private ?array $layouts = null;
+    private ?array $options = null;
 
     public function __construct(Connection $connection)
     {
@@ -32,15 +33,20 @@ class LayoutOptionsListener
 
     public function __invoke(): array
     {
-        if (null === $this->layouts) {
-            $this->layouts = [];
+        if (null === $this->options) {
+            $this->options = [];
             $layouts = $this->connection->fetchAllAssociative('SELECT l.id, l.name, t.name AS theme FROM tl_layout l LEFT JOIN tl_theme t ON l.pid=t.id ORDER BY t.name, l.name');
 
             foreach ($layouts as $layout) {
-                $this->layouts[$layout['theme']][$layout['id']] = $layout['name'];
+                $this->options[$layout['theme']][$layout['id']] = $layout['name'];
             }
         }
 
-        return $this->layouts;
+        return $this->options;
+    }
+
+    public function reset(): void
+    {
+        $this->options = null;
     }
 }
--- core-bundle/tests/EventListener/DataContainer/LayoutOptionsListenerTest.php
+++ core-bundle/tests/EventListener/DataContainer/LayoutOptionsListenerTest.php
@@ -15,8 +15,9 @@ namespace Contao\CoreBundle\Tests\EventListener\DataContainer;
 use Contao\CoreBundle\EventListener\DataContainer\LayoutOptionsListener;
 use Contao\CoreBundle\Tests\TestCase;
 use Doctrine\DBAL\Connection;
+use Symfony\Contracts\Service\ResetInterface;
 
-class LayoutOptionsTest extends TestCase
+class LayoutOptionsListenerTest extends TestCase
 {
     public function testGetsLayoutsOrderedByThemes(): void
     {
@@ -61,5 +62,31 @@ class LayoutOptionsTest extends TestCase
 
         $this->assertSame(['Theme A' => [1 => 'Layout 1']], $listener());
         $this->assertSame(['Theme A' => [1 => 'Layout 1']], $listener());
+    }
+
+    public function testServiceIsResetable(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->exactly(2))
+            ->method('fetchAllAssociative')
+            ->with('SELECT l.id, l.name, t.name AS theme FROM tl_layout l LEFT JOIN tl_theme t ON l.pid=t.id ORDER BY t.name, l.name')
+            ->willReturnOnConsecutiveCalls([
+                ['id' => 1, 'name' => 'Layout 1', 'theme' => 'Theme A'],
+            ], [
+                ['id' => 2, 'name' => 'Layout 2', 'theme' => 'Theme A'],
+            ])
+        ;
+
+        $listener = new LayoutOptionsListener($connection);
+
+        $this->assertInstanceOf(ResetInterface::class, $listener);
+
+        $this->assertSame(['Theme A' => [1 => 'Layout 1']], $listener());
+        $this->assertSame(['Theme A' => [1 => 'Layout 1']], $listener());
+
+        $listener->reset();
+
+        $this->assertSame(['Theme A' => [2 => 'Layout 2']], $listener());
     }
 }
