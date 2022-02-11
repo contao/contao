contao/contao

commit 377214da135f05ef2f07b24df41da43743c1f811

Add generic toggle operation handling (see #3889)
Description
-----------

since I needed the toggle operation _again_ in https://github.com/contao/contao/pull/3872 I had to copy the same stuff all over again.  And since we don't have [Haste](https://github.com/codefog/contao-haste/blob/main/docs/Dca/index.md#easy-ajax-toggle-operation) in the core, this adds a generic toggle handling to DC_Table.

I'm aware this is not by far as powerful as Haste is, but I think it serves the 99% case. If merged we might also rewrite the existing callbacks, but I think that should not be part of this PR.

Commits
-------

498ecbdd Added toggle operation to DC_Table
a7916ab7 Added icon handling for toggle operation
d92e32b0 CS
212f09fd Do not render toggle operation if user has no access to field
0c8d2cf7 Added examples for content elements
d66d8ec8 CS
c0c25a67 CS
0914ade5 Fixed icon path in directory
4d4f4618 Added reverse option and implementation in tl_member
69efecae CS
b5ffcb1e CS
ca6e8a65 Fixed permission check for content elements

Co-authored-by: Leo Feyer <github@contao.org>

core-bundle/src/Resources/contao/classes/DataContainer.php
@@ -14,6 +14,7 @@ use Contao\CoreBundle\Exception\AccessDeniedException;
 use Contao\CoreBundle\Exception\ResponseException;
 use Contao\CoreBundle\Picker\DcaPickerProviderInterface;
 use Contao\CoreBundle\Picker\PickerInterface;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\Image\ResizeConfiguration;
 use Imagine\Gd\Imagine;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
@@ -947,7 +948,42 @@ abstract class DataContainer extends Backend
 						$href = $this->addToUrl($v['href'] . '&amp;id=' . $arrRow['id'] . (Input::get('nb') ? '&amp;nc=1' : ''));
 					}
 
-					$return .= '<a href="' . $href . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($v['icon'], $label) . '</a> ';
+					parse_str(StringUtil::decodeEntities($v['href']), $params);
+
+					if (($params['act'] ?? null) == 'toggle' && isset($params['field']))
+					{
+						// Hide the toggle icon if the user does not have access to the field
+						if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, $this->strTable . '::' . $params['field']))
+						{
+							continue;
+						}
+
+						$icon = $v['icon'];
+						$_icon = pathinfo($v['icon'], PATHINFO_FILENAME) . '_.' . pathinfo($v['icon'], PATHINFO_EXTENSION);
+
+						if (false !== strpos($v['icon'], '/'))
+						{
+							$_icon = \dirname($v['icon']) . '/' . $_icon;
+						}
+
+						if ($icon == 'visible.svg')
+						{
+							$_icon = 'invisible.svg';
+						}
+
+						$state = $arrRow[$params['field']] ? 1 : 0;
+
+						if ($v['reverse'] ?? false)
+						{
+							$state = $arrRow[$params['field']] ? 0 : 1;
+						}
+
+						$return .= '<a href="' . $href . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,' . $arrRow['id'] . ')">' . Image::getHtml($state ? $icon : $_icon, $label, 'data-icon="' . Image::getPath($icon) . '" data-icon-disabled="' . Image::getPath($_icon) . '" data-state="' . $state . '"') . '</a> ';
+					}
+					else
+					{
+						$return .= '<a href="' . $href . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($v['icon'], $label) . '</a> ';
+					}
 				}
 
 				continue;

core-bundle/src/Resources/contao/classes/Backend.php
@@ -507,6 +507,8 @@ abstract class Backend extends Controller
 				case 'copyAll':
 				case 'move':
 				case 'edit':
+				case 'editAll':
+				case 'toggle':
 					if (!$dc instanceof EditableDataContainerInterface)
 					{
 						$this->log('Data container ' . $strTable . ' is not editable', __METHOD__, ContaoContext::ERROR);

core-bundle/src/Resources/contao/dca/tl_content.php
@@ -23,7 +23,6 @@ use Contao\Message;
 use Contao\PageModel;
 use Contao\StringUtil;
 use Contao\System;
-use Contao\Versions;
 
 $GLOBALS['TL_DCA']['tl_content'] = array
 (
@@ -102,8 +101,8 @@ $GLOBALS['TL_DCA']['tl_content'] = array
 			),
 			'toggle' => array
 			(
+				'href'                => 'act=toggle&amp;field=invisible',
 				'icon'                => 'visible.svg',
-				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
 				'button_callback'     => array('tl_content', 'toggleIcon')
 			),
 			'show' => array
@@ -1138,7 +1137,7 @@ class tl_content extends Backend
 		$objSession = System::getContainer()->get('session');
 
 		// Prevent editing content elements with not allowed types
-		if (Input::get('act') == 'edit' || Input::get('act') == 'delete' || (Input::get('act') == 'paste' && Input::get('mode') == 'copy'))
+		if (Input::get('act') == 'edit' || Input::get('act') == 'delete' || Input::get('act') == 'toggle' || (Input::get('act') == 'paste' && Input::get('mode') == 'copy'))
 		{
 			$objCes = $this->Database->prepare("SELECT type FROM tl_content WHERE id=?")
 									 ->execute(Input::get('id'));
@@ -1979,15 +1978,8 @@ class tl_content extends Backend
 	 */
 	public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
 	{
-		if (Input::get('cid'))
-		{
-			$this->toggleVisibility(Input::get('cid'), (Input::get('state') == 1), (func_num_args() <= 12 ? null : func_get_arg(12)));
-			$this->redirect($this->getReferer());
-		}
-
 		$security = System::getContainer()->get('security.helper');
 
-		// Check permissions AFTER checking the cid, so hacking attempts are logged
 		if (!$security->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, 'tl_content::invisible'))
 		{
 			return '';
@@ -1999,136 +1991,13 @@ class tl_content extends Backend
 			return Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)) . ' ';
 		}
 
-		$href .= '&amp;id=' . Input::get('id') . '&amp;cid=' . $row['id'] . '&amp;state=' . $row['invisible'];
+		$href .= '&amp;id=' . $row['id'];
 
 		if ($row['invisible'])
 		{
 			$icon = 'invisible.svg';
 		}
 
-		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($title) . '" data-tid="cid"' . $attributes . '>' . Image::getHtml($icon, $label, 'data-state="' . ($row['invisible'] ? 0 : 1) . '"') . '</a> ';
-	}
-
-	/**
-	 * Toggle the visibility of an element
-	 *
-	 * @param integer       $intId
-	 * @param boolean       $blnVisible
-	 * @param DataContainer $dc
-	 *
-	 * @throws AccessDeniedException
-	 */
-	public function toggleVisibility($intId, $blnVisible, DataContainer $dc=null)
-	{
-		// Set the ID and action
-		Input::setGet('id', $intId);
-		Input::setGet('act', 'toggle');
-
-		if ($dc)
-		{
-			$dc->id = $intId; // see #8043
-		}
-
-		// Trigger the onload_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}($dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$callback($dc);
-				}
-			}
-		}
-
-		// Check the field access
-		if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, 'tl_content::invisible'))
-		{
-			throw new AccessDeniedException('Not enough permissions to show/hide content element ID ' . $intId . '.');
-		}
-
-		$objRow = $this->Database->prepare("SELECT * FROM tl_content WHERE id=?")
-								 ->limit(1)
-								 ->execute($intId);
-
-		if ($objRow->numRows < 1)
-		{
-			throw new AccessDeniedException('Invalid content element ID ' . $intId . '.');
-		}
-
-		if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_ELEMENT_TYPE, $objRow->type))
-		{
-			throw new AccessDeniedException('Not enough permissions to modify content elements of type "' . $objRow->type . '".');
-		}
-
-		// Set the current record
-		if ($dc)
-		{
-			$dc->activeRecord = $objRow;
-		}
-
-		$objVersions = new Versions('tl_content', $intId);
-		$objVersions->initialize();
-
-		// Reverse the logic (elements have invisible=1)
-		$blnVisible = !$blnVisible;
-
-		// Trigger the save_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_content']['fields']['invisible']['save_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_content']['fields']['invisible']['save_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$blnVisible = $callback($blnVisible, $dc);
-				}
-			}
-		}
-
-		$time = time();
-
-		// Update the database
-		$this->Database->prepare("UPDATE tl_content SET tstamp=$time, invisible='" . ($blnVisible ? '1' : '') . "' WHERE id=?")
-					   ->execute($intId);
-
-		if ($dc)
-		{
-			$dc->activeRecord->tstamp = $time;
-			$dc->activeRecord->invisible = ($blnVisible ? '1' : '');
-		}
-
-		// Trigger the onsubmit_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_content']['config']['onsubmit_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_content']['config']['onsubmit_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}($dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$callback($dc);
-				}
-			}
-		}
-
-		$objVersions->create();
-
-		if ($dc)
-		{
-			$dc->invalidateCacheTags();
-		}
+		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,' . $row['id'] . ')">' . Image::getHtml($icon, $label, 'data-icon="' . Image::getPath('visible.svg') . '" data-icon-disabled="' . Image::getPath('invisible.svg') . '" data-state="' . ($row['invisible'] ? 0 : 1) . '"') . '</a> ';
 	}
 }

core-bundle/src/Resources/contao/dca/tl_member.php
@@ -12,17 +12,13 @@ use Contao\Backend;
 use Contao\BackendUser;
 use Contao\Config;
 use Contao\CoreBundle\EventListener\Widget\HttpUrlListener;
-use Contao\CoreBundle\Exception\AccessDeniedException;
-use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\DataContainer;
 use Contao\FrontendUser;
 use Contao\Image;
-use Contao\Input;
 use Contao\MemberGroupModel;
 use Contao\MemberModel;
 use Contao\StringUtil;
 use Contao\System;
-use Contao\Versions;
 
 $GLOBALS['TL_DCA']['tl_member'] = array
 (
@@ -91,9 +87,9 @@ $GLOBALS['TL_DCA']['tl_member'] = array
 			),
 			'toggle' => array
 			(
+				'href'                => 'act=toggle&amp;field=disable',
 				'icon'                => 'visible.svg',
-				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
-				'button_callback'     => array('tl_member', 'toggleIcon')
+				'reverse'             => true,
 			),
 			'show' => array
 			(
@@ -583,159 +579,5 @@ class tl_member extends Backend
 
 		$this->Database->prepare("UPDATE tl_member SET dateAdded=? WHERE id=?")
 					   ->execute($time, $dc->id);
-	}
-
-	/**
-	 * Return the "toggle visibility" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
-	{
-		if (Input::get('tid'))
-		{
-			$this->toggleVisibility(Input::get('tid'), (Input::get('state') == 1), (func_num_args() <= 12 ? null : func_get_arg(12)));
-			$this->redirect($this->getReferer());
-		}
-
-		// Check permissions AFTER checking the tid, so hacking attempts are logged
-		if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, 'tl_member::disable'))
-		{
-			return '';
-		}
-
-		$href .= '&amp;tid=' . $row['id'] . '&amp;state=' . $row['disable'];
-
-		if ($row['disable'])
-		{
-			$icon = 'invisible.svg';
-		}
-
-		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label, 'data-state="' . ($row['disable'] ? 0 : 1) . '"') . '</a> ';
-	}
-
-	/**
-	 * Disable/enable a user group
-	 *
-	 * @param integer       $intId
-	 * @param boolean       $blnVisible
-	 * @param DataContainer $dc
-	 *
-	 * @throws AccessDeniedException
-	 */
-	public function toggleVisibility($intId, $blnVisible, DataContainer $dc=null)
-	{
-		// Set the ID and action
-		Input::setGet('id', $intId);
-		Input::setGet('act', 'toggle');
-
-		if ($dc)
-		{
-			$dc->id = $intId; // see #8043
-		}
-
-		// Trigger the onload_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_member']['config']['onload_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_member']['config']['onload_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}($dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$callback($dc);
-				}
-			}
-		}
-
-		// Check the field access
-		if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, 'tl_member::disable'))
-		{
-			throw new AccessDeniedException('Not enough permissions to activate/deactivate member ID ' . $intId . '.');
-		}
-
-		$objRow = $this->Database->prepare("SELECT * FROM tl_member WHERE id=?")
-								 ->limit(1)
-								 ->execute($intId);
-
-		if ($objRow->numRows < 1)
-		{
-			throw new AccessDeniedException('Invalid member ID ' . $intId . '.');
-		}
-
-		// Set the current record
-		if ($dc)
-		{
-			$dc->activeRecord = $objRow;
-		}
-
-		$objVersions = new Versions('tl_member', $intId);
-		$objVersions->initialize();
-
-		// Reverse the logic (members have disabled=1)
-		$blnVisible = !$blnVisible;
-
-		// Trigger the save_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_member']['fields']['disable']['save_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_member']['fields']['disable']['save_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$blnVisible = $this->{$callback[0]}->{$callback[1]}($blnVisible, $dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$blnVisible = $callback($blnVisible, $dc);
-				}
-			}
-		}
-
-		$time = time();
-
-		// Update the database
-		$this->Database->prepare("UPDATE tl_member SET tstamp=$time, disable='" . ($blnVisible ? '1' : '') . "' WHERE id=?")
-					   ->execute($intId);
-
-		if ($dc)
-		{
-			$dc->activeRecord->tstamp = $time;
-			$dc->activeRecord->disable = ($blnVisible ? '1' : '');
-		}
-
-		// Trigger the onsubmit_callback
-		if (is_array($GLOBALS['TL_DCA']['tl_member']['config']['onsubmit_callback'] ?? null))
-		{
-			foreach ($GLOBALS['TL_DCA']['tl_member']['config']['onsubmit_callback'] as $callback)
-			{
-				if (is_array($callback))
-				{
-					$this->import($callback[0]);
-					$this->{$callback[0]}->{$callback[1]}($dc);
-				}
-				elseif (is_callable($callback))
-				{
-					$callback($dc);
-				}
-			}
-		}
-
-		$objVersions->create();
-
-		if ($dc)
-		{
-			$dc->invalidateCacheTags();
-		}
 	}
 }

core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -2673,6 +2673,123 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 	}
 
 	/**
+	 * Toggle a field (e.g. "published" or "disable")
+	 *
+	 * @param integer $intId
+	 *
+	 * @throws AccessDeniedException
+	 * @throws InternalServerErrorException
+	 */
+	public function toggle($intId=null)
+	{
+		if ($GLOBALS['TL_DCA'][$this->strTable]['config']['notEditable'] ?? null)
+		{
+			throw new InternalServerErrorException('Table "' . $this->strTable . '" is not editable.');
+		}
+
+		if ($intId)
+		{
+			$this->intId = $intId;
+		}
+
+		$this->strField = Input::get('field');
+
+		// Security check before using field in DB query!
+		if (!$this->Database->fieldExists($this->strField, $this->strTable))
+		{
+			throw new AccessDeniedException('Database field ' . $this->strTable . '.' . $this->strField . ' does not exist.');
+		}
+
+		// Check the field access
+		if (!System::getContainer()->get('security.helper')->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELD_OF_TABLE, $this->strTable . '::' . $this->strField))
+		{
+			throw new AccessDeniedException('Not enough permissions to toggle field ' . $this->strTable . '.' . $this->strField . ' of record ID ' . $intId . '.');
+		}
+
+		// Get the current record
+		$objRow = $this->Database->prepare("SELECT * FROM " . $this->strTable . " WHERE id=?")
+								 ->limit(1)
+								 ->execute($this->intId);
+
+		// Redirect if there is no record with the given ID
+		if ($objRow->numRows < 1)
+		{
+			throw new AccessDeniedException('Cannot load record "' . $this->strTable . '.id=' . $this->intId . '".');
+		}
+
+		$this->objActiveRecord = $objRow;
+		$this->values[] = $this->intId;
+		$this->procedure[] = 'id=?';
+		$this->blnCreateNewVersion = false;
+
+		$objVersions = new Versions($this->strTable, $this->intId);
+		$objVersions->initialize();
+
+		Input::setPost('FORM_SUBMIT', $this->strTable);
+		$this->varValue = $objRow->{$this->strField};
+
+		$this->save($this->varValue ? '' : '1');
+
+		// Trigger the onsubmit_callback
+		if (\is_array($GLOBALS['TL_DCA'][$this->strTable]['config']['onsubmit_callback'] ?? null))
+		{
+			foreach ($GLOBALS['TL_DCA'][$this->strTable]['config']['onsubmit_callback'] as $callback)
+			{
+				if (\is_array($callback))
+				{
+					$this->import($callback[0]);
+					$this->{$callback[0]}->{$callback[1]}($this);
+				}
+				elseif (\is_callable($callback))
+				{
+					$callback($this);
+				}
+			}
+		}
+
+		// Set the current timestamp before adding a new version
+		if ($GLOBALS['TL_DCA'][$this->strTable]['config']['dynamicPtable'] ?? null)
+		{
+			$this->Database->prepare("UPDATE " . $this->strTable . " SET ptable=?, tstamp=? WHERE id=?")
+						   ->execute($this->ptable, time(), $this->intId);
+		}
+		else
+		{
+			$this->Database->prepare("UPDATE " . $this->strTable . " SET tstamp=? WHERE id=?")
+						   ->execute(time(), $this->intId);
+		}
+
+		// Save the current version
+		if ($this->blnCreateNewVersion)
+		{
+			$objVersions->create();
+
+			// Call the onversion_callback
+			if (\is_array($GLOBALS['TL_DCA'][$this->strTable]['config']['onversion_callback'] ?? null))
+			{
+				trigger_deprecation('contao/core-bundle', '4.0', 'Using the "onversion_callback" has been deprecated and will no longer work in Contao 5.0. Use the "oncreate_version_callback" instead.');
+
+				foreach ($GLOBALS['TL_DCA'][$this->strTable]['config']['onversion_callback'] as $callback)
+				{
+					if (\is_array($callback))
+					{
+						$this->import($callback[0]);
+						$this->{$callback[0]}->{$callback[1]}($this->strTable, $this->intId, $this);
+					}
+					elseif (\is_callable($callback))
+					{
+						$callback($this->strTable, $this->intId, $this);
+					}
+				}
+			}
+		}
+
+		$this->invalidateCacheTags();
+
+		$this->redirect($this->getReferer());
+	}
+
+	/**
 	 * Auto-generate a form to override all records that are currently shown
 	 *
 	 * @return string

core-bundle/src/Resources/public/core.js
@@ -581,20 +581,20 @@ var AjaxRequest =
 			div.getParent('div').getElement('pre').toggleClass('disabled');
 		}
 
+		icon = image.get('data-icon') || AjaxRequest.themePath + 'icons/visible.svg';
+		icond = image.get('data-icon-disabled') || AjaxRequest.themePath + 'icons/invisible.svg';
+
 		// Send request
-		if (!published) {
-			image.src = AjaxRequest.themePath + 'icons/visible.svg';
-			image.set('data-state', 1);
+		if (el.href.indexOf('act=toggle') !== -1) {
+			image.src = !published ? icon : icond;
+			image.set('data-state', !published ? 1 : 0);
 
-			params = {'state':1, 'rt':Contao.request_token};
-			params[$(el).get('data-tid') || 'tid'] = id;
-
-			new Request.Contao({'url':window.location.href, 'followRedirects':false}).get(params);
+			new Request.Contao({'url':el.href, 'followRedirects':false}).get();
 		} else {
-			image.src = AjaxRequest.themePath + 'icons/invisible.svg';
-			image.set('data-state', 0);
+			image.src = published ? icond : icon;
+			image.set('data-state', published ? 0 : 1);
 
-			params = {'state':0, 'rt':Contao.request_token};
+			params = {'state':published ? 0 : 1, 'rt':Contao.request_token};
 			params[$(el).get('data-tid') || 'tid'] = id;
 
 			new Request.Contao({'url':window.location.href, 'followRedirects':false}).get(params);

commit 213108bcdefa99fa5d7f855d1a39bf80ba237950

Fix an undefiend array key (see #4096)
Description
-----------

This fixes the array key "href" being undefined<sup>1</sup> and throwing an ErrorException/Warning.

<sup>1</sup> This is the case when using the "route" param instead of the "href" param (see 3 lines above the change).

Issue introduced in #3889.

Commits
-------

3b7ed6f0 Fix array key being undefined

core-bundle/src/Resources/contao/classes/DataContainer.php
@@ -954,7 +954,7 @@ abstract class DataContainer extends Backend
 						$href = $this->addToUrl($v['href'] . '&amp;id=' . $arrRow['id'] . (Input::get('nb') ? '&amp;nc=1' : ''));
 					}
 
-					parse_str(StringUtil::decodeEntities($v['href']), $params);
+					parse_str(StringUtil::decodeEntities($v['href'] ?? ''), $params);
 
 					if (($params['act'] ?? null) == 'toggle' && isset($params['field']))
 					{