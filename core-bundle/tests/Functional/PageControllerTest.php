0cdfb350ceae5a341cc705509c944f6f2f081af5
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-03-15 14:33:22 +0100

    CS

--- core-bundle/tests/Functional/PageControllerTest.php
+++ core-bundle/tests/Functional/PageControllerTest.php
@@ -118,9 +118,7 @@ class PageControllerTest extends FunctionalTestCase
         self::$lastImport = $fileNames;
 
         static::loadFixtures(array_map(
-            static function ($file) {
-                return __DIR__.'/../Fixtures/Functional/PageController/'.$file.'.yml';
-            },
+            static fn ($file) => __DIR__.'/../Fixtures/Functional/PageController/'.$file.'.yml',
             $fileNames
         ));
     }
