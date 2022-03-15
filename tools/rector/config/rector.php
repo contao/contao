cc6e160dfcb9ef30b8ff3000cad92c455a2b822a
Leo Feyer <github@contao.org>
2022-03-15 16:12:15 +0100

    Add Rector to upgrade the codebase to PHP 8.1

---
+++ tools/rector/config/rector.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php55\Rector\Class_\ClassConstantToSelfClassRector;
+use Rector\Php73\Rector\FuncCall\ArrayKeyFirstLastRector;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php80\Rector\Class_\StringableForToStringRector;
+use Rector\Php80\Rector\FuncCall\ClassOnObjectRector;
+use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
+use Rector\Php80\Rector\Identical\StrEndsWithRector;
+use Rector\Php80\Rector\Identical\StrStartsWithRector;
+use Rector\Php80\Rector\NotIdentical\StrContainsRector;
+use Rector\Php81\Rector\ClassConst\FinalizePublicClassConstantRector;
+use Rector\Php81\Rector\FunctionLike\IntersectionTypesRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PARALLEL, true);
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/../../../*-bundle/bin',
+        __DIR__ . '/../../../*-bundle/src',
+        __DIR__ . '/../../../*-bundle/tests',
+    ]);
+
+    $parameters->set(Option::SKIP, [
+        '*/Fixtures/system/*',
+        '*/Resources/contao/*',
+        ClassPropertyAssignToConstructorPromotionRector::class => [
+            '*/src/Entity/*',
+            'core-bundle/src/Fragment/FragmentHandler.php',
+            'core-bundle/src/Translation/Translator.php',
+        ],
+    ]);
+
+    $services = $containerConfigurator->services();
+    $services->set(ArrayKeyFirstLastRector::class);
+    $services->set(ArraySpreadInsteadOfArrayMergeRector::class);
+    $services->set(ClassConstantToSelfClassRector::class);
+    $services->set(ClassOnObjectRector::class);
+    $services->set(ClassPropertyAssignToConstructorPromotionRector::class);
+    $services->set(FinalizePublicClassConstantRector::class);
+    $services->set(IntersectionTypesRector::class);
+    $services->set(JsonThrowOnErrorRector::class);
+    $services->set(RemoveUnusedVariableInCatchRector::class);
+    $services->set(RestoreDefaultNullToNullableTypePropertyRector::class);
+    $services->set(ReturnNeverTypeRector::class);
+    $services->set(StringableForToStringRector::class);
+    $services->set(StrContainsRector::class);
+    $services->set(StrEndsWithRector::class);
+    $services->set(StrStartsWithRector::class);
+    $services->set(TypedPropertyRector::class);
+    $services->set(UnionTypesRector::class);
+};
