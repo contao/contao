ab74564696d9b77fed6877e698612c29f488121b
M. Vondano <m-vo@users.noreply.github.com>
2022-03-19 10:04:11 +0100

    simplify usage

--- core-bundle/tests/DependencyInjection/Compiler/ConfigureFilesystemPassTest.php
+++ core-bundle/tests/DependencyInjection/Compiler/ConfigureFilesystemPassTest.php
@@ -105,8 +105,8 @@ class ConfigureFilesystemPassTest extends TestCase
         $filesystem->mkdir(Path::join($tempDir, 'files'));
         $filesystem->dumpFile($dummyFile = Path::join($tempDir, 'vendor/foo/dummy.txt'), 'dummy');
 
-        $this->createSymlink($target, $link, Path::join($tempDir, 'files'), true);
-        $this->createSymlink($dummyFile, 'dummy.txt', Path::join($tempDir, 'files'), false); // should get ignored
+        $this->createSymlink($target, $link, Path::join($tempDir, 'files'));
+        $this->createSymlink($dummyFile, 'dummy.txt', Path::join($tempDir, 'files')); // should get ignored
 
         $container = new ContainerBuilder(
             new ParameterBag([
@@ -164,9 +164,10 @@ class ConfigureFilesystemPassTest extends TestCase
         ];
     }
 
-    private function createSymlink(string $target, string $link, string $cwd, bool $isDirectory): void
+    private function createSymlink(string $target, string $link, string $cwd): void
     {
         if ('\\' === \DIRECTORY_SEPARATOR) {
+            $isDirectory = is_dir($target);
             $target = str_replace('/', '\\', $target);
             $link = str_replace('/', '\\', $link);
 
