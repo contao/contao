7970df697707bcdd3a38286302bf90d802a8fa4b
David Greminger <david.greminger@1up.io>
2022-03-21 16:54:46 +0100

    Fix test

--- core-bundle/tests/Fixtures/vendor/contao/test-bundle/Resources/contao/dca/tl_test_with_namespace1.php
+++ core-bundle/tests/Fixtures/vendor/contao/test-bundle/Resources/contao/dca/tl_test_with_namespace1.php
@@ -2,6 +2,8 @@
 
 namespace Foo\Bar;
 
-$GLOBALS['TL_DCA']['tl_test']['config']['dataContainer'] = Contao\DC_Table::class;
+use Contao\DC_Table;
+
+$GLOBALS['TL_DCA']['tl_test']['config']['dataContainer'] = DC_Table::class;
 
 ?>
