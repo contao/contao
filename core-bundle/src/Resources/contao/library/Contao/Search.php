a13883f4dc6eb07395ee853dfb0e288f3524b950
Yanick Witschi <yanick.witschi@terminal42.ch>
2022-03-22 22:53:05 +0100

    Also remove deprecated Search stuff that relied on old db methods

--- core-bundle/src/Resources/contao/library/Contao/Search.php
+++ core-bundle/src/Resources/contao/library/Contao/Search.php
@@ -441,33 +441,6 @@ class Search
 	 * @param string  $strKeywords  The keyword string
 	 * @param boolean $blnOrSearch  If true, the result can contain any keyword
 	 * @param array   $arrPid       An optional array of page IDs to limit the result to
-	 * @param integer $intRows      An optional maximum number of result rows
-	 * @param integer $intOffset    An optional result offset
-	 * @param boolean $blnFuzzy     If true, the search will be fuzzy
-	 * @param integer $intMinlength Ignore keywords deceeding the minimum length
-	 *
-	 * @return Result The database result object
-	 *
-	 * @throws \Exception If the cleaned keyword string is empty
-	 *
-	 * @deprecated Deprecated since Contao 4.12, to be removed in Contao 5.
-	 *             Use the Search::query() method instead.
-	 */
-	public static function searchFor($strKeywords, $blnOrSearch=false, $arrPid=array(), $intRows=0, $intOffset=0, $blnFuzzy=false, $intMinlength=0)
-	{
-		trigger_deprecation('contao/core-bundle', '4.12', 'Using "%s()" has been deprecated and will no longer work in Contao 5.0. Use "Contao\Search::query()" instead.', __METHOD__);
-
-		$objSearchResult = static::query((string) $strKeywords, (bool) $blnOrSearch, \is_array($arrPid) ? $arrPid : array(), (bool) $blnFuzzy, (int) $intMinlength);
-
-		return new Result($objSearchResult->getResults($intRows ?: PHP_INT_MAX, $intOffset), 'SELECT * FROM tl_search');
-	}
-
-	/**
-	 * Search the index and return the result object
-	 *
-	 * @param string  $strKeywords  The keyword string
-	 * @param boolean $blnOrSearch  If true, the result can contain any keyword
-	 * @param array   $arrPid       An optional array of page IDs to limit the result to
 	 * @param boolean $blnFuzzy     If true, the search will be fuzzy
 	 * @param integer $intMinlength Ignore keywords deceeding the minimum length
 	 *
