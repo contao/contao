857ba57b110c4792e41ebe037598febc4e8ac3eb
Martin Auswöger <martin@auswoeger.com>
2022-03-25 17:05:48 +0100

    Make the search accent insensitive (see #4263)
    Description
    -----------
    
    A search query for “Eïné Mass Bīėr” now matches the Text “Eine Maß Bier” and vice versa.
    
    Commits
    -------
    
    a912d6e5 Make search accent insensitive
    9876565d Make context matches accent insensitive

--- core-bundle/src/Resources/contao/library/Contao/Search.php
+++ core-bundle/src/Resources/contao/library/Contao/Search.php
@@ -11,7 +11,6 @@
 namespace Contao;
 
 use Contao\Database\Result;
-use Patchwork\Utf8;
 
 /**
  * Creates and queries the search index
@@ -225,7 +224,7 @@ class Search
 		unset($arrSet);
 
 		// Split words
-		$arrWords = self::splitIntoWords(Utf8::strtolower($strText), $arrData['language']);
+		$arrWords = self::splitIntoWords($strText, $arrData['language']);
 		$arrIndex = array();
 
 		// Index words
@@ -271,17 +270,71 @@ class Search
 		$iterator = \IntlRuleBasedBreakIterator::createWordInstance($strLocale);
 		$iterator->setText($strText);
 
+		// As the search index is shared across all languages, we can not use
+		// locale specific rules here (like de-ASCII or tr-Lower).
+		if (\in_array('Latin-ASCII', \Transliterator::listIDs(), true))
+		{
+			$transliterator = \Transliterator::createFromRules('::Latin-ASCII; ::Lower;');
+		}
+		else
+		{
+			$transliterator = \Transliterator::create('Lower');
+		}
+
 		$words = array();
 
 		foreach ($iterator->getPartsIterator() as $part)
 		{
 			if ($iterator->getRuleStatus() !== \IntlBreakIterator::WORD_NONE)
 			{
-				$words[] = $part;
+				$words[] = $transliterator->transliterate($part);
 			}
 		}
 
 		return $words;
+	}
+
+	/**
+	 * Get different variants of the matched words that are present in the text,
+	 * e.g. with accents or diaeresis.
+	 *
+	 * @return string[]
+	 */
+	public static function getMatchVariants(array $arrMatches, string $strText, string $strLocale): array
+	{
+		$iterator = \IntlRuleBasedBreakIterator::createWordInstance($strLocale);
+		$iterator->setText($strText);
+
+		// As the search index is shared across all languages, we can not use
+		// locale specific rules here (like de-ASCII or tr-Lower).
+		if (\in_array('Latin-ASCII', \Transliterator::listIDs(), true))
+		{
+			$transliterator = \Transliterator::createFromRules('::Latin-ASCII; ::Lower;');
+		}
+		else
+		{
+			$transliterator = \Transliterator::create('Lower');
+		}
+
+		$arrMatches = array_map(
+			static function ($match) use ($transliterator)
+			{
+				return $transliterator->transliterate($match);
+			},
+			$arrMatches
+		);
+
+		$variants = array();
+
+		foreach ($iterator->getPartsIterator() as $part)
+		{
+			if ($iterator->getRuleStatus() !== \IntlBreakIterator::WORD_NONE && !\in_array($part, $variants, true) && \in_array($transliterator->transliterate($part), $arrMatches, true))
+			{
+				$variants[] = $part;
+			}
+		}
+
+		return $variants;
 	}
 
 	/**
@@ -303,7 +356,6 @@ class Search
 	{
 		// Clean the keywords
 		$strKeywords = StringUtil::decodeEntities($strKeywords);
-		$strKeywords = Utf8::strtolower($strKeywords);
 
 		// Check keyword string
 		if (!\strlen($strKeywords))
@@ -313,7 +365,7 @@ class Search
 
 		// Split keywords
 		$arrChunks = array();
-		preg_match_all('/"[^"]+"|[+-]?[^ ]+\*?/', $strKeywords, $arrChunks);
+		preg_match_all('/"[^"]+"|\S+/', $strKeywords, $arrChunks);
 
 		$arrPhrases = array();
 		$arrKeywords = array();
@@ -323,9 +375,32 @@ class Search
 
 		foreach (array_unique($arrChunks[0]) as $strKeyword)
 		{
-			if (substr($strKeyword, -1) == '*' && \strlen($strKeyword) > 1)
+			if (($strKeyword[0] === '*' || substr($strKeyword, -1) === '*') && \strlen($strKeyword) > 1)
 			{
-				$arrWildcards[] = str_replace('*', '%', $strKeyword);
+				$arrWildcardWords = self::splitIntoWords(trim($strKeyword, '*'), $GLOBALS['TL_LANGUAGE']);
+
+				foreach ($arrWildcardWords as $intIndex => $strWord)
+				{
+					if ($intIndex === 0 && $strKeyword[0] === '*')
+					{
+						$strWord = '%' . $strWord;
+					}
+
+					if ($intIndex === \count($arrWildcardWords) - 1 && substr($strKeyword, -1) === '*')
+					{
+						$strWord .= '%';
+					}
+
+					if ($strWord[0] === '%' || substr($strWord, -1) === '%')
+					{
+						$arrWildcards[] = $strWord;
+					}
+					else
+					{
+						$arrKeywords[] = $strWord;
+					}
+				}
+
 				continue;
 			}
 
@@ -358,14 +433,6 @@ class Search
 						{
 							$arrExcluded[] = $strWord;
 						}
-					}
-					break;
-
-				// Wildcards
-				case '*':
-					if (\strlen($strKeyword) > 1)
-					{
-						$arrWildcards[] = str_replace('*', '%', $strKeyword);
 					}
 					break;
 
