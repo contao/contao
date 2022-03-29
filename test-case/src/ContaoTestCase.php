5d306fa190eefa29cb5b0aab6e882a427259fa1b
Leo Feyer <github@contao.org>
2022-03-29 17:37:56 +0200

    Fix the CI chain (see #4411)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    5b2e7122 Fix the CI chain

--- test-case/src/ContaoTestCase.php
+++ test-case/src/ContaoTestCase.php
@@ -29,20 +29,9 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 abstract class ContaoTestCase extends TestCase
 {
-    /**
-     * @var array
-     */
-    private static $tempDirs = [];
-
-    /**
-     * @var array
-     */
-    private static $betterReflectionCache = [];
-
-    /**
-     * @var array
-     */
-    private $backupServerEnvGetPost = [];
+    private static array $tempDirs = [];
+    private static array $betterReflectionCache = [];
+    private array $backupServerEnvGetPost = [];
 
     public static function tearDownAfterClass(): void
     {
@@ -95,7 +84,6 @@ abstract class ContaoTestCase extends TestCase
         $container->setParameter('kernel.cache_dir', $projectDir.'/var/cache');
         $container->setParameter('kernel.project_dir', $projectDir);
         $container->setParameter('kernel.root_dir', $projectDir.'/app');
-
         $container->setDefinition('request_stack', new Definition(RequestStack::class));
 
         // Load the default configuration
@@ -125,11 +113,7 @@ abstract class ContaoTestCase extends TestCase
 
         $framework
             ->method('getAdapter')
-            ->willReturnCallback(
-                static function (string $key) use ($adapters): ?Adapter {
-                    return $adapters[$key] ?? null;
-                }
-            )
+            ->willReturnCallback(static fn (string $key): ?Adapter => $adapters[$key] ?? null)
         ;
 
         return $framework;
@@ -163,7 +147,10 @@ abstract class ContaoTestCase extends TestCase
         $adapter = $this->mockAdapter(array_keys($configuration));
 
         foreach ($configuration as $method => $return) {
-            $adapter->method($method)->willReturn($return);
+            $adapter
+                ->method($method)
+                ->willReturn($return)
+            ;
         }
 
         return $adapter;
@@ -364,11 +351,7 @@ abstract class ContaoTestCase extends TestCase
 
         $adapter
             ->method('get')
-            ->willReturnCallback(
-                static function (string $key) {
-                    return $GLOBALS['TL_CONFIG'][$key] ?? null;
-                }
-            )
+            ->willReturnCallback(static fn (string $key) => $GLOBALS['TL_CONFIG'][$key] ?? null)
         ;
 
         $adapters[Config::class] = $adapter;
