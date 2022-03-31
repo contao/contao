d412d4e989b2d1434c927ed43e3753504d30f868
Leo Feyer <github@contao.org>
2022-03-31 14:15:18 +0200

    Allow to purge the preview cache in the user profile (see #4431)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    728a5ca4 Allow to purge the preview cache in the user profile

--- core-bundle/src/Resources/contao/dca/tl_user.php
+++ core-bundle/src/Resources/contao/dca/tl_user.php
@@ -798,6 +798,12 @@ class tl_user extends Backend
 					Message::addConfirmation($GLOBALS['TL_LANG']['tl_user']['htmlPurged']);
 				}
 
+				if (in_array('purge_previews', $arrPurge))
+				{
+					$this->Automator->purgePreviewCache();
+					Message::addConfirmation($GLOBALS['TL_LANG']['tl_user']['previewPurged']);
+				}
+
 				if (in_array('purge_pages', $arrPurge))
 				{
 					$this->Automator->purgePageCache();
@@ -815,7 +821,8 @@ class tl_user extends Backend
     <input type="checkbox" id="check_all_purge" class="tl_checkbox" onclick="Backend.toggleCheckboxGroup(this, \'ctrl_purge\')"> <label for="check_all_purge" style="color:#a6a6a6"><em>' . $GLOBALS['TL_LANG']['MSC']['selectAll'] . '</em></label><br>
     <input type="checkbox" name="purge[]" id="opt_purge_0" class="tl_checkbox" value="purge_session" onfocus="Backend.getScrollOffset()"> <label for="opt_purge_0">' . $GLOBALS['TL_LANG']['tl_user']['sessionLabel'] . '</label><br>
     <input type="checkbox" name="purge[]" id="opt_purge_1" class="tl_checkbox" value="purge_images" onfocus="Backend.getScrollOffset()"> <label for="opt_purge_1">' . $GLOBALS['TL_LANG']['tl_user']['htmlLabel'] . '</label><br>
-    <input type="checkbox" name="purge[]" id="opt_purge_2" class="tl_checkbox" value="purge_pages" onfocus="Backend.getScrollOffset()"> <label for="opt_purge_2">' . $GLOBALS['TL_LANG']['tl_user']['tempLabel'] . '</label>
+    <input type="checkbox" name="purge[]" id="opt_purge_2" class="tl_checkbox" value="purge_previews" onfocus="Backend.getScrollOffset()"> <label for="opt_purge_2">' . $GLOBALS['TL_LANG']['tl_user']['previewLabel'] . '</label><br>
+    <input type="checkbox" name="purge[]" id="opt_purge_3" class="tl_checkbox" value="purge_pages" onfocus="Backend.getScrollOffset()"> <label for="opt_purge_3">' . $GLOBALS['TL_LANG']['tl_user']['tempLabel'] . '</label>
   </fieldset>' . $dc->help() . '
 </div>';
 	}
