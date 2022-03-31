fd1a75351dfb8a88dd92982b6a83ffd1a54518f3
Leo Feyer <github@contao.org>
2022-03-31 08:35:42 +0200

    Also hover over the parent records in "paste" mode (see #4424)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    fea3fc38 Also hover over the parent records in "paste" mode

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -3909,7 +3909,17 @@ class DC_Table extends DataContainer implements \listable, \editable
 		}
 
 		$session[$node][$id] = (\is_int($session[$node][$id])) ? $session[$node][$id] : 0;
-		$mouseover = ($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['mode'] == 5 || $table == $this->strTable) ? ' toggle_select hover-div' : '';
+
+		$mouseover = '';
+
+		if ($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['mode'] == 5 || $table == $this->strTable)
+		{
+			$mouseover = ' toggle_select hover-div';
+		}
+		elseif ($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['mode'] == 6 && Input::get('act') == 'paste')
+		{
+			$mouseover = ' hover-div';
+		}
 
 		$return .= "\n  " . '<li class="' . ((($GLOBALS['TL_DCA'][$this->strTable]['list']['sorting']['mode'] == 5 && $objRow->type == 'root') || $table != $this->strTable) ? 'tl_folder' : 'tl_file') . ' click2edit' . $mouseover . ' cf"><div class="tl_left" style="padding-left:' . ($intMargin + $intSpacing + (empty($childs) ? 20 : 0)) . 'px">';
 
