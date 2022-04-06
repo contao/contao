2133608b7fd67e4a17e4cd17c87cf23678fc7846
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-04-06 07:20:41 +0200

    Resolve page model in request (see #3930)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    999cdb8a Added event listener to resolve page model in request
    f668144a Change priority to make pageModel available for other listeners
    f977ea11 Update ContaoContext to get pageModel from request
    15805a1f Full unit test coverage for new listener
    d84e5124 Fixed routing tests

--- core-bundle/tests/Functional/RoutingTest.php
+++ core-bundle/tests/Functional/RoutingTest.php
@@ -28,6 +28,7 @@ class RoutingTest extends FunctionalTestCase
         parent::setUp();
 
         $_GET = [];
+        unset($GLOBALS['objPage']);
 
         Config::set('useAutoItem', true);
         $GLOBALS['TL_CONFIG']['addLanguageToUrl'] = false;
