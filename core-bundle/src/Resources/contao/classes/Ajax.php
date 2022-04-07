4a17e81277e9bbe05d01ad9200992499015b7407
Martin Auswöger <martin@auswoeger.com>
2022-04-07 10:02:18 +0200

    Update version number in toggleSubpalette

--- core-bundle/src/Resources/contao/classes/Ajax.php
+++ core-bundle/src/Resources/contao/classes/Ajax.php
@@ -429,6 +429,10 @@ class Ajax extends Backend
 						{
 							throw new ResponseException($this->convertToResponse($dc->editAll($this->strAjaxId, Input::post('id'))));
 						}
+						elseif (($intLatestVersion = $objVersions->getLatestVersion()) !== null)
+						{
+							throw new ResponseException($this->convertToResponse('<input type="hidden" name="VERSION_NUMBER" value="' . $intLatestVersion . '">'));
+						}
 					}
 					else
 					{
@@ -442,6 +446,10 @@ class Ajax extends Backend
 						if (Input::post('load'))
 						{
 							throw new ResponseException($this->convertToResponse($dc->edit(false, Input::post('id'))));
+						}
+						elseif (($intLatestVersion = $objVersions->getLatestVersion()) !== null)
+						{
+							throw new ResponseException($this->convertToResponse('<input type="hidden" name="VERSION_NUMBER" value="' . $intLatestVersion . '">'));
 						}
 					}
 				}
