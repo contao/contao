fcd2ce30e2125c1ea08154f737e4d3e061bf52ae
Leo Feyer <github@contao.org>
2022-05-05 09:46:28 +0200

    Update the changelog and the language files

--- core-bundle/src/Resources/contao/config/constants.php
+++ core-bundle/src/Resources/contao/config/constants.php
@@ -11,7 +11,7 @@
 // Backwards compatibility
 // Core version
 define('VERSION', '4.13');
-define('BUILD', '3');
+define('BUILD', '4');
 define('LONG_TERM_SUPPORT', false);
 
 // Backwards compatibility
