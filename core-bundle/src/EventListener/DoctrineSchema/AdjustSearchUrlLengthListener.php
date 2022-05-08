078104f816be4ca6fa4ca0cebbbbe026cd1f6eee
fritzmg <fmg@inspiredminds.at>
2022-05-08 12:26:41 +0100

    auto reduce tl_search.url length

---
+++ core-bundle/src/EventListener/DoctrineSchema/AdjustSearchUrlLengthListener.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DoctrineSchema;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+/**
+ * Adjusts the length of tl_search.url if innodb_large_prefix is not enabled (#4615).
+ * 
+ * @internal
+ */
+class AdjustSearchUrlLengthListener
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function __invoke(GenerateSchemaEventArgs $event): void
+    {
+        // Get the tl_search table definition
+        try {
+            $table = $event->getSchema()->getTable('tl_search');
+        } catch (SchemaException $e) {
+            if ($e->getCode() === SchemaException::TABLE_DOESNT_EXIST) {
+                dump(__METHOD__); exit;
+                return;
+            }
+
+            throw $e;
+        }
+
+        // Get the tl_search.url column definition
+        try {
+            $column = $table->getColumn('url');
+        } catch (SchemaException $e) {
+            if ($e->getCode() === SchemaException::COLUMN_DOESNT_EXIST) {
+                return;
+            }
+
+            throw $e;
+        }
+
+        // Check if the field has an index
+        try {
+            $table->getIndex('url');
+        } catch (SchemaException $e) {
+            if ($e->getCode() === SchemaException::INDEX_DOESNT_EXIST) {
+                return;
+            }
+
+            throw $e;
+        }
+
+        // Get maximum index size for this table
+        $maximumIndexSize = $this->getMaximumIndexSize($table);
+
+        // Reduce maximum index size if collation is not "ascii_bin"
+        if ('ascii_bin' !== $column->getPlatformOption('collation')) {
+            $bytesPerChar = 'utf8mb4' === $table->getOption('charset') ? 4 : 3;
+            $maximumIndexSize = floor($maximumIndexSize / $bytesPerChar);
+        }
+
+        if ($column->getLength() <= $maximumIndexSize) {
+            return;
+        }
+
+        // Set the length
+        $column->setLength($maximumIndexSize);
+    }
+
+    private function getMaximumIndexSize(Table $table): int
+    {
+        $engine = $table->getOption('engine');
+
+        if ('innodb' !== strtolower($engine)) {
+            return 1000;
+        }
+
+        // The row format is not DYNAMIC or COMPRESSED
+        if (!\in_array($table->getOption('row_format'), ['DYNAMIC', 'COMPRESSED'], true)) {
+            return 767;
+        }
+
+        $largePrefix = $this->connection->fetchAssociative("SHOW VARIABLES LIKE 'innodb_large_prefix'");
+
+        // The variable no longer exists as of MySQL 8 and MariaDB 10.3
+        if (false === $largePrefix || '' === $largePrefix['Value']) {
+            return 3072;
+        }
+
+        $version = $this->connection->fetchAssociative('SELECT @@version as Value');
+
+        [$ver] = explode('-', $version['Value']);
+
+        // As there is no reliable way to get the vendor (see #84), we are
+        // guessing based on the version number. The check will not be run
+        // as of MySQL 8 and MariaDB 10.3, so this should be safe.
+        $vok = version_compare($ver, '10', '>=') ? '10.2.2' : '5.7.7';
+
+        // Large prefixes are always enabled as of MySQL 5.7.7 and MariaDB 10.2.2
+        if (version_compare($ver, $vok, '>=')) {
+            return 3072;
+        }
+
+        // The innodb_large_prefix option is disabled
+        if (!\in_array(strtolower((string) $largePrefix['Value']), ['1', 'on'], true)) {
+            return 767;
+        }
+
+        return 3072;
+    }
+}
---
+++ core-bundle/tests/EventListener/DoctrineSchema/AdjustSearchUrlLengthListenerTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Contao\CoreBundle\Tests\EventListener\DoctrineSchema;
+
+use Contao\CoreBundle\EventListener\DoctrineSchema\AdjustSearchUrlLengthListener;
+use Contao\CoreBundle\Tests\TestCase;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Index;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\StringType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+class AdjustSearchUrlLengthListenerTest extends TestCase
+{
+    public function testAdjustsLengthIfRowFormatIsEngineIsNotInnoDB(): void
+    {
+        $connection = $this->getConnection();
+
+        $schema = $this->getSchema([], ['engine' => 'MyISAM']);
+
+        $event = new GenerateSchemaEventArgs($this->createMock(EntityManagerInterface::class), $schema);
+
+        (new AdjustSearchUrlLengthListener($connection))($event);
+
+        $this->assertSame(1000, $schema->getTable('tl_search')->getColumn('url')->getLength());
+    }
+
+    public function testAdjustsLengthIfRowFormatIsNotDynamicOrCompressed(): void
+    {
+        $connection = $this->getConnection();
+
+        $schema = $this->getSchema([], ['row_format' => 'COMPACT']);
+
+        $event = new GenerateSchemaEventArgs($this->createMock(EntityManagerInterface::class), $schema);
+
+        (new AdjustSearchUrlLengthListener($connection))($event);
+
+        $this->assertSame(767, $schema->getTable('tl_search')->getColumn('url')->getLength());
+    }
+
+    public function testDoesNotAdjustLengthIfLargePrefixVariableIsNotAvailable(): void
+    {
+        $connection = $this->getConnection(["SHOW VARIABLES LIKE 'innodb_large_prefix'" => false]);
+
+        $schema = $this->getSchema();
+
+        $event = new GenerateSchemaEventArgs($this->createMock(EntityManagerInterface::class), $schema);
+
+        (new AdjustSearchUrlLengthListener($connection))($event);
+
+        $this->assertSame(2048, $schema->getTable('tl_search')->getColumn('url')->getLength());
+    }
+
+    public function testDoesNotAdjustLengthIfServerVersionIsHighEnough(): void
+    {
+        $connection = $this->getConnection([
+            "SHOW VARIABLES LIKE 'innodb_large_prefix'" => ['Value' => 'on'],
+            'SELECT @@version as Value' => ['Value' => '5.7.7'],
+        ]);
+
+        $schema = $this->getSchema();
+
+        $event = new GenerateSchemaEventArgs($this->createMock(EntityManagerInterface::class), $schema);
+
+        (new AdjustSearchUrlLengthListener($connection))($event);
+
+        $this->assertSame(2048, $schema->getTable('tl_search')->getColumn('url')->getLength());
+    }
+
+    private function getConnection(array $returnMap = []): Connection
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->exactly(count($returnMap)))
+            ->method('fetchAssociative')
+            ->willReturnCallback(
+                static function (string $query) use ($returnMap) {
+                    return $returnMap[$query] ?? null;
+                }
+            )
+        ;
+
+        return $connection;
+    }
+
+    public function testAdjustsLengthIfLargePrefixIsDisabled(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->exactly(2))
+            ->method('fetchAssociative')
+            ->willReturnCallback(
+                static function (string $query) {
+                    switch ($query) {
+                        case "SHOW VARIABLES LIKE 'innodb_large_prefix'": return ['Value' => 'off'];
+                        case 'SELECT @@version as Value': return ['Value' => '5.1'];
+                    }
+
+                    return null;
+                }
+            )
+        ;
+
+        $schema = $this->getSchema();
+
+        $event = new GenerateSchemaEventArgs($this->createMock(EntityManagerInterface::class), $schema);
+
+        (new AdjustSearchUrlLengthListener($connection))($event);
+
+        $this->assertSame(767, $schema->getTable('tl_search')->getColumn('url')->getLength());
+    }
+
+    private function getSchema(array $fieldOptions = [], array $tableOptions = [])
+    {
+        $fieldOptions = array_merge([
+            'length' => 2048,
+            'platformOptions' => ['collation' => 'ascii_bin']
+        ], $fieldOptions);
+
+        $tableOptions = array_merge([
+            'charset' => 'utf8mb4', 
+            'engine' => 'InnoDB', 
+            'row_format' => 'DYNAMIC',
+        ], $tableOptions);
+
+        $column = new Column('url', new StringType(), $fieldOptions);
+        $index = new Index('url', ['url'], true);
+        $table = new Table('tl_search', [$column], [$index], [], 0, $tableOptions);
+        $schema = new Schema([$table]);
+
+        return $schema;
+    }
+}
