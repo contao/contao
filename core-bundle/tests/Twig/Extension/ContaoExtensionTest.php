a37a930c908da6103072b3bdb35afaf45a87f239
M. Vondano <m-vo@users.noreply.github.com>
2022-05-10 19:42:21 +0200

    implement a token parser and compiler for the "{% add %}" Twig node

--- core-bundle/tests/Twig/Extension/ContaoExtensionTest.php
+++ core-bundle/tests/Twig/Extension/ContaoExtensionTest.php
@@ -23,6 +23,7 @@ use Contao\CoreBundle\Twig\Inheritance\DynamicIncludeTokenParser;
 use Contao\CoreBundle\Twig\Inheritance\TemplateHierarchyInterface;
 use Contao\CoreBundle\Twig\Interop\ContaoEscaperNodeVisitor;
 use Contao\CoreBundle\Twig\Interop\PhpTemplateProxyNodeVisitor;
+use Contao\CoreBundle\Twig\ResponseContext\AddTokenParser;
 use Contao\System;
 use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Component\Filesystem\Path;
@@ -65,10 +66,11 @@ class ContaoExtensionTest extends TestCase
     {
         $tokenParsers = $this->getContaoExtension()->getTokenParsers();
 
-        $this->assertCount(2, $tokenParsers);
+        $this->assertCount(3, $tokenParsers);
 
         $this->assertInstanceOf(DynamicExtendsTokenParser::class, $tokenParsers[0]);
         $this->assertInstanceOf(DynamicIncludeTokenParser::class, $tokenParsers[1]);
+        $this->assertInstanceOf(AddTokenParser::class, $tokenParsers[2]);
     }
 
     public function testAddsTheFunctions(): void
