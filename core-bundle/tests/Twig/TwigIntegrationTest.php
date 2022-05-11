5b49223dc0a8c39fa2688e09c2d921006358f7ce
M. Vondano <m-vo@users.noreply.github.com>
2022-05-11 11:01:30 +0200

    add a runtime for code highlighting

--- core-bundle/tests/Twig/TwigIntegrationTest.php
+++ core-bundle/tests/Twig/TwigIntegrationTest.php
@@ -17,6 +17,7 @@ use Contao\CoreBundle\Tests\TestCase;
 use Contao\CoreBundle\Twig\Extension\ContaoExtension;
 use Contao\CoreBundle\Twig\Inheritance\TemplateHierarchyInterface;
 use Contao\CoreBundle\Twig\Interop\ContextFactory;
+use Contao\CoreBundle\Twig\Runtime\HighlighterRuntime;
 use Contao\FormText;
 use Contao\System;
 use Contao\TemplateLoader;
@@ -24,6 +25,7 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 use Twig\Environment;
 use Twig\Loader\ArrayLoader;
+use Twig\RuntimeLoader\FactoryRuntimeLoader;
 
 class TwigIntegrationTest extends TestCase
 {
@@ -112,6 +114,47 @@ class TwigIntegrationTest extends TestCase
                 'style' => '',
                 'headline' => 'Test headline',
                 'text' => 'Some text',
+            ]
+        );
+
+        $this->assertSame($expectedOutput, $output);
+    }
+
+    public function testHighlightsCode(): void
+    {
+        $templateContent = <<<'TEMPLATE'
+            <h2>js</h2>
+            <pre>
+                {{ code|highlight('js') }}
+            </pre>
+
+            {% set highlighted = code|highlight_auto(['php', 'c++']) %}
+            <h2>{{ highlighted.language }}</h2>
+            <pre>
+                {{ highlighted }}
+            </pre>
+            TEMPLATE;
+
+        $expectedOutput = <<<'TEMPLATE'
+            <h2>js</h2>
+            <pre>
+                <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">foo</span>(<span class="hljs-params"></span>) </span>{ <span class="hljs-keyword">return</span> <span class="hljs-string">"&lt;b&gt;ar"</span>; };
+            </pre>
+
+            <h2>php</h2>
+            <pre>
+                <span class="hljs-function"><span class="hljs-keyword">function</span> <span class="hljs-title">foo</span><span class="hljs-params">()</span> </span>{ <span class="hljs-keyword">return</span> <span class="hljs-string">"&lt;b&gt;ar"</span>; };
+            </pre>
+            TEMPLATE;
+
+        $environment = new Environment(new ArrayLoader(['test.html.twig' => $templateContent]));
+        $environment->addExtension(new ContaoExtension($environment, $this->createMock(TemplateHierarchyInterface::class)));
+        $environment->addRuntimeLoader(new FactoryRuntimeLoader([HighlighterRuntime::class => static fn () => new HighlighterRuntime()]));
+
+        $output = $environment->render(
+            'test.html.twig',
+            [
+                'code' => 'function foo() { return "<b>ar"; };',
             ]
         );
 
