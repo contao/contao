6655cc2df6277351268ee12bddb248d023a4ec08
Yanick Witschi <yanick.witschi@terminal42.ch>
2022-05-12 07:41:43 +0200

    Vote only on action permissions

--- news-bundle/src/Security/Voter/NewsArchiveAccessVoter.php
+++ news-bundle/src/Security/Voter/NewsArchiveAccessVoter.php
@@ -24,25 +24,11 @@ use Symfony\Component\Security\Core\Security;
 class NewsArchiveAccessVoter extends Voter
 {
     private const SUPPORTED_ATTRIBUTES = [
-        // Action global
         ContaoCorePermissions::DC_ACTION_CREATE,
-
-        // Action on item
         ContaoCorePermissions::DC_ACTION_EDIT,
         ContaoCorePermissions::DC_ACTION_COPY,
         ContaoCorePermissions::DC_ACTION_DELETE,
         ContaoCorePermissions::DC_ACTION_VIEW,
-
-        // View global
-        ContaoCorePermissions::DC_VIEW_CREATE,
-        ContaoCorePermissions::DC_GLOBAL_OPERATION_PREFIX.'feeds',
-
-        // View on item
-        ContaoCorePermissions::DC_OPERATION_PREFIX.'edit',
-        ContaoCorePermissions::DC_OPERATION_PREFIX.'editheader',
-        ContaoCorePermissions::DC_OPERATION_PREFIX.'copy',
-        ContaoCorePermissions::DC_OPERATION_PREFIX.'delete',
-        ContaoCorePermissions::DC_OPERATION_PREFIX.'show',
     ];
 
     public function __construct(private ContaoFramework $contaoFramework, private Security $security)
@@ -59,34 +45,11 @@ class NewsArchiveAccessVoter extends Voter
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
     {
         return match ($attribute) {
-            // Actions
             ContaoCorePermissions::DC_ACTION_CREATE => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_CREATE_ARCHIVES),
             ContaoCorePermissions::DC_ACTION_EDIT,
             ContaoCorePermissions::DC_ACTION_COPY,
             ContaoCorePermissions::DC_ACTION_VIEW, => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_EDIT_ARCHIVE, $subject->id),
             ContaoCorePermissions::DC_ACTION_DELETE => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_DELETE_ARCHIVES),
-
-            // View global
-            ContaoCorePermissions::DC_VIEW_CREATE => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_CREATE_ARCHIVES),
-            ContaoCorePermissions::DC_GLOBAL_OPERATION_PREFIX.'feeds' => $this->canDisplayFeedOperation($token),
-
-            // View item
-            ContaoCorePermissions::DC_OPERATION_PREFIX.'edit',
-            ContaoCorePermissions::DC_OPERATION_PREFIX.'show' => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_EDIT_ARCHIVE, $subject->id),
-            ContaoCorePermissions::DC_OPERATION_PREFIX.'editheader' => $this->security->isGranted(ContaoCorePermissions::USER_CAN_EDIT_FIELDS_OF_TABLE, 'tl_news_archive'),
-            ContaoCorePermissions::DC_OPERATION_PREFIX.'copy' => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_CREATE_ARCHIVES),
-            ContaoCorePermissions::DC_OPERATION_PREFIX.'delete' => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_DELETE_ARCHIVES),
         };
-    }
-
-    private function canDisplayFeedOperation(TokenInterface $token): bool
-    {
-        $user = $token->getUser();
-
-        if (!$user instanceof BackendUser) {
-            return false;
-        }
-
-        return $user->isAdmin || !empty($user->newsfeeds) || !empty($user->newsfeedp);
     }
 }
