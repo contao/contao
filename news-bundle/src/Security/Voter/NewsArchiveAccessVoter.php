aea071b0353b60c98fa8ef4b10282e56874e7bee
Yanick Witschi <yanick.witschi@terminal42.ch>
2022-05-12 08:27:37 +0200

    Update news-bundle/src/Security/Voter/NewsArchiveAccessVoter.php
    Co-authored-by: Andreas Schempp <andreas.schempp@terminal42.ch>

--- news-bundle/src/Security/Voter/NewsArchiveAccessVoter.php
+++ news-bundle/src/Security/Voter/NewsArchiveAccessVoter.php
@@ -46,7 +46,7 @@ class NewsArchiveAccessVoter extends Voter
         return match ($attribute) {
             ContaoCorePermissions::DC_ACTION_CREATE => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_CREATE_ARCHIVES),
             ContaoCorePermissions::DC_ACTION_COPY => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_CREATE_ARCHIVES) &&
-                $this->security->isGranted(ContaoNewsPermissions::USER_CAN_DELETE_ARCHIVES),
+                $this->security->isGranted(ContaoNewsPermissions::USER_CAN_EDIT_ARCHIVE, $subject->id),
             ContaoCorePermissions::DC_ACTION_EDIT,
             ContaoCorePermissions::DC_ACTION_VIEW => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_EDIT_ARCHIVE, $subject->id),
             ContaoCorePermissions::DC_ACTION_DELETE => $this->security->isGranted(ContaoNewsPermissions::USER_CAN_EDIT_ARCHIVE, $subject->id) &&
