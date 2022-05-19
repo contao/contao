4aa1c90743ea59f7c15bffdb49212b5594a949c1
David Greminger <david.greminger@1up.io>
2022-05-19 14:35:25 +0100

    Remove hook addLogEntry and it's usages

--- core-bundle/tests/Monolog/ContaoTableHandlerTest.php
+++ core-bundle/tests/Monolog/ContaoTableHandlerTest.php
@@ -43,13 +43,8 @@ class ContaoTableHandlerTest extends TestCase
         $this->assertSame('foobar', $handler->getDbalServiceName());
     }
 
-    /**
-     * @group legacy
-     */
     public function testHandlesContaoRecords(): void
     {
-        $this->expectDeprecation('Since contao/core-bundle 4.0: Using the "addLogEntry" hook has been deprecated %s.');
-
         $record = [
             'level' => Logger::DEBUG,
             'level_name' => 'DEBUG',
@@ -72,23 +67,13 @@ class ContaoTableHandlerTest extends TestCase
             ->willReturn($statement)
         ;
 
-        $system = $this->mockConfiguredAdapter(['importStatic' => $this]);
-
         $container = $this->getContainerWithContaoConfiguration();
-        $container->set('contao.framework', $this->mockContaoFramework([System::class => $system]));
         $container->set('doctrine.dbal.default_connection', $connection);
-
-        $GLOBALS['TL_HOOKS']['addLogEntry'][] = [static::class, 'addLogEntry'];
 
         $handler = new ContaoTableHandler();
         $handler->setContainer($container);
 
         $this->assertFalse($handler->handle($record));
-    }
-
-    public function addLogEntry(): void
-    {
-        // Dummy method to test the addLogEntry hook
     }
 
     public function testDoesNotHandleARecordIfTheLogLevelDoesNotMatch(): void
