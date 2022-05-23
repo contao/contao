1e1c1aa8c6ba2c653369b3fa31256dfcf756cc46
M. Vondano <m-vo@users.noreply.github.com>
2022-05-23 21:08:20 +0200

    add a helper to easily get filesystem items from a serialized blob of UUIDs

--- core-bundle/tests/Filesystem/FilesystemUtilTest.php
+++ core-bundle/tests/Filesystem/FilesystemUtilTest.php
@@ -12,11 +12,104 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Tests\Filesystem;
 
+use Contao\CoreBundle\Filesystem\FilesystemItem;
+use Contao\CoreBundle\Filesystem\FilesystemItemIterator;
 use Contao\CoreBundle\Filesystem\FilesystemUtil;
+use Contao\CoreBundle\Filesystem\VirtualFilesystemInterface;
 use Contao\CoreBundle\Tests\TestCase;
+use Symfony\Component\Uid\Uuid;
 
 class FilesystemUtilTest extends TestCase
 {
+    /**
+     * @dataProvider provideResources
+     */
+    public function testListContentsFromMultiSRC(string|array $sources, array $expectedPaths): void
+    {
+        $storage = $this->createMock(VirtualFilesystemInterface::class);
+        $storage
+            ->method('get')
+            ->willReturnCallback(
+                static function (Uuid $uuid): FilesystemItem|null {
+                    return match ($uuid->toRfc4122()) {
+                        'd22b1ea8-dcab-4162-b690-30cb9206f694' => new FilesystemItem(true, 'file1'),
+                        'b1817d6d-188a-4c99-9204-b1e33733d5a9' => new FilesystemItem(true, 'file2'),
+                        '0af407bc-ced3-4688-9971-f30dca7005b6' => new FilesystemItem(true, 'directory/file3'),
+                        'f0f4bde3-2e1f-48cb-9182-ba804868d93b' => new FilesystemItem(true, 'directory/file4'),
+                        '1fc6c283-c0c8-420e-b1c7-712d388a6b3a' => new FilesystemItem(false, 'directory'),
+                        default => null,
+                    };
+                }
+            )
+        ;
+
+        $storage
+            ->method('listContents')
+            ->with('directory')
+            ->willReturn(
+                new FilesystemItemIterator([
+                    new FilesystemItem(true, 'directory/file3'),
+                    new FilesystemItem(true, 'directory/file4'),
+                    new FilesystemItem(false, 'directory/subdirectory'),
+                ])
+            )
+        ;
+
+        $paths = array_map(
+            function (FilesystemItem $item): string {
+                $this->assertTrue($item->isFile());
+
+                return $item->getPath();
+            },
+            FilesystemUtil::listContentsFromMultiSRC($storage, $sources)->toArray()
+        );
+
+        $this->assertSame($expectedPaths, $paths);
+    }
+
+    public function provideResources(): \Generator
+    {
+        $file1 = (new Uuid('d22b1ea8-dcab-4162-b690-30cb9206f694'))->toBinary();
+        $file2 = (new Uuid('b1817d6d-188a-4c99-9204-b1e33733d5a9'))->toBinary();
+        $file3 = (new Uuid('0af407bc-ced3-4688-9971-f30dca7005b6'))->toBinary();
+        $directory = (new Uuid('1fc6c283-c0c8-420e-b1c7-712d388a6b3a'))->toBinary();
+
+        yield 'single file' => [
+            $file1,
+            ['file1'],
+        ];
+
+        yield 'multiple files' => [
+            [$file1, $file2],
+            ['file1', 'file2'],
+        ];
+
+        yield 'files and directories' => [
+            [$file1, $directory],
+            ['file1', 'directory/file3', 'directory/file4'],
+        ];
+
+        yield 'duplicate files' => [
+            [$file1, $directory, $file1, $file3],
+            ['file1', 'directory/file3', 'directory/file4'],
+        ];
+
+        yield 'unknown UUID amongst valid' => [
+            [$file1, (new Uuid('a1695de1-90a8-486c-9e2f-e0567cd9c6ab'))->toBinary()],
+            ['file1'],
+        ];
+
+        yield 'unknown UUID' => [
+            '',
+            [],
+        ];
+
+        yield 'no files' => [
+            [],
+            [],
+        ];
+    }
+
     /**
      * @dataProvider provideInvalidArguments
      */
