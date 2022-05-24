a23a60c641e4f826ca140e9dc28fc73328379ede
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-05-24 08:02:16 +0200

    Use the table from hook instead of relying on DC

--- core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
+++ core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
@@ -81,14 +81,14 @@ class DefaultOperationsListener
                 'editheader' => [
                     'href' => 'act=edit',
                     'icon' => 'header.svg',
-                    'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_EDIT),
+                    'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_EDIT, $table),
                 ],
             ];
         } else {
             $operations['edit'] = [
                 'href' => 'act=edit',
                 'icon' => 'edit.svg',
-                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_EDIT),
+                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_EDIT, $table),
             ];
         }
 
@@ -97,7 +97,7 @@ class DefaultOperationsListener
                 'href' => 'act=paste&amp;mode=copy',
                 'icon' => 'copy.svg',
                 'attributes' => 'onclick="Backend.getScrollOffset()"',
-                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_COPY),
+                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_COPY, $table),
             ];
 
             if ($isTreeMode) {
@@ -105,7 +105,7 @@ class DefaultOperationsListener
                     'href' => 'act=paste&amp;mode=copy&amp;childs=1',
                     'icon' => 'copychilds.svg',
                     'attributes' => 'onclick="Backend.getScrollOffset()"',
-                    'button_callback' => $this->copyChildsButtonCallback(),
+                    'button_callback' => $this->copyChildsButtonCallback($table),
                 ];
             }
 
@@ -113,13 +113,13 @@ class DefaultOperationsListener
                 'href' => 'act=paste&amp;mode=cut',
                 'icon' => 'cut.svg',
                 'attributes' => 'onclick="Backend.getScrollOffset()"',
-                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_MOVE),
+                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_MOVE, $table),
             ];
         } else {
             $operations['copy'] = [
                 'href' => 'act=copy',
                 'icon' => 'copy.svg',
-                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_COPY),
+                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_COPY, $table),
             ];
         }
 
@@ -128,7 +128,7 @@ class DefaultOperationsListener
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
                 'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
-                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_DELETE),
+                'button_callback' => $this->isGrantedButtonCallback(ContaoCorePermissions::DC_ACTION_DELETE, $table),
             ],
             'show' => [
                 'href' => 'act=show',
@@ -137,25 +137,25 @@ class DefaultOperationsListener
         ];
     }
 
-    private function isGrantedButtonCallback(string $attribute): \Closure
+    private function isGrantedButtonCallback(string $attribute, string $table): \Closure
     {
-        return function (DataContainerOperation $operation) use ($attribute): void {
-            if (!$this->isGranted($attribute, $operation)) {
+        return function (DataContainerOperation $operation) use ($attribute, $table): void {
+            if (!$this->isGranted($attribute, $table, $operation)) {
                 $this->disableOperation($operation);
             }
         };
     }
 
-    private function copyChildsButtonCallback(): \Closure
+    private function copyChildsButtonCallback(string $table): \Closure
     {
-        return function (DataContainerOperation $operation): void {
-            if (!$this->isGranted(ContaoCorePermissions::DC_ACTION_COPY, $operation)) {
+        return function (DataContainerOperation $operation) use ($table): void {
+            if (!$this->isGranted(ContaoCorePermissions::DC_ACTION_COPY, $table, $operation)) {
                 $this->disableOperation($operation);
                 return;
             }
 
             $childCount = $this->connection->fetchOne(
-                "SELECT COUNT(*) FROM {$operation->getDataContainer()->table} WHERE pid=?",
+                "SELECT COUNT(*) FROM $table WHERE pid=?",
                 [(string) $operation->getRecord()['id']]
             );
 
@@ -165,9 +165,9 @@ class DefaultOperationsListener
         };
     }
 
-    private function isGranted(string $attribute, DataContainerOperation $operation): bool
+    private function isGranted(string $attribute, string $table, DataContainerOperation $operation): bool
     {
-        $subject = new DataContainerSubject($operation->getDataContainer()->table, rawurldecode((string) $operation->getRecord()['id']));
+        $subject = new DataContainerSubject($table, rawurldecode((string) $operation->getRecord()['id']));
 
         return $this->security->isGranted($attribute, $subject);
     }
