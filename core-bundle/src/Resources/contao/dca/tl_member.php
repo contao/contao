75d68c53d543cc809fdbc378b959edeb086db728
Martin Auswöger <martin@auswoeger.com>
2022-05-25 17:56:54 +0200

    Use uppercase country codes for tl_member

--- core-bundle/src/Resources/contao/dca/tl_member.php
+++ core-bundle/src/Resources/contao/dca/tl_member.php
@@ -218,13 +218,7 @@ $GLOBALS['TL_DCA']['tl_member'] = array
 			'sorting'                 => true,
 			'inputType'               => 'select',
 			'eval'                    => array('includeBlankOption'=>true, 'chosen'=>true, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'address', 'tl_class'=>'w50'),
-			'options_callback' => static function ()
-			{
-				$countries = System::getContainer()->get('contao.intl.countries')->getCountries();
-
-				// Convert to lower case for backwards compatibility, to be changed in Contao 5.0
-				return array_combine(array_map('strtolower', array_keys($countries)), $countries);
-			},
+			'options_callback'        => static fn () => System::getContainer()->get('contao.intl.countries')->getCountries(),
 			'sql'                     => "varchar(2) NOT NULL default ''"
 		),
 		'phone' => array
