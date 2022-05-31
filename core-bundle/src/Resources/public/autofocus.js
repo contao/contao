b4b4b733ab915ced966e20ea3f073d28bc27d52f
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-05-31 11:49:58 +0200

    Use attributes

--- core-bundle/src/Resources/public/autofocus.js
+++ core-bundle/src/Resources/public/autofocus.js
@@ -10,25 +10,19 @@ window.addEventListener('DOMContentLoaded', function () {
     const edit = document.querySelector('#main .tl_formbody_edit');
     if (!edit) return;
 
-    // Copy from MooTools
-    function isHidden (el) {
-        const w = el.offsetWidth, h = el.offsetHeight,
-            force = /^tr$/i.test(el.tagName);
-        return (w===0 && h===0 && !force) ? true : (w!==0 && h!==0 && !force) ? false : el.style.display === 'none';
-    }
-
     const inputs = edit.querySelectorAll('input, textarea');
 
     for (let i = 0; i < inputs.length; i++) {
         const input = inputs[i];
 
         if (
-            !input.getAttribute('disabled')
-            && !input.getAttribute('readonly')
-            && !isHidden(input)
-            && !['checkbox', 'radio', 'submit', 'image'].includes(input.getAttribute('type'))
+            !input.disabled
+            && !input.readonly
+            && input.offsetWidth
+            && input.offsetHeight
+            && !['checkbox', 'radio', 'submit', 'image'].includes(input.type)
             && !input.closest('.chzn-search')
-            && (!input.hasAttribute('autocomplete') || input.getAttribute('autocomplete') === 'off' || !input.value)
+            && (input.autocomplete === undefined || input.autocomplete === 'off' || !input.value)
         ) {
             input.focus();
             break;
