cd6d05b453a1436a1a678402b060195b4112b35f
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-05-31 11:44:13 +0200

    Correctly support legacy DC name in picker provider (see #4711)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    822f6628 Correctly support legacy DC name in picker provider
    9424537b Cache the DataContainer name

--- core-bundle/src/Picker/AbstractTablePickerProvider.php
+++ core-bundle/src/Picker/AbstractTablePickerProvider.php
@@ -137,7 +137,9 @@ abstract class AbstractTablePickerProvider implements PickerProviderInterface, D
         $this->framework->initialize();
         $this->framework->createInstance(DcaLoader::class, [$table])->load();
 
-        return $this->getDataContainer() === DataContainer::getDriverForTable($table)
+        $dcName = $this->getDataContainer();
+
+        return ($dcName === DataContainer::getDriverForTable($table) || $dcName === $GLOBALS['TL_DCA'][$table]['config']['dataContainer'])
             && 0 !== \count($this->getModulesForTable($table));
     }
 
