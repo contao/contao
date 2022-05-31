9424537b68da0134f578619579cfd818f087550a
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-05-31 11:41:41 +0200

    Cache the DataContainer name

--- core-bundle/src/Picker/AbstractTablePickerProvider.php
+++ core-bundle/src/Picker/AbstractTablePickerProvider.php
@@ -137,9 +137,10 @@ abstract class AbstractTablePickerProvider implements PickerProviderInterface, D
         $this->framework->initialize();
         $this->framework->createInstance(DcaLoader::class, [$table])->load();
 
-        return ($this->getDataContainer() === DataContainer::getDriverForTable($table)
-                || $this->getDataContainer() === $GLOBALS['TL_DCA'][$table]['config']['dataContainer']
-            ) && 0 !== \count($this->getModulesForTable($table));
+        $dcName = $this->getDataContainer();
+
+        return ($dcName === DataContainer::getDriverForTable($table) || $dcName === $GLOBALS['TL_DCA'][$table]['config']['dataContainer'])
+            && 0 !== \count($this->getModulesForTable($table));
     }
 
     public function supportsValue(PickerConfig $config): bool
