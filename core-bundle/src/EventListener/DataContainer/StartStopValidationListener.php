9245ba27c1faa39df708867599134062c6bdd216
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-06-15 09:11:31 +0200

    CS

--- core-bundle/src/EventListener/DataContainer/StartStopValidationListener.php
+++ core-bundle/src/EventListener/DataContainer/StartStopValidationListener.php
@@ -23,8 +23,8 @@ class StartStopValidationListener
 
     public function __invoke(array $values, DataContainer $dc): array
     {
-        $start = (string) (array_key_exists('start', $values) ? $values['start'] : $dc->activeRecord->start);
-        $stop = (string) (array_key_exists('stop', $values) ? $values['stop'] : $dc->activeRecord->stop);
+        $start = (string) (\array_key_exists('start', $values) ? $values['start'] : $dc->activeRecord->start);
+        $stop = (string) (\array_key_exists('stop', $values) ? $values['stop'] : $dc->activeRecord->stop);
 
         if ('' !== $start && '' !== $stop && $stop < $start) {
             throw new \RuntimeException($this->translator->trans('ERR.startStop', [], 'contao_default'));
