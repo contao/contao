481b8010cb35935bcba707ae75a9f1642e64fb53
Yanick Witschi <yanick.witschi@terminal42.ch>
2022-06-22 14:07:43 +0200

    Update core-bundle/src/Resources/contao/classes/DataContainer.php
    Co-authored-by: Andreas Schempp <andreas.schempp@terminal42.ch>

--- core-bundle/src/Resources/contao/classes/DataContainer.php
+++ core-bundle/src/Resources/contao/classes/DataContainer.php
@@ -326,7 +326,7 @@ abstract class DataContainer extends Backend
 				return $this->strPalette;
 
 			case 'activeRecord':
-				trigger_deprecation('contao/core-bundle', '5.0', 'The active record has been deprecated and will be removed in Contao 6. Use $dc->getCurrentRecord() instead.');
+				trigger_deprecation('contao/core-bundle', '5.0', 'The active record has been deprecated and will be removed in Contao 6. Use '.__CLASS__.'::getCurrentRecord() instead.');
 
 				return $this->objActiveRecord;
 
