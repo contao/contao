5df9e37c9b8b5733e64e43c2c955df18443b3c4f
David Greminger <david.greminger@1up.io>
2022-06-23 00:16:32 +0200

    CS

--- core-bundle/src/Security/Authentication/RememberMe/ExpiringTokenBasedRememberMeHandler.php
+++ core-bundle/src/Security/Authentication/RememberMe/ExpiringTokenBasedRememberMeHandler.php
@@ -53,7 +53,7 @@ class ExpiringTokenBasedRememberMeHandler extends AbstractRememberMeHandler
     {
         $series = base64_encode(random_bytes(64));
         $tokenValue = $this->generateHash(base64_encode(random_bytes(64)));
-        $token = new PersistentToken(\get_class($user), $user->getUserIdentifier(), $series, $tokenValue, new \DateTime());
+        $token = new PersistentToken($user::class, $user->getUserIdentifier(), $series, $tokenValue, new \DateTime());
         $rememberMe = new RememberMe($user, $series);
 
         $this->repository->persist($rememberMe);
