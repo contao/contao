2e5b923f7e955d58a04037d2a440afff356b6d94
David Greminger <david.greminger@1up.io>
2022-06-22 21:40:06 +0200

    CS

--- core-bundle/src/Security/Authenticator/ContaoLoginAuthenticator.php
+++ core-bundle/src/Security/Authenticator/ContaoLoginAuthenticator.php
@@ -45,7 +45,6 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\ParameterBagUtils;
 
