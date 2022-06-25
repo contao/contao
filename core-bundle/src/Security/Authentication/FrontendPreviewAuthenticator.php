bf0a177811c3a72246e0646e1b6cbd662025d313
David Greminger <david.greminger@1up.io>
2022-06-25 12:41:38 +0200

    CS

--- core-bundle/src/Security/Authentication/FrontendPreviewAuthenticator.php
+++ core-bundle/src/Security/Authentication/FrontendPreviewAuthenticator.php
@@ -24,7 +24,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class FrontendPreviewAuthenticator
 {
-    public final const SESSION_NAME = '_contao_frontend_preview';
+    final public const SESSION_NAME = '_contao_frontend_preview';
     /**
      * @internal Do not inherit from this class; decorate the "contao.security.frontend_preview_authenticator" service instead
      */
