54d7e592c6c14be56323293d3b1aa990571dcfb5
fritzmg <fmg@inspiredminds.at>
2022-06-27 09:21:51 +0100

    simplify code again

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -2690,7 +2690,7 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 		Input::setPost('FORM_SUBMIT', $this->strTable);
 		$this->varValue = $objRow->{$this->strField};
 
-		$this->save($this->varValue ? false : true);
+		$this->save(!$this->varValue);
 		$this->submit();
 
 		$this->redirect($this->getReferer());
