7e8122e932ef3fe7bc586380eccddffe20f14bd6
M. Vondano <m-vo@users.noreply.github.com>
2022-07-04 13:10:21 +0200

    add generic type hints

--- core-bundle/src/Util/CachingTraversable.php
+++ core-bundle/src/Util/CachingTraversable.php
@@ -3,20 +3,34 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Util;
 
+/**
+ * @template TKey
+ * @template TValue
+ * @implements \IteratorAggregate<TKey, TValue>
+ */
 class CachingTraversable implements \IteratorAggregate
 {
     /**
-     * @var list<array{0:mixed,1:mixed}>
+     * @var list<array{0:TKey, 1:TValue}>
      */
     private array $items = [];
 
+    /**
+     * @var \IteratorIterator<TKey, TValue, \Traversable<TKey, TValue>>
+     */
     private \IteratorIterator $iterator;
 
+    /**
+     * @param \Traversable<TKey, TValue> $traversable
+     */
     public function __construct(\Traversable $traversable)
     {
         $this->iterator = new \IteratorIterator($traversable);
     }
 
+    /**
+     * @return \Generator<TKey, TValue>
+     */
     public function getIterator(): \Generator
     {
         $current = 0;
