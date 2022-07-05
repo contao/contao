51a4b3c3ea01f6808342b84e2ebe99f84555e883
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-07-05 08:35:47 +0200

    Update core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
    Co-authored-by: M. Vondano <m-vo@users.noreply.github.com>

--- core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
+++ core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
@@ -205,7 +205,8 @@ class DefaultOperationsListener
         $subject = match ($actionClass) {
             CreateAction::class => new CreateAction($table),
             UpdateAction::class => new UpdateAction($table, $operation->getRecord()),
-            DeleteAction::class => new DeleteAction($table, $operation->getRecord())
+            DeleteAction::class => new DeleteAction($table, $operation->getRecord()),
+            default => throw new \InvalidArgumentException(sprinf('Invalid action class "%s".', $actionClass)),
         };
 
         return $this->security->isGranted(ContaoCorePermissions::DC_PREFIX.$table, $subject);
