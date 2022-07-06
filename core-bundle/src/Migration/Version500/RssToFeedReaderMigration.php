4322e7492ef94891ce50e1e343852ff42430a179
Benedict Massolle <bm@presentprogressive.de>
2022-07-06 20:05:24 +0200

    Refactor RssToFeedReaderMigration

--- core-bundle/src/Migration/Version500/RssToFeedReaderMigration.php
+++ core-bundle/src/Migration/Version500/RssToFeedReaderMigration.php
@@ -27,31 +27,29 @@ class RssToFeedReaderMigration extends AbstractMigration
         $schemaManager = $this->connection->createSchemaManager();
         $columns = $schemaManager->listTableColumns('tl_module');
 
-        return \array_key_exists('rss_feed', $columns) || \array_key_exists('rss_cache', $columns);
+        if (!$schemaManager->tablesExist(['tl_news_feed'])) {
+            return false;
+        }
+
+        return \array_key_exists('rss_feed', $columns) && \array_key_exists('rss_cache', $columns)
+            && !\array_key_exists('feed_urls', $columns) && !\array_key_exists('feed_cache', $columns);
     }
 
     public function run(): MigrationResult
     {
-        $this->renameField(
-            $this->connection->quoteIdentifier('rss_feed'),
-            $this->connection->quoteIdentifier('feed_urls'),
-            'text NULL'
-        );
-
-        $this->renameField(
-            $this->connection->quoteIdentifier('rss_cache'),
-            $this->connection->quoteIdentifier('feed_cache'),
-            'int(10) unsigned NOT NULL default 3600'
-        );
+        $this->renameField('rss_feed', 'feed_urls', 'text NULL');
+        $this->renameField('rss_cache', 'feed_cache', 'int(10) unsigned NOT NULL default 3600');
 
         $this->connection->update('tl_module', ['type' => 'feed_reader'], ['type' => 'rssReader']);
 
         return $this->createResult(true);
     }
 
-    private function renameField(string $fromQuoted, string $toQuoted, string $type): void
+    private function renameField(string $from, string $to, string $type): void
     {
         $tableQuoted = $this->connection->quoteIdentifier('tl_module');
+        $fromQuoted = $this->connection->quoteIdentifier($from);
+        $toQuoted = $this->connection->quoteIdentifier($to);
         $this->connection->executeQuery("ALTER TABLE $tableQuoted CHANGE $fromQuoted $toQuoted $type");
     }
 }
