f132755ac5c774e6b282a85ceeba99c6a3172602
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-07-15 15:48:34 +0200

    Validate start/stop date (see #4834)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    7c327f1d Validate start/stop date
    cf8c8b0d Adjust the error message
    9c3b505c Use the @Callback annotation to tag the listener
    82b2e107 Added tests
    b390366d Use PHP 8 attributes instead of service annotations
    d4f3cd1c Do not use the time() function in the unit tests
    
    Co-authored-by: Leo Feyer <github@contao.org>

---
+++ core-bundle/src/EventListener/DataContainer/StartStopValidationListener.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[AsCallback(table: 'tl_article', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_content', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_page', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_member', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_member_group', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_user', target: 'config.onbeforesubmit')]
+#[AsCallback(table: 'tl_user_group', target: 'config.onbeforesubmit')]
+class StartStopValidationListener
+{
+    public function __construct(private TranslatorInterface $translator)
+    {
+    }
+
+    public function __invoke(array $values, DataContainer $dc): array
+    {
+        $hasStart = \array_key_exists('start', $values);
+        $hasStop = \array_key_exists('stop', $values);
+
+        // Both fields were not submitted, do not validate since the user cannot change it anyway
+        if (!$hasStart && !$hasStop) {
+            return $values;
+        }
+
+        $start = (string) ($hasStart ? $values['start'] : $dc->getCurrentRecord()['start']);
+        $stop = (string) ($hasStop ? $values['stop'] : $dc->getCurrentRecord()['stop']);
+
+        if ('' !== $start && '' !== $stop && $stop < $start) {
+            throw new \RuntimeException($this->translator->trans('ERR.startStop', [], 'contao_default'));
+        }
+
+        return $values;
+    }
+}
