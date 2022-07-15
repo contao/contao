9c3b505cea0d49ed71913e38d0e284ff761b24e1
Leo Feyer <github@contao.org>
2022-07-15 10:46:16 +0200

    Use the @Callback annotation to tag the listener

--- core-bundle/src/EventListener/DataContainer/StartStopValidationListener.php
+++ core-bundle/src/EventListener/DataContainer/StartStopValidationListener.php
@@ -12,9 +12,19 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\EventListener\DataContainer;
 
+use Contao\CoreBundle\ServiceAnnotation\Callback;
 use Contao\DataContainer;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+/**
+ * @Callback(table="tl_article", target="config.onbeforesubmit")
+ * @Callback(table="tl_content", target="config.onbeforesubmit")
+ * @Callback(table="tl_page", target="config.onbeforesubmit")
+ * @Callback(table="tl_member", target="config.onbeforesubmit")
+ * @Callback(table="tl_member_group", target="config.onbeforesubmit")
+ * @Callback(table="tl_user", target="config.onbeforesubmit")
+ * @Callback(table="tl_user_group", target="config.onbeforesubmit")
+ */
 class StartStopValidationListener
 {
     public function __construct(private TranslatorInterface $translator)
