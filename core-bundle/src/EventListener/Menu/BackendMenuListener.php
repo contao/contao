dc6982189f06483e3e2cbcfb85e82346cd4ce41f
Leo Feyer <github@contao.org>
2022-07-19 16:37:10 +0200

    Update the backend look

--- core-bundle/src/EventListener/Menu/BackendMenuListener.php
+++ core-bundle/src/EventListener/Menu/BackendMenuListener.php
@@ -124,15 +124,9 @@ class BackendMenuListener
 
         $tree->addChild($manual);
 
-        $systemMessages = $this->translator->trans('MSC.systemMessages', [], 'contao_default');
-
         $alerts = $event->getFactory()
             ->createItem('alerts')
-            ->setLabel($this->getAlertsLabel($systemMessages))
-            ->setUri($this->router->generate('contao_backend_alerts'))
-            ->setLinkAttribute('class', 'icon-alert')
-            ->setLinkAttribute('title', $systemMessages)
-            ->setLinkAttribute('onclick', "Backend.openModalIframe({'title':'".StringUtil::specialchars(str_replace("'", "\\'", $systemMessages))."','url':this.href});return false")
+            ->setLabel($this->getAlertsLabel())
             ->setExtra('safe_label', true)
             ->setExtra('translation_domain', false)
         ;
@@ -182,7 +176,7 @@ class BackendMenuListener
 
         $buger = $factory
             ->createItem('burger')
-            ->setLabel('<button type="button" id="burger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12h18M3 6h18M3 18h18"/></svg></button>')
+            ->setLabel('<button type="button" id="burger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 12h18M3 6h18M3 18h18"/></svg></button>')
             ->setAttribute('class', 'burger')
             ->setExtra('safe_label', true)
             ->setExtra('translation_domain', false)
@@ -191,16 +185,26 @@ class BackendMenuListener
         $tree->addChild($buger);
     }
 
-    private function getAlertsLabel(string $systemMessages): string
+    private function getAlertsLabel(): string
     {
+        $systemMessages = $this->translator->trans('MSC.systemMessages', [], 'contao_default');
+
+        $label = sprintf(
+            '<a href="%s" class="icon-alert" title="%s" onclick="Backend.openModalIframe({\'title\':\'%s\',\'url\':this.href});return false">%s</a>',
+            $this->router->generate('contao_backend_alerts'),
+            htmlspecialchars($systemMessages),
+            StringUtil::specialchars(str_replace("'", "\\'", $systemMessages)),
+            $systemMessages
+        );
+
         $adapter = $this->framework->getAdapter(Backend::class);
         $count = substr_count($adapter->getSystemMessages(), 'class="tl_error');
 
         if ($count > 0) {
-            $systemMessages .= ' <sup>'.$count.'</sup>';
+            $label .= '<sup>'.$count.'</sup>';
         }
 
-        return $systemMessages;
+        return $label;
     }
 
     private function getRefererId(): string
