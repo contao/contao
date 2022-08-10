6ff9713141a037b497c1eebe3ff7e48b13570324
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-08-10 23:24:14 +0200

    introduce member activation mail event

---
+++ newsletter-bundle/src/EventListener/MemberActivationMailListener.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\NewsletterBundle\EventListener;
+
+use Contao\CoreBundle\Event\MemberActivationMailEvent;
+use Contao\StringUtil;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Types\Types;
+
+class MemberActivationMailListener
+{
+    public function __construct(private Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function __invoke(MemberActivationMailEvent $event): void
+    {
+        $newsletter = StringUtil::deserialize($event->getMember()->newsletter, true);
+
+        if (empty($newsletter)) {
+            return;
+        }
+
+        $channels = $this->connection->fetchFirstColumn("SELECT title FROM tl_newsletter_channel WHERE id IN (?)", [$newsletter], [Types::SIMPLE_ARRAY]);
+
+        if (empty($channels)) {
+            return;
+        }
+
+        $event->setSimpleToken('channels', implode("\n", $channels));
+    }
+}
