dc0c650f866df1cbb399a1db81424a62372f0590
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-08-11 14:16:06 +0200

    Fixed tests

--- core-bundle/tests/Cron/LegacyCronTest.php
+++ core-bundle/tests/Cron/LegacyCronTest.php
@@ -80,7 +80,7 @@ class LegacyCronTest extends TestCase
         // Create a LegacyCron instance and add cron jobs to the cron service
         $legacyCron = new LegacyCron($framework);
 
-        $cron = new Cron($this->createMock(CronJobRepository::class), $this->createMock(EntityManagerInterface::class));
+        $cron = new Cron($this->fn($this->createMock(CronJobRepository::class)), $this->fn($this->createMock(EntityManagerInterface::class)));
         $cron->addCronJob(new CronJob($legacyCron, '* * * * *', 'onMinutely'));
         $cron->addCronJob(new CronJob($legacyCron, '@hourly', 'onHourly'));
         $cron->addCronJob(new CronJob($legacyCron, '@daily', 'onDaily'));
@@ -105,5 +105,12 @@ class LegacyCronTest extends TestCase
         $legacyCron->onDaily();
         $legacyCron->onWeekly();
         $legacyCron->onMonthly();
+    }
+
+    private function fn($service)
+    {
+        return static function () use ($service) {
+            return $service;
+        };
     }
 }
