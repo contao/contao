967eb4ba50ed5c0c3cedf566ce17e360f376cef0
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-08-12 08:26:37 +0200

    Remove leftover code in the DataContainer class (see #5121)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    57ecad44 add missing BackendUser import
    5a124b17 remove  adjustments

--- core-bundle/src/Resources/contao/classes/DataContainer.php
+++ core-bundle/src/Resources/contao/classes/DataContainer.php
@@ -359,12 +359,6 @@ abstract class DataContainer extends Backend
 				{
 					$newPaletteFields[$k] = $v . '_' . $suffix;
 				}
-
-				if ($this->User->isAdmin)
-				{
-					$newPaletteFields['pid'] = 'pid_' . $suffix;
-					$newPaletteFields['sorting'] = 'sorting_' . $suffix;
-				}
 			}
 
 			$paletteFields = array_intersect($postPaletteFields, $newPaletteFields);
