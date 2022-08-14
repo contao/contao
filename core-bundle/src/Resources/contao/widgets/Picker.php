3a5f81dd8247254b8ea866895b4085f128395966
Martin Auswöger <martin@auswoeger.com>
2022-08-14 14:36:45 +0200

    Do not run the DC constructor inside the picker widget

--- core-bundle/src/Resources/contao/widgets/Picker.php
+++ core-bundle/src/Resources/contao/widgets/Picker.php
@@ -247,8 +247,8 @@ class Picker extends Widget
 
 			if ($objRows->numRows)
 			{
-				$dataContainer = DataContainer::getDriverForTable($strRelatedTable);
-				$dc = new $dataContainer($strRelatedTable);
+				$dc = (new \ReflectionClass(DataContainer::getDriverForTable($strRelatedTable)))->newInstanceWithoutConstructor();
+				$dc->table = $strRelatedTable;
 
 				while ($objRows->next())
 				{
