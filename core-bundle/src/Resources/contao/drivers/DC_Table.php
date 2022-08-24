e5190d7a5bcb4687f0771f4e5c60fe7d71809efa
Dennis Bohn <87128053+dennisbohn@users.noreply.github.com>
2022-08-24 23:01:31 +0200

    Changed code to pass ECS Legacy test

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -4833,7 +4833,7 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 				{
 					$arrField = explode(':', $field, 2);
 					
-					if ($arrField[0] === $key && count($arrField) === 2)
+					if ($arrField[0] === $key && \count($arrField) === 2)
 					{
 						$fieldRef = explode('.', $arrField[1], 2);
 						$orderBy[$k] = sprintf('(SELECT %s FROM %s WHERE %s.id = %s.%s)', $fieldRef[1], $fieldRef[0], $fieldRef[0], $this->strTable, $key);
