eae045ced8bfee1205b0fe0976094c54d60d41ee
Martin Auswöger <martin@auswoeger.com>
2022-09-21 18:18:45 +0200

    Do not use exponential notation for float values (see #5296)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    0ddb0c6d Do not use exponential notation for float values
    1d1fa886 Only deserialize non-float values
    a5624b00 Fix PHP 7 compatibility
    ae637ff5 Add comment
    
    Co-authored-by: Fritz Michael Gschwantner <fmg@inspiredminds.at>

--- core-bundle/src/Resources/public/core.js
+++ core-bundle/src/Resources/public/core.js
@@ -2721,7 +2721,7 @@ var Backend =
 					partElement.setStyle('display', null);
 				}
 				Object.each(values, function(value, key) {
-					inputElements[key].set('value', value);
+					inputElements[key].set('value', value.toFixed(15));
 				});
 			},
 			start = function(event) {
