cffcc2f427d4d951ac6d2fb07406c6bf74bf5edb
M. Vondano <m-vo@users.noreply.github.com>
2022-09-27 12:39:22 +0200

    rewrite TemplateOptionsListener

--- core-bundle/src/EventListener/DataContainer/TemplateOptionsListener.php
+++ core-bundle/src/EventListener/DataContainer/TemplateOptionsListener.php
@@ -13,74 +13,94 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\EventListener\DataContainer;
 
 use Contao\Controller;
-use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Twig\Finder\FinderFactory;
 use Contao\DataContainer;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class TemplateOptionsListener
 {
-    private array $customTemplates = [];
-
     /**
-     * @var Adapter<Controller>
+     * @var array<string, string>
      */
-    private Adapter $controller;
+    private array $defaultIdentifiersByType = [];
 
     public function __construct(
-        ContaoFramework $framework,
-        private RequestStack $requestStack,
-        private string $templatePrefix,
-        private string|null $proxyClass = null,
+        private readonly FinderFactory $finderFactory,
+        private readonly Connection $connection,
+        private readonly ContaoFramework $framework,
+        private readonly RequestStack $requestStack,
+        private readonly string $legacyTemplatePrefix,
+        private readonly string|null $legacyProxyClass = null,
     ) {
-        $controller = $framework->getAdapter(Controller::class);
-
-        $this->controller = $controller;
     }
 
     public function __invoke(DataContainer $dc): array
     {
-        if ($this->isOverrideAll()) {
-            // Add a blank option that allows us to reset all custom templates to the default one
-            return array_merge(['' => '-'], $this->controller->getTemplateGroup($this->templatePrefix));
+        $overrideAll = $this->isOverrideAll();
+
+        $type = $overrideAll ?
+            $this->getCommonOverrideAllType($dc) :
+            $dc->getCurrentRecord()['type'] ?? null
+        ;
+
+        if (null === $type) {
+            // Add a blank option that allows to reset all custom templates to
+            // the default one when in 'overrideAll' mode
+            return $overrideAll ? ['' => '-'] : [];
         }
 
-        $type = $dc->getCurrentRecord()['type'] ?? null;
+        $identifier = $this->defaultIdentifiersByType[$type] ?? null;
 
-        if (null !== $type) {
-            $defaultTemplate = $this->getLegacyDefaultTemplate($type) ?? $this->customTemplates[$type] ?? null;
+        // Handle legacy elements that aren't implemented as fragment
+        // controllers or that still use the old template naming scheme
+        if (null === $identifier || !str_contains($identifier, '/')) {
+            $identifier = $this->getLegacyDefaultIdentifier($type) ?? $this->legacyTemplatePrefix.$type;
+
+            return [
+                ...($overrideAll ? ['' => '-'] : []),
+                ...$this->framework
+                    ->getAdapter(Controller::class)
+                    ->getTemplateGroup($identifier.'_', [], $identifier),
+            ];
         }
 
-        if (empty($defaultTemplate)) {
-            $defaultTemplate = $this->templatePrefix.$type;
-        }
-
-        return $this->controller->getTemplateGroup($defaultTemplate.'_', [], $defaultTemplate);
+        return $this->finderFactory
+            ->create()
+            ->identifier($identifier)
+            ->withVariants()
+            ->asTemplateOptions()
+        ;
     }
 
-    public function setCustomTemplates(array $customTemplates): void
+    /**
+     * Called by the @see \Contao\CoreBundle\DependencyInjection\Compiler\RegisterFragmentsPass
+     * for all fragment controllers.
+     */
+    public function setDefaultIdentifiersByType(array $defaultIdentifiersByType): void
     {
-        $this->customTemplates = $customTemplates;
+        $this->defaultIdentifiersByType = $defaultIdentifiersByType;
     }
 
     /**
      * Uses the reflection API to return the default template from a legacy class.
      */
-    private function getLegacyDefaultTemplate(string $type): string|null
+    private function getLegacyDefaultIdentifier(string $type): string|null
     {
-        if (null === $this->proxyClass || !method_exists($this->proxyClass, 'findClass')) {
+        if (null === $this->legacyProxyClass || !method_exists($this->legacyProxyClass, 'findClass')) {
             return null;
         }
 
-        $class = $this->proxyClass::findClass($type);
+        $class = $this->legacyProxyClass::findClass($type);
 
-        if (empty($class) || $class === $this->proxyClass) {
+        if (empty($class) || $class === $this->legacyProxyClass) {
             return null;
         }
 
         $properties = (new \ReflectionClass($class))->getDefaultProperties();
 
-        return $properties['strTemplate'] ?? $this->templatePrefix.$type;
+        return $properties['strTemplate'] ?? null;
     }
 
     private function isOverrideAll(): bool
@@ -92,5 +112,27 @@ class TemplateOptionsListener
         }
 
         return 'overrideAll' === $request->query->get('act');
+    }
+
+    /**
+     * Returns the type that all currently edited items are sharing or null if
+     * there is no common type.
+     */
+    private function getCommonOverrideAllType(DataContainer $dc): string|null
+    {
+        $affectedIds = $this->requestStack->getSession()->all()['CURRENT']['IDS'] ?? [];
+        $table = $this->connection->quoteIdentifier($dc->table);
+
+        $result = $this->connection->executeQuery(
+            "SELECT type FROM $table WHERE id IN (?) GROUP BY type LIMIT 2",
+            [$affectedIds],
+            [Connection::PARAM_INT_ARRAY]
+        );
+
+        if (1 !== $result->rowCount()) {
+            return null;
+        }
+
+        return $result->fetchOne();
     }
 }
