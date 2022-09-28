82d802c2facd5eb8f002e8e50cbe57060f1408b0
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-09-28 12:55:22 +0200

    Improve JavaScript according to Martin's suggestions

--- core-bundle/public/ajax-form.js
+++ core-bundle/public/ajax-form.js
@@ -1,11 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('[data-ajax-form]').forEach(form => {
-        let triggeredButton = null;
-
-        function request(method, uri, body, callback) {
+    document.querySelectorAll('[data-ajax-form]').forEach(el => {
+        function request(form, body, callback) {
             const xhr = new XMLHttpRequest();
 
-            xhr.open(method, uri, true);
+            xhr.open('POST', form.action, true);
             xhr.setRequestHeader('Accept', 'text/html');
             xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
             xhr.setRequestHeader('X-Contao-Ajax-Form', form.querySelector('[name="FORM_SUBMIT"]').value);
@@ -14,17 +12,15 @@ window.addEventListener('DOMContentLoaded', () => {
             form.dataset.ajaxForm = 'loading';
 
             xhr.onload = () => {
-                callback(xhr);
+                form.dispatchEvent(new CustomEvent('ajax-form-onload', {
+                    bubbles: true,
+                    detail: { body, form, xhr },
+                }));
 
                 form.ariaBusy = 'false';
                 form.dataset.ajaxForm = '';
 
-                const event = new CustomEvent('ajax-form-onload', {
-                    detail: { body, form, xhr },
-                });
-
-                form.dispatchEvent(event);
-                window.dispatchEvent(event);
+                callback(xhr);
             };
 
             xhr.send(body || null)
@@ -37,11 +33,11 @@ window.addEventListener('DOMContentLoaded', () => {
                 const formData = new FormData(form);
 
                 // Send the triggered button data as well
-                if (triggeredButton) {
-                    formData.append(triggeredButton.name, triggeredButton.value);
+                if (e.submitter) {
+                    formData.append(e.submitter.name, e.submitter.value);
                 }
 
-                request('POST', form.action, formData, xhr => {
+                request(form, formData, xhr => {
                     const location = xhr.getResponseHeader('X-Ajax-Location');
 
                     // Handle the redirect header
@@ -58,12 +54,8 @@ window.addEventListener('DOMContentLoaded', () => {
                     initForm(newForm);
                 });
             });
-
-            form.querySelectorAll('input[type="submit"], button[type="submit"]').forEach(el => {
-                el.addEventListener('click', e => triggeredButton = e.currentTarget);
-            });
         }
 
-        initForm(form);
+        initForm(el);
     });
 });
