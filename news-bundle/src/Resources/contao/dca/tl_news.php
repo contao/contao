bb9c7d20c0f2ebfeeea8e34879a2614f096deabd
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-09-29 12:20:16 +0200

    Fix a potential error if the news archive model is not found in sitemap cache invalidation tag helper

--- news-bundle/src/Resources/contao/dca/tl_news.php
+++ news-bundle/src/Resources/contao/dca/tl_news.php
@@ -949,6 +949,12 @@ class tl_news extends Backend
 	public function addSitemapCacheInvalidationTag($dc, array $tags)
 	{
 		$archiveModel = NewsArchiveModel::findByPk($dc->activeRecord->pid);
+
+		if ($archiveModel === null)
+		{
+			return $tags;
+		}
+
 		$pageModel = PageModel::findWithDetails($archiveModel->jumpTo);
 
 		if ($pageModel === null)
