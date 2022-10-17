058e898c1c984f5f579ecd7f515bce78c9582877
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2022-10-17 16:54:11 +0200

    update dependencies

--- core-bundle/src/Translation/TranslatorCallback.php
+++ core-bundle/src/Translation/TranslatorCallback.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
 namespace Contao\CoreBundle\Translation;
 
 use Symfony\Contracts\Translation\TranslatorInterface;
