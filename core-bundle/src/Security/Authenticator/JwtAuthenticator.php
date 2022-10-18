1e3f35953e7940d07cf02e579f04d0b19de2f0da
David Greminger <david.greminger@1up.io>
2022-10-18 15:01:52 +0200

    Add a JWT authenticator

---
+++ core-bundle/src/Security/Authenticator/JwtAuthenticator.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Contao\CoreBundle\Security\Authenticator;
+
+use Contao\CoreBundle\Security\JwtManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+
+class JwtAuthenticator extends AbstractAuthenticator
+{
+    public function __construct(
+        public readonly JwtManager $jwtManager,
+        private readonly UserProviderInterface $userProvider,
+    ) {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->headers->has('Authorization') && str_starts_with($request->headers->get('Authorization'), 'Bearer ');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $header = $request->headers->get('Authorization');
+        $jwt = substr($header, 7);
+
+        $token = $this->jwtManager->parseToken($jwt);
+
+        if (null === $token) {
+            throw new CustomUserMessageAuthenticationException('No JWT token provided.');
+        }
+
+        return new SelfValidatingPassport(new UserBadge($token->claims()->get('username'), [$this->userProvider, 'loadUserByIdentifier']));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        return new JsonResponse([
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+}
