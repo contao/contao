371971efed7cb1226281fb595f757813c8bd33ff
fritzmg <fmg@inspiredminds.at>
2022-10-25 15:17:03 +0100

    check admin email

---
+++ core-bundle/src/EventListener/AdministratorEmailListener.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener;
+
+use Contao\Config;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @internal
+ */
+class AdministratorEmailListener
+{
+    public function __construct(private TranslatorInterface $translator, private RouterInterface $router, private RequestStack $requestStack)
+    {
+    }
+
+    public function __invoke(): ?string
+    {
+        if (Config::get('adminEmail')) {
+            return null;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        $settingsUrl = $this->router->generate('contao_backend', [
+            'do' => 'settings',
+            'ref' => $request->attributes->get('_contao_referer_id'),
+        ]);
+
+        return '<p class="tl_error">'.$this->translator->trans('ERR.noAdminEmail', ['settingsUrl' => $settingsUrl], 'contao_default').'</p>';
+    }
+}
