632a73d7b3504a1e4af6fb989ab647dd4b174c5d
Martin Auswöger <martin@auswoeger.com>
2022-10-31 17:24:50 +0100

    Use error_reporting instead of silencing operator

--- core-bundle/tests/Functional/StatementTest.php
+++ core-bundle/tests/Functional/StatementTest.php
@@ -16,6 +16,7 @@ use Contao\Database;
 use Contao\PageModel;
 use Contao\System;
 use Contao\TestCase\FunctionalTestCase;
+use Doctrine\DBAL\Driver\Mysqli\Driver as MysqliDriver;
 use Doctrine\DBAL\Driver\PDO\MySQL\Driver as PdoMysqlDriver;
 use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
@@ -42,15 +43,24 @@ class StatementTest extends FunctionalTestCase
 
         $connection = System::getContainer()->get('database_connection');
         $driver = $connection->getDriver();
+        $errorReporting = error_reporting();
+
+        // Silence MySQLi errors in PHP 7.4
+        if (\PHP_VERSION_ID < 80000 && $driver instanceof MysqliDriver) {
+            error_reporting($errorReporting & ~E_WARNING);
+        }
 
         $db = Database::getInstance();
 
-        $this->assertSame('1', (string) @$db->prepare('SELECT 1')->execute(2)->first()->fetchField());
-        $this->assertSame('1', (string) @$db->prepare('SELECT 1')->execute([2])->first()->fetchField());
-        $this->assertSame('1', (string) @$db->prepare('SELECT ?')->execute(1, 2)->first()->fetchField());
-        $this->assertSame('1', (string) @$db->prepare('SELECT ?')->execute([1, 2])->first()->fetchField());
-        $this->assertSame('1', (string) @$db->prepare('SELECT ?')->execute(1, 2, 3, 4, 5, 6)->first()->fetchField());
-        $this->assertSame('1', (string) @$db->prepare('SELECT ?')->execute([1, 2, 3, 4, 5, 6])->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT 1')->execute(2)->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT 1')->execute([2])->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute(1, 2)->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute([1, 2])->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute(1, 2, 3, 4, 5, 6)->first()->fetchField());
+        $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute([1, 2, 3, 4, 5, 6])->first()->fetchField());
+
+        // Restore the error reporting level
+        error_reporting($errorReporting);
 
         if ($driver instanceof PdoMysqlDriver) {
             $this->expectExceptionMessageMatches('/number of bound variables does not match number of tokens/i');
