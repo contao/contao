066a1ec6f8028c20036f889ee66e5e53ffb58bf2
Leo Feyer <github@contao.org>
2022-10-31 15:35:03 +0100

    Suppress mysqli_stmt::bind_param() warnings in PHP 7

--- core-bundle/tests/Functional/StatementTest.php
+++ core-bundle/tests/Functional/StatementTest.php
@@ -16,6 +16,8 @@ use Contao\Database;
 use Contao\PageModel;
 use Contao\System;
 use Contao\TestCase\FunctionalTestCase;
+use Doctrine\DBAL\Driver\Mysqli\Driver as MysqliDriver;
+use Doctrine\DBAL\Driver\PDO\MySQL\Driver as PdoMysqlDriver;
 use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 class StatementTest extends FunctionalTestCase
@@ -37,9 +39,20 @@ class StatementTest extends FunctionalTestCase
      */
     public function testExecuteStatementBackwardsCompatibility(): void
     {
-        $db = Database::getInstance();
-
         $this->expectDeprecation('%sPassing more parameters than "?" tokens has been deprecated%s');
+
+        $connection = System::getContainer()->get('database_connection');
+        $driver = $connection->getDriver();
+        $isMysqli = $driver instanceof MysqliDriver;
+
+        $errorReporting = error_reporting();
+
+        // Prevent "mysqli_stmt::bind_param()" warnings in PHP 7
+        if ($isMysqli && PHP_VERSION_ID < 80000) {
+            error_reporting($errorReporting & ~E_WARNING);
+        }
+
+        $db = Database::getInstance();
 
         $this->assertSame('1', (string) $db->prepare('SELECT 1')->execute(2)->first()->fetchField());
         $this->assertSame('1', (string) $db->prepare('SELECT 1')->execute([2])->first()->fetchField());
@@ -48,9 +61,16 @@ class StatementTest extends FunctionalTestCase
         $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute(1, 2, 3, 4, 5, 6)->first()->fetchField());
         $this->assertSame('1', (string) $db->prepare('SELECT ?')->execute([1, 2, 3, 4, 5, 6])->first()->fetchField());
 
-        $this->expectExceptionMessageMatches("/number of variables must match the number of parameters|number of bound variables does not match number of tokens|number of variables doesn't match number of parameters in prepared statement/i");
+        if ($driver instanceof PdoMysqlDriver) {
+            $this->expectExceptionMessageMatches('/number of bound variables does not match number of tokens/i');
+        }
 
         $db->prepare('SELECT ?, ?, ?')->execute(1, 2)->fetchRow();
+
+        // Restore the error reporting level
+        if ($isMysqli && PHP_VERSION_ID < 80000) {
+            error_reporting($errorReporting);
+        }
     }
 
     /**
