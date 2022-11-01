b39a9d0fd09aa396c3bdb013a35d5e7188363d7a
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-11-01 08:57:09 +0100

    Disable request token checks for idempotent actions

--- core-bundle/contao/drivers/DC_Table.php
+++ core-bundle/contao/drivers/DC_Table.php
@@ -116,13 +116,13 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 		$objSession = $container->get('request_stack')->getSession();
 
 		// Check the request token (see #4007)
-		if (Input::get('act') !== null)
-		{
-			if (Input::get('rt') === null || !$container->get('contao.csrf.token_manager')->isTokenValid(new CsrfToken($container->getParameter('contao.csrf_token_name'), Input::get('rt'))))
-			{
-				$objSession->set('INVALID_TOKEN_URL', Environment::get('requestUri'));
-				$this->redirect($container->get('router')->generate('contao_backend_confirm'));
-			}
+		if (
+			Input::get('act') !== null
+			&& (!\in_array(Input::get('act'), array('edit', 'show', 'select'), true) || Input::isPost())
+			&& (Input::get('rt') === null || !$container->get('contao.csrf.token_manager')->isTokenValid(new CsrfToken($container->getParameter('contao.csrf_token_name'), Input::get('rt'))))
+		) {
+			$objSession->set('INVALID_TOKEN_URL', Environment::get('requestUri'));
+			$this->redirect($container->get('router')->generate('contao_backend_confirm'));
 		}
 
 		$this->intId = Input::get('id');
