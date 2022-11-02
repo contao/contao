7198c1c94dbd6a0a5fc5015eead4acd941084388
fritzmg <fmg@inspiredminds.at>
2022-11-02 10:45:21 +0000

    check module, check permissions, add test

--- core-bundle/src/EventListener/AdministratorEmailListener.php
+++ core-bundle/src/EventListener/AdministratorEmailListener.php
@@ -13,8 +13,11 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\EventListener;
 
 use Contao\Config;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -22,23 +25,46 @@ use Symfony\Contracts\Translation\TranslatorInterface;
  */
 class AdministratorEmailListener
 {
-    public function __construct(private TranslatorInterface $translator, private RouterInterface $router, private RequestStack $requestStack)
-    {
+    public function __construct(
+        private ContaoFramework $framework,
+        private TranslatorInterface $translator,
+        private RouterInterface $router,
+        private RequestStack $requestStack,
+        private Security $security,
+    ) {
     }
 
     public function __invoke(): string|null
     {
-        if (Config::get('adminEmail')) {
+        /** @var Config $config */
+        $config = $this->framework->getAdapter(Config::class);
+
+        if ($config->get('adminEmail')) {
             return null;
         }
 
-        $request = $this->requestStack->getCurrentRequest();
+        if ($this->canAccessSettings()) {
+            $request = $this->requestStack->getCurrentRequest();
 
-        $settingsUrl = $this->router->generate('contao_backend', [
-            'do' => 'settings',
-            'ref' => $request->attributes->get('_contao_referer_id'),
-        ]);
+            $settingsUrl = $this->router->generate('contao_backend', [
+                'do' => 'settings',
+                'ref' => $request->attributes->get('_contao_referer_id'),
+            ]);
 
-        return '<p class="tl_error">'.$this->translator->trans('ERR.noAdminEmail', ['settingsUrl' => $settingsUrl], 'contao_default').'</p>';
+            $message = $this->translator->trans('ERR.noAdminEmailUrl', ['settingsUrl' => $settingsUrl], 'contao_default');
+        } else {
+            $message = $this->translator->trans('ERR.noAdminEmail', [], 'contao_default');
+        }
+
+        return '<p class="tl_error">'.$message.'</p>';
+    }
+
+    private function canAccessSettings(): bool
+    {
+        if (!isset($GLOBALS['BE_MOD']['system']['settings'])) {
+            return false;
+        }
+
+        return $this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'settings');
     }
 }
---
+++ core-bundle/tests/EventListener/AdministratorEmailListenerTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\EventListener;
+
+use Contao\Config;
+use Contao\CoreBundle\EventListener\AdministratorEmailListener;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Contao\CoreBundle\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class AdministratorEmailListenerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $GLOBALS['BE_MOD']['system']['settings']['tables'] = ['tl_settings'];
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($GLOBALS['BE_MOD']);
+    }
+
+    public function testDoesNotAddMessageIfAdminEmailIsSet(): void
+    {
+        $configAdapter = $this->mockAdapter(['get']);
+        $configAdapter
+            ->method('get')
+            ->with('adminEmail')
+            ->willReturn('foobar@example.com')
+        ;
+
+        $framework = $this->mockContaoFramework([Config::class => $configAdapter]);
+        $listener = $this->createAdministratorEmailListener($framework);
+
+        $this->assertNull($listener());
+    }
+
+    public function testShowsMessageWithoutLinkIfSettingsModuleIsDisabled(): void
+    {
+        unset($GLOBALS['BE_MOD']['system']['settings']);
+
+        $listener = $this->createAdministratorEmailListener();
+
+        $this->assertSame('<p class="tl_error">ERR.noAdminEmail</p>', $listener());
+    }
+
+    public function testShowsMessageWithoutLinkIfSettingsModuleIsDisallowed(): void
+    {
+        $security = $this->createMock(Security::class);
+        $security
+            ->method('isGranted')
+            ->with(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'settings')
+            ->willReturn(false)
+        ;
+
+        $listener = $this->createAdministratorEmailListener(security: $security);
+
+        $this->assertSame('<p class="tl_error">ERR.noAdminEmail</p>', $listener());
+    }
+
+    public function testShowsMessageWithLink(): void
+    {
+        $listener = $this->createAdministratorEmailListener();
+
+        $this->assertSame('<p class="tl_error">ERR.noAdminEmailUrl</p>', $listener());
+    }
+
+    private function createAdministratorEmailListener(ContaoFramework $framework = null, TranslatorInterface $translator = null, RouterInterface $router = null, RequestStack $requestStack = null, Security $security = null): AdministratorEmailListener
+    {
+        if (null === $framework) {
+            $configAdapter = $this->mockAdapter(['get']);
+            $configAdapter
+                ->method('get')
+                ->with('adminEmail')
+                ->willReturn(null)
+            ;
+
+            $framework = $this->mockContaoFramework([Config::class => $configAdapter]);
+        }
+
+        if (null === $translator) {
+            $translator = $this->createMock(TranslatorInterface::class);
+            $translator
+                ->method('trans')
+                ->willReturnMap([
+                    ['ERR.noAdminEmailUrl', ['settingsUrl' => 'https://example.com'], 'contao_default', null, 'ERR.noAdminEmailUrl'],
+                    ['ERR.noAdminEmail', [], 'contao_default', null, 'ERR.noAdminEmail'],
+                ])
+            ;
+        }
+
+        if (null === $router) {
+            $router = $this->createMock(RouterInterface::class);
+            $router
+                ->method('generate')
+                ->willReturn('https://example.com')
+            ;
+        }
+
+        if (null === $requestStack) {
+            $requestStack = $this->createMock(RequestStack::class);
+            $requestStack
+                ->method('getCurrentRequest')
+                ->willReturn(new Request())
+            ;
+        }
+
+        if (null === $security) {
+            $security = $this->createMock(Security::class);
+            $security
+                ->method('isGranted')
+                ->with(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'settings')
+                ->willReturn(true)
+            ;
+        }
+
+        return new AdministratorEmailListener($framework, $translator, $router, $requestStack, $security);
+    }
+}
