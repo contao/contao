4a4a7a5052c5ae6fb45d0c74d6f0b2b45a2457ce
M. Vondano <m-vo@users.noreply.github.com>
2022-11-13 01:04:06 +0100

    CS

--- core-bundle/src/Filesystem/Dbafs/Dbafs.php
+++ core-bundle/src/Filesystem/Dbafs/Dbafs.php
@@ -800,7 +800,6 @@ class Dbafs implements DbafsInterface, ResetInterface
 
                 continue;
             }
-            
             if (!$shallow && $this->filesystem->fileExists($searchPath, VirtualFilesystemInterface::BYPASS_DBAFS)) {
                 // Yield existing file
                 yield $searchPath => self::RESOURCE_FILE;
