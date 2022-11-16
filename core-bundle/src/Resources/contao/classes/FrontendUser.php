0d6437e2c46790e436e54ef15ab1c58edd76b00a
David Greminger <david.greminger@1up.io>
2022-11-16 13:17:14 +0100

    Check array length before access

--- core-bundle/src/Resources/contao/classes/FrontendUser.php
+++ core-bundle/src/Resources/contao/classes/FrontendUser.php
@@ -237,7 +237,7 @@ class FrontendUser extends User
 		}
 
 		// Get the group login page
-		if ($this->groups[0] > 0)
+		if (\count($this->groups) && $this->groups[0] > 0)
 		{
 			$objGroup = MemberGroupModel::findPublishedById($this->groups[0]);
 
