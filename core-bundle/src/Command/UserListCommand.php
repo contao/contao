b8a9f3815ab06ecdc20ab1e681e4eaca7f54dd1a
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2022-11-20 17:12:41 +0100

    Do not silence JSON errors

--- core-bundle/src/Command/UserListCommand.php
+++ core-bundle/src/Command/UserListCommand.php
@@ -80,7 +80,7 @@ class UserListCommand extends Command
             case 'json':
                 $data = $this->formatJson($users, $columns);
 
-                $io->write(json_encode($data));
+                $io->write(json_encode($data, JSON_THROW_ON_ERROR));
                 break;
 
             default:
