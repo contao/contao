3bc1afbe84262d3dba365b262ff0018671f11e07
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-11-28 11:04:07 +0100

    Make sure text content is always a string (see #5532)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    6a440ff3 Make sure text content is always a string
    9aa87f6e Updated content element annotations to match model

--- core-bundle/src/Resources/contao/elements/ContentText.php
+++ core-bundle/src/Resources/contao/elements/ContentText.php
@@ -30,10 +30,10 @@ class ContentText extends ContentElement
 		if ($staticUrl = System::getContainer()->get('contao.assets.files_context')->getStaticUrl())
 		{
 			$path = System::getContainer()->getParameter('contao.upload_path') . '/';
-			$this->text = str_replace(' src="' . $path, ' src="' . $staticUrl . $path, $this->text);
+			$this->text = str_replace(' src="' . $path, ' src="' . $staticUrl . $path, (string) $this->text);
 		}
 
-		$this->Template->text = StringUtil::encodeEmail($this->text);
+		$this->Template->text = StringUtil::encodeEmail((string) $this->text);
 		$this->Template->addImage = false;
 		$this->Template->addBefore = false;
 
