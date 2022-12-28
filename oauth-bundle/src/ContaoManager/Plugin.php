f5efebb95ec7bc6962caf48ed15ccff503f9e114
fritzmg <fmg@inspiredminds.at>
2022-12-28 16:53:35 +0000

    wip

---
+++ oauth-bundle/src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\OAuthBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\OAuthBundle\ContaoOAuthBundle;
+
+/**
+ * @internal
+ */
+class Plugin implements BundlePluginInterface
+{
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ContaoOAuthBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class])
+        ];
+    }
+}
---
+++ oauth-bundle/src/ContaoOAuthBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoOAuthBundle extends Bundle
+{
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
