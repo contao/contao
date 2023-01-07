af0015f1858d6ed8e1fd5de4746bf0dea7849f96
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-01-07 15:46:54 +0100

    Update the dependencies

--- core-bundle/assets/backend.js
+++ core-bundle/assets/backend.js
@@ -1,14 +1,14 @@
 import { Application } from '@hotwired/stimulus'
 import { definitionForModuleAndIdentifier, identifierForContextKey } from '@hotwired/stimulus-webpack-helpers'
 
-import './scripts/mootao'
-import './scripts/core'
-import './scripts/autofocus'
-import './scripts/limit-height'
-import './scripts/modulewizard'
-import './scripts/sectionwizard'
-import './scripts/tips'
-import './scripts/toggle-fieldset'
+import './scripts/mootao.js'
+import './scripts/core.js'
+import './scripts/autofocus.js'
+import './scripts/limit-height.js'
+import './scripts/modulewizard.js'
+import './scripts/sectionwizard.js'
+import './scripts/tips.js'
+import './scripts/toggle-fieldset.js'
 
 // Start Stimulus application and register all controllers with `contao--` prefix.
 const application = Application.start()
