e4ead8934bbe7b7cb12e06390734e4567efb8e74
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-01-09 16:43:42 +0000

    Check administrator email address (see #5427)
    Description
    -----------
    
    -
    
    Commits
    -------
    
    371971ef check admin email
    7198c1c9 check module, check permissions, add test
    2ecc2ff5 Update core-bundle/contao/languages/en/default.xlf
    74f0dea2 update translation
    
    Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

---
+++ core-bundle/src/EventListener/AdministratorEmailListener.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener;
+
+use Contao\Config;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @internal
+ */
+class AdministratorEmailListener
+{
+    public function __construct(
+        private ContaoFramework $framework,
+        private TranslatorInterface $translator,
+        private RouterInterface $router,
+        private RequestStack $requestStack,
+        private Security $security,
+    ) {
+    }
+
+    public function __invoke(): string|null
+    {
+        /** @var Config $config */
+        $config = $this->framework->getAdapter(Config::class);
+
+        if ($config->get('adminEmail')) {
+            return null;
+        }
+
+        if ($this->canAccessSettings()) {
+            $request = $this->requestStack->getCurrentRequest();
+
+            $settingsUrl = $this->router->generate('contao_backend', [
+                'do' => 'settings',
+                'ref' => $request->attributes->get('_contao_referer_id'),
+            ]);
+
+            $message = $this->translator->trans('ERR.noAdminEmailUrl', ['settingsUrl' => $settingsUrl], 'contao_default');
+        } else {
+            $message = $this->translator->trans('ERR.noAdminEmail', [], 'contao_default');
+        }
+
+        return '<p class="tl_error">'.$message.'</p>';
+    }
+
+    private function canAccessSettings(): bool
+    {
+        if (!isset($GLOBALS['BE_MOD']['system']['settings'])) {
+            return false;
+        }
+
+        return $this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'settings');
+    }
+}
