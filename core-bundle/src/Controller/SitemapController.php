0c3054691856dab086f79147cd285e6353bcd8c7
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-01-11 21:37:58 +0100

    Skip pages in sitemap.xml if URL cannot be generated

--- core-bundle/src/Controller/SitemapController.php
+++ core-bundle/src/Controller/SitemapController.php
@@ -22,6 +22,7 @@ use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Exception\ExceptionInterface;
 
 /**
  * @Route(defaults={"_scope" = "frontend"})
@@ -149,7 +150,11 @@ class SitemapController extends AbstractController
                 && $this->pageRegistry->isRoutable($pageModel)
                 && 'html' === $this->pageRegistry->getRoute($pageModel)->getDefault('_format')
             ) {
-                $urls = [$pageModel->getAbsoluteUrl()];
+                try {
+                    $urls = [$pageModel->getAbsoluteUrl()];
+                } catch (ExceptionInterface $exception) {
+                    continue;
+                }
 
                 // Get articles with teaser
                 if (null !== ($articleModels = $articleModelAdapter->findPublishedWithTeaserByPid($pageModel->id, ['ignoreFePreview' => true]))) {
