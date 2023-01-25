93b3d623067a7834e9a403f2f9d85563d71c5e85
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-01-25 13:34:09 +0100

    Check for trail page before rendering the navigation (see #5709)
    Description
    -----------
    
    Fixes https://github.com/contao/contao/issues/5699
    
    Commits
    -------
    
    671b2201 Check for trail page before rendering navigation

--- core-bundle/src/Resources/contao/modules/ModuleNavigation.php
+++ core-bundle/src/Resources/contao/modules/ModuleNavigation.php
@@ -82,7 +82,7 @@ class ModuleNavigation extends Module
 		$this->Template->request = StringUtil::ampersand(Environment::get('indexFreeRequest'));
 		$this->Template->skipId = 'skipNavigation' . $this->id;
 		$this->Template->skipNavigation = StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['skipNavigation']);
-		$this->Template->items = $this->renderNavigation($trail[$level], 1, $host, $lang);
+		$this->Template->items = isset($trail[$level]) ? $this->renderNavigation($trail[$level], 1, $host, $lang) : '';
 	}
 }
 
