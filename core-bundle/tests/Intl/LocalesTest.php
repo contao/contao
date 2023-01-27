0f753b8929463719748915721817e4e33502a238
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-01-27 11:32:29 +0100

    Fix the locales test

--- core-bundle/tests/Intl/LocalesTest.php
+++ core-bundle/tests/Intl/LocalesTest.php
@@ -154,7 +154,7 @@ class LocalesTest extends TestCase
 
         $this->assertSame(
             [
-                'gsw_Hans_AT' => 'Schwiizertüütsch (Veräifachti Chineesischi Schrift, Ööschtriich)',
+                'gsw_Hans_AT' => 'Schwiizertüütsch (Veräifachti Chineesischi Schrift, Ööschtriich) - Schweizerdeutsch (Vereinfacht, Österreich)',
                 'de_CH' => 'Tüütsch (Schwiiz) - Deutsch (Schweiz)',
             ],
             $this->getLocalesService()->getDisplayNames(['gsw_Hans_AT', 'de_CH'], 'gsw', true)
