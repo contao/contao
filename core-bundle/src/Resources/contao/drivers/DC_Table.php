4e472d342f91d0e98a6f87e1f869a0be81586293
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-02-24 16:32:07 +0000

    Fix invalid HTML output of `DC_Table` (see #5817)
    Description
    -----------
    
    `DC_Table` currently produces invalid HTML output in "Edit multiple" mode. Specifically a `<ul>` is never closed, since the opening tag is added under different conditions than the closing tag.
    
    ### Reproduction
    
    1. Edit the content elements of an article.
    2. Click on "Edit multiple".
    3. Validate the HTML output.
    
    <img src="https://user-images.githubusercontent.com/4970961/220998508-094e6773-2d1c-4b33-ac84-9eb106888456.png" width="380">
    
    This PR fixes that by using the same condition for the closing tag as for the opening tag:
    
    https://github.com/contao/contao/blob/90a747c9f4bf708256f8a530fc93e559aca3c4aa/core-bundle/src/Resources/contao/drivers/DC_Table.php#L4549-L4555
    
    Commits
    -------
    
    8e3f4c50 fix HTML syntax error
    729b3da2 do not always add makeParentViewSortable JS
    69e71861 Revert an unrelated change
    
    Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -4691,13 +4691,18 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 		}
 
 		// Make items sortable
-		if ($blnHasSorting && !($GLOBALS['TL_DCA'][$this->strTable]['config']['notSortable'] ?? null) && Input::get('act') != 'select')
+		if ($blnHasSorting)
 		{
 			$return .= '
-</ul>
+</ul>';
+
+			if (!($GLOBALS['TL_DCA'][$this->strTable]['config']['notSortable'] ?? null) && Input::get('act') != 'select')
+			{
+				$return .= '
 <script>
   Backend.makeParentViewSortable("ul_' . CURRENT_ID . '");
 </script>';
+			}
 		}
 
 		$return .= ($this->strPickerFieldType == 'radio' ? '
