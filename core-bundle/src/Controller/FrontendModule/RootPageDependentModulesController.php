620dae09090f60828a3176fc9468b44e435a7874
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-03-06 16:13:41 +0100

    Update and run the tools (see #5844)
    Description
    -----------
    
    Commits
    -------
    
    01e23fb4 Update and run the tools

--- core-bundle/src/Controller/FrontendModule/RootPageDependentModulesController.php
+++ core-bundle/src/Controller/FrontendModule/RootPageDependentModulesController.php
@@ -49,7 +49,7 @@ class RootPageDependentModulesController extends AbstractFrontendModuleControlle
             $cssID[0] = substr($idAttribute, 5, -1);
         }
 
-        $cssID[1] = trim(sprintf('%s %s', $cssID[1] ?? '', implode(' ', (array) $model->classes)));
+        $cssID[1] = trim(sprintf('%s %s', $cssID[1] ?? '', implode(' ', $model->classes)));
 
         $module->cssID = $cssID;
 
