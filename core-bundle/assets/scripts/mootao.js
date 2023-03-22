3e97b42043e6390937b0be75e80c53ef8e1ec084
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-03-22 10:29:12 +0100

    Handle basic entities in the SERP preview (see #5897)
    Description
    -----------
    
    Follow-up on #5566
    
    ### Before
    
    <img width="603" alt="" src="https://user-images.githubusercontent.com/1192057/226675628-8ce425f6-ab9c-49a7-b5a3-2ac903a999ba.png">
    
    ### After
    
    <img width="607" alt="" src="https://user-images.githubusercontent.com/1192057/226675690-644166a3-f04c-46e5-9cf5-d3567f22abf1.png">
    
    Commits
    -------
    
    d1f5ba25 Handle basic entities in the SERP preview
    7b0dd4dd Add the "basicEntities" flag to the page title fields

--- core-bundle/assets/scripts/mootao.js
+++ core-bundle/assets/scripts/mootao.js
@@ -328,7 +328,7 @@ Contao.SerpPreview = new Class(
 	},
 
 	html2string: function(html) {
-		return new DOMParser().parseFromString(html, 'text/html').body.textContent;
+		return new DOMParser().parseFromString(html, 'text/html').body.textContent.replace(/\[-]/g, '\xAD').replace(/\[nbsp]/g, '\xA0');
 	},
 
 	getTinymce: function() {
@@ -353,9 +353,9 @@ Contao.SerpPreview = new Class(
 
 		titleField && titleField.addEvent('input', function() {
 			if (titleField.value) {
-				serpTitle.set('text', this.shorten(titleTag.replace(/%s/, titleField.value).replace(/%%/g, '%').replace(/\[-\]/g, '\xAD').replace(/\[nbsp\]/g, '\xA0'), 64));
+				serpTitle.set('text', this.shorten(this.html2string(titleTag.replace(/%s/, titleField.value)).replace(/%%/g, '%'), 64));
 			} else if (titleFallbackField && titleFallbackField.value) {
-				serpTitle.set('text', this.shorten(this.html2string(titleTag.replace(/%s/, titleFallbackField.value)).replace(/%%/g, '%').replace(/\[-\]/g, '\xAD').replace(/\[nbsp\]/g, '\xA0'), 64));
+				serpTitle.set('text', this.shorten(this.html2string(titleTag.replace(/%s/, titleFallbackField.value)).replace(/%%/g, '%'), 64));
 			} else {
 				serpTitle.set('text', '');
 			}
@@ -363,7 +363,7 @@ Contao.SerpPreview = new Class(
 
 		titleFallbackField && titleFallbackField.addEvent('input', function() {
 			if (titleField && titleField.value) return;
-			serpTitle.set('text', this.shorten(this.html2string(titleTag.replace(/%s/, titleFallbackField.value)).replace(/%%/g, '%').replace(/\[-\]/g, '\xAD').replace(/\[nbsp\]/g, '\xA0'), 64));
+			serpTitle.set('text', this.shorten(this.html2string(titleTag.replace(/%s/, titleFallbackField.value)).replace(/%%/g, '%'), 64));
 		}.bind(this));
 
 		aliasField && aliasField.addEvent('input', function() {
