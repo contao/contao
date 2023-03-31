95652fd0e116bcda119430894dde4cdbadafd341
Dennis Bohn <87128053+dennisbohn@users.noreply.github.com>
2023-03-31 17:24:10 +0200

    Update DC_Table.php
    Moved if-condition for findInSet to the end of the orderBy-loop, so  else isnt needed anymore.

--- core-bundle/src/Resources/contao/drivers/DC_Table.php
+++ core-bundle/src/Resources/contao/drivers/DC_Table.php
@@ -4872,6 +4872,8 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 			{
 				list($key, $direction) = explode(' ', $v, 2) + array(null, null);
 
+				$orderBy[$k] = $key;
+				
 				// If there is no direction, check the global flag in sorting mode 1 or the field flag in all other sorting modes
 				if (!$direction)
 				{
@@ -4883,6 +4885,22 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 					{
 						$direction = 'DESC';
 					}
+				}
+
+				if (isset($GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['foreignKey']))
+				{
+					$chunks = explode('.', $GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['foreignKey'], 2);
+					$orderBy[$k] = "(SELECT " . Database::quoteIdentifier($chunks[1]) . " FROM " . $chunks[0] . " WHERE " . $chunks[0] . ".id=" . $this->strTable . "." . $key . ")";
+				}
+
+				if (\in_array($GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['flag'] ?? null, array(self::SORT_DAY_ASC, self::SORT_DAY_DESC, self::SORT_MONTH_ASC, self::SORT_MONTH_DESC, self::SORT_YEAR_ASC, self::SORT_YEAR_DESC)))
+				{
+					$orderBy[$k] = "CAST(" . $orderBy[$k] . " AS SIGNED)"; // see #5503
+				}
+
+				if ($direction)
+				{
+					$orderBy[$k] .= ' ' . $direction;
 				}
 
 				if ($GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['eval']['findInSet'] ?? null)
@@ -4910,26 +4928,6 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 					}
 
 					$orderBy[$k] = $this->Database->findInSet($v, $keys);
-				}
-				else
-				{
-					$orderBy[$k] = $key;
-
-					if (isset($GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['foreignKey']))
-					{
-						$chunks = explode('.', $GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['foreignKey'], 2);
-						$orderBy[$k] = "(SELECT " . Database::quoteIdentifier($chunks[1]) . " FROM " . $chunks[0] . " WHERE " . $chunks[0] . ".id=" . $this->strTable . "." . $key . ")";
-					}
-
-					if (\in_array($GLOBALS['TL_DCA'][$this->strTable]['fields'][$key]['flag'] ?? null, array(self::SORT_DAY_ASC, self::SORT_DAY_DESC, self::SORT_MONTH_ASC, self::SORT_MONTH_DESC, self::SORT_YEAR_ASC, self::SORT_YEAR_DESC)))
-					{
-						$orderBy[$k] = "CAST(" . $orderBy[$k] . " AS SIGNED)"; // see #5503
-					}
-
-					if ($direction)
-					{
-						$orderBy[$k] .= ' ' . $direction;
-					}
 				}
 			}
 
