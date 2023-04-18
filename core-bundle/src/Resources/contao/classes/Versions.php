999ebfbd5a5742142f0fc730668a738c3e3fdb44
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-04-18 17:10:13 +0200

    CS

--- core-bundle/src/Resources/contao/classes/Versions.php
+++ core-bundle/src/Resources/contao/classes/Versions.php
@@ -465,7 +465,7 @@ class Versions extends Controller
 				// Find the changed fields and highlight the changes
 				foreach (array_keys(array_merge($to, $from)) as $k)
 				{
-					$deleted = !array_key_exists($k, $to);
+					$deleted = !\array_key_exists($k, $to);
 					$to[$k] ??= null;
 					$from[$k] ??= null;
 
