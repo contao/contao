80af0769ec612e532bc35ee822b207dfbc6abd36
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-04-18 12:29:53 +0200

    Mark deleted fields

--- core-bundle/src/Resources/contao/classes/Versions.php
+++ core-bundle/src/Resources/contao/classes/Versions.php
@@ -465,6 +465,7 @@ class Versions extends Controller
 				// Find the changed fields and highlight the changes
 				foreach (array_keys(array_merge($to, $from)) as $k)
 				{
+					$deleted = !isset($to[$k]);
 					$to[$k] ??= null;
 					$from[$k] ??= null;
 
@@ -588,6 +589,11 @@ class Versions extends Controller
 						elseif (isset($GLOBALS['TL_LANG']['MSC'][$k]))
 						{
 							$field = \is_array($GLOBALS['TL_LANG']['MSC'][$k]) ? $GLOBALS['TL_LANG']['MSC'][$k][0] : $GLOBALS['TL_LANG']['MSC'][$k];
+						}
+
+						if ($deleted)
+						{
+							$field = "<del>$field</del>";
 						}
 
 						$objDiff = new \Diff($from[$k], $to[$k]);
