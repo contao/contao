a268229b46a781a9148be32de2bdad986a75af47
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-05-24 09:30:59 +0200

    Update comments-bundle/contao/dca/tl_comments.php
    Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

--- comments-bundle/contao/dca/tl_comments.php
+++ comments-bundle/contao/dca/tl_comments.php
@@ -638,7 +638,7 @@ class tl_comments extends Backend
 			return Image::getHtml($icon) . ' ';
 		}
 
-		$titleDisabled = (is_array($GLOBALS['TL_DCA']['tl_comments']['list']['operations']['toggle']['label']) && isset($GLOBALS['TL_DCA']['tl_comments']['list']['operations']['toggle']['label'][2])) ? sprintf($GLOBALS['TL_DCA']['tl_comments']['list']['operations']['toggle']['label'][2], $row['id']) : $title;
+		$titleDisabled = isset($GLOBALS['TL_DCA']['tl_comments']['list']['operations']['toggle']['label'][2]) ? sprintf($GLOBALS['TL_DCA']['tl_comments']['list']['operations']['toggle']['label'][2], $row['id']) : $title;
 
 		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($row['published'] ? $title : $titleDisabled) . '" data-title="' . StringUtil::specialchars($title) . '" data-title-disabled="' . StringUtil::specialchars($titleDisabled) . '" onclick="Backend.getScrollOffset();return AjaxRequest.toggleField(this,true)">' . Image::getHtml($icon, $label, 'data-icon="visible.svg" data-icon-disabled="invisible.svg" data-state="' . ($row['published'] ? 1 : 0) . '"') . '</a> ';
 	}
