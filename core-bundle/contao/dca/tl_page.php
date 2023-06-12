4d3066118ab0112dff7faf743dd9c6c5060b9d79
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-12 13:56:21 +0200

    Use a status-aware title for toggle fields (see #5419)
    Description
    -----------
    
    Fixes https://github.com/contao/contao/issues/5393
    
    This can only be added in Contao 5 since we re-implemented the tooltips script (it previously did not "respect" the title tag). Since this is also changing the labels markup, I think it is better to target a new minor version.
    
    Commits
    -------
    
    424b0737 Use a status-aware title for toggle fields
    a77a55f7 Update the scripts
    a268229b Update comments-bundle/contao/dca/tl_comments.php
    1af088aa Add the missing is_array() check to the tl_comments DCA
    e470ef34 Also apply the logic to the `generateHeaderButtons()` method
    b9881589 Add the missing tl_calendar_events labels
    7f5436ab Fix a duplicate trans-unit ID
    8458dbb0 Correctly load the labels for the global button
    
    Co-authored-by: leofeyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/contao/dca/tl_page.php
+++ core-bundle/contao/dca/tl_page.php
@@ -1544,7 +1544,9 @@ class tl_page extends Backend
 			return Image::getHtml($icon) . ' ';
 		}
 
-		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.getScrollOffset();return AjaxRequest.toggleField(this,true)">' . Image::getHtml($icon, $label, 'data-icon="visible.svg" data-icon-disabled="invisible.svg" data-state="' . ($row['published'] ? 1 : 0) . '"') . '</a> ';
+		$titleDisabled = (is_array($GLOBALS['TL_DCA']['tl_page']['list']['operations']['toggle']['label']) && isset($GLOBALS['TL_DCA']['tl_page']['list']['operations']['toggle']['label'][2])) ? sprintf($GLOBALS['TL_DCA']['tl_page']['list']['operations']['toggle']['label'][2], $row['id']) : $title;
+
+		return '<a href="' . $this->addToUrl($href) . '" title="' . StringUtil::specialchars($row['published'] ? $title : $titleDisabled) . '" data-title="' . StringUtil::specialchars($title) . '" data-title-disabled="' . StringUtil::specialchars($titleDisabled) . '" onclick="Backend.getScrollOffset();return AjaxRequest.toggleField(this,true)">' . Image::getHtml($icon, $label, 'data-icon="visible.svg" data-icon-disabled="invisible.svg" data-state="' . ($row['published'] ? 1 : 0) . '"') . '</a> ';
 	}
 
 	/**
