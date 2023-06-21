05670716e200395f3fb2c9a580ded6ded63fe45c
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-06-21 10:51:26 +0200

    CS

--- core-bundle/src/Resources/contao/classes/StyleSheets.php
+++ core-bundle/src/Resources/contao/classes/StyleSheets.php
@@ -1009,7 +1009,7 @@ class StyleSheets extends Backend
 	 */
 	protected function shortenHexColor($color)
 	{
-		if (6 === strlen($color) && $color[0] == $color[1] && $color[2] == $color[3] && $color[4] == $color[5])
+		if (\strlen($color) == 6 && $color[0] == $color[1] && $color[2] == $color[3] && $color[4] == $color[5])
 		{
 			return $color[0] . $color[2] . $color[4];
 		}
