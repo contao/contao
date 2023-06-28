3fb6046c2db0fed7077554101eafed8d2eeaf8c2
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-28 11:27:24 +0200

    Unset PID for copy button callbacks

--- core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
+++ core-bundle/src/EventListener/DataContainer/DefaultOperationsListener.php
@@ -227,6 +227,15 @@ class DefaultOperationsListener
         unset($new['id']);
         $new['tstamp'] = 0;
 
+        // Unset the PID field for the copy operation (act=paste&mode=copy), so a voter can differentiate
+        // between the copy operation (without PID) and the paste button (with new target PID)
+        if (
+            ($GLOBALS['TL_DCA'][$table]['list']['sorting']['mode'] ?? null) === DataContainer::MODE_TREE
+            || !empty($GLOBALS['TL_DCA'][$table]['config']['ptable'] ?? null)
+        ) {
+            unset($new['pid']);
+        }
+
         return new CreateAction($table, $new);
     }
 
--- core-bundle/tests/EventListener/DataContainer/DefaultOperationsListenerTest.php
+++ core-bundle/tests/EventListener/DataContainer/DefaultOperationsListenerTest.php
@@ -572,9 +572,9 @@ class DefaultOperationsListenerTest extends TestCase
         yield 'copy operation' => [
             'copy',
             CreateAction::class,
-            ['id' => 15, 'foo' => 'bar'],
+            ['id' => 15, 'pid' => 42, 'foo' => 'bar'],
             [],
-            ['foo' => 'bar', 'tstamp' => 0],
+            ['pid' => 42, 'foo' => 'bar', 'tstamp' => 0],
         ];
 
         yield 'delete operation' => [
@@ -586,7 +586,7 @@ class DefaultOperationsListenerTest extends TestCase
         yield 'copy operation in tree mode' => [
             'copy',
             CreateAction::class,
-            ['id' => 15, 'foo' => 'bar'],
+            ['id' => 15, 'pid' => 0, 'foo' => 'bar'],
             ['list' => ['sorting' => ['mode' => DataContainer::MODE_TREE]]],
             ['foo' => 'bar', 'tstamp' => 0],
         ];
@@ -594,7 +594,7 @@ class DefaultOperationsListenerTest extends TestCase
         yield 'copy operation with parent table' => [
             'copy',
             CreateAction::class,
-            ['id' => 15, 'foo' => 'bar'],
+            ['id' => 15, 'pid' => 42, 'foo' => 'bar'],
             ['config' => ['ptable' => 'tl_bar']],
             ['foo' => 'bar', 'tstamp' => 0],
         ];
