4f8dcc0453a234394d43f8b11ebc6a0265b0fba9
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-06-28 15:55:17 +0200

    CS

--- core-bundle/src/Resources/contao/classes/DataContainer.php
+++ core-bundle/src/Resources/contao/classes/DataContainer.php
@@ -1735,7 +1735,7 @@ abstract class DataContainer extends Backend
 
 					$implode = static function ($v) use (&$implode)
 					{
-						return implode(', ', array_map(static fn($vv) => \is_array($vv) ? $implode($vv) : $vv, $v));
+						return implode(', ', array_map(static fn ($vv) => \is_array($vv) ? $implode($vv) : $vv, $v));
 					};
 
 					$args[$k] = $implode($args_k);
