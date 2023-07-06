73344363c50fec0fcdb725ddb9dee24f15280afb
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-07-06 17:15:04 +0200

    Use match instead of switch

--- core-bundle/src/InsertTag/Resolver/DateInsertTag.php
+++ core-bundle/src/InsertTag/Resolver/DateInsertTag.php
@@ -29,27 +29,19 @@ class DateInsertTag implements InsertTagResolverNestedResolvedInterface
         $result = new InsertTagResult(Date::parse($format), OutputType::text);
 
         preg_match_all('/['.implode('', Date::SUPPORTED_FORMAT_CHARACTERS).']/', $format, $matches);
-        $usedFormatChars = $matches[0] ?? [];
+        $usedFormatChars = array_unique($matches[0] ?? []);
         sort($usedFormatChars);
 
         // Add caching headers for supported formats
-        if (0 === \count(array_diff($usedFormatChars, self::CACHEABLE_FORMAT_CHARACTERS))) {
+        $result = $result->withExpiresAt(match ($usedFormatChars) {
+            ['Y', 'd', 'm'] => new \DateTimeImmutable('today 23:59:59'),
+            ['Y', 'm'] => new \DateTimeImmutable('last day of this month 23:59:59'),
+            ['Y'] => new \DateTimeImmutable('last day of December this year 23:59:59'),
+            default => null,
+        });
 
-            switch (true) {
-                case $usedFormatChars === ['Y', 'd', 'm']:
-                    $result = $result->withExpiresAt(new \DateTimeImmutable('today 23:59:59'));
-                    break;
-                case $usedFormatChars === ['Y', 'm']:
-                    $result = $result->withExpiresAt(new \DateTimeImmutable('last day of this month 23:59:59'));
-                    break;
-                case $usedFormatChars === ['Y']:
-                    $result = $result->withExpiresAt(new \DateTimeImmutable('last day of December this year 23:59:59'));
-                    break;
-            }
-
-            if ($rootId = $GLOBALS['objPage']->rootId ?? null) {
-                $result = $result->withCacheTags(["contao.db.tl_page.$rootId"]);
-            }
+        if (null !== $result->getExpiresAt() && ($rootId = $GLOBALS['objPage']->rootId ?? null)) {
+            $result = $result->withCacheTags(["contao.db.tl_page.$rootId"]);
         }
 
         return $result;
