34801842aeceff290ba8915dcfdb4fabe05b36f6
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-07-06 14:23:43 +0200

    Improve caching of date insert tag

--- core-bundle/src/InsertTag/Resolver/DateInsertTag.php
+++ core-bundle/src/InsertTag/Resolver/DateInsertTag.php
@@ -21,14 +21,31 @@ use Contao\Date;
 #[AsInsertTag('date', asFragment: true)]
 class DateInsertTag implements InsertTagResolverNestedResolvedInterface
 {
+    private const CACHEABLE_FORMAT_CHARACTERS = ['Y', 'm', 'd'];
+
     public function __invoke(ResolvedInsertTag $insertTag): InsertTagResult
     {
         $format = $insertTag->getParameters()->get(0) ?? $GLOBALS['objPage']->dateFormat ?? $GLOBALS['TL_CONFIG']['dateFormat'] ?? '';
         $result = new InsertTagResult(Date::parse($format), OutputType::text);
 
-        // Special handling for the very common {{date::Y}} (e.g. in the website footer) case
-        if ('Y' === $format) {
-            $result = $result->withExpiresAt(new \DateTimeImmutable($result->getValue().'-12-31 23:59:59'));
+        preg_match_all('/[dmY]/', $format, $matches);
+        $usedFormatChars = $matches[0] ?? [];
+        sort($usedFormatChars);
+
+        // Add caching headers for supported formats
+        if (0 === \count(array_diff($usedFormatChars, self::CACHEABLE_FORMAT_CHARACTERS))) {
+
+            switch (true) {
+                case $usedFormatChars === ['Y', 'd', 'm']:
+                    $result = $result->withExpiresAt(new \DateTimeImmutable('today 23:59:59'));
+                    break;
+                case $usedFormatChars === ['Y', 'm']:
+                    $result = $result->withExpiresAt(new \DateTimeImmutable('last day of this month 23:59:59'));
+                    break;
+                case $usedFormatChars === ['Y']:
+                    $result = $result->withExpiresAt(new \DateTimeImmutable('last day of December this year 23:59:59'));
+                    break;
+            }
 
             if ($rootId = $GLOBALS['objPage']->rootId ?? null) {
                 $result = $result->withCacheTags(["contao.db.tl_page.$rootId"]);
---
+++ core-bundle/tests/InsertTag/Resolver/DateInsertTagTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Contao\CoreBundle\Tests\InsertTag\Resolver;
+
+use Contao\CoreBundle\InsertTag\ResolvedInsertTag;
+use Contao\CoreBundle\InsertTag\ResolvedParameters;
+use Contao\CoreBundle\InsertTag\Resolver\DateInsertTag;
+use PHPUnit\Framework\TestCase;
+
+class DateInsertTagTest extends TestCase
+{
+    /**
+     * @dataProvider expiresAtProvider
+     */
+    public function testExpiresAt(string $format, \DateTimeImmutable|null $expectedExpiresAt): void
+    {
+        $insertTag = new DateInsertTag();
+        $result = $insertTag(new ResolvedInsertTag('date', new ResolvedParameters([$format]), []));
+        $this->assertSame($expectedExpiresAt?->getTimestamp(), $result->getExpiresAt()?->getTimestamp());
+    }
+
+    public function expiresAtProvider(): \Generator
+    {
+        yield 'Null when not cacheable' => [
+            'H:i:s',
+            null,
+        ];
+
+        yield 'Cacheable until the end of the year' => [
+            'Y',
+            new \DateTimeImmutable('last day of December this year 23:59:59'),
+        ];
+
+        yield 'Cacheable until the end of the month (Y-m)' => [
+            'Y-m',
+            new \DateTimeImmutable('last day of this month 23:59:59'),
+        ];
+
+        yield 'Cacheable until the end of the month (m.Y)' => [
+            'm.Y',
+            new \DateTimeImmutable('last day of this month 23:59:59'),
+        ];
+
+        yield 'Cacheable until the end of the day (d.m.Y)' => [
+            'd.m.Y',
+            new \DateTimeImmutable('today 23:59:59'),
+        ];
+
+        yield 'Cacheable until the end of the day (Y-d-m)' => [
+            'Y-d-m',
+            new \DateTimeImmutable('today 23:59:59'),
+        ];
+    }
+}
