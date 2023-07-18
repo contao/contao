70a2e2f966fedf6804b878efefd1e7de6260d36f
Benedict Massolle <bm@presentprogressive.de>
2023-07-18 11:09:10 +0200

    Use onsubmit callback to update permissions and add labels

--- core-bundle/src/EventListener/DataContainer/AddPermissionsListener.php
+++ core-bundle/src/EventListener/DataContainer/AddPermissionsListener.php
@@ -14,63 +14,49 @@ namespace Contao\CoreBundle\EventListener\DataContainer;
 
 use Contao\CheckBox;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\Translation\Translator;
 use Contao\DataContainer;
 use Contao\StringUtil;
 use Doctrine\DBAL\ArrayParameterType;
 use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class AddPermissionsListener
 {
     public function __construct(
         private readonly Connection $connection,
+        private readonly Translator $translator,
+        private readonly RequestStack $requestStack,
     ) {
     }
 
     #[AsCallback('tl_image_size', 'fields.permissions.input_field')]
     public function __invoke(DataContainer $dc): string
     {
-        $widget = new CheckBox([
-            'id' => 'permissions',
-            'name' => 'permissions',
-            'options' => $this->getUserAndGroupOptions(),
-            'multiple' => true,
-        ]);
+        $widget = $this->getWidget();
+        return '<div class="widget">'.$widget->generateWithError().'</div>';
+    }
 
-        // TODO: Add
-
-        $widget->validate();
-
-        if (\is_array($widget->value)) {
-            $this->updateUserAndGroupPermissions($dc->id, $widget->value);
+    #[AsCallback('tl_image_size', 'config.onsubmit')]
+    public function updateUserAndGroupPermissions(DataContainer $dc): void
+    {
+        if (!$request = $this->requestStack->getCurrentRequest()) {
+            return;
         }
 
-        return '<div class="widget w50">'.$widget->generateWithError().'</div>';
-    }
+        $widget = $this->getWidget();
+        $widget->value = $request->get('permissions');
+        $widget->validate();
 
-    private function getUserAndGroupOptions(): array
-    {
-        $groups = $this->connection
-            ->prepare('SELECT id, name FROM tl_user_group ORDER BY name ASC')
-            ->executeQuery()
-            ->fetchAllAssociative()
-        ;
+        if ($widget->hasErrors()) {
+            // TODO: Handle error case
+            return;
+        }
 
-        $users = $this->connection
-            ->prepare("SELECT id, name FROM tl_user WHERE inherit IN ('extend', 'custom') AND admin = false ORDER BY name ASC")
-            ->executeQuery()
-            ->fetchAllAssociative()
-        ;
+        $id = $dc->id;
 
-        return [
-            'groups' => array_map(static fn ($row) => ['label' => $row['name'], 'value' => 'g_'.$row['id']], $groups),
-            'users' => array_map(static fn ($row) => ['label' => $row['name'], 'value' => 'u_'.$row['id']], $users),
-        ];
-    }
-
-    private function updateUserAndGroupPermissions(string $id, array $values): void
-    {
         ['group_ids' => $groupIds, 'user_ids' => $userIds] = array_reduce(
-            $values,
+            $widget->value,
             static function ($carry, $item) {
                 if (str_starts_with($item, 'g_')) {
                     $carry['group_ids'][] = (int) ltrim($item, 'g_');
@@ -130,5 +116,30 @@ class AddPermissionsListener
                 ['id' => $user['id']]
             );
         }
+    }
+
+    private function getUserAndGroupOptions(): array
+    {
+        $groups = $this->connection->fetchAllAssociative("SELECT id, name FROM tl_user_group ORDER BY name ASC");
+        $users = $this->connection->fetchAllAssociative("SELECT id, name FROM tl_user WHERE inherit IN ('extend', 'custom') AND admin = false ORDER BY name ASC");
+
+        $groupLabel = $this->translator->trans('tl_image_size.group_permissions', [], 'contao_default');
+        $userLabel = $this->translator->trans('tl_image_size.user_permissions', [], 'contao_default');
+
+        return [
+            $groupLabel => array_map(static fn ($row) => ['label' => $row['name'], 'value' => 'g_'.$row['id']], $groups),
+            $userLabel => array_map(static fn ($row) => ['label' => $row['name'], 'value' => 'u_'.$row['id']], $users),
+        ];
+    }
+
+    public function getWidget(): CheckBox
+    {
+        return new CheckBox([
+            'id'       => 'permissions',
+            'name'     => 'permissions',
+            'label'    => $this->translator->trans('tl_image_size.permissions', [], 'contao_default'),
+            'options'  => $this->getUserAndGroupOptions(),
+            'multiple' => true,
+        ]);
     }
 }
