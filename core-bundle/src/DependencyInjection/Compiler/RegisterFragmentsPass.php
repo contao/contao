contao/contao

commit 55a78ce9117bb5379363c2d959735bad9026c70e

Allow using both modern fragments and Twig templates in extensions (see #5619)
Description
-----------

This backports #5315 for Contao 4.13.

With this PR, it should be possible to use modern fragments and Twig templates in extensions and be compatible with Contao 4.13 and Contao 5.

Aside from the syntax changes to be compatible with PHP 7.4, the only difference to #5315 now basically is the default template name, that gets set in the `RegisterFragmentsPass` (see fe115410e3e2a4b19f8fe78e618b11fa228275ca).

I went through a lot of manual testing but please also test this PR, preferably in a real project.  :relaxed: There a so many setup variants, so it's easy to miss one.

Commits
-------

6dca91a2 cherry-pick + adjust PHP syntax
fe115410 default to "ce_<type>" and "mod_<type>" for the template names
ac340717 Merge branch '4.13' into twig/backport-5315
45b48c7e adjust PHP syntax (7.4 compat)
66cf7700 remove redundant service DI
db496915 Merge branch '4.13' into twig/backport-5315
e5cd244f Merge branch '4.13' into twig/backport-5315

Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

core-bundle/src/DependencyInjection/Compiler/RegisterFragmentsPass.php
@@ -101,9 +101,14 @@ class RegisterFragmentsPass implements CompilerPassInterface
 
                 $config = $this->getFragmentConfig($container, new Reference($serviceId), $attributes);
 
-                if (!empty($attributes['template'])) {
-                    $templates[$attributes['type']] = $attributes['template'];
-                }
+                // Set a default template name if not defined. We are falling back to "ce_<type>" and
+                // "mod_<type>" in Contao 4.13; as of Contao 5 this will be "content_element/<type"
+                // and "frontend_module/<type>" instead.
+                $attributes['template'] = $attributes['template'] ??
+                    ['content_element' => 'ce_', 'frontend_module' => 'mod_'][substr($tag, 7)].$attributes['type']
+                ;
+
+                $templates[$attributes['type']] = $attributes['template'];
 
                 if (is_a($definition->getClass(), FragmentPreHandlerInterface::class, true)) {
                     $preHandlers[$identifier] = new Reference($serviceId);
@@ -140,7 +145,7 @@ class RegisterFragmentsPass implements CompilerPassInterface
         $this->addGlobalsMapListener($globals, $container);
 
         if (null !== $this->templateOptionsListener && $container->hasDefinition($this->templateOptionsListener)) {
-            $container->findDefinition($this->templateOptionsListener)->addMethodCall('setCustomTemplates', [$templates]);
+            $container->findDefinition($this->templateOptionsListener)->addMethodCall('setDefaultIdentifiersByType', [$templates]);
         }
     }
 

core-bundle/src/EventListener/DataContainer/TemplateOptionsListener.php
@@ -13,66 +13,119 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\EventListener\DataContainer;
 
 use Contao\Controller;
-use Contao\CoreBundle\Framework\Adapter;
 use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Twig\Finder\FinderFactory;
 use Contao\DataContainer;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class TemplateOptionsListener
 {
+    private FinderFactory $finderFactory;
+    private Connection $connection;
+    private ContaoFramework $framework;
     private RequestStack $requestStack;
-    private string $templatePrefix;
-    private ?string $proxyClass;
-    private array $customTemplates = [];
+    private string $legacyTemplatePrefix;
+    private ?string $legacyProxyClass;
 
     /**
-     * @var Adapter<Controller>
+     * @var array<string, string>
      */
-    private Adapter $controller;
+    private array $defaultIdentifiersByType = [];
 
-    public function __construct(ContaoFramework $framework, RequestStack $requestStack, string $templatePrefix, string $proxyClass = null)
+    public function __construct(FinderFactory $finderFactory, Connection $connection, ContaoFramework $framework, RequestStack $requestStack, string $legacyTemplatePrefix, string $legacyProxyClass = null)
     {
-        $controller = $framework->getAdapter(Controller::class);
-
-        $this->controller = $controller;
+        $this->finderFactory = $finderFactory;
+        $this->connection = $connection;
+        $this->framework = $framework;
         $this->requestStack = $requestStack;
-        $this->templatePrefix = $templatePrefix;
-        $this->proxyClass = $proxyClass;
+        $this->legacyTemplatePrefix = $legacyTemplatePrefix;
+        $this->legacyProxyClass = $legacyProxyClass;
     }
 
     public function __invoke(DataContainer $dc): array
     {
-        if ($this->isOverrideAll()) {
-            // Add a blank option that allows us to reset all custom templates to the default one
-            return array_merge(['' => '-'], $this->controller->getTemplateGroup($this->templatePrefix));
+        $overrideAll = $this->isOverrideAll();
+
+        $type = $overrideAll
+            ? $this->getCommonOverrideAllType($dc)
+            : $dc->activeRecord->type ?? null;
+
+        if (null === $type) {
+            // Add a blank option that allows to reset all custom templates to
+            // the default one when in "overrideAll" mode
+            return $overrideAll ? ['' => '-'] : [];
         }
 
-        $defaultTemplate = $this->customTemplates[$dc->activeRecord->type] ?? $this->getLegacyDefaultTemplate($dc);
+        $identifier = $this->defaultIdentifiersByType[$type] ?? null;
 
-        if (empty($defaultTemplate)) {
-            $defaultTemplate = $this->templatePrefix.$dc->activeRecord->type;
+        if (null !== ($legacyTemplateOptions = $this->handleLegacyTemplates($type, $identifier, $overrideAll))) {
+            return $legacyTemplateOptions;
         }
 
-        return $this->controller->getTemplateGroup($defaultTemplate.'_', [], $defaultTemplate);
+        $templateOptions = $this->finderFactory
+            ->create()
+            ->identifier($identifier)
+            ->extension('html.twig')
+            ->withVariants()
+            ->asTemplateOptions()
+        ;
+
+        if (0 === \count($templateOptions)) {
+            throw new \LogicException(sprintf('Tried to list template options for the modern fragment type "%s" but could not find any template. Did you forget to create the default template?', $identifier));
+        }
+
+        return $templateOptions;
     }
 
-    public function setCustomTemplates(array $customTemplates): void
+    /**
+     * Called by the RegisterFragmentsPass for all fragment controllers.
+     *
+     * @see \Contao\CoreBundle\DependencyInjection\Compiler\RegisterFragmentsPass
+     */
+    public function setDefaultIdentifiersByType(array $defaultIdentifiersByType): void
     {
-        $this->customTemplates = $customTemplates;
+        $this->defaultIdentifiersByType = $defaultIdentifiersByType;
+    }
+
+    /**
+     * Handles legacy elements that aren't implemented as fragment controllers
+     * or that still use the old template naming scheme.
+     */
+    private function handleLegacyTemplates(string $type, ?string $identifier, bool $overrideAll): ?array
+    {
+        $isModernIdentifier = $identifier && str_contains($identifier, '/');
+        $legacyDefaultIdentifier = $this->getLegacyDefaultIdentifier($type);
+
+        // Do not use the legacy logic for modern templates
+        if (null !== $identifier && $isModernIdentifier && !$legacyDefaultIdentifier) {
+            return null;
+        }
+
+        if (null === $identifier || $isModernIdentifier) {
+            $identifier = $legacyDefaultIdentifier ?? $this->legacyTemplatePrefix.$type;
+        }
+
+        return array_merge(
+            $overrideAll ? ['' => '-'] : [],
+            $this->framework
+                ->getAdapter(Controller::class)
+                ->getTemplateGroup($identifier.'_', [], $identifier),
+        );
     }
 
     /**
      * Uses the reflection API to return the default template from a legacy class.
      */
-    private function getLegacyDefaultTemplate(DataContainer $dc): ?string
+    private function getLegacyDefaultIdentifier(?string $type): ?string
     {
-        if (null === $this->proxyClass || !method_exists($this->proxyClass, 'findClass')) {
+        if (null === $type || null === $this->legacyProxyClass || !method_exists($this->legacyProxyClass, 'findClass')) {
             return null;
         }
 
-        $class = $this->proxyClass::findClass($dc->activeRecord->type);
+        $class = $this->legacyProxyClass::findClass($type);
 
-        if (empty($class) || $class === $this->proxyClass) {
+        if (empty($class) || $class === $this->legacyProxyClass) {
             return null;
         }
 
@@ -90,5 +143,27 @@ class TemplateOptionsListener
         }
 
         return 'overrideAll' === $request->query->get('act');
+    }
+
+    /**
+     * Returns the type that all currently edited items are sharing or null if
+     * there is no common type.
+     */
+    private function getCommonOverrideAllType(DataContainer $dc): ?string
+    {
+        $affectedIds = $this->requestStack->getSession()->all()['CURRENT']['IDS'] ?? [];
+        $table = $this->connection->quoteIdentifier($dc->table);
+
+        $result = $this->connection->executeQuery(
+            "SELECT type FROM $table WHERE id IN (?) GROUP BY type LIMIT 2",
+            [$affectedIds],
+            [Connection::PARAM_INT_ARRAY]
+        );
+
+        if (1 !== $result->rowCount()) {
+            return null;
+        }
+
+        return $result->fetchOne();
     }
 }

core-bundle/src/Resources/config/listener.yml
@@ -204,6 +204,8 @@ services:
     contao.listener.element_template_options:
         class: Contao\CoreBundle\EventListener\DataContainer\TemplateOptionsListener
         arguments:
+            - '@contao.twig.finder_factory'
+            - '@database_connection'
             - '@contao.framework'
             - '@request_stack'
             - ce_
@@ -345,6 +347,8 @@ services:
     contao.listener.module_template_options:
         class: Contao\CoreBundle\EventListener\DataContainer\TemplateOptionsListener
         arguments:
+            - '@contao.twig.finder_factory'
+            - '@database_connection'
             - '@contao.framework'
             - '@request_stack'
             - mod_

core-bundle/src/Resources/config/services.yml
@@ -990,6 +990,13 @@ services:
             - { name: kernel.cache_warmer }
             - { name: kernel.event_listener, event: kernel.request, method: onKernelRequest }
 
+    contao.twig.finder_factory:
+        class: Contao\CoreBundle\Twig\Finder\FinderFactory
+        arguments:
+            - '@contao.twig.filesystem_loader'
+            - '@contao.twig.loader.theme_namespace'
+            - '@translator'
+
     contao.twig.insert_tag_runtime:
         class: Contao\CoreBundle\Twig\Runtime\InsertTagRuntime
         arguments:

core-bundle/src/Resources/contao/library/Contao/Controller.php
@@ -109,6 +109,11 @@ abstract class Controller extends System
 	 */
 	public static function getTemplateGroup($strPrefix, array $arrAdditionalMapper=array(), $strDefaultTemplate='')
 	{
+		if (str_contains($strPrefix, '/') || str_contains($strDefaultTemplate, '/'))
+		{
+			throw new \InvalidArgumentException(sprintf('Using %s() with modern fragment templates is not supported. Use the "contao.twig.finder_factory" service instead.', __METHOD__));
+		}
+
 		$arrTemplates = array();
 		$arrBundleTemplates = array();
 

core-bundle/src/Twig/ContaoTwigUtil.php
@@ -40,7 +40,9 @@ final class ContaoTwigUtil
      */
     public static function getIdentifier(string $name): string
     {
-        return preg_replace('%(?:@[^/]+/)?(.*)(?:\.html5|\.html\.twig)%', '$1', $name);
+        preg_match('/^(?:@[^\/]+\/)?(.*?)\.(?:(?:[^.]+\.)?twig|html5)$/', $name, $matches);
+
+        return $matches[1] ?? $name;
     }
 
     /**
@@ -54,5 +56,15 @@ final class ContaoTwigUtil
         }
 
         return 'html5' === Path::getExtension($parts[1] ?? '', true);
+    }
+
+    /**
+     * Returns the file extension of a path including an optional .twig suffix.
+     */
+    public static function getExtension(string $path): string
+    {
+        preg_match('/\.([^.]+(?:\.twig)?)$/', $path, $matches);
+
+        return $matches[1] ?? '';
     }
 }

core-bundle/src/Twig/Finder/Finder.php
@@ -1,0 +1,198 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Twig\Finder;
+
+use Contao\CoreBundle\Twig\ContaoTwigUtil;
+use Contao\CoreBundle\Twig\Inheritance\TemplateHierarchyInterface;
+use Contao\CoreBundle\Twig\Loader\ThemeNamespace;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @experimental
+ * @implements \IteratorAggregate<string, string>
+ */
+final class Finder implements \IteratorAggregate, \Countable
+{
+    private TemplateHierarchyInterface $hierarchy;
+    private ThemeNamespace $themeNamespace;
+    private TranslatorInterface $translator;
+
+    private ?string $identifier = null;
+    private ?string $themeSlug = null;
+    private ?string $extension = null;
+    private bool $variantsExclusive = false;
+    private bool $variants = false;
+
+    /**
+     * @var array<string, list<string>>
+     */
+    private array $sources = [];
+
+    /**
+     * @internal
+     */
+    public function __construct(TemplateHierarchyInterface $hierarchy, ThemeNamespace $themeNamespace, TranslatorInterface $translator)
+    {
+        $this->hierarchy = $hierarchy;
+        $this->themeNamespace = $themeNamespace;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Filters templates based on the identifier, e.g. "content_element/text".
+     */
+    public function identifier(string $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    /**
+     * Filters templates based on the file extension, e.g. "html.twig" or "json.twig".
+     */
+    public function extension(string $extension): self
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * Filters templates based on the logical name or short name, e.g.
+     * "@Contao/content_element/text.html.twig" or "content_element/text.html.twig".
+     */
+    public function name(string $name): self
+    {
+        $this->identifier = ContaoTwigUtil::getIdentifier($name);
+        $this->extension = ContaoTwigUtil::getExtension($name);
+
+        return $this;
+    }
+
+    /**
+     * Also includes variant templates, e.g: "content_element/text/special" when
+     * filtering for "content_element/text". If $exclusive is set to true, only
+     * the variants will be output.
+     */
+    public function withVariants(bool $exclusive = false): self
+    {
+        $this->variants = true;
+        $this->variantsExclusive = $exclusive;
+
+        return $this;
+    }
+
+    /**
+     * Also includes templates of a certain theme. Only one theme at a time can
+     * be queried.
+     */
+    public function withTheme(string $themeSlug): self
+    {
+        $this->themeSlug = $themeSlug;
+
+        return $this;
+    }
+
+    /**
+     * Returns the result as template options.
+     *
+     * @return array<string, string>
+     */
+    public function asTemplateOptions(): array
+    {
+        $options = [];
+
+        $getSourceLabel = function (string $name): string {
+            if (null !== ($themeSlug = $this->themeNamespace->match($name))) {
+                return $this->translator->trans('MSC.templatesTheme', [$themeSlug], 'contao_default');
+            }
+
+            if (preg_match('/^@Contao_([^\/]+?)(?:Bundle)?\//', $name, $matches)) {
+                return $matches[1];
+            }
+
+            return $this->translator->trans('MSC.global', [], 'contao_default');
+        };
+
+        foreach (array_keys(iterator_to_array($this->getIterator())) as $identifier) {
+            $label = sprintf(
+                '%s [%s]',
+                $identifier,
+                implode(', ', array_map($getSourceLabel, $this->sources[$identifier]))
+            );
+
+            $options[$identifier !== $this->identifier ? $identifier : ''] = $label;
+        }
+
+        return $options;
+    }
+
+    /**
+     * @return \Generator<string, string>
+     */
+    public function getIterator(): \Generator
+    {
+        // Only include chains that contain at least one non-legacy template
+        $chains = array_filter(
+            $this->hierarchy->getInheritanceChains($this->themeSlug),
+            static function (array $chain) {
+                foreach (array_keys($chain) as $path) {
+                    if ('html5' !== Path::getExtension($path, true)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        );
+
+        $this->sources = [];
+
+        $matchIdentifier = function (string $identifier): bool {
+            if (!$this->variantsExclusive && $this->identifier === $identifier) {
+                return true;
+            }
+
+            if (!$this->variants) {
+                return false;
+            }
+
+            return str_starts_with($identifier, "$this->identifier/");
+        };
+
+        foreach ($chains as $identifier => $chain) {
+            if ($this->identifier && !$matchIdentifier($identifier)) {
+                continue;
+            }
+
+            // The loader makes sure that all files grouped under one
+            // identifier have the same extension
+            $extension = ContaoTwigUtil::getExtension(array_key_first($chain));
+
+            if (null !== $this->extension && $this->extension !== $extension) {
+                continue;
+            }
+
+            $this->sources[$identifier] = array_values($chain);
+
+            yield $identifier => $extension;
+        }
+    }
+
+    public function count(): int
+    {
+        return iterator_count($this->getIterator());
+    }
+}

core-bundle/src/Twig/Finder/FinderFactory.php
@@ -1,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Twig\Finder;
+
+use Contao\CoreBundle\Twig\Inheritance\TemplateHierarchyInterface;
+use Contao\CoreBundle\Twig\Loader\ThemeNamespace;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class FinderFactory
+{
+    private TemplateHierarchyInterface $hierarchy;
+    private ThemeNamespace $themeNamespace;
+    private TranslatorInterface $translator;
+
+    /**
+     * @internal
+     */
+    public function __construct(TemplateHierarchyInterface $hierarchy, ThemeNamespace $themeNamespace, TranslatorInterface $translator)
+    {
+        $this->hierarchy = $hierarchy;
+        $this->themeNamespace = $themeNamespace;
+        $this->translator = $translator;
+    }
+
+    /**
+     * Creates a new template finder instance.
+     */
+    public function create(): Finder
+    {
+        return new Finder($this->hierarchy, $this->themeNamespace, $this->translator);
+    }
+}

commit 22cd145a0d41bf2ac8facb2ab0060b0b23c33e8e

Make the RegisterFragmentType fragment type agnostic (see #5829)
Description
-----------

I use the [`RegisterFragmentsPass`](https://github.com/contao/contao/blob/4.13/core-bundle/src/DependencyInjection/Compiler/RegisterFragmentsPass.php) for custom fragment types. I utilize the `RegisterFragmentsPass` by simply registering it in my App Bundle:
```php
$container->addCompilerPass(new \Contao\CoreBundle\DependencyInjection\Compiler\RegisterFragmentsPass(\App\DashboardWidgetReference::TAG_NAME));
```

What changed from #5619 is, that the compiler pass now requires the `template` attribute for the registered services, and if the attribute is not given, falls back to `ce_` or `mod_` templates. However, my fragments do not require a `template` attribute because it is hardcoded.

```yaml
services:
    App\Controller\ContentElement\ExampleController:
        tags:
            -
                name: contao.content_element # <- this should not be limited to contao.content_element or contao.content_element
                template: ce_example  # <- this should not be mandatory (will be automatically guessed in the compiler pass)
```

How I perceived the `RegisterFragmentsPass` is, that it is a) _fragment type agnostic_ and b) _non-internal._
Consequently, the compiler pass should not expect `contao.content_element` and `contao.frontend_module` to be the only fragment types.

This issue was introduced in 55a78ce9117bb5379363c2d959735bad9026c70e.

Commits
-------

267282ce Make template attribute optional
c2a09c8c Only set template if templateOptionsListener is given

Co-authored-by: aschempp <andreas.schempp@terminal42.ch>

core-bundle/src/DependencyInjection/Compiler/RegisterFragmentsPass.php
@@ -104,11 +104,12 @@ class RegisterFragmentsPass implements CompilerPassInterface
                 // Set a default template name if not defined. We are falling back to "ce_<type>" and
                 // "mod_<type>" in Contao 4.13; as of Contao 5 this will be "content_element/<type"
                 // and "frontend_module/<type>" instead.
-                $attributes['template'] = $attributes['template'] ??
-                    ['content_element' => 'ce_', 'frontend_module' => 'mod_'][substr($tag, 7)].$attributes['type']
-                ;
+                if ($this->templateOptionsListener) {
+                    $attributes['template'] = $attributes['template']
+                        ?? ['content_element' => 'ce_', 'frontend_module' => 'mod_'][substr($tag, 7)].$attributes['type'];
 
-                $templates[$attributes['type']] = $attributes['template'];
+                    $templates[$attributes['type']] = $attributes['template'];
+                }
 
                 if (is_a($definition->getClass(), FragmentPreHandlerInterface::class, true)) {
                     $preHandlers[$identifier] = new Reference($serviceId);