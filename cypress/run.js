c264afe88c18886d5da5dc305acb4c0676353e5e
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-07-27 18:03:32 +0200

    Use shelljs

--- cypress/run.js
+++ cypress/run.js
@@ -1,23 +1,21 @@
-// require('child_process').spawn('bash', ['cypress/run.sh'], {
-//     cwd: process.cwd(),
-//     detached: true,
-//     stdio: 'inherit'
-// });
-
 const shell = require('shelljs');
+const live = require('shelljs-live');
 
+// The manager-bundle is mirrored instead of symlinked, so the console script
+// finds the correct project directory. Therefore, the folder needs to be
+// removed before the Composer update.
 shell.rm('-rf', 'cypress/webspace/vendor/contao/manager-bundle');
 
-if (shell.exec('composer up --working-dir=cypress/webspace').code !== 0) {
+if (live(['composer', 'up', '--working-dir=cypress/webspace']) !== 0) {
   shell.exit(1);
 }
 
-if (shell.exec('symfony server:start --port=8765 --dir=cypress/webspace --daemon').code !== 0) {
+if (live(['symfony', 'server:start', '--port=8765', '--dir=cypress/webspace', '--daemon']) !== 0) {
   shell.exit(1);
 }
 
-if (shell.exec('cypress run --config-file cypress/cypress.config.js').code !== 0) {
+if (live(['cypress', 'run', '--config-file=cypress/cypress.config.js']) !== 0) {
   shell.exit(1);
 }
 
-shell.exec('symfony server:stop --dir=cypress/webspace');
+live(['symfony', 'server:stop', '--dir=cypress/webspace']);
