6e2cf28a512038aa20092f9f327ef60ce223b963
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-08-11 15:25:57 +0300

    Convert another regular expression (see #6284)
    Description
    -----------
    
    The same as #6278 for the minify regex.
    
    Commits
    -------
    
    ce53190d Convert another regular expression

--- core-bundle/tests/Controller/ContentElement/ContentElementTestCase.php
+++ core-bundle/tests/Controller/ContentElement/ContentElementTestCase.php
@@ -177,28 +177,26 @@ class ContentElementTestCase extends TestCase
     protected function normalizeWhiteSpaces(string $string): string
     {
         // see https://stackoverflow.com/questions/5312349/minifying-final-html-output-using-regular-expressions-with-codeigniter
-        $minifyRegex = <<<'EOD'
-            (                                         # Collapse ws everywhere but in blacklisted elements
-                (?>                                   # Match all whitespans other than single space
-                    [^\S ]\s*                         # Either one [\t\r\n\f\v] and zero or more ws,
-                    | \s{2,}                          # or two or more consecutive-any-whitespace
-                )                                     # Note: The remaining regex consumes no text at all
-                (?=                                   # Ensure we are not in a blacklist tag
-                    (?:                               # Begin (unnecessary) group.
-                        (?:                           # Zero or more of...
-                            [^<]++                    # Either one or more non-"<"
-                            | <                       # or a < starting a non-blacklist tag
-                            (?!/?(?:textarea|pre)\b)
-                        )*+                           # (This could be "unroll-the-loop"ified)
-                    )                                 # End (unnecessary) group
-                    (?:                               # Begin alternation group
-                        <                             # Either a blacklist start tag
-                        (?>textarea|pre)\b
-                        | \z                          # or end of file
-                    )                                 # End alternation group
-                )                                     # If we made it here, we are not in a blacklist tag
-            )ix
-            EOD;
+        $minifyRegex = '(                         # Collapse ws everywhere but in blacklisted elements
+            (?>                                   # Match all whitespans other than single space
+                [^\S ]\s*                         # Either one [\t\r\n\f\v] and zero or more ws,
+                | \s{2,}                          # or two or more consecutive-any-whitespace
+            )                                     # Note: The remaining regex consumes no text at all
+            (?=                                   # Ensure we are not in a blacklist tag
+                (?:                               # Begin (unnecessary) group.
+                    (?:                           # Zero or more of...
+                        [^<]++                    # Either one or more non-"<"
+                        | <                       # or a < starting a non-blacklist tag
+                        (?!/?(?:textarea|pre)\b)
+                    )*+                           # (This could be "unroll-the-loop"ified)
+                )                                 # End (unnecessary) group
+                (?:                               # Begin alternation group
+                    <                             # Either a blacklist start tag
+                    (?>textarea|pre)\b
+                    | \z                          # or end of file
+                )                                 # End alternation group
+            )                                     # If we made it here, we are not in a blacklist tag
+        )ix';
 
         return trim(preg_replace($minifyRegex, ' ', $string));
     }
