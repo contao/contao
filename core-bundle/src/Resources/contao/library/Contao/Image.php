b549d42a4750daa7141504d8a03e5abf469dcfcb
fritzmg <fmg@inspiredminds.at>
2023-08-17 12:08:42 +0100

    remove debug throw

--- core-bundle/src/Resources/contao/library/Contao/Image.php
+++ core-bundle/src/Resources/contao/library/Contao/Image.php
@@ -889,7 +889,6 @@ class Image
 		catch (\Exception $e)
 		{
 			System::getContainer()->get('monolog.logger.contao.error')->error('Image "' . $image . '" could not be processed: ' . $e->getMessage());
-			throw $e;
 		}
 
 		return null;
