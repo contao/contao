d97a7913c1f9569c818ce40a90cd8bd45018ae79
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-08-24 17:10:15 +0200

    CS

--- core-bundle/src/Routing/Matcher/PublishedFilter.php
+++ core-bundle/src/Routing/Matcher/PublishedFilter.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Routing\Matcher;
 
-use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\PageModel;
 use Symfony\Cmf\Component\Routing\NestedMatcher\RouteFilterInterface;
 use Symfony\Component\HttpFoundation\Request;
