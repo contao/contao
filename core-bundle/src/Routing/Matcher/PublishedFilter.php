89368ce90ed1df339b146bf268da1ea898240d06
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-08-24 17:15:06 +0200

    Always allow unpublished pages in preview entry point (see #6332)
    Description
    -----------
    
    Should fix https://github.com/contao/contao/issues/6216
    
    Commits
    -------
    
    bab44029 Always allow unpublished pages in preview entry point
    b5761ded Fixed tests

--- core-bundle/src/Routing/Matcher/PublishedFilter.php
+++ core-bundle/src/Routing/Matcher/PublishedFilter.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Routing\Matcher;
 
-use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\PageModel;
 use Symfony\Cmf\Component\Routing\NestedMatcher\RouteFilterInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,19 +24,10 @@ use Symfony\Component\Routing\RouteCollection;
  */
 class PublishedFilter implements RouteFilterInterface
 {
-    private TokenChecker $tokenChecker;
-
-    /**
-     * @internal
-     */
-    public function __construct(TokenChecker $tokenChecker)
-    {
-        $this->tokenChecker = $tokenChecker;
-    }
-
     public function filter(RouteCollection $collection, Request $request): RouteCollection
     {
-        if ($this->tokenChecker->isPreviewMode()) {
+        // Route all pages in the preview entry point
+        if ($request->attributes->get('_preview', false)) {
             return $collection;
         }
 
