7e55ad35b9ee746a5433fe6ea1b153f12a43f7b7
zonky2 <info@e-spin.de>
2023-08-25 16:41:37 +0200

    Fix PHP8 warning

--- core-bundle/src/Resources/contao/dca/tl_article.php
+++ core-bundle/src/Resources/contao/dca/tl_article.php
@@ -643,7 +643,7 @@ class tl_article extends Backend
 				// Find all sections with an article module (see #6094)
 				foreach ($arrModules as $arrModule)
 				{
-					if ($arrModule['mod'] == 0 && $arrModule['enable'])
+					if ($arrModule['mod'] == 0 && !empty($arrModule['enable']))
 					{
 						$arrSections[] = $arrModule['col'];
 					}
