4b7b82f7c52ea273329b64d402882f840bfb628f
fritzmg <fmg@inspiredminds.at>
2023-09-09 16:56:41 +0100

    remove tests

--- core-bundle/tests/Routing/Page/PageRegistryTest.php
+++ core-bundle/tests/Routing/Page/PageRegistryTest.php
@@ -56,38 +56,6 @@ class PageRegistryTest extends TestCase
         $this->assertSame('/.+?', $route->getRequirement('parameters'));
     }
 
-    public function testReturnsUnparameteredPageRouteForForwardPages(): void
-    {
-        $pageModel = $this->mockClassWithProperties(PageModel::class);
-        $pageModel->type = 'forward';
-        $pageModel->alias = 'bar';
-        $pageModel->urlPrefix = 'foo';
-        $pageModel->urlSuffix = '.baz';
-
-        $registry = new PageRegistry($this->createMock(Connection::class));
-        $route = $registry->getRoute($pageModel);
-
-        $this->assertSame('/foo/bar.baz', $route->getPath());
-        $this->assertNull($route->getDefault('parameters'));
-    }
-
-    public function testReturnsParameteredPageRouteIfTheAlwaysForwardOptionIsSet(): void
-    {
-        $pageModel = $this->mockClassWithProperties(PageModel::class);
-        $pageModel->type = 'forward';
-        $pageModel->alias = 'bar';
-        $pageModel->urlPrefix = 'foo';
-        $pageModel->urlSuffix = '.baz';
-        $pageModel->alwaysForward = true;
-
-        $registry = new PageRegistry($this->createMock(Connection::class));
-        $route = $registry->getRoute($pageModel);
-
-        $this->assertSame('/foo/bar{!parameters}.baz', $route->getPath());
-        $this->assertSame('', $route->getDefault('parameters'));
-        $this->assertSame('(/.+?)?', $route->getRequirement('parameters'));
-    }
-
     public function testReturnsUnparameteredPageRouteForRedirectPages(): void
     {
         $pageModel = $this->mockClassWithProperties(PageModel::class);
