35e8f9704be9e3d9c35b5ea07f3f4ca3e2bf1b1b
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-10-11 13:20:13 +0200

    Make sure valid URLs start with a slash

--- core-bundle/src/Routing/RouteProvider.php
+++ core-bundle/src/Routing/RouteProvider.php
@@ -47,8 +47,8 @@ class RouteProvider extends AbstractPageRouteProvider
 
         $pathInfo = rawurldecode($request->getPathInfo());
 
-        // The request string must not contain "auto_item" (see #4012)
-        if (false !== strpos($pathInfo, '/auto_item/')) {
+        // The request string must start with "/" not contain "auto_item" (see #4012)
+        if (!str_starts_with($pathInfo, '/') || false !== strpos($pathInfo, '/auto_item/')) {
             return new RouteCollection();
         }
 
