a286cad64abc3efd0922da62b7a48a48ce749bef
zonky2 <info@e-spin.de>
2023-10-24 11:13:45 +0200

    Fix PHP8 warning

--- core-bundle/src/Resources/contao/classes/Frontend.php
+++ core-bundle/src/Resources/contao/classes/Frontend.php
@@ -430,7 +430,7 @@ abstract class Frontend extends Controller
 		// Merge the new request string
 		foreach ($arrFragments as $strFragment)
 		{
-			list($key, $value) = explode('=', $strFragment);
+			list($key, $value) = explode('=', $strFragment) + [null, null];
 
 			if (!$value)
 			{
