eba6e34924a382f0e2ee46826790bfb4ed2156fe
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-11-14 23:22:33 +0100

    Also remove global operations in bundles

--- newsletter-bundle/contao/dca/tl_newsletter_recipients.php
+++ newsletter-bundle/contao/dca/tl_newsletter_recipients.php
@@ -69,12 +69,7 @@ $GLOBALS['TL_DCA']['tl_newsletter_recipients'] = array
 				'class'               => 'header_css_import',
 				'attributes'          => 'onclick="Backend.getScrollOffset()"'
 			),
-			'all' => array
-			(
-				'href'                => 'act=select',
-				'class'               => 'header_edit_all',
-				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
-			)
+			'all'
 		),
 		'operations' => array
 		(
