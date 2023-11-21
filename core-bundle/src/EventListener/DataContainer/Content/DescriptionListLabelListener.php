df1603b8f33928ce100006ffe3c05ba97ecebfed
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-11-21 07:17:54 +0100

    CS

--- core-bundle/src/EventListener/DataContainer/Content/DescriptionListLabelListener.php
+++ core-bundle/src/EventListener/DataContainer/Content/DescriptionListLabelListener.php
@@ -21,7 +21,7 @@ class DescriptionListLabelListener
     {
     }
 
-    public function __invoke(array $attributes, DataContainer $dc = null): array
+    public function __invoke(array $attributes, DataContainer|null $dc = null): array
     {
         if (null === $dc || 'description_list' !== ($dc->getCurrentRecord()['type'] ?? null)) {
             return $attributes;
