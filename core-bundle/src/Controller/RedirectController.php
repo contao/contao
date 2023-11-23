431b12e590ea03a3c1b86d9644cc6ad6dc052fb5
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-11-23 17:45:15 +0100

    Disable HSTS if root page is configured on http (#14)

---
+++ core-bundle/src/Controller/RedirectController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Controller;
+
+use Contao\PageModel;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController as SymfonyRedirectController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @internal
+ */
+class RedirectController
+{
+    public function __construct(private readonly SymfonyRedirectController $controller)
+    {
+    }
+
+    public function urlRedirectAction(Request $request, string $path, bool $permanent = false, string $scheme = null, int $httpPort = null, int $httpsPort = null, bool $keepRequestMethod = false): Response
+    {
+        $response = $this->controller->urlRedirectAction($request, $path, $permanent, $scheme, $httpPort, $httpsPort, $keepRequestMethod);
+        $pageModel = $request->attributes->get('pageModel');
+
+        if ($pageModel instanceof PageModel && !$pageModel->useSSL && $request->isSecure()) {
+            $response->headers->set('Strict-Transport-Security', 'max-age=0');
+        }
+
+        return $response;
+    }
+}
