34fec9d336fc4736c873fcdee777c8eaeace838a
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-04 11:47:33 +0100

    Updated message

--- core-bundle/contao/library/Contao/Controller.php
+++ core-bundle/contao/library/Contao/Controller.php
@@ -1189,7 +1189,7 @@ abstract class Controller extends System
 	 */
 	protected function redirectToFrontendPage($intPage, $strArticle=null, $blnReturn=false)
 	{
-		trigger_deprecation('contao/core-bundle', '5.3', __METHOD__ . ' is deprecated and will be removed in Contao 6. Use the preview controller arguments instead.');
+		trigger_deprecation('contao/core-bundle', '5.3', __METHOD__ . ' is deprecated and will be removed in Contao 6. Use PageModel::getAbsoluteUrl() and the contao_backend_preview route instead.');
 
 		if (($intPage = (int) $intPage) <= 0)
 		{
