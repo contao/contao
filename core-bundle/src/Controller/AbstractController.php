ee8ac1997d5c3d4a6da873598d33f436de7b51ff
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-12-05 10:11:28 +0100

    CS

--- core-bundle/src/Controller/AbstractController.php
+++ core-bundle/src/Controller/AbstractController.php
@@ -21,7 +21,6 @@ use Contao\PageModel;
 use FOS\HttpCacheBundle\Http\SymfonyResponseTagger;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
-use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Response;
 
