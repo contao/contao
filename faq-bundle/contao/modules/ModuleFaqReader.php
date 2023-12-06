0ffa546bf12b44ec41d2a179ce07d149186ab6cb
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-06 10:23:58 +0100

    Handle missing overviewPage in reader module

--- faq-bundle/contao/modules/ModuleFaqReader.php
+++ faq-bundle/contao/modules/ModuleFaqReader.php
@@ -74,9 +74,9 @@ class ModuleFaqReader extends Module
 		/** @var PageModel $objPage */
 		global $objPage;
 
-		if ($this->overviewPage)
+		if ($this->overviewPage && ($overviewPage = PageModel::findById($this->overviewPage)))
 		{
-			$this->Template->referer = System::getContainer()->get('contao.routing.content_url_generator')->generate(PageModel::findById($this->overviewPage));
+			$this->Template->referer = System::getContainer()->get('contao.routing.content_url_generator')->generate($overviewPage);
 			$this->Template->back = $this->customLabel ?: $GLOBALS['TL_LANG']['MSC']['faqOverview'];
 		}
 
