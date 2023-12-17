fe45eed8cfb04b9d4dfd7bcad83cac3cebd17cff
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-12-17 12:57:52 +0000

    validate CSP

---
+++ core-bundle/src/EventListener/DataContainer/CspSaveCallbackListener.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use ParagonIE\CSPBuilder\CSPBuilder;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[AsCallback('tl_page', 'fields.csp.save')]
+class CspSaveCallbackListener
+{
+    public function __construct(private readonly TranslatorInterface $translator)
+    {
+    }
+
+    public function __invoke($value)
+    {
+        try {
+            CSPBuilder::fromHeader(trim((string) $value));
+        } catch (\Throwable $e) {
+            throw new \Exception($this->translator->trans('ERR.invalidCsp', [$e->getMessage()], 'contao_default'));
+        }
+
+        return $value;
+    }
+}
