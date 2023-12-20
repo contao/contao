72ea08d01c5044fab4ad27d8f367c700dac56cde
Yanick Witschi <yanick.witschi@terminal42.ch>
2023-12-20 09:54:27 +0100

    Automatically enable the Strict Transport Security (HSTS) header (see #6553)
    Description
    -----------
    
    This will automatically send the `Strict-Transport-Security` header for all Contao requests (front end and back end). Imho, this always makes sense for `https` requests (for non-https, the header is irrelevant anyway).
    
    I decided to enable it by default, I think this is the sane way to go. If you want to disable it, you can easily do that:
    
    ```yaml
    contao:
        security:
            hsts: false # or enable: false
    ```
    
    The TTL is configured to 1 year by default. You can also change this in case you want to:
    
    
    ```yaml
    contao:
        security:
            hsts:
                ttl: 3600
    ```
    
    Commits
    -------
    
    445be0a9 Automatically enable Strict-Transport-Security (HSTS) header
    e5fff282 Ignore unsafe request methods and do not set the header if already pr…
    431b12e5 Disable HSTS if root page is configured on http (#14)
    5f90c3d8 Ensure a default ttl
    2030ae24 Also apply the header on non-safe requests
    
    Co-authored-by: aschempp <andreas.schempp@terminal42.ch>

---
+++ core-bundle/src/EventListener/TransportSecurityHeaderListener.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener;
+
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+/**
+ * @internal
+ */
+class TransportSecurityHeaderListener
+{
+    public function __construct(
+        private readonly ScopeMatcher $scopeMatcher,
+        private readonly int $ttl = 31536000,
+    ) {
+    }
+
+    public function __invoke(ResponseEvent $event): void
+    {
+        if (
+            !$this->scopeMatcher->isContaoMainRequest($event)
+            || !$event->getRequest()->isSecure()
+            || $event->getResponse()->headers->has('Strict-Transport-Security')
+        ) {
+            return;
+        }
+
+        $event->getResponse()->headers->set('Strict-Transport-Security', 'max-age='.$this->ttl);
+    }
+}
---
+++ core-bundle/tests/EventListener/TransportSecurityHeaderListenerTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\EventListener;
+
+use Contao\CoreBundle\EventListener\TransportSecurityHeaderListener;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class TransportSecurityHeaderListenerTest extends TestCase
+{
+    public function testIgnoresNonContaoMainRequests(): void
+    {
+        $response = new Response();
+        $request = Request::create('https://contao.org');
+
+        $listener = new TransportSecurityHeaderListener($this->createScopeMatcher(false), 31536000);
+        $listener($this->createEvent($request, $response));
+
+        $this->assertFalse($response->headers->has('Strict-Transport-Security'));
+    }
+
+    public function testIgnoresContaoMainRequestsThatAreNotSecure(): void
+    {
+        $response = new Response();
+        $request = Request::create('http://contao.org');
+
+        $listener = new TransportSecurityHeaderListener($this->createScopeMatcher(true), 31536000);
+        $listener($this->createEvent($request, $response));
+
+        $this->assertFalse($response->headers->has('Strict-Transport-Security'));
+    }
+
+    public function testIgnoresIfTheResponseAlreadyHasAnStsHeaderPresent(): void
+    {
+        $response = new Response();
+        $response->headers->set('Strict-Transport-Security', 'max-age=500; includeSubDomains; preload');
+        $request = Request::create('https://contao.org');
+
+        $listener = new TransportSecurityHeaderListener($this->createScopeMatcher(true), 31536000);
+        $listener($this->createEvent($request, $response));
+
+        $this->assertTrue($response->headers->has('Strict-Transport-Security'));
+        $this->assertSame('max-age=500; includeSubDomains; preload', $response->headers->get('Strict-Transport-Security'));
+    }
+
+    public function testAppliesCorrectTtl(): void
+    {
+        $response = new Response();
+        $request = Request::create('https://contao.org');
+
+        $listener = new TransportSecurityHeaderListener($this->createScopeMatcher(true));
+        $listener($this->createEvent($request, $response));
+
+        $this->assertTrue($response->headers->has('Strict-Transport-Security'));
+        $this->assertSame('max-age=31536000', $response->headers->get('Strict-Transport-Security'));
+
+        $response = new Response();
+        $request = Request::create('https://contao.org');
+
+        $listener = new TransportSecurityHeaderListener($this->createScopeMatcher(true), 500);
+        $listener($this->createEvent($request, $response));
+
+        $this->assertTrue($response->headers->has('Strict-Transport-Security'));
+        $this->assertSame('max-age=500', $response->headers->get('Strict-Transport-Security'));
+    }
+
+    private function createEvent(Request $request, Response $response): ResponseEvent
+    {
+        return new ResponseEvent(
+            $this->createMock(KernelInterface::class),
+            $request,
+            HttpKernelInterface::MAIN_REQUEST,
+            $response,
+        );
+    }
+
+    private function createScopeMatcher(bool $isContaoMainRequest): ScopeMatcher
+    {
+        $scopeMatcher = $this->createMock(ScopeMatcher::class);
+        $scopeMatcher
+            ->expects($this->once())
+            ->method('isContaoMainRequest')
+            ->willReturn($isContaoMainRequest)
+        ;
+
+        return $scopeMatcher;
+    }
+}
