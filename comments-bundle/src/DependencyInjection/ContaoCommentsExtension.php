bf4f0f3b826c15c346e371de7a8e6be14aa90e2a
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2023-12-22 01:56:34 +0100

    Conditionally load fields via DataContainerListener
    - use loadDataContainer Hook
    - universal size
      - notify is now varchar(32) for faq and news
      - sortOrder is now varchar(32) for faq

--- comments-bundle/src/DependencyInjection/ContaoCommentsExtension.php
+++ comments-bundle/src/DependencyInjection/ContaoCommentsExtension.php
@@ -22,6 +22,7 @@ class ContaoCommentsExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
+        $loader->load('listener.yaml');
         $loader->load('services.yaml');
     }
 }
---
+++ comments-bundle/src/EventListener/DataContainer/CommentsConditionalFieldsListener.php
@@ -0,0 +1,186 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CommentsBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+
+#[AsHook('loadDataContainer')]
+class CommentsConditionalFieldsListener
+{
+    public function __invoke(string $table): void
+    {
+        if (!isset($GLOBALS['TL_DCA'][$table])) {
+            return;
+        }
+
+        switch ($table)
+        {
+            case 'tl_news_archive':
+            case 'tl_calendar':
+            case 'tl_faq_category':
+                self::applyParentFields($table);
+                break;
+
+            case 'tl_news':
+            case 'tl_calendar_events':
+            case 'tl_faq':
+                self::applyChildrenFields($table);
+                break;
+        }
+    }
+
+    private function applyParentFields($table): void
+    {
+        $GLOBALS['TL_DCA'][$table]['palettes']['__selector__'][] = 'allowComments';
+        $GLOBALS['TL_DCA'][$table]['subpalettes']['allowComments'] = 'notify,sortOrder,perPage,moderate,bbcode,requireLogin,disableCaptcha';
+
+        $GLOBALS['TL_DCA'][$table]['fields']['allowComments'] = [
+            'filter'                  => true,
+            'inputType'               => 'checkbox',
+            'eval'                    => array('submitOnChange'=>true),
+            'sql'                     => array('type' => 'boolean', 'default' => false)
+        ];
+
+        $GLOBALS['TL_DCA'][$table]['fields']['notify'] = array
+        (
+            'inputType'               => 'select',
+            'options'                 => array('notify_admin', 'notify_author', 'notify_both'),
+            'reference'               => &$GLOBALS['TL_LANG'][$table],
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => "varchar(32) NOT NULL default 'notify_admin'"
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['sortOrder'] = array
+        (
+            'inputType'               => 'select',
+            'options'                 => array('ascending', 'descending'),
+            'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+            'eval'                    => array('tl_class'=>'w50 clr'),
+            'sql'                     => "varchar(32) NOT NULL default 'ascending'"
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['perPage'] = array
+        (
+            'inputType'               => 'text',
+            'eval'                    => array('rgxp'=>'natural', 'tl_class'=>'w50'),
+            'sql'                     => "smallint(5) unsigned NOT NULL default 0"
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['moderate'] = array
+        (
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => array('type' => 'boolean', 'default' => false)
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['bbcode'] = array
+        (
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => array('type' => 'boolean', 'default' => false)
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['requireLogin'] = array
+        (
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => array('type' => 'boolean', 'default' => false)
+        );
+
+        $GLOBALS['TL_DCA'][$table]['fields']['disableCaptcha'] = array
+        (
+            'inputType'               => 'checkbox',
+            'eval'                    => array('tl_class'=>'w50'),
+            'sql'                     => array('type' => 'boolean', 'default' => false)
+        );
+
+        switch ($table)
+        {
+            case 'tl_news':
+            case 'tl_calendar_events':
+                PaletteManipulator::create()
+                    ->addLegend('comments_legend', 'protected_legend', PaletteManipulator::POSITION_AFTER, true)
+                    ->addField(['allowComments'], 'comments_legend', PaletteManipulator::POSITION_APPEND)
+                    ->applyToPalette('default', $table)
+                ;
+                break;
+
+            case 'tl_faq':
+                PaletteManipulator::create()
+                    ->addLegend('comments_legend', 'title_legend', PaletteManipulator::POSITION_AFTER, true)
+                    ->addField(['allowComments'], 'comments_legend', PaletteManipulator::POSITION_APPEND)
+                    ->applyToPalette('default', $table)
+                ;
+                break;
+        }
+    }
+
+    private function applyChildrenFields($table): void
+    {
+        $GLOBALS['TL_DCA'][$table]['list']['sorting']['headerFields'][] = 'allowComments';
+
+        switch ($table)
+        {
+            case 'tl_news':
+                $GLOBALS['TL_DCA'][$table]['fields']['noComments'] = array
+                (
+                    'filter'                  => true,
+                    'inputType'               => 'checkbox',
+                    'eval'                    => array('tl_class'=>'w50 m12'),
+                    'sql'                     => array('type' => 'boolean', 'default' => false)
+                );
+                break;
+
+            case 'tl_calendar_events':
+                $GLOBALS['TL_DCA'][$table]['fields']['noComments'] = array
+                (
+                    'inputType'               => 'checkbox',
+                    'eval'                    => array('tl_class'=>'w50 m12'),
+                    'sql'                     => array('type' => 'boolean', 'default' => false)
+                );
+                break;
+
+            case 'tl_faq':
+                $GLOBALS['TL_DCA'][$table]['fields']['noComments'] = array
+                (
+                    'filter'                  => true,
+                    'inputType'               => 'checkbox',
+                    'sql'                     => array('type' => 'boolean', 'default' => false)
+                );
+                break;
+        }
+
+        switch ($table)
+        {
+            case 'tl_news':
+            case 'tl_calendar_events':
+                PaletteManipulator::create()
+                    ->addField(['noComments'], 'expert_legend', PaletteManipulator::POSITION_APPEND)
+                    ->applyToPalette('default', $table)
+                    ->applyToPalette('internal', $table)
+                    ->applyToPalette('article', $table)
+                    ->applyToPalette('external', $table)
+                ;
+                break;
+
+            case 'tl_faq':
+                PaletteManipulator::create()
+                    ->addLegend('expert_legend', 'publish_legend', PaletteManipulator::POSITION_BEFORE, true)
+                    ->addField(['noComments'], 'expert_legend', PaletteManipulator::POSITION_APPEND)
+                    ->applyToPalette('default', $table)
+                ;
+                break;
+        }
+    }
+}
