376e5b1802333f07bef527ebfd312485256b2900
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-12-24 16:56:06 +0100

    Fix toggle operation parameter check

--- core-bundle/contao/classes/DataContainer.php
+++ core-bundle/contao/classes/DataContainer.php
@@ -961,7 +961,7 @@ abstract class DataContainer extends Backend
 				$href = $this->addToUrl(($config['href'] ?? '') . '&amp;id=' . $arrRow['id'] . (Input::get('nb') ? '&amp;nc=1' : '') . ($isPopup ? '&amp;popup=1' : ''));
 			}
 
-			parse_str(StringUtil::decodeEntities($config['href'] ?? ''), $params);
+			parse_str(StringUtil::decodeEntities($config['href'] ?? $v['href'] ?? ''), $params);
 
 			if (($params['act'] ?? null) == 'toggle' && isset($params['field']))
 			{
