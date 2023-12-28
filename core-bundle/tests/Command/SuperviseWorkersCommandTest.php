fd38b7fc2de60b418b835400087f42a3dc9de10c
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-12-28 13:06:15 +0100

    Fix a few minor issues

--- core-bundle/tests/Command/SuperviseWorkersCommandTest.php
+++ core-bundle/tests/Command/SuperviseWorkersCommandTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Command;
+namespace Contao\CoreBundle\Tests\Command;
 
 use Contao\CoreBundle\Command\SuperviseWorkersCommand;
 use Contao\CoreBundle\Tests\TestCase;
