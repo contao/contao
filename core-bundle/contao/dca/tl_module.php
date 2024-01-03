0e3dcefddc9443640f9fa5d60ec59a5883067efd
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-01-03 13:31:09 +0100

    Allow adding a lost-password page to the login module (see #6639)
    Description
    -----------
    
    **Feature**
    
    - Implements #4710
    
    **Description**
    
    - This PR introduces the function of setting up a new redirect page for embedding the "Lost password" module in the login module in order to improve UX.
    
    **Related**
    - #6635
    - #4710
    
    ___
    
    It's a feature, not a bug - no need to merge it into 5.2
    
    Commits
    -------
    
    9e68c327 Add picker for lost-password redirect within login module (#4710)
    5e6ba215 Move passwordReset-link within template
    efe08825 Revert translations
    f296e490 Code cleanup
    be65091e Update template
    aaa4b0c2 Rename the `pwResetJumpTo` field to `pwResetPage`
    
    Co-authored-by: leofeyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/contao/dca/tl_module.php
+++ core-bundle/contao/dca/tl_module.php
@@ -73,7 +73,7 @@ $GLOBALS['TL_DCA']['tl_module'] = array
 		'booknav'                     => '{title_legend},name,headline,type;{nav_legend},showProtected,showHidden;{reference_legend:hide},rootPage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
 		'articlenav'                  => '{title_legend},name,headline,type;{config_legend},loadFirst;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
 		'sitemap'                     => '{title_legend},name,headline,type;{nav_legend},showProtected,showHidden;{reference_legend:hide},rootPage;{template_legend:hide},customTpl,navigationTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
-		'login'                       => '{title_legend},name,headline,type;{config_legend},autologin;{redirect_legend},jumpTo,redirectBack;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
+		'login'                       => '{title_legend},name,headline,type;{config_legend},autologin,pwResetPage;{redirect_legend},jumpTo,redirectBack;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
 		'logout'                      => '{title_legend},name,type;{redirect_legend},jumpTo,redirectBack;{protected_legend:hide},protected;{expert_legend:hide},cssID',
 		'personalData'                => '{title_legend},name,headline,type;{config_legend},editable;{redirect_legend},jumpTo;{template_legend:hide},memberTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
 		'registration'                => '{title_legend},name,headline,type;{config_legend},editable,newsletters,disableCaptcha;{account_legend},reg_groups,reg_allowLogin,reg_assignDir;{redirect_legend},jumpTo;{email_legend},reg_activate;{template_legend:hide},memberTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID',
@@ -250,6 +250,14 @@ $GLOBALS['TL_DCA']['tl_module'] = array
 		(
 			'inputType'               => 'checkbox',
 			'sql'                     => array('type' => 'boolean', 'default' => false),
+		),
+		'pwResetPage' => array
+		(
+			'inputType'               => 'pageTree',
+			'foreignKey'              => 'tl_page.title',
+			'eval'                    => array('fieldType'=>'radio'),
+			'sql'                     => "int(10) unsigned NOT NULL default 0",
+			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
 		),
 		'editable' => array
 		(
