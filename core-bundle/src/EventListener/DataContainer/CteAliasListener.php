33b090b8286bae096565a2c322b28d7dfef1e825
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-01-04 12:12:48 +0100

    Cache the `cteAlias` references (see #6434)
    Description
    -----------
    
    We have a `button_callback` for the `delete` button within `tl_content` which checks for each individual element whether the element is referenced by an `alias` element, so that the delete button is then disabled. If you have a lot of content elements on the same page (and you are displaying all of them) then this adds up in database query times. Querying all `cteAlias` references once and caching them statically can shave off a few hundred milliseconds, depending on the situation.
    
    However, in most situations this is only a negligible improvement. The main improvement to this comes from the `type` index that we already have. Otherwise the query time for each individual query would be much, much longer.
    
    Commits
    -------
    
    7cc6311f cache the cteAlias references
    b8d14d91 code style
    6f798425 cs fix
    b15fbaa3 set all array values to true
    21362602 simplify code
    f83ce26c implement as listener service
    bb765a6c add tests
    a48df707 reset global state
    266de767 make cache member instead of static
    c6739649 remove unnecessary reset
    c23437b2 use Annotations instead of Attributes
    0306177e add internal

---
+++ core-bundle/src/EventListener/DataContainer/CteAliasListener.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\Backend;
+use Contao\CoreBundle\Exception\InternalServerErrorException;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
+use Contao\CoreBundle\ServiceAnnotation\Callback;
+use Contao\Image;
+use Contao\StringUtil;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\Service\ResetInterface;
+
+/**
+ * @internal
+ */
+class CteAliasListener implements ResetInterface
+{
+    private RequestStack $requestStack;
+    private Security $security;
+    private Connection $db;
+    private ContaoFramework $framework;
+    private ?array $cteAliasCache = null;
+
+    public function __construct(RequestStack $requestStack, Security $security, Connection $db, ContaoFramework $framework)
+    {
+        $this->requestStack = $requestStack;
+        $this->security = $security;
+        $this->db = $db;
+        $this->framework = $framework;
+    }
+
+    /**
+     * Prevent deleting referenced elements (see #4898).
+     *
+     * @Callback(table="tl_content", target="config.onload")
+     */
+    public function preserveReferenced(): void
+    {
+        $aliasRefs = $this->getAliasReferences();
+        $request = $this->requestStack->getCurrentRequest();
+        $id = $request->query->get('id');
+
+        if ($id && 'delete' === $request->query->get('act') && isset($aliasRefs[$id])) {
+            throw new InternalServerErrorException(sprintf('Content element ID %s is used in an alias element and can therefore not be deleted.', $id));
+        }
+
+        if ('deleteAll' === $request->query->get('act')) {
+            $session = $request->getSession();
+            $sessionData = $session->all();
+            $sessionData['CURRENT']['IDS'] = array_diff($sessionData['CURRENT']['IDS'], array_map('intval', array_keys($aliasRefs)));
+            $session->replace($sessionData);
+        }
+    }
+
+    /**
+     * Return the delete content element button.
+     *
+     * @Callback(table="tl_content", target="list.operations.delete.button")
+     */
+    public function deleteElement(array $row, ?string $href, string $label, string $title, ?string $icon, string $attributes): string
+    {
+        $permission = ContaoCorePermissions::USER_CAN_ACCESS_ELEMENT_TYPE;
+        $imageAdapter = $this->framework->getAdapter(Image::class);
+
+        // Disable the button if the element type is not allowed or the element is referenced
+        if (!$this->security->isGranted($permission, $row['type']) || isset($this->getAliasReferences()[(int) $row['id']])) {
+            return $imageAdapter->getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
+        }
+
+        $backendAdapter = $this->framework->getAdapter(Backend::class);
+
+        return '<a href="'.$backendAdapter->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.$imageAdapter->getHtml($icon, $label).'</a> ';
+    }
+
+    public function reset(): void
+    {
+        $this->cteAliasCache = null;
+    }
+
+    private function getAliasReferences(): array
+    {
+        if (null === $this->cteAliasCache) {
+            $this->cteAliasCache = $this->db->fetchAllKeyValue("SELECT cteAlias, TRUE FROM tl_content WHERE type='alias' GROUP BY cteAlias");
+        }
+
+        return $this->cteAliasCache;
+    }
+}
