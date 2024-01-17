62784b0cf5dea02b14877ec795b03d0b11ab9aae
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-01-17 16:37:50 +0100

    Generate FAQ URLs using the content URL generator (see #6597)
    Description
    -----------
    
    This is an example of https://github.com/contao/contao/pull/6596 in action. I deliberatly chose a "simple" bundle here 😉 
    This obviously fails in all places because it requires https://github.com/contao/contao/pull/6596 to be merged first. But it shows the necessary/possible adjustments.
    
    Commits
    -------
    
    51bc793c Generate FAQ URLs using content URL generator
    0ffa546b Handle missing overviewPage in reader module
    037f84fa Added FaqResolverTest
    cd14d9b0 Merge remote-tracking branch 'upstream/5.x' into feature/faq-content-url
    a1247232 Merge remote-tracking branch 'upstream/5.x' into feature/faq-content-url
    d4ef708f Return NULL to abstain
    9a2ffd13 Correctly generate absolute URLs
    8bb46293 Fix phpstan

--- faq-bundle/contao/modules/ModuleFaqReader.php
+++ faq-bundle/contao/modules/ModuleFaqReader.php
@@ -74,9 +74,9 @@ class ModuleFaqReader extends Module
 		/** @var PageModel $objPage */
 		global $objPage;
 
-		if ($this->overviewPage)
+		if ($this->overviewPage && ($overviewPage = PageModel::findById($this->overviewPage)))
 		{
-			$this->Template->referer = PageModel::findById($this->overviewPage)->getFrontendUrl();
+			$this->Template->referer = System::getContainer()->get('contao.routing.content_url_generator')->generate($overviewPage);
 			$this->Template->back = $this->customLabel ?: $GLOBALS['TL_LANG']['MSC']['faqOverview'];
 		}
 
