ddc515a661121d06509d6f3ff7ff6f0c18b09673
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-01-18 17:04:03 +0100

    Return empty string if URL cannot be generated

--- core-bundle/src/Twig/Runtime/ContentUrlRuntime.php
+++ core-bundle/src/Twig/Runtime/ContentUrlRuntime.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Twig\Runtime;
 
 use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Symfony\Component\Routing\Exception\ExceptionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
@@ -25,11 +26,12 @@ final class ContentUrlRuntime implements RuntimeExtensionInterface
     {
     }
 
-    /**
-     * Convert a byte value into a human-readable format.
-     */
     public function generate(object $content, array $parameters = [], int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH): string
     {
-        return $this->urlGenerator->generate($content, $parameters, $referenceType);
+        try {
+            return $this->urlGenerator->generate($content, $parameters, $referenceType);
+        } catch (ExceptionInterface) {
+            return '';
+        }
     }
 }
