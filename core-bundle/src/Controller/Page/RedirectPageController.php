763dd1c117a048c24a543e9c4399964556897709
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-01-26 10:58:25 +0100

    Use the content URL generator in the redirect page controller (see #6788)
    Description
    -----------
    
    Follow-up to https://github.com/contao/contao/pull/6392
    
    Commits
    -------
    
    73ce3730 Use ContentUrlGenerator in RedirectPageController

--- core-bundle/src/Controller/Page/RedirectPageController.php
+++ core-bundle/src/Controller/Page/RedirectPageController.php
@@ -13,26 +13,26 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Controller\Page;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsPage;
-use Contao\CoreBundle\InsertTag\InsertTagParser;
-use Contao\CoreBundle\Util\UrlUtil;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
 use Contao\PageModel;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-#[AsPage('redirect', '', contentComposition: false)]
+/**
+ * @internal
+ */
+#[AsPage(path: '', contentComposition: false)]
 class RedirectPageController
 {
-    public function __construct(private readonly InsertTagParser $insertTagParser)
+    public function __construct(private readonly ContentUrlGenerator $urlGenerator)
     {
     }
 
-    public function __invoke(Request $request, PageModel $pageModel): Response
+    public function __invoke(PageModel $pageModel): Response
     {
         $status = 'temporary' === $pageModel->redirect ? Response::HTTP_SEE_OTHER : Response::HTTP_MOVED_PERMANENTLY;
-
-        $url = $this->insertTagParser->replaceInline($pageModel->url);
-        $url = UrlUtil::makeAbsolute($url, $request->getSchemeAndHttpHost().$request->getBasePath().'/');
+        $url = $this->urlGenerator->generate($pageModel, [], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return new RedirectResponse($url, $status);
     }
