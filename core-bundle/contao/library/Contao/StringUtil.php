4c43127536d2d28fa20bcda2612bd4b3a31f69dd
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-01-26 15:36:31 +0100

    Fix entities

--- core-bundle/contao/library/Contao/StringUtil.php
+++ core-bundle/contao/library/Contao/StringUtil.php
@@ -228,7 +228,7 @@ class StringUtil
 	 */
 	public static function convertBasicEntities($strBuffer)
 	{
-		return str_replace(array('&amp;', '&lt;', '&gt;', '&nbsp;', '&shy;', '&#8203;'), array('[&]', '[lt]', '[gt]', '[nbsp]', '[-]', '[ ]'), $strBuffer);
+		return str_replace(array('&amp;', '&lt;', '&gt;', '&nbsp;', '&shy;', '&ZeroWidthSpace;'), array('[&]', '[lt]', '[gt]', '[nbsp]', '[-]', '[zwsp]'), $strBuffer);
 	}
 
 	/**
@@ -240,7 +240,7 @@ class StringUtil
 	 */
 	public static function restoreBasicEntities($strBuffer)
 	{
-		return str_replace(array('[&]', '[&amp;]', '[lt]', '[gt]', '[nbsp]', '[-]', '[ ]'), array('&amp;', '&amp;', '&lt;', '&gt;', '&nbsp;', '&shy;', '&#8203;'), $strBuffer);
+		return str_replace(array('[&]', '[&amp;]', '[lt]', '[gt]', '[nbsp]', '[-]', '[zwsp]'), array('&amp;', '&amp;', '&lt;', '&gt;', '&nbsp;', '&shy;', '&ZeroWidthSpace;'), $strBuffer);
 	}
 
 	/**
