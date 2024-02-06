9362576ae799813d50d4217041889c5ec11db21d
fritzmg <fmg@inspiredminds.at>
2024-02-06 14:38:13 +0000

    fix TokenCheckerTest

--- core-bundle/tests/Security/Authentication/Token/TokenCheckerTest.php
+++ core-bundle/tests/Security/Authentication/Token/TokenCheckerTest.php
@@ -196,7 +196,7 @@ class TokenCheckerTest extends TestCase
         $connection = $this->createMock(Connection::class);
         $connection
             ->method('fetchAssociative')
-            ->willReturn($previewLinkRow)
+            ->willReturn($previewLinkRow ?? false)
         ;
 
         $tokenChecker = new TokenChecker(
