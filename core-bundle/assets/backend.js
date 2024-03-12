bbf8962f57d111e207fffb97de065d1221532d73
M. Vondano <m-vo@users.noreply.github.com>
2024-03-12 15:35:58 +0100

    add basic support for turbo drive and enable it by default

--- core-bundle/assets/backend.js
+++ core-bundle/assets/backend.js
@@ -1,5 +1,6 @@
 import { Application } from '@hotwired/stimulus'
 import { definitionForModuleAndIdentifier, identifierForContextKey } from '@hotwired/stimulus-webpack-helpers'
+import * as Turbo from "@hotwired/turbo"
 
 import './scripts/mootao.js'
 import './scripts/core.js'
@@ -8,6 +9,7 @@ import './scripts/modulewizard.js'
 import './scripts/sectionwizard.js'
 import './scripts/tips.js'
 
+/* Stimulus */
 // Start Stimulus application and register all controllers with `contao--` prefix.
 const application = Application.start()
 application.debug = process.env.NODE_ENV === 'development'
@@ -19,3 +21,16 @@ application.load(context.keys().map((key) => {
         return definitionForModuleAndIdentifier(context(key), `contao--${ identifier }`);
     }
 }).filter((value) => value));
+
+/* Turbo support */
+// Cancel all prefetch requests that contain a request token
+document.documentElement.addEventListener('turbo:before-prefetch', e => {
+    if ((new URLSearchParams(e.target.href)).has('rt')) {
+        e.preventDefault();
+    }
+});
+
+// Make mootools scripts reinitialize themselves
+document.documentElement.addEventListener('turbo:render', () => {
+    window.fireEvent('domready');
+});
