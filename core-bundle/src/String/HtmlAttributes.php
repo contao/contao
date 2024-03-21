c9371e196ca0491687500ae3259dcc877e9fb724
Martin Auswöger <martin@auswoeger.com>
2024-03-21 21:12:12 +0100

    Fix html attributes style parsing for edge cases

--- core-bundle/src/String/HtmlAttributes.php
+++ core-bundle/src/String/HtmlAttributes.php
@@ -420,6 +420,9 @@ class HtmlAttributes implements \Stringable, \JsonSerializable, \IteratorAggrega
         return str_replace(['{{', '}}'], ['&#123;&#123;', '&#125;&#125;'], $value);
     }
 
+    /**
+     * @return array<string, list<string>>
+     */
     private function parseStyles(string $styles): array
     {
         // Regular expression to match declarations according to
@@ -432,6 +435,7 @@ class HtmlAttributes implements \Stringable, \JsonSerializable, \IteratorAggrega
                 |\{(?:(?R)|[^}])*+(?:}|$)         # {}-block
                 |\[(?:(?R)|[^]])*+(?:]|$)         # []-block
                 |\((?:(?R)|[^)])*+(?:\)|$)        # ()-block
+                |\/\*(?:(?!\*\/).)*+(?:\*\/|$)    # Comment block
                 |[^;{}\[\]()"\']                  # Anything else
             )++
         /ixs';
@@ -440,14 +444,28 @@ class HtmlAttributes implements \Stringable, \JsonSerializable, \IteratorAggrega
         // https://www.w3.org/TR/css-syntax-3/#ident-token-diagram
         $propertyRegex = '/
             ^
-            (?!\d)                              # Must not start with a digit
-            (?!-\d)                             # Must not start with a dash followed by a digit
-            -?+                                 # Optional leading dash
             (?:
-                [a-z0-9\x80-\xFF_-]
-                |\\\(?:[0-9a-f]{1,6}\s?|[^\n])  # Escape
-            )++
-            $
+                \/\*                                # Comment start
+                (?:(?!\*\/).)*+                     # Anything but comment end
+                (?:\*\/|$(*SKIP)(*FAIL))            # Comment end
+                |[ \n\r\t\v\f\x00]                  # Or whitespace
+            )*+
+            (                                       # Match property name
+                (?!\d)                              # Must not start with a digit
+                (?!-\d)                             # Must not start with a dash followed by a digit
+                -?+                                 # Optional leading dash
+                (?:
+                    [a-z0-9\x80-\xFF_-]
+                    |\\\(?:[0-9a-f]{1,6}\s?|[^\n])  # Escape
+                )++
+            )
+            (?:
+                \/\*                                # Comment start
+                (?:(?!\*\/).)*+                     # Anything but comment end
+                (?:\*\/|$(*SKIP)(*FAIL))            # Comment end
+                |[ \n\r\t\v\f\x00]                  # Or whitespace
+            )*+
+            :                                       # Colon
         /ixs';
 
         preg_match_all($declarationRegex, $styles, $matches, PREG_SET_ORDER | PREG_UNMATCHED_AS_NULL);
@@ -455,27 +473,22 @@ class HtmlAttributes implements \Stringable, \JsonSerializable, \IteratorAggrega
         $result = [];
 
         foreach ($matches as [0 => $declaration]) {
-            [$property, $value] = explode(':', $declaration, 2) + [null, null];
-            $property = trim($property, " \n\r\t\v\f\x00");
-
-            if (null !== $value && preg_match($propertyRegex, $property)) {
-                $result[$property][] = trim($value);
+            if (preg_match($propertyRegex, $declaration, $match)) {
+                if (!str_starts_with($match[1], '--')) {
+                    $match[1] = strtolower($match[1]);
+                }
+                $result[$match[1]][] = $declaration;
             }
         }
 
         return $result;
     }
 
+    /**
+     * @param array<string, list<string>> $styles
+     */
     private function serializeStyles(array $styles): string
     {
-        $serialized = [];
-
-        foreach ($styles as $prop => $values) {
-            foreach ($values as $value) {
-                $serialized[] = "$prop:$value";
-            }
-        }
-
-        return implode(';', $serialized);
+        return implode(';', array_merge(...array_values($styles)));
     }
 }
