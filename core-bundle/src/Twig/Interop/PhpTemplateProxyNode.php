790cd5bf4698fa6f216712c9471c7220b69c2feb
Martin Auswöger <martin@auswoeger.com>
2024-04-18 17:30:06 +0200

    Make the `PhpTemplateProxyNode` class compatible with Twig 3.9 (see #7131)
    Description
    -----------
    
    Same as https://github.com/contao/contao/pull/7129 for Contao 4.13
    
    Commits
    -------
    
    080203c2 Fix Twig 3.9 compatibility of PhpTemplateProxyNode
    060c8dc4 Apply suggestions from code review

--- core-bundle/src/Twig/Interop/PhpTemplateProxyNode.php
+++ core-bundle/src/Twig/Interop/PhpTemplateProxyNode.php
@@ -12,12 +12,14 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Twig\Interop;
 
+use Twig\Attribute\YieldReady;
 use Twig\Compiler;
 use Twig\Node\Node;
 
 /**
  * @experimental
  */
+#[YieldReady]
 final class PhpTemplateProxyNode extends Node
 {
     public function __construct(string $extensionName)
@@ -27,19 +29,15 @@ final class PhpTemplateProxyNode extends Node
         ]);
     }
 
+    /**
+     * @todo Remove output buffer handling once Twig is yield-only (probably version 4.0)
+     */
     public function compile(Compiler $compiler): void
     {
-        // echo $this->extensions["Contao\\…\\ContaoExtension"]->renderLegacyTemplate(
-        //     $this->getTemplateName(),
-        //     array_map(
-        //         function(callable $block) use ($context): string {
-        //             if ($this->env->isDebug()) { ob_start(); } else { ob_start(static function () { return ''; }); }
-        //             try { $block($context); return ob_get_contents(); } finally { ob_end_clean(); }
-        //         }, $blocks
-        //     ), $context
-        // );
+        /** @see PhpTemplateProxyNodeTest::testCompilesProxyCode() */
         $compiler
-            ->write('echo $this->extensions[')
+            ->write(class_exists(YieldReady::class) ? 'yield' : 'echo') // Backwards compatibility
+            ->write(' $this->extensions[')
             ->repr($this->getAttribute('extension_name'))
             ->raw(']->renderLegacyTemplate('."\n")
             ->indent()
@@ -49,7 +47,18 @@ final class PhpTemplateProxyNode extends Node
             ->write('function(callable $block) use ($context): string {'."\n")
             ->indent()
             ->write('if ($this->env->isDebug()) { ob_start(); } else { ob_start(static function () { return \'\'; }); }'."\n")
-            ->write('try { $block($context); return ob_get_contents(); } finally { ob_end_clean(); }'."\n")
+            ->write('try {'."\n")
+            ->indent()
+            ->write('$content = \'\';'."\n")
+            ->write('foreach ($block($context) ?? [\'\'] as $chunk) {'."\n")
+            ->indent()
+            ->write('$content .= ob_get_contents() . $chunk;'."\n")
+            ->write('ob_clean();'."\n")
+            ->outdent()
+            ->write('}'."\n")
+            ->write('return $content . ob_get_contents();'."\n")
+            ->outdent()
+            ->write('} finally { ob_end_clean(); }'."\n")
             ->outdent()
             ->write('}, $blocks'."\n")
             ->outdent()
