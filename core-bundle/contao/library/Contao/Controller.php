61a2ba2d2fd3db4ee1445b459a3d290775e307e4
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-04-24 12:58:47 +0200

    CS

--- core-bundle/contao/library/Contao/Controller.php
+++ core-bundle/contao/library/Contao/Controller.php
@@ -854,9 +854,9 @@ abstract class Controller extends System
 		// Add the component style sheets
 		if (!empty($GLOBALS['TL_STYLE_SHEETS']) && \is_array($GLOBALS['TL_STYLE_SHEETS']))
 		{
-			foreach (array_unique($GLOBALS['TL_STYLE_SHEETS']) as $head)
+			foreach (array_unique($GLOBALS['TL_STYLE_SHEETS']) as $stylesheet)
 			{
-				$strScripts .= $head;
+				$strScripts .= $stylesheet;
 			}
 		}
 
