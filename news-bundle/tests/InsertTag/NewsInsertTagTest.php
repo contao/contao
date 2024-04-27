99a451dedd844d04b5fb36db8a6f911bff40321f
Martin Auswöger <martin@auswoeger.com>
2024-04-27 22:19:50 +0200

    Do not use deprecated replaceInsertTags hook

--- news-bundle/tests/InsertTag/NewsInsertTagTest.php
+++ news-bundle/tests/InsertTag/NewsInsertTagTest.php
@@ -10,35 +10,20 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\NewsBundle\Tests\EventListener;
+namespace Contao\NewsBundle\Tests\InsertTag;
 
 use Contao\CoreBundle\Exception\ForwardPageNotFoundException;
+use Contao\CoreBundle\InsertTag\InsertTagResult;
+use Contao\CoreBundle\InsertTag\ResolvedInsertTag;
+use Contao\CoreBundle\InsertTag\ResolvedParameters;
 use Contao\CoreBundle\Routing\ContentUrlGenerator;
-use Contao\NewsBundle\EventListener\InsertTagsListener;
+use Contao\NewsBundle\InsertTag\NewsInsertTag;
 use Contao\NewsModel;
 use Contao\TestCase\ContaoTestCase;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class InsertTagsListenerTest extends ContaoTestCase
+class NewsInsertTagTest extends ContaoTestCase
 {
-    public function testTriggersWarningForNewsFeedTag(): void
-    {
-        $urlGenerator = $this->createMock(ContentUrlGenerator::class);
-
-        $logger = $this->createMock(LoggerInterface::class);
-        $logger
-            ->expects($this->once())
-            ->method('warning')
-            ->with('The "news_feed" insert tag has been removed in Contao 5.0. Use "link_url" instead.')
-        ;
-
-        $listener = new InsertTagsListener($this->mockContaoFramework(), $urlGenerator, $logger);
-        $url = $listener('news_feed::2', false, null, []);
-
-        $this->assertFalse($url);
-    }
-
     public function testReplacesTheNewsTags(): void
     {
         $newsModel = $this->mockClassWithProperties(NewsModel::class);
@@ -79,78 +64,67 @@ class InsertTagsListenerTest extends ContaoTestCase
             )
         ;
 
-        $logger = $this->createMock(LoggerInterface::class);
-        $listener = new InsertTagsListener($this->mockContaoFramework($adapters), $urlGenerator, $logger);
+        $listener = new NewsInsertTag($this->mockContaoFramework($adapters), $urlGenerator);
 
         $this->assertSame(
             '<a href="news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;">"Foo" is not "bar"</a>',
-            $listener('news::2', false, null, []),
+            $listener(new ResolvedInsertTag('news', new ResolvedParameters(['2']), []))->getValue(),
         );
 
         $this->assertSame(
             '<a href="news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;" target="_blank" rel="noreferrer noopener">"Foo" is not "bar"</a>',
-            $listener('news::2::blank', false, null, []),
+            $listener(new ResolvedInsertTag('news', new ResolvedParameters(['2', 'blank']), []))->getValue(),
         );
 
         $this->assertSame(
             '<a href="news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;">',
-            $listener('news_open::2', false, null, []),
+            $listener(new ResolvedInsertTag('news_open', new ResolvedParameters(['2']), []))->getValue(),
         );
 
         $this->assertSame(
             '<a href="news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;" target="_blank" rel="noreferrer noopener">',
-            $listener('news_open::2::blank', false, null, []),
+            $listener(new ResolvedInsertTag('news_open', new ResolvedParameters(['2', 'blank']), []))->getValue(),
         );
 
         $this->assertSame(
             '<a href="http://domain.tld/news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;" target="_blank" rel="noreferrer noopener">',
-            $listener('news_open::2::absolute::blank', false, null, []),
+            $listener(new ResolvedInsertTag('news_open', new ResolvedParameters(['2', 'absolute', 'blank']), []))->getValue(),
         );
 
         $this->assertSame(
             '<a href="http://domain.tld/news/foo-is-not-bar.html" title="&quot;Foo&quot; is not &quot;bar&quot;" target="_blank" rel="noreferrer noopener">',
-            $listener('news_open::2::blank::absolute', false, null, []),
+            $listener(new ResolvedInsertTag('news_open', new ResolvedParameters(['2', 'blank', 'absolute']), []))->getValue(),
         );
 
         $this->assertSame(
             'news/foo-is-not-bar.html',
-            $listener('news_url::2', false, null, []),
+            $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['2']), []))->getValue(),
         );
 
         $this->assertSame(
             'http://domain.tld/news/foo-is-not-bar.html',
-            $listener('news_url::2', false, null, ['absolute']),
+            $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['2', 'absolute']), []))->getValue(),
         );
 
         $this->assertSame(
             'http://domain.tld/news/foo-is-not-bar.html',
-            $listener('news_url::2::absolute', false, null, []),
+            $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['2', 'absolute']), []))->getValue(),
         );
 
         $this->assertSame(
             'http://domain.tld/news/foo-is-not-bar.html',
-            $listener('news_url::2::blank::absolute', false, null, []),
+            $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['2', 'blank', 'absolute']), []))->getValue(),
         );
 
-        $this->assertSame(
-            '&quot;Foo&quot; is not &quot;bar&quot;',
-            $listener('news_title::2', false, null, []),
+        $this->assertEquals(
+            new InsertTagResult('"Foo" is not "bar"'),
+            $listener(new ResolvedInsertTag('news_title', new ResolvedParameters(['2']), [])),
         );
 
         $this->assertSame(
             '<p>Foo does not equal bar.</p>',
-            $listener('news_teaser::2', false, null, []),
+            $listener(new ResolvedInsertTag('news_teaser', new ResolvedParameters(['2']), []))->getValue(),
         );
-    }
-
-    public function testReturnsFalseIfTheTagIsUnknown(): void
-    {
-        $urlGenerator = $this->createMock(ContentUrlGenerator::class);
-        $logger = $this->createMock(LoggerInterface::class);
-
-        $listener = new InsertTagsListener($this->mockContaoFramework(), $urlGenerator, $logger);
-
-        $this->assertFalse($listener('link_url::2', false, null, []));
     }
 
     public function testReturnsAnEmptyStringIfThereIsNoModel(): void
@@ -160,10 +134,9 @@ class InsertTagsListenerTest extends ContaoTestCase
         ];
 
         $urlGenerator = $this->createMock(ContentUrlGenerator::class);
-        $logger = $this->createMock(LoggerInterface::class);
-        $listener = new InsertTagsListener($this->mockContaoFramework($adapters), $urlGenerator, $logger);
+        $listener = new NewsInsertTag($this->mockContaoFramework($adapters), $urlGenerator);
 
-        $this->assertSame('', $listener('news_url::3', false, null, []));
+        $this->assertSame('', $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['3']), []))->getValue());
     }
 
     public function testReturnsAnEmptyUrlIfTheUrlGeneratorThrowsException(): void
@@ -180,9 +153,8 @@ class InsertTagsListenerTest extends ContaoTestCase
             ->willThrowException(new ForwardPageNotFoundException())
         ;
 
-        $logger = $this->createMock(LoggerInterface::class);
-        $listener = new InsertTagsListener($this->mockContaoFramework($adapters), $urlGenerator, $logger);
+        $listener = new NewsInsertTag($this->mockContaoFramework($adapters), $urlGenerator);
 
-        $this->assertSame('', $listener('news_url::4', false, null, []));
+        $this->assertSame('', $listener(new ResolvedInsertTag('news_url', new ResolvedParameters(['4']), []))->getValue());
     }
 }
