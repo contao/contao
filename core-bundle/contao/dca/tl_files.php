c1fac9833b59a35675416f1decd406fb8eba8085
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-05-02 09:48:22 +0200

    CS

--- core-bundle/contao/dca/tl_files.php
+++ core-bundle/contao/dca/tl_files.php
@@ -675,7 +675,7 @@ class tl_files extends Backend
 		$security = System::getContainer()->get('security.helper');
 		$subject = new UpdateAction('tl_files', $row);
 
-		return $security->isGranted(ContaoCorePermissions::DC_PREFIX.'tl_files', $subject) && $security->isGranted(ContaoCorePermissions::USER_CAN_RENAME_FILE) ? '<a href="' . $this->addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label) . '</a> ' : Image::getHtml(str_replace('.svg', '--disabled.svg', $icon)) . ' ';
+		return $security->isGranted(ContaoCorePermissions::DC_PREFIX . 'tl_files', $subject) && $security->isGranted(ContaoCorePermissions::USER_CAN_RENAME_FILE) ? '<a href="' . $this->addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . StringUtil::specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label) . '</a> ' : Image::getHtml(str_replace('.svg', '--disabled.svg', $icon)) . ' ';
 	}
 
 	/**
