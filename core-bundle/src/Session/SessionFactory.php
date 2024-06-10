6a826575f0d6093978f031f695e88d0986a0b3a6
fritzmg <fmg@inspiredminds.at>
2024-06-10 13:59:58 +0100

    automatically store contao_backend session attributes for the popup under a different storage key

--- core-bundle/src/Session/SessionFactory.php
+++ core-bundle/src/Session/SessionFactory.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Session;
 
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionBagInterface;
 use Symfony\Component\HttpFoundation\Session\SessionFactoryInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -22,14 +24,23 @@ class SessionFactory implements SessionFactoryInterface
         readonly private SessionFactoryInterface $inner,
         readonly private SessionBagInterface $backendBag,
         readonly private SessionBagInterface $frontendBag,
+        readonly private SessionBagInterface $backendPopupBag,
+        readonly private ScopeMatcher $scopeMatcher,
+        readonly private RequestStack $requestStack,
     ) {
     }
 
     public function createSession(): SessionInterface
     {
         $session = $this->inner->createSession();
+        $request = $this->requestStack->getMainRequest();
 
-        $session->registerBag($this->backendBag);
+        if ($this->scopeMatcher->isBackendRequest($request) && $request->query->has('popup')) {
+            $session->registerBag($this->backendPopupBag);
+        } else {
+            $session->registerBag($this->backendBag);
+        }
+
         $session->registerBag($this->frontendBag);
 
         return $session;
