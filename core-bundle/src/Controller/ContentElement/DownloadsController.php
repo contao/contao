b765fb68e941f3307e54a7ad6d74eee343da0538
Marcel Pitz <marcel@marcel-pitz.de>
2024-07-10 16:43:17 +0200

    Don't limit the download items

--- core-bundle/src/Controller/ContentElement/DownloadsController.php
+++ core-bundle/src/Controller/ContentElement/DownloadsController.php
@@ -48,14 +48,6 @@ class DownloadsController extends AbstractDownloadContentElementController
         $template->set('sort_mode', $sortMode);
         $template->set('randomize_order', $randomize = 'random' === $model->sortBy);
 
-        // Limit elements; use client-side logic for only displaying the first $limit
-        // elements in case we are dealing with a random order
-        if ($model->numberOfItems > 0 && !$randomize) {
-            $filesystemItems = $filesystemItems->limit($model->numberOfItems);
-        }
-
-        $template->set('limit', $model->numberOfItems > 0 && $randomize ? $model->numberOfItems : null);
-
         $downloads = $this->compileDownloadsList($filesystemItems, $model, $request);
 
         // Explicitly define title/text metadata for a single file
