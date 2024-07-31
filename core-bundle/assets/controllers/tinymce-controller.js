99b99b1be2c8d7bc189aecbc019b2d186ba67f91
M. Vondano <m-vo@users.noreply.github.com>
2024-07-31 20:39:19 +0200

    use automatic JSON parsing and proper disconnect

--- core-bundle/assets/controllers/tinymce-controller.js
+++ core-bundle/assets/controllers/tinymce-controller.js
@@ -2,14 +2,14 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     static values = {
-        config: String
+        config: Object
     }
 
     connect() {
-        tinymce?.init(JSON.parse(this.configValue));
+        tinymce?.init(this.configValue);
     }
 
     disconnect() {
-        tinymce?.remove(this.element);
+        tinymce?.remove(this.configValue.selector);
     }
 }
