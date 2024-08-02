07ca6e9561b9bf8a300b49ce32f737f6f99a77a4
M. Vondano <m-vo@users.noreply.github.com>
2024-08-02 11:36:30 +0200

    use an expando property instead of serializing the tinyMCE config

--- core-bundle/assets/controllers/tinymce-controller.js
+++ core-bundle/assets/controllers/tinymce-controller.js
@@ -1,15 +1,20 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static values = {
-        config: Object
-    }
-
     connect() {
-        tinymce?.init(this.configValue);
+        if(!this.element.tinymceConfig) {
+            console.error('No tinymce config was attached to the DOM element, expected an expando property called "tinymceConfig".', this.element);
+
+            return;
+        }
+
+        this.config = this.element.tinymceConfig;
+        delete this.element.tinymceConfig;
+
+        tinymce?.init(this.config);
     }
 
     disconnect() {
-        tinymce?.remove(this.configValue.selector);
+        tinymce?.remove(this.config.selector);
     }
 }
