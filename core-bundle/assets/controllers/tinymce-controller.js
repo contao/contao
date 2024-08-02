0c1933da6524ef44317b59a636888095b83ea88b
M. Vondano <m-vo@users.noreply.github.com>
2024-08-02 17:06:58 +0200

    don't cache pages having the tinyMCE editor on them

--- core-bundle/assets/controllers/tinymce-controller.js
+++ core-bundle/assets/controllers/tinymce-controller.js
@@ -1,6 +1,10 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    initialize() {
+        Turbo.cache.exemptPageFromCache();
+    }
+
     connect() {
         if(!this.element.tinymceConfig) {
             console.error('No tinymce config was attached to the DOM element, expected an expando property called "tinymceConfig".', this.element);
