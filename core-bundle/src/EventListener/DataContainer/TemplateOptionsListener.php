21f60263ba897656656e5365ba9ba59c90f7929e
Yanick Witschi <yanick.witschi@terminal42.ch>
2024-08-02 18:02:36 +0200

    Added DataContainer::getActiveRecord() and DataContainer::getSubmittedRecord()

--- core-bundle/src/EventListener/DataContainer/TemplateOptionsListener.php
+++ core-bundle/src/EventListener/DataContainer/TemplateOptionsListener.php
@@ -50,7 +50,7 @@ class TemplateOptionsListener
 
         $type = $overrideAll
             ? $this->getCommonOverrideAllType($dc)
-            : $dc->getCurrentRecord()['type'] ?? null;
+            : $dc->getActiveRecord()['type'] ?? null;
 
         if (null === $type) {
             // Add a blank option that allows to reset all custom templates to the default
