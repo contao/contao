523d6791c2487902e4c1c092f5f25a2bca2729a8
M. Vondano <m-vo@users.noreply.github.com>
2024-08-04 12:41:28 +0200

    allow ace config to be made in the template again

--- core-bundle/assets/controllers/ace-controller.js
+++ core-bundle/assets/controllers/ace-controller.js
@@ -17,43 +17,8 @@ export default class extends Controller {
         this.element.style['display'] = 'none';
 
         // Instantiate the editor
-        this.editor = ace.edit(this.container, {
-            maxLines: Infinity,
-            theme: document.documentElement.dataset.colorScheme === 'dark' ? 'ace/theme/twilight' : 'ace/theme/clouds',
-            autoScrollEditorIntoView: true,
-            readOnly: this.readOnlyValue,
-        });
-
-        this.editor.$blockScrolling = Infinity;
-        this.editor.renderer.setScrollMargin(3, 3, 0, 0);
-        this.editor.renderer.scrollBy(0, -6);
-        this.editor.container.style.lineHeight = 1.45;
+        this.editor = ace.edit(this.container, {maxLines: Infinity});
         this.editor.getSession().setValue(this.element.value);
-        this.editor.getSession().setMode(`ace/mode/${this.typeValue}`);
-        this.editor.getSession().setUseSoftTabs(false);
-        this.editor.getSession().setUseWrapMode(true);
-
-        // Auto-detect the indentation
-        const whitespace = ace.require('ace/ext/whitespace');
-        whitespace.detectIndentation(this.editor.getSession());
-
-        // Add the fullscreen command
-        this.editor.commands.addCommand({
-            name: 'Fullscreen',
-            bindKey: 'F11',
-            exec: function (editor) {
-                const dom = ace.require('ace/lib/dom');
-                dom.toggleCssClass(document.body, 'ace-fullsize');
-                editor.resize();
-            }
-        });
-
-        // Enable code autocompletion
-        this.editor.setOptions({
-            enableSnippets: true,
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true
-        });
 
         this.editor.on('focus', () => {
             window.dispatchEvent(new Event('store-scroll-offset'));
@@ -65,6 +30,9 @@ export default class extends Controller {
 
         // Disable command conflicts with AltGr (see #5792)
         this.editor.commands.bindKey('Ctrl-alt-a|Ctrl-alt-e|Ctrl-alt-h|Ctrl-alt-l|Ctrl-alt-s', null);
+
+        // Execute the config callback
+        this.element?.configCallback(this.editor);
     }
 
     disconnect() {
