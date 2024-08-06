91c5cf92a58d59c37e754faa89e900ac2faaa858
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-08-06 11:36:26 +0200

    CS

--- core-bundle/assets/controllers/tinymce-controller.js
+++ core-bundle/assets/controllers/tinymce-controller.js
@@ -6,19 +6,18 @@ export default class extends Controller {
     }
 
     connect() {
-        if(!this.element.tinymceConfig) {
-            console.error('No tinymce config was attached to the DOM element, expected an expando property called "tinymceConfig".', this.element);
-
+        if (!this.element.tinymceConfig) {
+            console.error('No TinyMCE config was attached to the DOM element, expected an expando property called "tinymceConfig".', this.element);
             return;
         }
 
         const config = this.element.tinymceConfig;
         config.target = this.element;
 
-        // TinyMCE creates the editor id based on the target element's id (if set).
-        // This leads to a problem if an editor with the same id is still around,
+        // TinyMCE creates the editor ID based on the target element's ID (if set).
+        // This leads to a problem if an editor with the same ID is still around,
         // which would be the case if the disconnect event cleaning up the resource
-        // did not run, yet. We therefore remove the id and re-add it after
+        // has not run yet. We therefore remove the ID and re-add it after
         // initializing the editor.
         const elementId = this.element.id;
         this.element.removeAttribute('id');
@@ -41,7 +40,7 @@ export default class extends Controller {
             return;
         }
 
-        // Trigger a beforeunload event like when navigating away to capture the tinyMCE autosave message
+        // Trigger a beforeunload event like when navigating away to capture the TinyMCE autosave message
         const delegate = document.createEvent('BeforeUnloadEvent');
         delegate.initEvent('beforeunload', false, true);
 
