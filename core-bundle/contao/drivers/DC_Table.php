807ca16d5144b2917faaf067d11d787b6934607c
Lukas Bableck <lukas@bableck.dev>
2024-08-06 08:53:33 +0200

    codestyle

--- core-bundle/contao/drivers/DC_Table.php
+++ core-bundle/contao/drivers/DC_Table.php
@@ -3861,17 +3861,19 @@ class DC_Table extends DataContainer implements ListableDataContainerInterface, 
 		{
 			$blnClipboard = true;
 			$arrClipboard = $arrClipboard[$this->strTable];
-			if(is_array($arrClipboard['id']))
+			if (\is_array($arrClipboard['id']))
 			{
 				foreach ($arrClipboard['id'] as $k=>$v)
 				{
-					if($this->getCurrentRecord($v) === null)
+					if ($this->getCurrentRecord($v) === null)
 					{
 						unset($arrClipboard['id'][$k]);
 					}
 				}
-			}else{
-				if($this->getCurrentRecord($arrClipboard['id']) === null)
+			}
+			else
+			{
+				if ($this->getCurrentRecord($arrClipboard['id']) === null)
 				{
 					$blnClipboard = false;
 					$arrClipboard = null;
