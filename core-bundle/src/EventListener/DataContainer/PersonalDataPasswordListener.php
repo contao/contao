b3d044672bcbddfc9c3837e356cffc07d3ee4b59
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-08-14 16:43:52 +0200

    Show a warning if a personal data module allows to change the password

---
+++ core-bundle/src/EventListener/DataContainer/PersonalDataPasswordListener.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\Message;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+#[AsCallback(table: 'tl_module', target: 'config.onload')]
+class PersonalDataPasswordListener
+{
+    public function __construct(
+        private readonly ContaoFramework $framework,
+        private readonly Connection $connection,
+        private readonly TranslatorInterface $translator,
+        private readonly RequestStack $requestStack,
+    ) {
+    }
+
+    public function __invoke(): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if ($request?->query->has('act') && 'select' !== $request?->query->get('act')) {
+            return;
+        }
+
+        $count = $this->connection
+            ->executeQuery("
+                SELECT COUNT(*)
+                FROM tl_module
+                WHERE
+                    type = 'personalData'
+                    AND editable LIKE '%\"password\"%'
+            ")
+            ->fetchOne()
+        ;
+
+        if ($count < 1) {
+            return;
+        }
+
+        $messages = $this->framework->getAdapter(Message::class);
+        $messages->addError($this->translator->trans('ERR.personalDataPassword', [], 'contao_default'));
+    }
+}
