7c0bbbd7f202cdf79bb4013e5af7f7a37266414e
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-08-16 12:23:00 +0200

    Only show the warning if the user has access to the module

--- core-bundle/src/EventListener/DataContainer/PersonalDataPasswordListener.php
+++ core-bundle/src/EventListener/DataContainer/PersonalDataPasswordListener.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\EventListener\DataContainer;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Doctrine\DBAL\Connection;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
@@ -25,28 +27,40 @@ class PersonalDataPasswordListener
     public function __construct(
         private readonly Connection $connection,
         private readonly TranslatorInterface $translator,
+        private readonly Security $security,
     ) {
     }
 
     public function __invoke(): string|null
     {
-        $count = $this->connection
-            ->executeQuery("
-                SELECT COUNT(*)
-                FROM tl_module
-                WHERE
-                    type = 'personalData'
-                    AND editable LIKE '%\"password\"%'
-            ")
-            ->fetchOne()
-        ;
+        if (!$this->canAccessSettings()) {
+            return null;
+        }
 
-        if ($count < 1) {
+        $query = <<<'SQL'
+            SELECT COUNT(*)
+            FROM tl_module
+            WHERE
+                type = 'personalData'
+                AND editable LIKE '%"password"%'
+            SQL;
+
+        if ($this->connection->executeQuery($query)->fetchOne() < 1) {
             return null;
         }
 
         $message = $this->translator->trans('ERR.personalDataPassword', [], 'contao_default');
 
-        return '<p class="tl_error">' . $message . '</p>';
+        return '<p class="tl_error">'.$message.'</p>';
+    }
+
+    private function canAccessSettings(): bool
+    {
+        if (!isset($GLOBALS['BE_MOD']['design']['themes'])) {
+            return false;
+        }
+
+        return $this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_MODULE, 'themes')
+            && $this->security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_THEME, 'modules');
     }
 }
