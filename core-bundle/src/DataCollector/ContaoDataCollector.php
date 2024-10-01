5e8fbaf149954fa889fa07ee9fadc720f42b67fd
Martin Auswöger <martin@auswoeger.com>
2024-10-01 15:11:03 +0200

    Improve profiler toolbar and panel

--- core-bundle/src/DataCollector/ContaoDataCollector.php
+++ core-bundle/src/DataCollector/ContaoDataCollector.php
@@ -12,17 +12,25 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\DataCollector;
 
+use Contao\ArticleModel;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\CoreBundle\Framework\FrameworkAwareInterface;
 use Contao\CoreBundle\Framework\FrameworkAwareTrait;
+use Contao\CoreBundle\Routing\PageFinder;
 use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\LayoutModel;
-use Contao\Model\Registry;
-use Contao\PageModel;
 use Contao\StringUtil;
+use Imagine\Driver\InfoProvider;
+use Imagine\Gd\Imagine as GdImagine;
+use Imagine\Gmagick\Imagine as GmagickImagine;
+use Imagine\Image\Box;
+use Imagine\Image\ImagineInterface;
+use Imagine\Imagick\Imagine as ImagickImagine;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @internal
@@ -31,8 +39,13 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
 {
     use FrameworkAwareTrait;
 
-    public function __construct(private readonly TokenChecker $tokenChecker)
-    {
+    public function __construct(
+        private readonly TokenChecker $tokenChecker,
+        private readonly RequestStack $requestStack,
+        private readonly ImagineInterface $imagine,
+        private readonly RouterInterface $router,
+        private readonly PageFinder $pageFinder,
+    ) {
     }
 
     public function collect(Request $request, Response $response, \Throwable|null $exception = null): void
@@ -41,8 +54,8 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
 
         $this->addSummaryData();
 
-        if (isset($GLOBALS['TL_DEBUG'])) {
-            $this->data = [...$this->data, ...$GLOBALS['TL_DEBUG']];
+        if ($this->requestStack->getMainRequest() === $request) {
+            $this->addImageChecks();
         }
     }
 
@@ -60,39 +73,11 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
     }
 
     /**
-     * @return array<string|bool>
+     * @return array<string, string|bool>
      */
-    public function getClassesSet(): array
+    public function getImageChecks(): array
     {
-        $data = $this->getData('classes_set');
-
-        sort($data);
-
-        return $data;
-    }
-
-    /**
-     * @return array<string|bool>
-     */
-    public function getClassesAliased(): array
-    {
-        $data = $this->getData('classes_aliased');
-
-        ksort($data);
-
-        return $data;
-    }
-
-    /**
-     * @return array<string|bool>
-     */
-    public function getClassesComposerized(): array
-    {
-        $data = $this->getData('classes_composerized');
-
-        ksort($data);
-
-        return $data;
+        return $this->getData('image_checks');
     }
 
     public function getAdditionalData(): array
@@ -101,11 +86,8 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
 
         unset(
             $data['summary'],
+            $data['image_checks'],
             $data['contao_version'],
-            $data['classes_set'],
-            $data['classes_aliased'],
-            $data['classes_composerized'],
-            $data['database_queries'],
         );
 
         return $data;
@@ -135,23 +117,114 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
 
     private function addSummaryData(): void
     {
-        $framework = false;
-        $modelCount = 0;
-
-        if (isset($GLOBALS['TL_DEBUG'])) {
-            $framework = true;
-            $modelCount = Registry::getInstance()->count();
-        }
-
         $this->data['summary'] = [
             'version' => $this->getContaoVersion(),
-            'framework' => $framework,
-            'models' => $modelCount,
-            'frontend' => isset($GLOBALS['objPage']),
+            'framework' => $this->framework->isInitialized(),
+            'frontend' => (bool) $this->pageFinder->getCurrentPage(),
             'preview' => $this->tokenChecker->isPreviewMode(),
+            'page' => $this->getPageName(),
+            'page_url' => $this->getPageUrl(),
             'layout' => $this->getLayoutName(),
+            'layout_url' => $this->getLayoutUrl(),
+            'articles' => $this->getArticles(),
             'template' => $this->getTemplateName(),
         ];
+    }
+
+    private function addImageChecks(): void
+    {
+        $this->data['image_checks'] = [
+            'imagine_service' => $this->imagine::class,
+            'formats' => [],
+        ];
+
+        foreach (['jpg', 'png', 'gif', 'webp', 'avif', 'heic', 'jxl'] as $imageFormat) {
+            $this->data['image_checks']['formats'][$imageFormat] = $this->checkImageFormat($imageFormat);
+        }
+
+        foreach (['pdf'] as $previewFormat) {
+            $this->data['image_checks']['formats'][$previewFormat] = $this->checkPreviewFormat($previewFormat);
+        }
+    }
+
+    private function checkImageFormat(string $format): array
+    {
+        $info = [
+            'label' => $format,
+            'supported' => true,
+            'error' => '',
+        ];
+
+        try {
+            $this->imagine->create(new Box(1, 1))->get($format);
+        } catch (\Throwable $exception) {
+            while ($exception->getPrevious()) {
+                $exception = $exception->getPrevious();
+            }
+
+            $info['supported'] = false;
+            $info['error'] = $exception::class.': '.$exception->getMessage();
+        }
+
+        return $info;
+    }
+
+    private function checkPreviewFormat(string $format): array
+    {
+        $info = [
+            'label' => $format,
+            'supported' => false,
+            'error' => '',
+        ];
+
+        if ($this->imagine instanceof ImagickImagine) {
+            $info['supported'] = \in_array(strtoupper($format), \Imagick::queryFormats(strtoupper($format)), true);
+
+            if ('pdf' === $format) {
+                try {
+                    (new \Imagick('data:application/pdf;base64,JVBERi0xLgoxIDAgb2JqPDwvUGFnZXMgMiAwIFI+PmVuZG9iagoyIDAgb2JqPDwvS2lkc1szIDAgUl0vQ291bnQgMT4+ZW5kb2JqCjMgMCBvYmo8PC9QYXJlbnQgMiAwIFI+PmVuZG9iagp0cmFpbGVyIDw8L1Jvb3QgMSAwIFI+Pg=='))->getImageWidth();
+                } catch (\Throwable $exception) {
+                    $info['supported'] = false;
+                    $info['error'] = $exception->getMessage();
+                }
+            }
+        }
+
+        if ($this->imagine instanceof GmagickImagine) {
+            $info['supported'] = \in_array(strtoupper($format), (new \Gmagick())->queryformats(strtoupper($format)), true);
+        }
+
+        if ($this->imagine instanceof GdImagine) {
+            $info['supported'] = \function_exists('image'.$format);
+        }
+
+        if ($this->imagine instanceof InfoProvider) {
+            $info['supported'] = $this->imagine->getDriverInfo()->isFormatSupported($format);
+        }
+
+        if (!$info['supported'] && !$info['error']) {
+            $info['error'] = 'Not supported';
+        }
+
+        return $info;
+    }
+
+    private function getPageName(): string
+    {
+        if (!$page = $this->pageFinder->getCurrentPage()) {
+            return '';
+        }
+
+        return \sprintf('%s (ID %s)', StringUtil::decodeEntities($page->title), $page->id);
+    }
+
+    private function getPageUrl(): string
+    {
+        if (!$pageId = $this->pageFinder->getCurrentPage()?->id) {
+            return '';
+        }
+
+        return $this->router->generate('contao_backend', ['do' => 'page', 'act' => 'edit', 'id' => $pageId]);
     }
 
     private function getLayoutName(): string
@@ -161,6 +234,34 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
         }
 
         return \sprintf('%s (ID %s)', StringUtil::decodeEntities($layout->name), $layout->id);
+    }
+
+    private function getLayoutUrl(): string
+    {
+        if (!$layout = $this->getLayout()) {
+            return '';
+        }
+
+        return $this->router->generate('contao_backend', ['do' => 'themes', 'table' => 'tl_layout', 'act' => 'edit', 'id' => $layout->id]);
+    }
+
+    private function getArticles(): array
+    {
+        if (!$page = $this->pageFinder->getCurrentPage()) {
+            return [];
+        }
+
+        $models = $this->framework->getAdapter(ArticleModel::class)->findByPid($page->id, ['order' => 'tl_article.sorting']);
+        $articles = [];
+
+        foreach ($models as $article) {
+            $articles[] = [
+                'url' => $this->router->generate('contao_backend', ['do' => 'article', 'act' => 'edit', 'id' => $article->id]),
+                'label' => \sprintf('%s (ID %s)', StringUtil::decodeEntities($article->title), $article->id),
+            ];
+        }
+
+        return $articles;
     }
 
     private function getTemplateName(): string
@@ -174,17 +275,10 @@ class ContaoDataCollector extends DataCollector implements FrameworkAwareInterfa
 
     private function getLayout(): LayoutModel|null
     {
-        if (!isset($GLOBALS['objPage'])) {
+        if (!$layoutId = $this->pageFinder->getCurrentPage()?->layoutId) {
             return null;
         }
 
-        /** @var PageModel $objPage */
-        $objPage = $GLOBALS['objPage'];
-
-        if (!$objPage->layoutId) {
-            return null;
-        }
-
-        return $this->framework->getAdapter(LayoutModel::class)->findById($objPage->layoutId);
+        return $this->framework->getAdapter(LayoutModel::class)->findById($layoutId);
     }
 }
