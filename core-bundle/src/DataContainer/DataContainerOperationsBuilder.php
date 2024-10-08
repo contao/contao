b9afb4cb93f54cd025d43f3efb5754298593896e
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-10-08 11:30:21 +0200

    CS

--- core-bundle/src/DataContainer/DataContainerOperationsBuilder.php
+++ core-bundle/src/DataContainer/DataContainerOperationsBuilder.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\DataContainer;
 
 use Contao\Backend;
-use Contao\Controller;
 use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\DataContainer;
 use Contao\Image;
