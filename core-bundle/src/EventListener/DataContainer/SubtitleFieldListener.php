4d3fc9dc4f6b367fd50962652408375fe4b94d5b
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-10-09 19:17:06 +0200

    Only show subtitle fields for `.vtt` files

---
+++ core-bundle/src/EventListener/DataContainer/SubtitleFieldListener.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\DataContainer;
+
+#[AsCallback(table: 'tl_files', target: 'config.onpalette')]
+class SubtitleFieldListener
+{
+    public function __invoke(string $palette, DataContainer $dc): string
+    {
+        // $dc->id is the file name in this case
+        if (str_ends_with($dc->id, '.vtt')) {
+            $palette = PaletteManipulator::create()
+                ->addField(['subtitlesLanguage', 'subtitlesType'], 'name')
+                ->applyToString($palette)
+            ;
+        }
+
+        return $palette;
+    }
+}
