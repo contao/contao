13b467d32ef6ef8e68afff8c138f24c6fb06c888
David Greminger <david.greminger@1up.io>
2024-10-13 18:27:00 +0200

    Simplify logic

--- core-bundle/src/Csp/CspParser.php
+++ core-bundle/src/Csp/CspParser.php
@@ -32,7 +32,7 @@ class CspParser
 
         $parser = new ContentSecurityPolicyParser();
         $names = $directiveSet->getNames();
-        $directives = array_filter(array_map(static fn (string $item) => trim(preg_replace('/\s+/', ' ', $item)), explode(';', $header)));
+        $directives = explode(';', preg_replace('/\s+/', ' ', $header));
 
         foreach ($directives as $directive) {
             [$name, $value] = explode(' ', trim($directive), 2) + [null, null];
