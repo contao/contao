5cdea4c175867fcf230110068f3cf93badafbb23
Martin Auswöger <martin@auswoeger.com>
2024-10-14 21:41:28 +0200

    Fix prefer lowest tests

--- core-bundle/tests/Doctrine/Backup/DumperTest.php
+++ core-bundle/tests/Doctrine/Backup/DumperTest.php
@@ -19,6 +19,7 @@ use Contao\CoreBundle\Doctrine\Backup\Dumper;
 use Contao\TestCase\ContaoTestCase;
 use Doctrine\DBAL\Cache\ArrayResult;
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Platforms\MySQL57Platform;
 use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Schema\AbstractSchemaManager;
@@ -66,8 +67,19 @@ class DumperTest extends ContaoTestCase
 
     public static function successfulDumpProvider(): iterable
     {
+        $tableOptions = ['charset' => 'utf8', 'collation' => 'utf8_unicode_ci', 'engine' => 'InnoDB'];
+
         yield 'Empty table without data' => [
-            [new Table('tl_page', [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])], [], [], [], ['charset' => 'utf8', 'collation' => 'utf8_unicode_ci', 'engine' => 'InnoDB'])],
+            [
+                new Table(
+                    'tl_page',
+                    [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
+            ],
             [],
             [
                 'SELECT `foobar` AS `foobar` FROM `tl_page`' => [],
@@ -84,11 +96,18 @@ class DumperTest extends ContaoTestCase
 
         yield 'Table with data' => [
             [
-                new Table('tl_page', [
-                    new Column('stringCol', Type::getType(Types::STRING), ['length' => 255]),
-                    new Column('integerCol', Type::getType(Types::INTEGER)),
-                    new Column('booleanCol', Type::getType(Types::BOOLEAN)),
-                ]),
+                new Table(
+                    'tl_page',
+                    [
+                        new Column('stringCol', Type::getType(Types::STRING), ['length' => 255]),
+                        new Column('integerCol', Type::getType(Types::INTEGER)),
+                        new Column('booleanCol', Type::getType(Types::BOOLEAN)),
+                    ],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
             ],
             [],
             [
@@ -109,7 +128,7 @@ class DumperTest extends ContaoTestCase
                 'SET FOREIGN_KEY_CHECKS = 0;',
                 '-- BEGIN STRUCTURE tl_page',
                 'DROP TABLE IF EXISTS `tl_page`;',
-                'CREATE TABLE `tl_page` (`stringCol` VARCHAR(255) NOT NULL, `integerCol` INT NOT NULL, `booleanCol` TINYINT(1) NOT NULL);',
+                'CREATE TABLE `tl_page` (`stringCol` VARCHAR(255) NOT NULL, `integerCol` INT NOT NULL, `booleanCol` TINYINT(1) NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;',
                 '-- BEGIN DATA tl_page',
                 "INSERT INTO `tl_page` (`stringCol`, `integerCol`, `booleanCol`) VALUES ('value1', 42, 1);",
                 "INSERT INTO `tl_page` (`stringCol`, `integerCol`, `booleanCol`) VALUES ('', NULL, 0);",
@@ -119,14 +138,21 @@ class DumperTest extends ContaoTestCase
 
         yield 'Table with float and integer data' => [
             [
-                new Table('tl_page', [
-                    new Column('stringCol', Type::getType(Types::STRING), ['length' => 255]),
-                    new Column('integerCol', Type::getType(Types::INTEGER)),
-                    new Column('floatCol', Type::getType(Types::FLOAT)),
-                    new Column('bigintCol', Type::getType(Types::BIGINT)),
-                    new Column('decimalCol', Type::getType(Types::DECIMAL), ['precision' => 10]),
-                    new Column('booleanCol', Type::getType(Types::BOOLEAN)),
-                ]),
+                new Table(
+                    'tl_page',
+                    [
+                        new Column('stringCol', Type::getType(Types::STRING), ['length' => 255]),
+                        new Column('integerCol', Type::getType(Types::INTEGER)),
+                        new Column('floatCol', Type::getType(Types::FLOAT)),
+                        new Column('bigintCol', Type::getType(Types::BIGINT)),
+                        new Column('decimalCol', Type::getType(Types::DECIMAL), ['precision' => 10]),
+                        new Column('booleanCol', Type::getType(Types::BOOLEAN)),
+                    ],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
             ],
             [],
             [
@@ -153,7 +179,7 @@ class DumperTest extends ContaoTestCase
                 'SET FOREIGN_KEY_CHECKS = 0;',
                 '-- BEGIN STRUCTURE tl_page',
                 'DROP TABLE IF EXISTS `tl_page`;',
-                'CREATE TABLE `tl_page` (`stringCol` VARCHAR(255) NOT NULL, `integerCol` INT NOT NULL, `floatCol` DOUBLE PRECISION NOT NULL, `bigintCol` BIGINT NOT NULL, `decimalCol` NUMERIC(10, 0) NOT NULL, `booleanCol` TINYINT(1) NOT NULL);',
+                'CREATE TABLE `tl_page` (`stringCol` VARCHAR(255) NOT NULL, `integerCol` INT NOT NULL, `floatCol` DOUBLE PRECISION NOT NULL, `bigintCol` BIGINT NOT NULL, `decimalCol` NUMERIC(10, 0) NOT NULL, `booleanCol` TINYINT(1) NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;',
                 '-- BEGIN DATA tl_page',
                 "INSERT INTO `tl_page` (`stringCol`, `integerCol`, `floatCol`, `bigintCol`, `decimalCol`, `booleanCol`) VALUES ('value1', 42, '4.2', '92233720368547758079223372036854775807', '4.2', 1);",
                 "INSERT INTO `tl_page` (`stringCol`, `integerCol`, `floatCol`, `bigintCol`, `decimalCol`, `booleanCol`) VALUES ('value1', 42, '4.2', '92233720368547758079223372036854775807', '4.2', 1);",
@@ -163,8 +189,22 @@ class DumperTest extends ContaoTestCase
 
         yield 'Multiple tables with data' => [
             [
-                new Table('tl_news', [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])]),
-                new Table('tl_page', [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])]),
+                new Table(
+                    'tl_news',
+                    [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
+                new Table(
+                    'tl_page',
+                    [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
             ],
             [],
             [
@@ -186,12 +226,12 @@ class DumperTest extends ContaoTestCase
                 'SET FOREIGN_KEY_CHECKS = 0;',
                 '-- BEGIN STRUCTURE tl_news',
                 'DROP TABLE IF EXISTS `tl_news`;',
-                'CREATE TABLE `tl_news` (`foobar` VARCHAR(255) NOT NULL);',
+                'CREATE TABLE `tl_news` (`foobar` VARCHAR(255) NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;',
                 '-- BEGIN DATA tl_news',
                 "INSERT INTO `tl_news` (`foobar`) VALUES ('value1');",
                 '-- BEGIN STRUCTURE tl_page',
                 'DROP TABLE IF EXISTS `tl_page`;',
-                'CREATE TABLE `tl_page` (`foobar` VARCHAR(255) NOT NULL);',
+                'CREATE TABLE `tl_page` (`foobar` VARCHAR(255) NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;',
                 '-- BEGIN DATA tl_page',
                 "INSERT INTO `tl_page` (`foobar`) VALUES ('value1');",
                 'INSERT INTO `tl_page` (`foobar`) VALUES (NULL);',
@@ -200,7 +240,16 @@ class DumperTest extends ContaoTestCase
         ];
 
         yield 'Table structure and views' => [
-            [new Table('tl_page', [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])])],
+            [
+                new Table(
+                    'tl_page',
+                    [new Column('foobar', Type::getType(Types::STRING), ['length' => 255])],
+                    [],
+                    [],
+                    [],
+                    $tableOptions,
+                ),
+            ],
             [new View('view_name', 'SELECT `tl_page`.`id` AS `id` FROM `tl_page`')],
             [
                 'SELECT `foobar` AS `foobar` FROM `tl_page`' => [],
@@ -209,7 +258,7 @@ class DumperTest extends ContaoTestCase
                 'SET FOREIGN_KEY_CHECKS = 0;',
                 '-- BEGIN STRUCTURE tl_page',
                 'DROP TABLE IF EXISTS `tl_page`;',
-                'CREATE TABLE `tl_page` (`foobar` VARCHAR(255) NOT NULL);',
+                'CREATE TABLE `tl_page` (`foobar` VARCHAR(255) NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;',
                 '-- BEGIN DATA tl_page',
                 '-- BEGIN VIEW view_name',
                 'CREATE OR REPLACE VIEW `view_name` AS SELECT `tl_page`.`id` AS `id` FROM `tl_page`;',
@@ -217,15 +266,31 @@ class DumperTest extends ContaoTestCase
             ],
         ];
 
+        $jsonDecl = 'JSON NOT NULL';
+        $arrayDecl = 'LONGTEXT NOT NULL';
+
+        // Backwards compatibility for doctrine/dbal 3.x
+        if (class_exists(MySQL57Platform::class)) {
+            $jsonDecl = "LONGTEXT NOT NULL COMMENT '(DC2Type:json)'";
+            $arrayDecl = "LONGTEXT NOT NULL COMMENT '(DC2Type:simple_array)'";
+        }
+
         yield 'Table with binary data' => [
-            [new Table('tl_page', [
-                new Column('x_string', Type::getType(Types::STRING), ['length' => 255, 'platformOptions' => ['charset' => 'utf8mb4']]),
-                new Column('x_json', Type::getType(Types::JSON)),
-                new Column('x_ascii', Type::getType(Types::ASCII_STRING), ['length' => 255]),
-                new Column('x_binary', Type::getType(Types::BINARY), ['length' => 255]),
-                new Column('x_blob', Type::getType(Types::BLOB)),
-                new Column('x_simple_array', Type::getType(Types::SIMPLE_ARRAY)),
-            ])],
+            [new Table(
+                'tl_page',
+                [
+                    new Column('x_string', Type::getType(Types::STRING), ['length' => 255, 'platformOptions' => ['charset' => 'utf8mb4']]),
+                    new Column('x_json', Type::getType(Types::JSON)),
+                    new Column('x_ascii', Type::getType(Types::ASCII_STRING), ['length' => 255]),
+                    new Column('x_binary', Type::getType(Types::BINARY), ['length' => 255]),
+                    new Column('x_blob', Type::getType(Types::BLOB)),
+                    new Column('x_simple_array', Type::getType(Types::SIMPLE_ARRAY)),
+                ],
+                [],
+                [],
+                [],
+                $tableOptions,
+            )],
             [],
             [
                 'SELECT `x_string` AS `x_string`, `x_json` AS `x_json`, `x_ascii` AS `x_ascii`, `x_binary` AS `x_binary`, `x_blob` AS `x_blob`, `x_simple_array` AS `x_simple_array` FROM `tl_page`' => [
@@ -259,7 +324,7 @@ class DumperTest extends ContaoTestCase
                 'SET FOREIGN_KEY_CHECKS = 0;',
                 '-- BEGIN STRUCTURE tl_page',
                 'DROP TABLE IF EXISTS `tl_page`;',
-                'CREATE TABLE `tl_page` (`x_string` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, `x_json` JSON NOT NULL, `x_ascii` VARCHAR(255) NOT NULL, `x_binary` VARBINARY(255) NOT NULL, `x_blob` LONGBLOB NOT NULL, `x_simple_array` LONGTEXT NOT NULL);',
+                "CREATE TABLE `tl_page` (`x_string` VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL, `x_json` $jsonDecl, `x_ascii` VARCHAR(255) NOT NULL, `x_binary` VARBINARY(255) NOT NULL, `x_blob` LONGBLOB NOT NULL, `x_simple_array` $arrayDecl) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB;",
                 '-- BEGIN DATA tl_page',
                 'INSERT INTO `tl_page` (`x_string`, `x_json`, `x_ascii`, `x_binary`, `x_blob`, `x_simple_array`) VALUES (\'ascii\', \'a:1:{i:0;s:5:"ascii";}\', \'ascii\', \'ascii\', \'ascii\', \'asc,ii\');',
                 'INSERT INTO `tl_page` (`x_string`, `x_json`, `x_ascii`, `x_binary`, `x_blob`, `x_simple_array`) VALUES (\'ütf-🎱\', 0x613a313a7b693a303b733a393a22c3bc74662df09f8eb1223b7d, 0xc3bc74662df09f8eb1, 0xc3bc74662df09f8eb1, 0xc3bc74662df09f8eb1, 0xc3bc74662cf09f8eb1);',
