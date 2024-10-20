c2f00da075b06a288dc51bdbd673550e6491c1be
M. Vondano <m-vo@users.noreply.github.com>
2024-10-20 23:29:23 +0200

    disallow adjusting templates from the "frontend_preview" and "web_debug_toolbar" directories in the template studio

--- core-bundle/src/Controller/BackendTemplateStudioController.php
+++ core-bundle/src/Controller/BackendTemplateStudioController.php
@@ -360,7 +360,7 @@ class BackendTemplateStudioController extends AbstractBackendController
     {
         return $this->finder
             ->create()
-            ->identifierRegex('%^backend/%', false)
+            ->identifierRegex('%^(?:backend|frontend_preview|web_debug_toolbar|)/%', false)
         ;
     }
 }
