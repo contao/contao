c0645456dbf84e96e2285ff4029650fe3c670356
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-10-22 13:48:49 +0200

    Update and run the tools

--- core-bundle/tests/Contao/ValidatorTest.php
+++ core-bundle/tests/Contao/ValidatorTest.php
@@ -62,7 +62,7 @@ class ValidatorTest extends TestCase
         yield ['test@example.photography', 1];
         yield ['test@sub-domain.example.photography', 1];
 
-        // Valid with unicode characters in the local part
+        // Valid with Unicode characters in the local part
         yield ['niceändsimple@example.com', 1];
         yield ['véry.çommon@example.com', 1];
         yield ['a.lîttle.lengthy.but.fiñe@dept.example.com', 1];
@@ -74,21 +74,21 @@ class ValidatorTest extends TestCase
         yield ['"üñîçøðé"@example.com', 1];
         yield ['ǅǼ੧ఘⅧ⒇৪@example.com', 1];
 
-        // Valid with IP addresses and unicode characters in the local part
+        // Valid with IP addresses and Unicode characters in the local part
         yield ['üser@[255.255.255.255]', 1];
         yield ['üser@[IPv6:2001:db8:1ff::a0b:dbd0]', 1];
         yield ['üser@[IPv6:2001:0db8:85a3:08d3:1319:8a2e:0370:7344]', 1];
         yield ['üser@[IPv6:2001::7344]', 1];
         yield ['üser@[IPv6:1111:2222:3333:4444:5555:6666:255.255.255.255]', 1];
 
-        // Valid with IDNA domains and unicode characters in the local part
+        // Valid with IDNA domains and Unicode characters in the local part
         yield ['tést@exämple.com', 1];
         yield ['tést@ä.xe', 1];
         yield ['tést@subexample.wizard', 1];
         yield ['tést@wähwähwäh.ümläüts.de', 1];
         yield ['"tés@t"@wähwähwäh.ümläüts.de', 1];
 
-        // Valid with new TLDs and unicode characters in the local part
+        // Valid with new TLDs and Unicode characters in the local part
         yield ['tést@example.photography', 1];
         yield ['tést@sub-domain.example.photography', 1];
 
@@ -128,7 +128,7 @@ class ValidatorTest extends TestCase
         yield ['tes@t@example.photography', 0];
         yield [' test@sub-domain.example.photography', 0];
 
-        // Invalid with unicode characters in the local part
+        // Invalid with Unicode characters in the local part
         yield ['tést..child@example.com', 0];
         yield ['tést@sub.-example.com', 0];
         yield ['tést@_smtp_.example.com', 0];
@@ -145,7 +145,7 @@ class ValidatorTest extends TestCase
         yield ['tést@iana.org-', 0];
         yield ['tést@', 0];
 
-        // Invalid with IP addresses and unicode characters in the local part
+        // Invalid with IP addresses and Unicode characters in the local part
         yield ['tést@a[255.255.255.255]', 0];
         yield ['tést@[255.255.255]', 0];
         yield ['tést@[255.255.255.255.255]', 0];
@@ -153,11 +153,11 @@ class ValidatorTest extends TestCase
         yield ['tést@[2001::7344]', 0];
         yield ['tést@[IPv6:1111:2222:3333:4444:5555:6666:7777:255.255.255.255]', 0];
 
-        // Invalid with IDNA domains and unicode characters in the local part
+        // Invalid with IDNA domains and Unicode characters in the local part
         yield ['tés@t@wähwähwäh.ümläüts.de', 0];
         yield [' tést@wähwähwäh.ümläüts.de', 0];
 
-        // Invalid with new TLDs and unicode characters in the local part
+        // Invalid with new TLDs and Unicode characters in the local part
         yield ['tés@t@example.photography', 0];
         yield [' tést@sub-domain.example.photography', 0];
     }
@@ -168,8 +168,8 @@ class ValidatorTest extends TestCase
             This is a niceandsimple@example.com and this a very.common@example.com. Another little.lengthy.but.fine@dept.example.com and also a disposable.style.email.with+symbol@example.com or an other.email-with-dash@example.com. There are "very.unusual.@.unusual.com"@example.com and "very.(),:;<>[]\".VERY.\"very@\ \"very\".unusual"@strange.example.com and even !#$%&'*+-/=?^_`{}|~@example.org or "()<>[]:,;@\"!#$%&'*+-/=?^_`{}|~.a"@example.org but they are all valid.
             IP addresses as in user@[255.255.255.255], user@[IPv6:2001:db8:1ff::a0b:dbd0], user@[IPv6:2001:0db8:85a3:08d3:1319:8a2e:0370:7344], user@[IPv6:2001::7344] or user@[IPv6:1111:2222:3333:4444:5555:6666:255.255.255.255] are valid, too.
             We also support IDNA domains as in test@exämple.com, test@ä.xe, test@subexample.wizard, test@wähwähwäh.ümläüts.de or "tes@t"@wähwähwäh.ümläüts.de. And we support new TLDs as in test@example.photography or test@sub-domain.example.photography.
-            And we support unicode characters in the local part (RFC 6531) as in niceändsimple@example.com, véry.çommon@example.com, a.lîttle.lengthy.but.fiñe@dept.example.com, dîsposable.style.émail.with+symbol@example.com, other.émail-with-dash@example.com, "verî.uñusual.@.uñusual.com"@example.com, "verî.(),:;<>[]\".VERÎ.\"verî@\ \"verî\".unüsual"@strange.example.com, üñîçøðé@example.com, "üñîçøðé"@example.com or ǅǼ੧ఘⅧ⒇৪@example.com.
-            Of course also with IP addresses: üser@[255.255.255.255], üser@[IPv6:2001:db8:1ff::a0b:dbd0], üser@[IPv6:2001:0db8:85a3:08d3:1319:8a2e:0370:7344], üser@[IPv6:2001::7344] or üser@[IPv6:1111:2222:3333:4444:5555:6666:255.255.255.255] and unicode characters in the local part: tést@exämple.com, tést@ä.xe, tést@subexample.wizard, tést@wähwähwäh.ümläüts.de, "tés@t"@wähwähwäh.ümläüts.de. New TLDs? No problem: tést@example.photography or tést@sub-domain.example.photography.
+            And we support Unicode characters in the local part (RFC 6531) as in niceändsimple@example.com, véry.çommon@example.com, a.lîttle.lengthy.but.fiñe@dept.example.com, dîsposable.style.émail.with+symbol@example.com, other.émail-with-dash@example.com, "verî.uñusual.@.uñusual.com"@example.com, "verî.(),:;<>[]\".VERÎ.\"verî@\ \"verî\".unüsual"@strange.example.com, üñîçøðé@example.com, "üñîçøðé"@example.com or ǅǼ੧ఘⅧ⒇৪@example.com.
+            Of course also with IP addresses: üser@[255.255.255.255], üser@[IPv6:2001:db8:1ff::a0b:dbd0], üser@[IPv6:2001:0db8:85a3:08d3:1319:8a2e:0370:7344], üser@[IPv6:2001::7344] or üser@[IPv6:1111:2222:3333:4444:5555:6666:255.255.255.255] and Unicode characters in the local part: tést@exämple.com, tést@ä.xe, tést@subexample.wizard, tést@wähwähwäh.ümläüts.de, "tés@t"@wähwähwäh.ümläüts.de. New TLDs? No problem: tést@example.photography or tést@sub-domain.example.photography.
             And hopefully we do not match invalid addresses such as test..child@example.com, test@sub.-example.com, test@_smtp_.example.com, test@sub..example.com, test@subexamplecom, Abc.example.com, a"b(c)d,e:f;gi[j\k]l@example.com, this is"not\allowed@example.com, this\ still\"not\allowed@example.com, (comment)test@iana.org, test@[1.2.3.4, @ or test@.
             Also, we should correctly parse <a href="mailto:tricky@example.com">tricky@example.com</a>, <a href="mailto:more-tricky@example.com">write us</a> and <strong>even-more-tricky@example.com</strong>.
             EOF;
