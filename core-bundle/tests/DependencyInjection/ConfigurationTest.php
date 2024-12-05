a2aa0a005e20bc0f2d41766e49ce7e4b21e2d50a
Martin Auswöger <martin@auswoeger.com>
2024-12-05 13:47:26 +0100

    Do not disable normalizeKeys for imagine_options

--- core-bundle/tests/DependencyInjection/ConfigurationTest.php
+++ core-bundle/tests/DependencyInjection/ConfigurationTest.php
@@ -464,7 +464,7 @@ class ConfigurationTest extends TestCase
         ];
     }
 
-    public function testDoesNotNormalizeResamplingFilter(): void
+    public function testDoesNormalizeResamplingFilter(): void
     {
         $params = [
             [
@@ -480,6 +480,21 @@ class ConfigurationTest extends TestCase
 
         $this->assertArrayHasKey('resampling-filter', $configuration['image']['imagine_options']);
         $this->assertSame(ImageInterface::FILTER_LANCZOS, $configuration['image']['imagine_options']['resampling-filter']);
+
+        $params = [
+            [
+                'image' => [
+                    'imagine_options' => [
+                        'resampling_filter' => ImageInterface::FILTER_UNDEFINED,
+                    ],
+                ],
+            ],
+        ];
+
+        $configuration = (new Processor())->processConfiguration($this->configuration, $params);
+
+        $this->assertArrayHasKey('resampling-filter', $configuration['image']['imagine_options']);
+        $this->assertSame(ImageInterface::FILTER_UNDEFINED, $configuration['image']['imagine_options']['resampling-filter']);
     }
 
     /**
