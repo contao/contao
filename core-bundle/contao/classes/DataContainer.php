92d3f17b04667cfd1a3badc0c95fa3f73b61f15e
Andreas Schempp <andreas.schempp@terminal42.ch>
2024-12-05 09:57:17 +0100

    Replace the DCA calls

--- core-bundle/contao/classes/DataContainer.php
+++ core-bundle/contao/classes/DataContainer.php
@@ -1310,7 +1310,7 @@ abstract class DataContainer extends Backend
 		// Make sure tags are unique and empty ones are removed
 		$tags = array_filter(array_unique($tags));
 
-		System::getContainer()->get('contao.cache.tag_invalidator')->invalidateTags($tags);
+		System::getContainer()->get('contao.cache.tag_manager')->invalidateTags($tags);
 	}
 
 	public function addPtableTags($strTable, $intId, &$tags)
