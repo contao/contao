2f9580ef5d173afb5a69220d01be62ebe4268ebe
David Greminger <david.greminger@1up.io>
2024-12-16 15:23:54 +0100

    Render nested fragments with allowedTypes option correctly

--- core-bundle/src/Command/DebugFragmentsCommand.php
+++ core-bundle/src/Command/DebugFragmentsCommand.php
@@ -86,6 +86,8 @@ class DebugFragmentsCommand extends Command
         foreach ($values as $k => $v) {
             if (\is_bool($v)) {
                 $v = $v ? 'true' : 'false';
+            } elseif ('nestedFragments' === $k && array_key_exists('allowedTypes', $v)) {
+                $v = 'true (allowed types: ' . implode(', ', $v['allowedTypes']) . ')';
             }
 
             $return[] = \sprintf('%s : %s', str_pad($k, $length, ' ', STR_PAD_RIGHT), $v);
