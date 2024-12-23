c4b8dbb294346aa6cb31388cbc7b0859a3eb2e9d
M. Vondano <m-vo@users.noreply.github.com>
2024-12-23 12:31:15 +0100

    Support themes in the template studio (see #7683)
    Description
    -----------
    
    We're slowly getting there - this is probably the last big chunk for the initial template studio implementation which adds support for themes.
    
    If at least one theme exists and has a template directory set, a new selector will appear on top of the tree which allows switching the context (look at the changes to the tree and which templates are displayed):
    
    [themes.webm](https://github.com/user-attachments/assets/7996b536-fb6b-419c-af35-de15f3de3d03)
    
    As usual, there is some refactoring going on plus some small changes, namely:
    
    * I renamed `TemplateContext` to` OperationContext` as it is specific to the studio namespace anyways.
    * The `AbstractBackendController` now got a `getBackendSessionBag()` method. 
    * Theme paths outside of the template directory are deprecated. Otherwise the concept of a user template storage makes no sense. In the future, either each themes will have their own storage or supporting anything from outside must be done by mounting things in the VFS.
    
    Commits
    -------
    
    3013520c deprecate relative theme paths and add a new getPath() function
    5f7bdd92 refactor and rename operations context and add a factory service
    f0007b9d add a getBackendSessionBag() method to the AbstractBackendController
    95723b8a add theme support
    bf49ed89 remove invalid trans_default_domain
    18ff3c22 build assets
    7e810ff0 adjust tests to not trigger self deprecations
    f9a93c0d remove unnecessary statement
    515ab3eb fix unit-tests
    6765fb52 ignore empty paths
    7f4f718f Merge remote-tracking branch 'origin/5.x' into feature/template-studi…
    b1206fe7 build assets

--- core-bundle/src/Twig/Studio/Operation/OperationContext.php
+++ core-bundle/src/Twig/Studio/Operation/OperationContext.php
@@ -12,26 +12,22 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Twig\Studio\Operation;
 
+use Contao\CoreBundle\Twig\Loader\ThemeNamespace;
+
 /**
  * @experimental
  */
-class TemplateContext
+class OperationContext
 {
     /**
      * @internal
-     *
-     * @param array<string, mixed> $parameters
      */
     public function __construct(
+        private readonly ThemeNamespace $themeNamespace,
         private readonly string $identifier,
         private readonly string $extension,
-        public readonly array $parameters = [],
+        private readonly string|null $themeSlug,
     ) {
-    }
-
-    public function getParameter(string $parameter, mixed $default = null): mixed
-    {
-        return $this->parameters[$parameter] ?? $default;
     }
 
     public function getIdentifier(): string
@@ -44,9 +40,25 @@ class TemplateContext
         return $this->extension;
     }
 
+    public function isThemeContext(): bool
+    {
+        return null !== $this->themeSlug;
+    }
+
+    public function getThemeSlug(): string|null
+    {
+        return $this->themeSlug;
+    }
+
     public function getUserTemplatesStoragePath(): string
     {
-        return "$this->identifier.$this->extension";
+        $path = "$this->identifier.$this->extension";
+
+        if (!$this->isThemeContext()) {
+            return $path;
+        }
+
+        return "{$this->themeNamespace->getPath($this->themeSlug)}/$path";
     }
 
     public function getManagedNamespaceName(): string
