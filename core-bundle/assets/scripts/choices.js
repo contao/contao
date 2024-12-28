a24a5dd20d8b4037b6639982a2c886d181e78b85
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-12-28 11:10:27 +0100

    Rewrite choices initialization

--- core-bundle/assets/scripts/choices.js
+++ core-bundle/assets/scripts/choices.js
@@ -1,55 +1,47 @@
-/*!
- * This file is part of Contao.
- *
- * (c) Leo Feyer
- *
- * @license LGPL-3.0-or-later
- */
 (function () {
-    let choicesList = [];
+    'use strict';
 
-    function initChoices(node) {
-        node.querySelectorAll('select.tl_chosen').forEach(function (select) {
-            if (choicesList.includes(select)) {
-                return;
-            }
+    const initializedChoices = new WeakMap();
 
-            choicesList.push(select);
+    const init = (select) => {
+        // Check if this select has already been initialized
+        if (initializedChoices.has(select)) {
+            return;
+        }
 
-            new Choices(select, {
-                shouldSort: false,
-                duplicateItemsAllowed: false,
-                allowHTML: false,
-                removeItemButton: true,
-                //searchEnabled: select.options.length > 7,
-                classNames: {
-                    containerOuter: ['choices', ...Array.from(select.classList)],
-                    flippedState: ''
-                },
-                callbackOnInit: () => {
-                   const choices = select.closest('.choices')?.querySelector('.choices__list--dropdown > .choices__list');
-                   if (choices && select.dataset.placeholder) {
-                       choices.dataset.placeholder = select.dataset.placeholder;
-                   }
-                },
-                loadingText: Contao.lang.loading,
-                noResultsText: Contao.lang.noResults,
-                noChoicesText: Contao.lang.noChoices,
-                itemSelectText: Contao.lang.pressToSelect,
-                uniqueItemText: Contao.lang.onlyUnique,
-                customAddItemText: Contao.lang.onlyConditions,
-            })
-        });
+        initializedChoices.set(select, true);
+
+        new Choices(select, {
+            shouldSort: false,
+            duplicateItemsAllowed: false,
+            allowHTML: false,
+            removeItemButton: true,
+            //searchEnabled: select.options.length > 7,
+            classNames: {
+                containerOuter: ['choices', ...Array.from(select.classList)],
+                flippedState: ''
+            },
+            callbackOnInit: () => {
+                const choices = select.closest('.choices')?.querySelector('.choices__list--dropdown > .choices__list');
+                if (choices && select.dataset.placeholder) {
+                    choices.dataset.placeholder = select.dataset.placeholder;
+                }
+            },
+            loadingText: Contao.lang.loading,
+            noResultsText: Contao.lang.noResults,
+            noChoicesText: Contao.lang.noChoices,
+            itemSelectText: Contao.lang.pressToSelect,
+            uniqueItemText: Contao.lang.onlyUnique,
+            customAddItemText: Contao.lang.onlyConditions,
+        })
     }
-
-    initChoices(document);
 
     new MutationObserver(function (mutationsList) {
         for(const mutation of mutationsList) {
             if (mutation.type === 'childList') {
                 mutation.addedNodes.forEach(function (element) {
-                    if (element.querySelectorAll) {
-                        initChoices(element)
+                    if (element.matches && element.matches('select.tl_chosen')) {
+                        init(element)
                     }
                 })
             }
