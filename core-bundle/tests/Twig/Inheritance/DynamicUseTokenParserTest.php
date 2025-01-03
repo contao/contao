d73a6ac8a57bbb264f8f57abbbf1eba0e0de3408
M. Vondano <m-vo@users.noreply.github.com>
2025-01-03 13:33:29 +0100

    fix dynamic inheritance with "use" for theme contexts

--- core-bundle/tests/Twig/Inheritance/DynamicUseTokenParserTest.php
+++ core-bundle/tests/Twig/Inheritance/DynamicUseTokenParserTest.php
@@ -22,6 +22,7 @@ use Contao\CoreBundle\Twig\Inheritance\DynamicUseTokenParser;
 use Contao\CoreBundle\Twig\Loader\ContaoFilesystemLoader;
 use Contao\CoreBundle\Twig\Loader\TemplateLocator;
 use Contao\CoreBundle\Twig\Loader\ThemeNamespace;
+use Contao\PageModel;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\Cache\Adapter\NullAdapter;
 use Symfony\Component\Filesystem\Path;
@@ -38,33 +39,7 @@ class DynamicUseTokenParserTest extends TestCase
 
     public function testHandlesContaoUses(): void
     {
-        $projectDir = Path::canonicalize(__DIR__.'/../../Fixtures/Twig/use');
-
-        $resourceFinder = $this->createMock(ResourceFinder::class);
-        $resourceFinder
-            ->method('getExistingSubpaths')
-            ->with('templates')
-            ->willReturn(['FooBundle' => Path::join($projectDir, 'bundle/contao/templates'), 'App' => Path::join($projectDir, 'templates')])
-        ;
-
-        $templateLocator = new TemplateLocator(
-            $projectDir,
-            $resourceFinder,
-            $themeNamespace = new ThemeNamespace(),
-            $this->createMock(Connection::class),
-        );
-
-        $filesystemLoader = new ContaoFilesystemLoader(new NullAdapter(), $templateLocator, $themeNamespace, $this->createMock(ContaoFramework::class), $projectDir);
-
-        $environment = new Environment($filesystemLoader);
-        $environment->addExtension(
-            new ContaoExtension(
-                $environment,
-                $filesystemLoader,
-                $this->createMock(ContaoCsrfTokenManager::class),
-                $this->createMock(ContaoVariable::class),
-            ),
-        );
+        $environment = $this->getDemoEnvironment();
 
         // A component is adjusted by overwriting the component's template (here by
         // adding the item "ice" and turning apples into pineapples). The changes should
@@ -105,5 +80,72 @@ class DynamicUseTokenParserTest extends TestCase
                 HTML,
             trim($environment->render('@Contao/element/recipe.html.twig')),
         );
+    }
+
+    public function testHandlesContaoUsesWithThemeContext(): void
+    {
+        $environment = $this->getDemoEnvironment();
+
+        $GLOBALS['objPage'] = $this->mockClassWithProperties(PageModel::class, ['templateGroup' => 'templates/theme']);
+
+        // When in a theme context at runtime, the theme's component is used as first
+        // template in the chain:
+        $this->assertSame(
+            <<<'HTML'
+                <h1>Summer menu</h1>
+                <div class="themed_shake">
+                    <h2>Fruit shake</h2>
+                    <b>Ingredients:</b>
+                    <ul>
+                        <li>pineapple and banana</li>
+                        <li>milk</li>
+                        <li>ice</li>
+                    </ul>
+                </div>
+                HTML,
+            trim($environment->render('@Contao/element/menu.html.twig')),
+        );
+
+        unset($GLOBALS['objPage']);
+    }
+
+    private function getDemoEnvironment(): Environment
+    {
+        $projectDir = Path::canonicalize(__DIR__.'/../../Fixtures/Twig/use');
+
+        $resourceFinder = $this->createMock(ResourceFinder::class);
+        $resourceFinder
+            ->method('getExistingSubpaths')
+            ->with('templates')
+            ->willReturn(['FooBundle' => Path::join($projectDir, 'bundle/contao/templates'), 'App' => Path::join($projectDir, 'templates')])
+        ;
+
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->method('fetchFirstColumn')
+            ->with("SELECT templates FROM tl_theme WHERE templates != ''")
+            ->willReturn(['templates/theme'])
+        ;
+
+        $templateLocator = new TemplateLocator(
+            $projectDir,
+            $resourceFinder,
+            $themeNamespace = new ThemeNamespace(),
+            $connection,
+        );
+
+        $filesystemLoader = new ContaoFilesystemLoader(new NullAdapter(), $templateLocator, $themeNamespace, $this->createMock(ContaoFramework::class), $projectDir);
+
+        $environment = new Environment($filesystemLoader);
+        $environment->addExtension(
+            new ContaoExtension(
+                $environment,
+                $filesystemLoader,
+                $this->createMock(ContaoCsrfTokenManager::class),
+                $this->createMock(ContaoVariable::class),
+            ),
+        );
+
+        return $environment;
     }
 }
