0113650b9dc491cc286e4aab850858301a9eea00
M. Vondano <m-vo@users.noreply.github.com>
2025-01-06 16:48:18 +0100

    Implement search invalidation on file storage DBAFS updates (see #7835)
    Description
    -----------
    
    This is another step towards #7700.
    
    Commits
    -------
    
    cb39af51 add an event that dispatches dbafs changes
    025f17b6 backend search: listen to the dbafs change event and reindex files st…
    813dff21 simplify logic
    6448e158 fix existing tests
    15024e93 add tests for dbafs change event
    efe28fca add tests for reindexing with a dbafs event
    e551d3fc fix functional tests
    ca8c3cbf fix annotation
    35f8e315 Merge remote-tracking branch 'origin/5.x' into feature/backend-search…
    4b49fa44 also dispatch the DbafsChangeEvent when extra metadata is set

---
+++ core-bundle/tests/Search/Backend/EventListener/TriggerReindexOnFilesStorageUpdateListenerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Search\Backend\EventListener;
+
+use Contao\CoreBundle\Filesystem\Dbafs\ChangeSet\ChangeSet;
+use Contao\CoreBundle\Filesystem\Dbafs\DbafsChangeEvent;
+use Contao\CoreBundle\Filesystem\FilesystemItem;
+use Contao\CoreBundle\Filesystem\VirtualFilesystem;
+use Contao\CoreBundle\Filesystem\VirtualFilesystemInterface;
+use Contao\CoreBundle\Search\Backend\BackendSearch;
+use Contao\CoreBundle\Search\Backend\EventListener\TriggerReindexOnFilesStorageUpdateListener;
+use Contao\CoreBundle\Search\Backend\ReindexConfig;
+use Contao\CoreBundle\Tests\TestCase;
+
+class TriggerReindexOnFilesStorageUpdateListenerTest extends TestCase
+{
+    public function testInvokeTriggersReindexOnFilesStorageElements(): void
+    {
+        $storage = $this->createMock(VirtualFilesystem::class);
+        $storage
+            ->method('getPrefix')
+            ->willReturn('files')
+        ;
+
+        $storage
+            ->method('get')
+            ->willReturnMap([
+                ['foo', VirtualFilesystemInterface::NONE, new FilesystemItem(true, 'foo')],
+                ['bar2', VirtualFilesystemInterface::NONE, new FilesystemItem(true, 'bar2')],
+                ['meta', VirtualFilesystemInterface::NONE, new FilesystemItem(true, 'meta')],
+                ['dir1', VirtualFilesystemInterface::NONE, new FilesystemItem(false, 'dir1')],
+                ['dir2', VirtualFilesystemInterface::NONE, new FilesystemItem(false, 'dir2')],
+            ])
+        ;
+
+        $backendSearch = $this->createMock(BackendSearch::class);
+        $backendSearch
+            ->expects($this->once())
+            ->method('reindex')
+            ->with($this->callback(
+                static fn (ReindexConfig $config) => ['contao.vfs.files' => [
+                    'foo', // created file
+                    'bar2', // renamed file (new)
+                    'bar', // renamed file (old)
+                    'meta', // file with updated metadata
+                    'baz', // deleted file
+                ]] === $config->getLimitedDocumentIds()->toArray(),
+            ))
+        ;
+
+        $event = new DbafsChangeEvent(
+            new ChangeSet(
+                [
+                    ['hash' => '123', 'path' => 'files/foo', 'type' => ChangeSet::TYPE_FILE],
+                    ['hash' => '123', 'path' => 'files/dir1', 'type' => ChangeSet::TYPE_DIRECTORY],
+                ],
+                [
+                    'files/bar' => ['path' => 'files/bar2'],
+                    'files/dir2' => ['hash' => '123'],
+                    'files/meta' => [],
+                ],
+                [
+                    'files/baz' => ChangeSet::TYPE_FILE,
+                    'files/dir3' => ChangeSet::TYPE_DIRECTORY,
+                ],
+            ),
+        );
+
+        $listener = new TriggerReindexOnFilesStorageUpdateListener($backendSearch, $storage);
+        $listener($event);
+    }
+}
