797c37fa692fee42498b34278e992f2684bb6af6
Martin Auswöger <martin@auswoeger.com>
2025-01-21 21:30:40 +0100

    Remove obsolete check

--- core-bundle/contao/library/Contao/Database/Statement.php
+++ core-bundle/contao/library/Contao/Database/Statement.php
@@ -257,11 +257,6 @@ class Statement
 			$arrParams[$key] = serialize($varParam);
 		}
 
-		// null types are deprecated in doctrine/dbal 3.x
-		$arrTypes = array_map(static function ($type) {
-			return null === $type ? ParameterType::STRING : $type;
-		}, $arrTypes);
-
 		// Execute the query
 		$this->statement = $this->resConnection->executeQuery($this->strQuery, $arrParams, $arrTypes);
 
