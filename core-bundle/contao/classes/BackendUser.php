ec24524b896b799d0c3c267829df828cb9da34f3
M. Vondano <m-vo@users.noreply.github.com>
2025-02-12 22:39:20 +0100

    Update core-bundle/contao/classes/BackendUser.php
    Co-authored-by: Martin Auswöger <martin@auswoeger.com>

--- core-bundle/contao/classes/BackendUser.php
+++ core-bundle/contao/classes/BackendUser.php
@@ -227,7 +227,7 @@ class BackendUser extends User
 		Config::set('useRTE', $this->useRTE);
 		Config::set('useCE', $this->useCE);
 		Config::set('doNotCollapse', $this->doNotCollapse);
-		Config::set('doNotHideMessages', $this->doNotCollapse);
+		Config::set('doNotHideMessages', $this->doNotHideMessages);
 		Config::set('thumbnails', $this->thumbnails);
 		Config::set('backendTheme', $this->backendTheme);
 
