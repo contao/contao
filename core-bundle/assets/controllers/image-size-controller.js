2eeebc1d98f864b657e2760e7619fe1036738a11
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-02-14 17:10:47 +0100

    CS

--- core-bundle/assets/controllers/image-size-controller.js
+++ core-bundle/assets/controllers/image-size-controller.js
@@ -2,6 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     static targets = ['select', 'button', 'image'];
+
     static values = {
         config: Object,
     }
@@ -19,12 +20,14 @@ export default class extends Controller {
         if (this.canEdit()) {
             this.buttonTarget.title = this.configValue.title;
             this.buttonTarget.disabled = false;
+
             this.imageTargets.forEach((img) => {
                 img.src = this.configValue.icon;
             });
         } else {
             this.buttonTarget.title = '';
             this.buttonTarget.disabled = true;
+
             this.imageTargets.forEach((img) => {
                 img.src = this.configValue.iconDisabled;
             });
