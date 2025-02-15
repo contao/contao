53c261663bb4e4ff694cfed0cf093ab3858a8b81
Christian Barkowsky <christian@plenta.io>
2025-02-15 16:03:08 +0100

    Please change Tempalte to FragmentTemplate in Maker Bundle

--- maker-bundle/skeleton/content-element/ContentElement.tpl.php
+++ maker-bundle/skeleton/content-element/ContentElement.tpl.php
@@ -7,14 +7,14 @@ namespace App\Controller\ContentElement;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
-use Contao\Template;
+use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsContentElement(category: '<?= $category ?>')]
 class <?= $className ?> extends AbstractContentElementController
 {
-    protected function getResponse(Template $template, ContentModel $model, Request $request): Response
+    protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
         return $template->getResponse();
     }
