70058a01af12e4d52600596e8cd519907d793268
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-02-17 15:46:45 +0100

    Use the `prevent` flag instead
    Co-authored-by: M. Vondano <m-vo@users.noreply.github.com>

--- core-bundle/assets/controllers/backend-search-controller.js
+++ core-bundle/assets/controllers/backend-search-controller.js
@@ -93,14 +93,12 @@ export default class BackendSearchController extends Controller {
 
     focusTrapNext(event) {
         if ("results" === this.state && document.activeElement === this.lastFocus) {
-            event.preventDefault();
             this.firstFocus?.focus();
         }
     }
 
     focusTrapPrev(event) {
         if ("results" === this.state && document.activeElement === this.firstFocus) {
-            event.preventDefault();
             this.lastFocus?.focus();
         }
     }
