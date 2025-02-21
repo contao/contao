8c17c02e718286fd0fed6c54add4dc3771b083dd
Christian Barkowsky <christianbarkowsky@users.noreply.github.com>
2025-02-21 10:45:10 +0100

    Change `Template` to `FragmentTemplate` in the maker bundle (see #8093)
    Description
    -----------
    
    Fixes #
    
    Please change Template to FragmentTemplate in **Maker-Bundle**
    
    Commits
    -------
    
    c5c89cc9 Use frontend module/content element type as CSS class again
    53c26166 Please change Tempalte to FragmentTemplate in Maker Bundle
    4913171d In the dca-callback skeleton, the DataContainer is missing in the nam…
    
    Co-authored-by: veronikaplenta <veronika@plenta.io>
    Co-authored-by:  <christian@plenta.io>

--- maker-bundle/skeleton/frontend-module/FrontendModule.tpl.php
+++ maker-bundle/skeleton/frontend-module/FrontendModule.tpl.php
@@ -7,14 +7,14 @@ namespace App\Controller\FrontendModule;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\ModuleModel;
-use Contao\Template;
+use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsFrontendModule(category: '<?= $category ?>')]
 class <?= $className ?> extends AbstractFrontendModuleController
 {
-    protected function getResponse(Template $template, ModuleModel $model, Request $request): Response
+    protected function getResponse(FragmentTemplate $template, ModuleModel $model, Request $request): Response
     {
         return $template->getResponse();
     }
