c659cad522ce0018477982b0262f0bd87988b6b0
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-03-10 12:16:48 +0100

    Add the missing `scroll-offset#store` action to the `all` button (see #8179)
    Description
    -----------
    
    ### Description
    
    Regression from #6672
    
    The mentioned PR did remove `onclick="Backend.getScrollOffset()"` but the data-action was not added
    
    Commits
    -------
    
    c1eb2271 Add the missing `scroll-offset#store` action to the `all` button

--- core-bundle/src/EventListener/DataContainer/DefaultGlobalOperationsListener.php
+++ core-bundle/src/EventListener/DataContainer/DefaultGlobalOperationsListener.php
@@ -114,7 +114,7 @@ class DefaultGlobalOperationsListener
                 'all' => [
                     'href' => 'act=select',
                     'class' => 'header_edit_all',
-                    'attributes' => 'accesskey="e"',
+                    'attributes' => 'data-action="contao--scroll-offset#store" accesskey="e"',
                 ],
             ];
         }
