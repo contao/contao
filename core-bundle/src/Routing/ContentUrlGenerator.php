40b3098b0c38c6b9eb72e5829bee48f0fe73b0b4
fritzmg <fmg@inspiredminds.at>
2025-03-14 13:26:23 +0000

    revert unrelated change

--- core-bundle/src/Routing/ContentUrlGenerator.php
+++ core-bundle/src/Routing/ContentUrlGenerator.php
@@ -184,7 +184,7 @@ class ContentUrlGenerator implements ResetInterface, RequestContextAwareInterfac
             $metadata = null;
         }
 
-        if (!$metadata) {
+        if (null === $metadata) {
             return $content::class.'->'.spl_object_id($content);
         }
 
