dfa78bc5b32bd6a3139bb5c9b8ebd8c755d9e43d
M. Vondano <m-vo@users.noreply.github.com>
2025-03-14 19:09:10 +0100

    add message module to ease working with messages

---
+++ core-bundle/assets/modules/message.js
@@ -0,0 +1,29 @@
+export class Message {
+    /**
+     * Display an info message.
+     *
+     * @param message Text or HTML that will be put into the message body.
+     */
+    static info(message) {
+        Message._addMessage(message, 'info');
+    }
+
+    /**
+     * Display an error message.
+     *
+     * @param message Text or HTML that will be put into the message body.
+     */
+    static error(message) {
+        Message._addMessage(message, 'error');
+    }
+
+    static _addMessage(message, type) {
+        const event = new CustomEvent('contao--message', {
+            detail: {
+                type, message
+            }
+        });
+
+        document.dispatchEvent(event);
+    }
+}
