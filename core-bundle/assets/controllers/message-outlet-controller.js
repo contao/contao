6d628b3103e109b4024d3f68493ca2edb9d06be8
M. Vondano <m-vo@users.noreply.github.com>
2025-03-14 19:08:51 +0100

    add message prototypes system, so that we can render messages from JS

--- core-bundle/assets/controllers/message-outlet-controller.js
+++ core-bundle/assets/controllers/message-outlet-controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['message'];
+    static targets = ['message', 'messagePrototype'];
 
     messageTargetConnected(el) {
         if (!el.querySelector('button.close')) {
@@ -11,6 +11,21 @@ export default class extends Controller {
 
     close(event) {
         this._hide(event.target.closest('*[data-contao--message-outlet-target]'));
+    }
+
+    renderMessage(event) {
+        const { type, message } = event.detail;
+
+        for (const target of this.messagePrototypeTargets) {
+            if(target.dataset.type === type) {
+                const html = target.getHTML().replace('{{message}}', message);
+                this.element.append(document.createRange().createContextualFragment(html));
+
+                return;
+            }
+        }
+
+        console.error(`Did not find any message prototypes for type "${type}".`);
     }
 
     _hide(el) {
