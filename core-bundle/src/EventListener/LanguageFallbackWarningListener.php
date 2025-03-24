0e110adfe09b9e1581b11352b0e8f2d14ee8d3a8
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-03-24 16:00:19 +0000

    Deprecate the `Contao\Messages` class (see #8212)
    Description
    -----------
    
    Our legacy `Contao\Messages` class currently only serves as a pseudo-service for generating the warning messages related to misconfigured fallbacks per domain in your website roots. This PR deprecates it and puts the functionality into a proper listener instead.
    
    Commits
    -------
    
    bf930b3d Deprecate the Contao\Messages class
    8abb4493 Mark as internal and add comments
    308a90e0 add test
    376a654b rename method
    ec1ecd0d rework for tests
    8b9dc9bf rename service

---
+++ core-bundle/src/EventListener/LanguageFallbackWarningListener.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\Date;
+use Contao\Message;
+use Doctrine\DBAL\Connection;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * Generates the warning messages related to misconfigured website roots.
+ *
+ * @internal
+ */
+class LanguageFallbackWarningListener
+{
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly Connection $connection,
+        private readonly TranslatorInterface $translator,
+        private readonly ContaoFramework $contaoFramework,
+    ) {
+    }
+
+    #[AsCallback('tl_page', 'config.onload')]
+    public function onPageLoad(): void
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        if (!\in_array($request?->query->get('act'), ['paste', 'select', null], true)) {
+            return;
+        }
+
+        $this->contaoFramework->getAdapter(Message::class)->addRaw($this->getMessages());
+    }
+
+    #[AsHook('getSystemMessages')]
+    public function getMessages(): string
+    {
+        $time = Date::floorToMinute();
+        $records = $this->connection->fetchAllAssociative("SELECT fallback, dns FROM tl_page WHERE type='root' AND published=1 AND (start='' OR start<=$time) AND (stop='' OR stop>$time) ORDER BY dns");
+        $roots = [];
+
+        foreach ($records as $root) {
+            $dns = $root['dns'] ?: '*';
+
+            if (true === ($roots[$dns] ?? null)) {
+                continue;
+            }
+
+            $roots[$dns] = (bool) $root['fallback'];
+        }
+
+        $messages = [];
+
+        foreach ($roots as $k => $v) {
+            if ($v) {
+                continue;
+            }
+
+            if ('*' === $k) {
+                $messages[] = $this->translator->trans('ERR.noFallbackEmpty', [], 'contao_default');
+            } else {
+                $messages[] = $this->translator->trans('ERR.noFallbackDns', [$k], 'contao_default');
+            }
+        }
+
+        return implode('', array_map(static fn (string $message): string => '<p class="tl_error">'.$message.'</p>', $messages));
+    }
+}
