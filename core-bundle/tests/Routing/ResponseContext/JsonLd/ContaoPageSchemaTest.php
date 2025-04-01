7cce21eecb5617272c363cb82bf319f0e61434c4
CMSworker <31544192+CMSworker@users.noreply.github.com>
2025-04-01 18:11:26 +0200

    Update ContaoPageSchemaTest.php
    Further adjustments are probably required: Test all options for "searchIndexer": '' (=empty string), 'default_index', 'always_index', 'never_index'

--- core-bundle/tests/Routing/ResponseContext/JsonLd/ContaoPageSchemaTest.php
+++ core-bundle/tests/Routing/ResponseContext/JsonLd/ContaoPageSchemaTest.php
@@ -20,11 +20,11 @@ class ContaoPageSchemaTest extends TestCase
 {
     public function testGeneralSettersAndGetters(): void
     {
-        $schema = new ContaoPageSchema('title', 42, false, false, [], false);
+        $schema = new ContaoPageSchema('title', 42, 'default_index', false, [], false);
 
         $this->assertSame('title', $schema->getTitle());
         $this->assertSame(42, $schema->getPageId());
-        $this->assertSame('', $schema->isSearchIndexer());
+        $this->assertSame('default_index', $schema->isSearchIndexer());
         $this->assertFalse($schema->isProtected());
         $this->assertSame([], $schema->getGroups());
         $this->assertFalse($schema->isFePreview());
@@ -35,8 +35,8 @@ class ContaoPageSchemaTest extends TestCase
         $schema->setPageId(43);
         $this->assertSame(43, $schema->getPageId());
 
-        $schema->setSearchIndexer('default_index');
-        $this->assertSame('default_index', $schema->isSearchIndexer());
+        $schema->setSearchIndexer('always_index');
+        $this->assertSame('always_index', $schema->isSearchIndexer());
 
         $schema->setProtected(true);
         $this->assertTrue($schema->isProtected());
