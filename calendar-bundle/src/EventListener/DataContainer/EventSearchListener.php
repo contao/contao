da009623183e6af90a0af1add09b8f38494b1aad
CMSworker <31544192+CMSworker@users.noreply.github.com>
2025-04-09 23:47:28 +0200

    Add searchListener to News, Events and FAQ

---
+++ calendar-bundle/src/EventListener/DataContainer/EventSearchListener.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CalendarBundle\EventListener\DataContainer;
+
+use Contao\CalendarEventsModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Contao\DataContainer;
+use Contao\Search;
+use Symfony\Component\Routing\Exception\ExceptionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * @internal
+ */
+class EventSearchListener
+{
+    public function __construct(
+        private readonly ContaoFramework $framework,
+        private readonly ContentUrlGenerator $urlGenerator,
+    ) {
+    }
+
+    #[AsCallback(table: 'tl_calendar_events', target: 'fields.alias.save')]
+    public function onSaveAlias(string $value, DataContainer $dc): string
+    {
+        if ($value === ($dc->getCurrentRecord()['alias'] ?? null)) {
+            return $value;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+
+        return $value;
+    }
+
+    #[AsCallback(table: 'tl_calendar_events', target: 'fields.robots.save')]
+    public function onSaveRobots(string $value, DataContainer $dc): string
+    {
+        if (str_starts_with($value, 'index')) {
+            return $value;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+
+        return $value;
+    }
+
+    #[AsCallback(table: 'tl_calendar_events', target: 'config.ondelete', priority: 16)]
+    public function onDelete(DataContainer $dc): void
+    {
+        if (!$dc->id) {
+            return;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+    }
+
+    private function purgeSearchIndex(int $eventId): void
+    {
+        $objEvent = CalendarEventsModel::findById($eventId);
+
+        try {
+            $eventUrl = $this->urlGenerator->generate($objEvent, [], UrlGeneratorInterface::ABSOLUTE_URL);
+        } catch (ExceptionInterface) {
+        }
+
+        $search = $this->framework->getAdapter(Search::class);
+
+        $search->removeEntry($eventUrl);
+    }
+}
---
+++ news-bundle/src/EventListener/DataContainer/NewsSearchListener.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\NewsBundle\EventListener\DataContainer;
+
+use Contao\NewsModel;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Contao\DataContainer;
+use Contao\Search;
+use Symfony\Component\Routing\Exception\ExceptionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * @internal
+ */
+class NewsSearchListener
+{
+    public function __construct(
+        private readonly ContaoFramework $framework,
+        private readonly ContentUrlGenerator $urlGenerator,
+    ) {
+    }
+
+    #[AsCallback(table: 'tl_news', target: 'fields.alias.save')]
+    public function onSaveAlias(string $value, DataContainer $dc): string
+    {
+        if ($value === ($dc->getCurrentRecord()['alias'] ?? null)) {
+            return $value;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+
+        return $value;
+    }
+
+    #[AsCallback(table: 'tl_news', target: 'fields.robots.save')]
+    public function onSaveRobots(string $value, DataContainer $dc): string
+    {
+        if (str_starts_with($value, 'index')) {
+            return $value;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+
+        return $value;
+    }
+
+    #[AsCallback(table: 'tl_news', target: 'config.ondelete', priority: 16)]
+    public function onDelete(DataContainer $dc): void
+    {
+        if (!$dc->id) {
+            return;
+        }
+
+        $this->purgeSearchIndex((int) $dc->id);
+    }
+
+    private function purgeSearchIndex(int $newsId): void
+    {
+        $objNews = NewsModel::findById($newsId);
+
+        try {
+            $newsUrl = $this->urlGenerator->generate($objNews, [], UrlGeneratorInterface::ABSOLUTE_URL);
+        } catch (ExceptionInterface) {
+        }
+
+        $search = $this->framework->getAdapter(Search::class);
+
+        $search->removeEntry($newsUrl);
+    }
+}
