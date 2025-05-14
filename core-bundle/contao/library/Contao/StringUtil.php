1965d054c322597b6108ea00837d1f292614f1cf
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-05-14 08:55:30 +0200

    Update core-bundle/contao/library/Contao/StringUtil.php
    Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/contao/library/Contao/StringUtil.php
+++ core-bundle/contao/library/Contao/StringUtil.php
@@ -209,15 +209,12 @@ class StringUtil
 	public static function decodeEntities($strString, $strQuoteStyle=ENT_QUOTES)
 	{
 		$replace = static function (&$value) use ($strQuoteStyle) {
-			if ((string) $value === '')
+			if (\is_string($value))
 			{
-				$value = '';
-				return;
+				$value = preg_replace('/(&#*\w+)[\x00-\x20]+;/i', '$1;', $value);
+				$value = preg_replace('/(&#x*)([0-9a-f]+);/i', '$1$2;', $value);
+				$value = html_entity_decode($value, $strQuoteStyle | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8');
 			}
-
-			$value = preg_replace('/(&#*\w+)[\x00-\x20]+;/i', '$1;', $value);
-			$value = preg_replace('/(&#x*)([0-9a-f]+);/i', '$1$2;', $value);
-			$value = html_entity_decode($value, $strQuoteStyle | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8');
 		};
 
 		if (\is_array($strString))
