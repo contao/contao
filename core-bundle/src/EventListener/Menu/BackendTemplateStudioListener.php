dfa70f08a0d884baaa85fb715e74b9cd035eec8c
Dennis Schmidt <de-es@users.noreply.github.com>
2025-05-14 22:46:58 +0200

    Set translation domain

--- core-bundle/src/EventListener/Menu/BackendTemplateStudioListener.php
+++ core-bundle/src/EventListener/Menu/BackendTemplateStudioListener.php
@@ -61,7 +61,8 @@ class BackendTemplateStudioListener
 
         $templateStudioNode = $event->getFactory()
             ->createItem('template-studio')
-            ->setLabel($this->translator->trans('MOD.template_studio.0', [], 'contao_modules'))
+            ->setLabel('MOD.template_studio.0')
+            ->setExtra('translation_domain', 'contao_modules')
             ->setUri($this->router->generate('contao_template_studio'))
             ->setLinkAttribute('class', 'navigation template-studio')
             ->setLinkAttribute('title', $this->translator->trans('MOD.template_studio.1', [], 'contao_modules'))
