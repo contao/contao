f708ab86b9be67c7c1bbf7afd763c55bc51a9dbb
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-05-22 10:20:21 +0200

    Updated to SEAL 0.9

--- core-bundle/contao/classes/RebuildBackendSearchIndex.php
+++ core-bundle/contao/classes/RebuildBackendSearchIndex.php
@@ -54,6 +54,7 @@ class RebuildBackendSearchIndex extends Backend implements MaintenanceModuleInte
 
 		if (Input::post('FORM_SUBMIT') == 'tl_rebuild_backend_search' && $backendSearch->isAvailable())
 		{
+			$backendSearch->clear(); // Drop the entire index as the schema might have changed after an update, etc.
 			$backendSearch->reindex(new ReindexConfig());
 			Message::addConfirmation($GLOBALS['TL_LANG']['tl_maintenance']['backend_search']['confirmation'], self::class);
 			$this->reload();
