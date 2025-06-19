f85273917916e1f71ca1686aaec23913fc6f92ee
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-06-19 16:02:27 +0200

    Catch parse errors in auto service registration

--- manager-bundle/skeleton/config/services.php
+++ manager-bundle/skeleton/config/services.php
@@ -39,7 +39,12 @@ return static function (ContainerConfigurator $configurator) use ($container): v
 
         // Trigger __destruct handler
         unset($config);
-    } catch (Throwable) {
+    } catch (Throwable $e) {
+        // Throw parse errors. Otherwise, you are having a hard time debugging why your service is not registered.
+        if ($e instanceof ParseError) {
+            throw $e;
+        }
+
         // Ignore failed autoloading
     }
 
