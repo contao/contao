a3a9c25897d036d8455ea730844e1fc5fc4e6003
fritzmg <fmg@inspiredminds.at>
2025-06-26 16:06:43 +0100

    Add HTML attributes and meta tags

--- core-bundle/contao/pages/PageRegular.php
+++ core-bundle/contao/pages/PageRegular.php
@@ -272,6 +272,12 @@ class PageRegular extends Frontend
 		$this->Template->onload = trim($objLayout->onload);
 		$this->Template->class = trim($objLayout->cssClass . ' ' . $objPage->cssClass);
 
+		// Additional meta tags
+		$this->Template->metaTags = array_combine(
+			array_map(StringUtil::specialcharsAttribute(...), array_keys($headBag->getMetaTags())),
+			array_map(StringUtil::specialcharsAttribute(...), array_values($headBag->getMetaTags()))
+		);
+
 		// Execute AFTER the modules have been generated and create footer scripts first
 		$this->createFooterScripts($objPage, $objLayout);
 		$this->createHeaderScripts($objPage, $objLayout);
