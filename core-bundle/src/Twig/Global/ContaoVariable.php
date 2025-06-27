f969a5b1245127f45c0572d5c7dd1781aa585b8f
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-06-27 12:41:17 +0300

    Add the Contao date formats to the Twig global (see #8472)
    Description
    -----------
    
    This makes the Contao date formats (`datimFormat`, `dateFormat`, `timeFormat`) available in Twig templates via the `contao` Twig global. This is needed for https://github.com/contao/contao/pull/8011, https://github.com/contao/contao/pull/8020 and #8066 - e.g.
    
    ```twig
    {{ app.user.lastLogin|date(contao.datim_format) }}
    ```
    
    _Note:_ I noticed that we currently use two different styles for the function names within `ContaoVariable`:
    
    * `getFoo_bar` (e.g. `getHas_backend_user`)
    * `foo_bar` (e.g. `backend_user`)
    
    Should we normalise this?
    
    Commits
    -------
    
    a2241e97 Add Contao date formats to the Twig global
    bcf47ca1 correctly inject framework
    8ea33a2f fix tests
    87fd5ac7 Initialize Contao Framework and add fallbacks
    ccf67132 update tests

--- core-bundle/src/Twig/Global/ContaoVariable.php
+++ core-bundle/src/Twig/Global/ContaoVariable.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Twig\Global;
 
 use Contao\BackendUser;
+use Contao\Config;
 use Contao\CoreBundle\Csrf\ContaoCsrfTokenManager;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Security\Authentication\Token\TokenChecker;
 use Contao\PageModel;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -18,6 +20,7 @@ class ContaoVariable
         private readonly TokenChecker $tokenChecker,
         private readonly ContaoCsrfTokenManager $tokenManager,
         private readonly Security $security,
+        private readonly ContaoFramework $framework,
     ) {
     }
 
@@ -45,6 +48,39 @@ class ContaoVariable
     public function getRequest_token(): string
     {
         return $this->tokenManager->getDefaultTokenValue();
+    }
+
+    public function getDatim_format(): string
+    {
+        if ($pageFormat = $this->getPage()?->datimFormat) {
+            return $pageFormat;
+        }
+
+        $this->framework->initialize();
+
+        return $this->framework->getAdapter(Config::class)->get('datimFormat') ?? 'Y-m-d H:i';
+    }
+
+    public function getDate_format(): string
+    {
+        if ($pageFormat = $this->getPage()?->dateFormat) {
+            return $pageFormat;
+        }
+
+        $this->framework->initialize();
+
+        return $this->framework->getAdapter(Config::class)->get('dateFormat') ?? 'Y-m-d';
+    }
+
+    public function getTime_format(): string
+    {
+        if ($pageFormat = $this->getPage()?->timeFormat) {
+            return $pageFormat;
+        }
+
+        $this->framework->initialize();
+
+        return $this->framework->getAdapter(Config::class)->get('timeFormat') ?? 'H:i';
     }
 
     public function backend_user(): BackendUser|null
