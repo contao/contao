31edf2376211ffa407f1daa7e8f4995cae268334
M. Vondano <m-vo@users.noreply.github.com>
2025-07-02 15:11:21 +0200

    use "export const Message" and run linter

--- core-bundle/assets/modules/message.js
+++ core-bundle/assets/modules/message.js
@@ -1,23 +1,23 @@
-export class Message {
+export const Message = {
     /**
      * Display an info message.
      *
      * @param message Text or HTML that will be put into the message body.
      */
-    static info(message) {
+    info(message) {
         Message._addMessage(message, 'info');
-    }
+    },
 
     /**
      * Display an error message.
      *
      * @param message Text or HTML that will be put into the message body.
      */
-    static error(message) {
+    error(message) {
         Message._addMessage(message, 'error');
-    }
+    },
 
-    static _addMessage(message, type) {
+    _addMessage(message, type) {
         const event = new CustomEvent('contao--message', {
             detail: {
                 type,
@@ -26,5 +26,5 @@ export class Message {
         });
 
         document.dispatchEvent(event);
-    }
-}
+    },
+};
