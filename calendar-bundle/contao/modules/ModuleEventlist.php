b0bcbd92268d60a35e72517949ddfa90035daa43
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-07-20 13:42:44 +0100

    Add group index and count to events

--- calendar-bundle/contao/modules/ModuleEventlist.php
+++ calendar-bundle/contao/modules/ModuleEventlist.php
@@ -41,6 +41,11 @@ class ModuleEventlist extends Events
 	protected $strTemplate = 'mod_eventlist';
 
 	/**
+	 * @var array<int, int>
+	 */
+	private static $groupCounts = array();
+
+	/**
 	 * Display a wildcard in the back end
 	 *
 	 * @return string
@@ -308,6 +313,11 @@ class ModuleEventlist extends Events
 		// Preload all images in one query, so they are loaded into the model registry
 		FilesModel::findMultipleByUuids($uuids);
 
+		// Track group index and group count (#6402)
+		$groupIndex = -1;
+		$eventIndex = 0;
+		$templates = array();
+
 		// Parse events
 		for ($i=$offset; $i<$limit; $i++)
 		{
@@ -328,7 +338,16 @@ class ModuleEventlist extends Events
 			{
 				$objTemplate->header = true;
 				$strDate = $event['firstDate'];
+				$eventIndex = 0;
+				++$groupIndex;
 			}
+
+			self::$groupCounts[$groupIndex] = (self::$groupCounts[$groupIndex] ?? 0) + 1;
+
+			$objTemplate->groupIndex = $eventIndex++;
+			$objTemplate->groupCount = Template::once(static function () use ($groupIndex): int {
+				return self::$groupCounts[$groupIndex];
+			});
 
 			// Show the teaser text of redirect events (see #6315)
 			if (\is_bool($event['details']) && $event['source'] == 'default')
@@ -421,7 +440,7 @@ class ModuleEventlist extends Events
 				return $jsonLd;
 			};
 
-			$strEvents .= $objTemplate->parse();
+			$templates[] = $objTemplate;
 
 			++$eventCount;
 		}
@@ -434,7 +453,7 @@ class ModuleEventlist extends Events
 
 		// See #3672
 		$this->Template->headline = $this->headline;
-		$this->Template->events = $strEvents;
+		$this->Template->events = implode('', array_map(static fn (FrontendTemplate $template): string => $template->parse(), $templates));
 		$this->Template->eventCount = $eventCount;
 
 		// Clear the $_GET array (see #2445)
