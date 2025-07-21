a54ad4e324e710884c3e8e5fb026ae352df9a750
fritzmg <fmg@inspiredminds.at>
2025-07-21 12:15:51 +0100

    Add file icons to the back end preview view

--- core-bundle/tests/Twig/Runtime/BackendHelperRuntimeTest.php
+++ core-bundle/tests/Twig/Runtime/BackendHelperRuntimeTest.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Tests\Twig\Runtime;
 
+use Contao\CoreBundle\Filesystem\FilesystemItem;
 use Contao\CoreBundle\String\HtmlAttributes;
 use Contao\CoreBundle\Tests\TestCase;
 use Contao\CoreBundle\Twig\Runtime\BackendHelperRuntime;
@@ -19,7 +20,7 @@ use Contao\Image;
 
 class BackendHelperRuntimeTest extends TestCase
 {
-    public function testDelegatesCalls(): void
+    public function testDelegatesCallsForIcon(): void
     {
         $imageAdapter = $this->mockAdapter(['getHtml']);
         $imageAdapter
@@ -33,6 +34,75 @@ class BackendHelperRuntimeTest extends TestCase
 
         $this->assertSame('icon HTML', (new BackendHelperRuntime($framework))->icon(
             'icon.svg', 'alt', (new HtmlAttributes())->set('foo', 'bar'),
+        ));
+    }
+
+    public function testReturnsDefaultIconForNoMimeType(): void
+    {
+        $imageAdapter = $this->mockAdapter(['getHtml']);
+        $imageAdapter
+            ->expects($this->once())
+            ->method('getHtml')
+            ->with('regular.svg', 'alt', 'foo="bar"')
+            ->willReturn('icon HTML')
+        ;
+
+        $framework = $this->mockContaoFramework([Image::class => $imageAdapter]);
+
+        $this->assertSame('icon HTML', (new BackendHelperRuntime($framework))->file_icon(
+            $this->createMock(FilesystemItem::class), 'alt', (new HtmlAttributes())->set('foo', 'bar'),
+        ));
+    }
+
+    public function testReturnsIconForMimeType(): void
+    {
+        $imageAdapter = $this->mockAdapter(['getHtml']);
+        $imageAdapter
+            ->expects($this->once())
+            ->method('getHtml')
+            ->with('image.svg', 'alt', 'foo="bar"')
+            ->willReturn('icon HTML')
+        ;
+
+        $framework = $this->mockContaoFramework([Image::class => $imageAdapter]);
+
+        $fileitem = $this->createMock(FilesystemItem::class);
+        $fileitem
+            ->expects($this->once())
+            ->method('getMimeType')
+            ->willReturn('image/jpeg')
+        ;
+
+        $GLOBALS['TL_MIME'] = ['jpg' => ['image/jpeg', 'image.svg']];
+
+        $this->assertSame('icon HTML', (new BackendHelperRuntime($framework))->file_icon(
+            $fileitem, 'alt', (new HtmlAttributes())->set('foo', 'bar'),
+        ));
+
+        unset($GLOBALS['TL_MIME']);
+    }
+
+    public function testReturnsDefaultIconForMissingRegisteredMimeType(): void
+    {
+        $imageAdapter = $this->mockAdapter(['getHtml']);
+        $imageAdapter
+            ->expects($this->once())
+            ->method('getHtml')
+            ->with('regular.svg', 'alt', 'foo="bar"')
+            ->willReturn('icon HTML')
+        ;
+
+        $framework = $this->mockContaoFramework([Image::class => $imageAdapter]);
+
+        $fileitem = $this->createMock(FilesystemItem::class);
+        $fileitem
+            ->expects($this->once())
+            ->method('getMimeType')
+            ->willReturn('image/jpeg')
+        ;
+
+        $this->assertSame('icon HTML', (new BackendHelperRuntime($framework))->file_icon(
+            $fileitem, 'alt', (new HtmlAttributes())->set('foo', 'bar'),
         ));
     }
 }
