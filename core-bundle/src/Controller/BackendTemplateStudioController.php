d3fa623008c6b132e95cf0bbcfd5c5efbd8ae07a
M. Vondano <m-vo@users.noreply.github.com>
2025-09-04 18:12:19 +0200

    show layouts first

--- core-bundle/src/Controller/BackendTemplateStudioController.php
+++ core-bundle/src/Controller/BackendTemplateStudioController.php
@@ -452,7 +452,7 @@ class BackendTemplateStudioController extends AbstractBackendController
         $sortRecursive($prefixTree);
 
         // Apply opinionated ordering
-        return ['content_element' => [], 'frontend_module' => [], 'component' => [], ...$prefixTree];
+        return ['layout' => [], 'content_element' => [], 'frontend_module' => [], 'component' => [], ...$prefixTree];
     }
 
     /**
