0a3adb8dcfc10c5b178d1d8520796f3a1f9170f0
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-09-08 23:58:28 +0200

    Backport the class check fix

--- core-bundle/contao/pages/PageRegular.php
+++ core-bundle/contao/pages/PageRegular.php
@@ -295,7 +295,7 @@ class PageRegular extends Frontend
 			/** @var class-string<Module> $strClass */
 			$strClass = Module::findClass($strType);
 
-			if (!$strClass::shouldPreload($strType, $objPage, $request))
+			if (!$strClass || !$strClass::shouldPreload($strType, $objPage, $request))
 			{
 				continue;
 			}
