b4b839c669c9583641a41fddd8d5f03e6c91ad66
David Greminger <david.greminger@1up.io>
2025-09-10 11:12:09 +0200

    Wip

--- core-bundle/src/Controller/ContentElement/LostPasswordController.php
+++ core-bundle/src/Controller/ContentElement/LostPasswordController.php
@@ -13,16 +13,52 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Controller\ContentElement;
 
 use Contao\ContentModel;
+use Contao\Controller;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsContentElement(category: 'miscellaneous')]
 class LostPasswordController extends AbstractContentElementController
 {
+    public function __construct(
+        private readonly ContaoFramework $framework,
+    ){
+
+    }
+
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
+        $this->framework->initialize();
+
+        $this->executeOnloadCallbacks();
+
+        // Set new password
+        if (str_starts_with($request->get('token', ''), 'pw-'))
+        {
+            $this->setNewPassword();
+
+            return $template->getResponse();
+        }
+
         return $template->getResponse();
+    }
+
+    private function executeOnloadCallbacks(): void
+    {
+        $this->framework->getAdapter(Controller::class)->loadDataContainer('tl_member');
+
+        if (\is_array($GLOBALS['TL_DCA']['tl_member']['config']['onload_callback'] ?? null)) {
+            foreach ($GLOBALS['TL_DCA']['tl_member']['config']['onload_callback'] as $callback) {
+                if (\is_array($callback)) {
+                    $this->framework->getAdapter(System::class)->importStatic($callback[0])->{$callback[1]}();
+                } elseif (\is_callable($callback)) {
+                    $callback();
+                }
+            }
+        }
     }
 }
