7980e0a0ffa0b9913e1e36018d618a0b8607821e
David Greminger <david.greminger@1up.io>
2025-10-01 21:00:07 +0200

    Don't test double

--- core-bundle/tests/Form/Type/ChangePasswordTypeTest.php
+++ core-bundle/tests/Form/Type/ChangePasswordTypeTest.php
@@ -12,42 +12,12 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Tests\Form\Type;
 
-use Contao\CoreBundle\Csrf\ContaoCsrfTokenManager;
 use Contao\CoreBundle\Form\Type\ChangePasswordType;
 use Contao\CoreBundle\Tests\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Form\FormBuilder;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ChangePasswordTypeTest extends TestCase
 {
-    public function testConfiguresOptions(): void
-    {
-        $formType = new ChangePasswordType();
-
-        $container = new ContainerBuilder();
-        $container->set('contao.csrf.token_manager', $this->createMock(ContaoCsrfTokenManager::class));
-        $container->set('parameter_bag', $this->createMock(ParameterBagInterface::class));
-
-        $optionsResolver = $this->createMock(OptionsResolver::class);
-        $optionsResolver
-            ->expects($this->once())
-            ->method('setDefaults')
-        ;
-
-        $formType->setContainer($container);
-        $formType->configureOptions($optionsResolver);
-
-        $this->assertSame(
-            [
-                'contao.csrf.token_manager' => '?'.ContaoCsrfTokenManager::class,
-                'parameter_bag' => '?'.ParameterBagInterface::class,
-            ],
-            $formType::getSubscribedServices(),
-        );
-    }
-
     public function testBuildForm(): void
     {
         $formType = new ChangePasswordType();
