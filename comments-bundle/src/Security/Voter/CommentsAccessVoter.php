7a2f5855cb44adce5ec4be807f9473bcd3ee5758
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-10-30 10:40:25 +0100

    Update and run the tools

--- comments-bundle/src/Security/Voter/CommentsAccessVoter.php
+++ comments-bundle/src/Security/Voter/CommentsAccessVoter.php
@@ -52,7 +52,7 @@ class CommentsAccessVoter implements VoterInterface, CacheableVoterInterface
 
             $isGranted = match (true) {
                 $subject instanceof CreateAction => $this->accessDecisionManager->decide($token, [ContaoCommentsPermissions::USER_CAN_ACCESS_COMMENT], $subject->getNew()),
-                $subject instanceof UpdateAction => $this->accessDecisionManager->decide($token, [ContaoCommentsPermissions::USER_CAN_ACCESS_COMMENT], array_merge($subject->getCurrent() ?? [], $subject->getNew() ?? [])),
+                $subject instanceof UpdateAction => $this->accessDecisionManager->decide($token, [ContaoCommentsPermissions::USER_CAN_ACCESS_COMMENT], [...$subject->getCurrent() ?? [], ...$subject->getNew() ?? []]),
                 $subject instanceof DeleteAction => $this->accessDecisionManager->decide($token, [ContaoCommentsPermissions::USER_CAN_ACCESS_COMMENT], $subject->getCurrent()),
                 default => null,
             };
