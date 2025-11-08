b144d1460a091b7ac3d8a482217076ba1b88aca6
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-11-08 14:38:49 +0000

    Re-add maxPaginationLinks setting value

--- core-bundle/contao/config/default.php
+++ core-bundle/contao/config/default.php
@@ -101,3 +101,4 @@ $GLOBALS['TL_CONFIG']['backendTheme']         = 'flexible';
 $GLOBALS['TL_CONFIG']['minPasswordLength']    = 8;
 $GLOBALS['TL_CONFIG']['defaultFileChmod']     = 0644;
 $GLOBALS['TL_CONFIG']['defaultFolderChmod']   = 0755;
+$GLOBALS['TL_CONFIG']['maxPaginationLinks']   = 7;
