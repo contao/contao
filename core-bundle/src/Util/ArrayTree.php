ab369c5702d33255055a0238a7306fb7fd639fb4
M. Vondano <m-vo@users.noreply.github.com>
2025-11-09 13:06:27 +0100

    add a utility to iteratively create tree an array tree structure

---
+++ core-bundle/src/Util/ArrayTree.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Contao\CoreBundle\Util;
+
+/**
+ * @internal
+ */
+final class ArrayTree implements \IteratorAggregate
+{
+    private array $rootNode = [];
+
+    private array $currentNode;
+
+    private array $parentNodes = [];
+
+    public function __construct()
+    {
+        $this->currentNode = &$this->rootNode;
+    }
+
+    public function addContentNode(mixed $content): void
+    {
+        $this->currentNode[] = $content;
+    }
+
+    public function enterChildNode(string|null $key = null): void
+    {
+        $this->parentNodes[] = &$this->currentNode;
+
+        if (\array_key_exists($key, $this->currentNode)) {
+            $childNode = &$this->currentNode[$key];
+        } else {
+            $childNode = [];
+        }
+
+        if (null !== $key) {
+            $this->currentNode[$key] = &$childNode;
+        } else {
+            $this->currentNode[] = &$childNode;
+        }
+
+        $this->currentNode = &$childNode;
+    }
+
+    public function up(): void
+    {
+        if ([] === $this->parentNodes) {
+            throw new \OutOfBoundsException('Cannot go up - already at root level.');
+        }
+
+        $this->currentNode = &$this->parentNodes[array_key_last($this->parentNodes)];
+
+        array_pop($this->parentNodes);
+    }
+
+    public function &current(): self
+    {
+        $subTree = new self();
+        $subTree->rootNode = &$this->currentNode;
+
+        return $subTree;
+    }
+
+    public function toArray(): array
+    {
+        return $this->rootNode;
+    }
+
+    public function getIterator(): \Traversable
+    {
+        return new \ArrayIterator($this->rootNode);
+    }
+}
---
+++ core-bundle/tests/Util/ArrayTreeTest.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Util;
+
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\CoreBundle\Util\ArrayTree;
+
+class ArrayTreeTest extends TestCase
+{
+    public function testSetAndGetTree(): void
+    {
+        $tree = new ArrayTree();
+
+        $tree->addContentNode('A');
+        $tree->enterChildNode('B');
+        $tree->addContentNode('B1');
+        $tree->addContentNode('B2');
+        $tree->up();
+        $tree->enterChildNode();
+        $tree->enterChildNode();
+        $tree->addContentNode('C');
+        $tree->up();
+        $tree->up();
+        $tree->addContentNode('D');
+
+        $this->assertSame(
+            [
+                0 => 'A',
+                'B' => [
+                    'B1',
+                    'B2',
+                ],
+                1 => [
+                    0 => [
+                        'C',
+                    ],
+                ],
+                2 => 'D',
+            ],
+            $tree->toArray(),
+            'return as array',
+        );
+
+        $dumpTreeAsString = static function (iterable $node) use (&$dumpTreeAsString): string {
+            $return = '';
+
+            foreach ($node as $key => $nodeOrValue) {
+                if (is_iterable($nodeOrValue)) {
+                    $return .= \sprintf(',%s:{%s}', $key, $dumpTreeAsString($nodeOrValue));
+                } else {
+                    $return .= \sprintf(',%s', $nodeOrValue);
+                }
+            }
+
+            return substr($return, 1);
+        };
+
+        $this->assertSame(
+            'A,B:{B1,B2},1:{0:{C}},D',
+            $dumpTreeAsString($tree),
+            'use as iterable',
+        );
+    }
+
+    public function testReturnsReferenceToSubTree(): void
+    {
+        $tree = new ArrayTree();
+
+        $tree->enterChildNode('foo');
+        $tree->addContentNode(1);
+
+        $reference = $tree->current();
+
+        $tree->enterChildNode('bar');
+        $tree->addContentNode(2);
+        $tree->up();
+        $tree->addContentNode(3);
+        $tree->up();
+        $tree->addContentNode(4);
+
+        $this->assertSame(
+            [
+                'foo' => [
+                    1,
+                    'bar' => [2],
+                    3,
+                ],
+                4,
+            ],
+            $tree->toArray(),
+            'full tree',
+        );
+
+        $this->assertSame(
+            [
+                1,
+                'bar' => [2],
+                3,
+            ],
+            $reference->toArray(),
+            'sub tree',
+        );
+    }
+
+    public function testAllowsEnteringAChildNodeMultipleTimes(): void
+    {
+        $tree = new ArrayTree();
+
+        $tree->enterChildNode('foo');
+        $tree->addContentNode(1);
+
+        $tree->up();
+
+        $tree->enterChildNode('foo');
+        $tree->addContentNode(2);
+
+        $this->assertSame(['foo' => [1, 2]], $tree->toArray());
+    }
+
+    public function testThrowsExceptionIfTryingToExceedTheRootLevel(): void
+    {
+        $tree = new ArrayTree();
+
+        $tree->enterChildNode();
+        $tree->up();
+
+        $this->expectException(\OutOfBoundsException::class);
+        $this->expectExceptionMessage('Cannot go up - already at root level.');
+
+        $tree->up();
+    }
+}
