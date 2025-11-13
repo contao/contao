9eafa37bcdbfb13417538d822047981e58deaaf8
Martin Auswöger <martin@auswoeger.com>
2025-11-13 19:39:56 +0100

    Backend diff template

--- core-bundle/contao/classes/Versions.php
+++ core-bundle/contao/classes/Versions.php
@@ -613,6 +613,7 @@ class Versions extends Controller
 		$objTemplate->theme = Backend::getTheme();
 		$objTemplate->language = $GLOBALS['TL_LANGUAGE'];
 		$objTemplate->title = StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['showDifferences']);
+		$objTemplate->host = Backend::getDecodedHostname();
 		$objTemplate->charset = System::getContainer()->getParameter('kernel.charset');
 
 		throw new ResponseException($objTemplate->getResponse());
