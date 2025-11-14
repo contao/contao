c9a06588757b9aaa1d5159219594e53776c3767e
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-11-14 11:31:53 +0100

    Simplify the controller

--- core-bundle/assets/controllers/sticky-observer-controller.js
+++ core-bundle/assets/controllers/sticky-observer-controller.js
@@ -2,19 +2,16 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        this.observer = new IntersectionObserver(this.#handleIntersect.bind(this), this.observerOptions);
+        this.observer = new IntersectionObserver(this.#handleIntersect.bind(this), {
+            threshold: 1.0,
+            rootMargin: '0px 0px -1px 0px',
+        });
+
         this.observer.observe(this.element);
     }
 
     disconnect() {
         this.observer.disconnect();
-    }
-
-    get observerOptions() {
-        return {
-            threshold: 1.0,
-            rootMargin: '0px 0px -1px 0px',
-        };
     }
 
     #handleIntersect(entries) {
