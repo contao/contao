4a0474d78f357073881980b3e55cb0489cf96856
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-12-05 12:27:26 +0100

    Support downloading multiple job attachments (see #9016)
    Description
    -----------
    
    That fixes another todo for 5.7. Previously, only one download was supported. This is now fixed:
    
    <img width="438" height="214" alt="Bildschirmfoto 2025-11-19 um 11 38 54" src="https://github.com/user-attachments/assets/39cce8c3-5ca9-4bdc-985f-dec6a96b7245" />
    
    Please, feel free to adjust icons etc. directly if you think a different one would be more appropriate 😊
    
    Commits
    -------
    
    e8dc41f0 Support downloading multiple job attachments
    e596c635 Change icon and link download directly if there's just one attachment
    2d800208 Switch back the icon
    c9e8775c Configure the alt text to "View attachments" for multiple attachments
    d8f507f4 Fix translation
    e16d47d3 Move the "download attachment" label to the global language file
    
    Co-authored-by: leofeyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/src/EventListener/DataContainer/JobsListener.php
+++ core-bundle/src/EventListener/DataContainer/JobsListener.php
@@ -67,21 +67,45 @@ class JobsListener
         $attachments = $this->jobs->getAttachments($job);
         $numberOfAttachments = \count($attachments);
 
+        // Hide the operation if there are no attachments
         if (0 === $numberOfAttachments) {
             $operation->hide();
 
             return;
         }
 
-        // TODO: we need a template and Stimulus logic to have an operation with sub
-        // operations just like the [...] in the current context menu to be able to
-        // display more than just one download
-        $attachment = $attachments[0];
+        // Link directly to the one attachment, if there is only one
+        if (1 === $numberOfAttachments) {
+            $operation['icon'] = 'theme_import.svg';
+            $operation['title'] = $attachments[0]->getFileLabel();
+            $operation->setUrl($attachments[0]->getDownloadUrl());
 
-        $operation['icon'] = 'theme_import.svg';
-        $operation['title'] = $attachment->getFileLabel();
+            return;
+        }
 
-        $operation->setUrl($attachment->getDownloadUrl());
+        // Otherwise, we build a button with submenu
+        $operations = [];
+
+        foreach ($attachments as $attachment) {
+            $operations[] = new DataContainerOperation(
+                'download',
+                [
+                    'icon' => 'theme_import.svg',
+                    'label' => $attachment->getFileLabel(),
+                    'href' => $attachment->getDownloadUrl(),
+                ],
+                null,
+                $operation->getDataContainer(),
+            );
+        }
+
+        $operation->setHtml($this->twig->render('@Contao/backend/data_container/operations.html.twig', [
+            'operations' => $operations,
+            'has_primary' => true,
+            'more_icon' => 'theme_import.svg',
+            'more_alt' => 'MSC.viewAttachments',
+            'globalOperations' => false,
+        ]));
     }
 
     #[AsCallback(table: 'tl_job', target: 'list.operations.children.button')]
