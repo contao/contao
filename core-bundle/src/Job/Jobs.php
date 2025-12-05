e16d47d36fff8e7b1dd0ba28ec631675b2a02636
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-12-05 12:07:10 +0100

    Move the "download attachment" label to the global language file

--- core-bundle/src/Job/Jobs.php
+++ core-bundle/src/Job/Jobs.php
@@ -264,7 +264,7 @@ class Jobs
     {
         return new Attachment(
             $filesystemItem,
-            new TranslatableMessage('jobs.download_attachment', [$filesystemItem->getName()], 'contao_jobs'),
+            new TranslatableMessage('MSC.downloadAttachment', [$filesystemItem->getName()], 'contao_default'),
             $this->router->generate(
                 '_contao_jobs.download',
                 ['jobUuid' => $job->getUuid(), 'identifier' => $filesystemItem->getName()],
