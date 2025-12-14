d38210a74f6a5ae62ff2d02162972b1a9e78bf79
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-14 23:45:01 +0100

    Rebuild the assets

--- core-bundle/assets/controllers/scroll-offset-controller.js
+++ core-bundle/assets/controllers/scroll-offset-controller.js
@@ -131,7 +131,7 @@ export default class extends Controller {
         this.#autoFocus = false;
     }
 
-    widgetErrorTargetConnected() {x
+    widgetErrorTargetConnected() {
         this.widgetErrorTarget.scrollIntoView({
             behavior: 'smooth',
             block: 'start',
