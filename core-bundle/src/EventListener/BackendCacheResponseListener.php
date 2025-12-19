10679f97c7bbd040f107b06b98f459c804a4707b
fritzmg <fmg@inspiredminds.at>
2025-12-19 11:19:57 +0000

    Merge existing Vary header

--- core-bundle/src/EventListener/BackendCacheResponseListener.php
+++ core-bundle/src/EventListener/BackendCacheResponseListener.php
@@ -36,7 +36,7 @@ class BackendCacheResponseListener
         $response = $event->getResponse();
 
         // Vary on Accept and Turbo-Frame (#9128)
-        $response->headers->set('Vary', 'Accept, Turbo-Frame');
+        $response->setVary(array_unique([...$response->getVary(), 'Accept', 'Turbo-Frame']));
 
         if ($request->headers->has('x-turbo-request-id') && $request->isMethodCacheable() && Response::HTTP_OK === $response->getStatusCode()) {
             $response->headers->set('Cache-Control', 'private, max-age='.$this->turboMaxAge.', must-revalidate');
