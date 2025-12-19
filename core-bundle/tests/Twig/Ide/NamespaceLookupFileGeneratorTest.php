50b94ed3db29e33cde7bc8fb4d0add0a2775c9e7
M. Vondano <m-vo@users.noreply.github.com>
2025-12-19 17:01:40 +0100

    Improve how the `ide-twig.json` file is generated (see #8200)
    Description
    -----------
    
    Implements #8051
    
    This PR improves the way the `ide-twig.json` IDE helper file is generated. Currently we are only creating the file during cache warmup in the dev environment in an *optional* cache warmer. This is unfortunate because you typically do not explicitly warm the dev cache. We also dumped the file to `var/cache` which is semantically wrong.
    
    Now the file gets dumped to `var/contao-twig` in a *mandatory* cache warmer (still only in dev). In order to use it you can un-exclude the above directory in your IDE.
    
    I also added a new command `contao:dump-twig-ide-file` that dumps the file to a given location in your project dir. This way it will be included by default and can be checked in to version control. In this case, you own the file, so in order to update it, you must manually re-run the command.
    
    /cc @bytehead
    
    Commits
    -------
    
    3ec5d53c move ide namespace lookup file generation into a service and its own …
    fa20d008 add a command to manually dump the ide namespace lookup file
    927ead03 write to ""%kernel.build_dir%/contao-ide" by default
    25937e0c also pass build dir, when manually warming up in the Automator
    8aba7bbe fix default value
    5034ccd6 make abbreviation in class names camel case
    586d2079 normalize console output when testing strings
    be6140ba Also rename the test namespace
    
    Co-authored-by: leofeyer <1192057+leofeyer@users.noreply.github.com>

---
+++ core-bundle/tests/Twig/Ide/NamespaceLookupFileGeneratorTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\Twig\Ide;
+
+use Contao\CoreBundle\Tests\TestCase;
+use Contao\CoreBundle\Twig\Ide\NamespaceLookupFileGenerator;
+use Contao\CoreBundle\Twig\Loader\ContaoFilesystemLoader;
+use Symfony\Component\Filesystem\Filesystem;
+
+class NamespaceLookupFileGeneratorTest extends TestCase
+{
+    public function testWritesFile(): void
+    {
+        $loader = $this->createStub(ContaoFilesystemLoader::class);
+        $loader
+            ->method('getInheritanceChains')
+            ->willReturn([
+                'a' => [
+                    '/project/templates/a.html.twig' => '@Contao_Global/a.html.twig',
+                    '/project/contao/templates/a.html.twig' => '@Contao_App/a.html.twig',
+                ],
+                'b' => [
+                    '/project/templates/b.html.twig' => '@Contao_Global/b.html.twig',
+                ],
+                'foo/c' => [
+                    '/project/templates/foo/c.html.twig' => '@Contao_Global/foo/c.html.twig',
+                ],
+                'bar/d' => [
+                    '/project/vendor/demo/bar/d.html.twig' => '@Contao_DemoBundle/bar/d.html.twig',
+                ],
+            ])
+        ;
+
+        $filesystem = $this->createMock(Filesystem::class);
+        $filesystem
+            ->expects($this->once())
+            ->method('dumpFile')
+            ->with(
+                '/project/foo/bar/ide-twig.json',
+                $this->callback(
+                    function (string $json): bool {
+                        $expectedData = [
+                            'namespaces' => [
+                                ['namespace' => 'Contao', 'path' => '../../templates'],
+                                ['namespace' => 'Contao_Global', 'path' => '../../templates'],
+                                ['namespace' => 'Contao', 'path' => '../../contao/templates'],
+                                ['namespace' => 'Contao_App', 'path' => '../../contao/templates'],
+                                ['namespace' => 'Contao', 'path' => '../../vendor/demo'],
+                                ['namespace' => 'Contao_DemoBundle', 'path' => '../../vendor/demo'],
+                            ],
+                        ];
+
+                        $this->assertJson($json);
+                        $this->assertSame($expectedData, json_decode($json, true, 512, JSON_THROW_ON_ERROR));
+
+                        return true;
+                    },
+                ),
+            )
+        ;
+
+        $namespaceLookupFileGenerator = new NamespaceLookupFileGenerator($loader, $filesystem);
+        $namespaceLookupFileGenerator->write('/project/foo/bar');
+    }
+}
