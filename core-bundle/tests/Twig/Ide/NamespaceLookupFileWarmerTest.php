be6140baf7a30fa2404b7811f7403157df84f218
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2025-12-19 16:58:21 +0100

    Also rename the test namespace

--- core-bundle/tests/Twig/Ide/NamespaceLookupFileWarmerTest.php
+++ core-bundle/tests/Twig/Ide/NamespaceLookupFileWarmerTest.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-namespace Contao\CoreBundle\Tests\Twig\IDE;
+namespace Contao\CoreBundle\Tests\Twig\Ide;
 
 use Contao\CoreBundle\Tests\TestCase;
 use Contao\CoreBundle\Twig\Ide\NamespaceLookupFileGenerator;
