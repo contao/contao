9fbd075f24a7aea3984a5c5130591f6dbee84edc
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-24 00:55:35 +0100

    Lint

--- core-bundle/assets/controllers/color-scheme-controller.js
+++ core-bundle/assets/controllers/color-scheme-controller.js
@@ -1,4 +1,4 @@
-import {Controller} from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus';
 
 const prefersDark = () => {
     const prefersDark = localStorage.getItem('contao--prefers-dark');
