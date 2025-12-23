264a5a3350b71ac89179b886dab2e06016f516c3
M. Vondano <m-vo@users.noreply.github.com>
2025-12-23 11:38:33 +0100

    Update core-bundle/src/Controller/BackendCsvImportController.php
    Co-authored-by: Leo Feyer <1192057+leofeyer@users.noreply.github.com>

--- core-bundle/src/Controller/BackendCsvImportController.php
+++ core-bundle/src/Controller/BackendCsvImportController.php
@@ -14,7 +14,7 @@ namespace Contao\CoreBundle\Controller;
 
 use Contao\CoreBundle\Controller\Backend\CsvImportController as BaseController;
 
-trigger_deprecation('contao/core-bundle', '5.7', 'Using "Contao\CoreBundle\Controller\CsvImportController" is deprecated and will no longer work in Contao 6. Use "Contao\CoreBundle\Controller\Backend\CsvImportController" instead.');
+trigger_deprecation('contao/core-bundle', '5.7', 'Using "Contao\CoreBundle\Controller\BackendCsvImportController" is deprecated and will no longer work in Contao 6. Use "Contao\CoreBundle\Controller\Backend\CsvImportController" instead.');
 
 /**
  * @deprecated Deprecated since Contao 5.7, to be removed in Contao 6;
