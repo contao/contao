5be43b33151873c60f1590180b3c4764642d75f2
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-12-24 00:50:18 +0100

    Remove the timeout on changing the label

--- core-bundle/assets/controllers/color-scheme-controller.js
+++ core-bundle/assets/controllers/color-scheme-controller.js
@@ -1,4 +1,4 @@
-import { Controller } from '@hotwired/stimulus';
+import {Controller} from '@hotwired/stimulus';
 
 const prefersDark = () => {
     const prefersDark = localStorage.getItem('contao--prefers-dark');
@@ -53,15 +53,13 @@ export default class extends Controller {
         }
 
         setColorScheme();
+        this.setLabel();
 
         this.dispatch('change', {
             detail: {
                 mode: isDark ? 'dark' : 'light',
             },
         });
-
-        // Change the label after the dropdown is hidden
-        setTimeout(this.setLabel, 300);
     }
 
     setLabel() {
