7c3aa48265eaccc5960b497434c534a7ef319db8
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-01-01 16:02:47 +0100

    Add dynamic scroll buttons to the jump-targets-navigation

--- core-bundle/assets/controllers/jump-targets-controller.js
+++ core-bundle/assets/controllers/jump-targets-controller.js
@@ -1,7 +1,34 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    #btn = null;
+    #prevButton = null;
+    #nextButton = null;
+    #links = null;
+    #onScroll = () => this.#updateScrollButtonVisibility();
+    #onResize = () => this.#updateScrollButtonVisibility();
+
     static targets = ['navigation', 'section'];
+
+    static values = {
+        prevLabel: String,
+        nextLabel: String,
+        scrollBy: Number,
+    };
+
+    initialize() {
+        this.#initButtonElement();
+    }
+
+    navigationTargetConnected(element) {
+        element.addEventListener('scroll', this.#onScroll);
+        window.addEventListener('resize', this.#onResize);
+    }
+
+    navigationTargetDisconnected(element) {
+        element.removeEventListener('scroll', this.#onScroll);
+        this.#links.destroy();
+    }
 
     sectionTargetConnected() {
         this.rebuildNavigation();
@@ -16,14 +43,18 @@ export default class extends Controller {
             return;
         }
 
-        const links = document.createElement('ul');
+        this.#links = document.createElement('ul');
+
+        // BC (having implemented the structure without the values)
+        if (this.hasScrollByValue) {
+            this.#links.append(this.#prevButton ?? this.#createScrollButton());
+        }
 
         for (const el of this.sectionTargets) {
-            const action = document.createElement('button');
+            const action = this.#btn.cloneNode();
             action.innerText = el.getAttribute(`data-${this.identifier}-label-value`);
 
-            action.addEventListener('click', (event) => {
-                event.preventDefault();
+            action.addEventListener('click', () => {
                 this.dispatch('scrollto', { target: el });
                 el.scrollIntoView();
             });
@@ -31,9 +62,58 @@ export default class extends Controller {
             const li = document.createElement('li');
             li.append(action);
 
-            links.append(li);
+            this.#links.append(li);
         }
 
-        this.navigationTarget.replaceChildren(links);
+        if (this.hasScrollByValue) {
+            this.#links.append(this.#nextButton ?? this.#createScrollButton(false));
+        }
+
+        this.navigationTarget.replaceChildren(this.#links);
+
+        this.#updateScrollButtonVisibility();
+    }
+
+    #updateScrollButtonVisibility() {
+        if (!this.hasNavigationTarget) {
+            return;
+        }
+
+        const el = this.navigationTarget;
+        const maxScroll = el.scrollWidth - el.clientWidth;
+
+        if (this.#prevButton) {
+            this.#prevButton.classList.toggle('is-visible', el.scrollLeft > 0);
+        }
+
+        if (this.#nextButton) {
+            this.#nextButton.classList.toggle('is-visible', el.scrollLeft < maxScroll);
+        }
+    }
+
+    #createScrollButton(start = true) {
+        const li = document.createElement('li');
+        li.classList.add('jump-target-scroll');
+
+        const btn = this.#btn.cloneNode();
+        btn.textContent = start ? this.nextLabelValue : this.prevLabelValue;
+
+        btn.addEventListener('click', () => {
+            this.navigationTarget.scrollBy({
+                behavior: 'smooth',
+                left: this.scrollByValue * (start ? -1 : 1),
+            });
+        });
+
+        li.append(btn);
+
+        start ? (this.#prevButton = li) : (this.#nextButton = li);
+
+        return li;
+    }
+
+    #initButtonElement() {
+        this.#btn = document.createElement('button');
+        this.#btn.type = 'button';
     }
 }
