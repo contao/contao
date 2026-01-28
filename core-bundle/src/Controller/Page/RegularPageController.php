50a1c24772f06766d61c6245590e47d075c448ac
M. Vondano <m-vo@users.noreply.github.com>
2026-01-28 15:45:53 +0100

    use argument resolved page
    Signed-off-by: M. Vondano <m-vo@users.noreply.github.com>

--- core-bundle/src/Controller/Page/RegularPageController.php
+++ core-bundle/src/Controller/Page/RegularPageController.php
@@ -15,16 +15,15 @@ namespace Contao\CoreBundle\Controller\Page;
 use Contao\CoreBundle\ContentComposition\ContentComposition;
 use Contao\CoreBundle\Controller\AbstractController;
 use Contao\CoreBundle\EventListener\SubrequestCacheSubscriber;
-use Contao\CoreBundle\Routing\PageFinder;
 use Contao\CoreBundle\Routing\ResponseContext\CoreResponseContextFactory;
 use Contao\CoreBundle\Routing\ResponseContext\ResponseContextAccessor;
 use Contao\CoreBundle\Twig\Renderer\RendererInterface;
+use Contao\PageModel;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegularPageController extends AbstractController
 {
     public function __construct(
-        private readonly PageFinder $pageFinder,
         private readonly ContentComposition $contentComposition,
         private readonly CoreResponseContextFactory $responseContextFactory,
         private readonly ResponseContextAccessor $responseContextAccessor,
@@ -32,12 +31,8 @@ class RegularPageController extends AbstractController
     ) {
     }
 
-    public function __invoke(): Response
+    public function __invoke(PageModel $page): Response
     {
-        if (!$page = $this->pageFinder->getCurrentPage()) {
-            throw $this->createNotFoundException();
-        }
-
         $responseContext = $this->responseContextFactory->createContaoWebpageResponseContext($page);
 
         $layoutTemplate = $this->contentComposition
