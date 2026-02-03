a6cb190b9d950a7257aa1846d98cf2c3a6eddda0
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-02-03 10:02:44 +0100

    Replace insert tags in the layout/page cssClass

--- core-bundle/contao/pages/PageRegular.php
+++ core-bundle/contao/pages/PageRegular.php
@@ -267,13 +267,17 @@ class PageRegular extends Frontend
 			$objLayout->titleTag = '{{page::pageTitle}} - {{page::rootPageTitle}}';
 		}
 
+		$replaceInsertTags = static function (string $string): string {
+			return System::getContainer()->get('contao.insert_tag.parser')->replaceInline($string);
+		};
+
 		// Assign the title and description
-		$this->Template->title = strip_tags(System::getContainer()->get('contao.insert_tag.parser')->replaceInline($objLayout->titleTag));
+		$this->Template->title = strip_tags($replaceInsertTags($objLayout->titleTag));
 		$this->Template->description = htmlspecialchars($headBag->getMetaDescription() ?? '', ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5);
 
 		// Body onload and body classes
 		$this->Template->onload = trim($objLayout->onload);
-		$this->Template->class = trim($objLayout->cssClass . ' ' . $objPage->cssClass);
+		$this->Template->class = trim($replaceInsertTags($objLayout->cssClass) . ' ' . $replaceInsertTags($objPage->cssClass));
 
 		// Additional meta tags
 		$this->Template->metaTags = $headBag->getMetaTags();
