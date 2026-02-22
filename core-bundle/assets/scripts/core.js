8b0252fa758bb93c0e4e0e71d725d51d11d782b6
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-02-23 00:49:40 +0100

    Merge `system/themes/flexible` into the new backend build chain

--- core-bundle/assets/scripts/core.js
+++ core-bundle/assets/scripts/core.js
@@ -517,7 +517,7 @@ window.Backend =
 	 * The theme path
 	 * @member {string}
 	 */
-	themePath: Contao.script_url + 'system/themes/' + Contao.theme + '/',
+	//themePath: Contao.script_url + 'system/themes/' + Contao.theme + '/',
 
 	/**
 	 * Open a modal window
