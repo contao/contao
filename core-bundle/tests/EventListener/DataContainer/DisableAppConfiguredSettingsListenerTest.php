e034b3ea3b91fce64c6782647dd0c9049eb7635a
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-02-23 12:52:22 +0100

    Remove support for service annotations (see #9479)
    Description
    -----------
    
    
    
    Commits
    -------
    
    1bf16c10 Remove support for service annotations
    56e22c35 Also remove doctrine/annotations

--- core-bundle/tests/EventListener/DataContainer/DisableAppConfiguredSettingsListenerTest.php
+++ core-bundle/tests/EventListener/DataContainer/DisableAppConfiguredSettingsListenerTest.php
@@ -16,8 +16,6 @@ use Contao\CoreBundle\EventListener\DataContainer\DisableAppConfiguredSettingsLi
 use Contao\CoreBundle\Tests\TestCase;
 use Contao\DataContainer;
 use Contao\Image;
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Doctrine\Common\Annotations\DocParser;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DisableAppConfiguredSettingsListenerTest extends TestCase
@@ -25,8 +23,6 @@ class DisableAppConfiguredSettingsListenerTest extends TestCase
     protected function tearDown(): void
     {
         unset($GLOBALS['TL_DCA']);
-
-        $this->resetStaticProperties([[AnnotationRegistry::class, ['failedToAutoload']], DocParser::class]);
 
         parent::tearDown();
     }
