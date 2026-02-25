ed96bc80a25dcdd37ef074824b31de6f1233c6eb
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-02-25 03:31:20 +0100

    Revert "`trans` insert tag - double colons only"
    This reverts commit 2daadf2d705c9985f65d4981fe4171b170de4e92.

--- core-bundle/src/InsertTag/Resolver/TranslationInsertTag.php
+++ core-bundle/src/InsertTag/Resolver/TranslationInsertTag.php
@@ -28,6 +28,11 @@ class TranslationInsertTag implements InsertTagResolverNestedResolvedInterface
     {
         $parameters = \array_slice($insertTag->getParameters()->all(), 2);
 
+        if (1 === \count($parameters) && str_contains($parameters[0], ':')) {
+            trigger_deprecation('contao/core-bundle', '5.3', 'Using single colons to pass parameters to the "trans" insert tag is deprecated and will no longer work in Contao 6. Use double colons instead.');
+            $parameters = explode(':', $parameters[0]);
+        }
+
         return new InsertTagResult(
             $this->translator->trans($insertTag->getParameters()->get(0), $parameters, $insertTag->getParameters()->get(1)),
         );
