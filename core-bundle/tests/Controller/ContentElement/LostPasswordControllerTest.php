1074e5b6b73c46827e3024d1b6f9f57774859d85
David Greminger <david.greminger@1up.io>
2026-02-25 14:58:25 +0100

    Add more test

--- core-bundle/tests/Controller/ContentElement/LostPasswordControllerTest.php
+++ core-bundle/tests/Controller/ContentElement/LostPasswordControllerTest.php
@@ -19,9 +19,11 @@ use Contao\CoreBundle\Controller\ContentElement\LostPasswordController;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\OptIn\OptIn;
 use Contao\CoreBundle\Routing\ContentUrlGenerator;
+use Contao\CoreBundle\Routing\PageFinder;
 use Contao\CoreBundle\String\SimpleTokenParser;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Contao\MemberModel;
+use Contao\PageModel;
 use Contao\System;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\MockObject\Stub;
@@ -32,6 +34,8 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+use Symfony\Component\RateLimiter\LimiterInterface;
+use Symfony\Component\RateLimiter\RateLimit;
 use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
@@ -73,6 +77,89 @@ class LostPasswordControllerTest extends ContentElementTestCase
         $response = $controller($request, $model, 'main');
 
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testRateLimitForSendPasswordLink(): void
+    {
+        $member = $this->createClassWithPropertiesStub(MemberModel::class);
+
+        $container = $this->getContainerWithFrameworkTemplate($member, true);
+        $container->set('contao.framework', $this->mockFrameworkWithTemplate($member));
+
+        $controller = new LostPasswordController(
+            $this->createStub(TranslatorInterface::class),
+            $this->createStub(RateLimiterFactoryInterface::class),
+            $this->createStub(OptIn::class),
+            $this->createStub(SimpleTokenParser::class),
+            $this->createStub(LoggerInterface::class),
+        );
+        $controller->setContainer($container);
+
+        $model = $this->createClassWithPropertiesStub(ContentModel::class);
+        $request = new Request();
+
+        $response = $controller($request, $model, 'main');
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function testSendPasswordLink(): void
+    {
+        $member = $this->createClassWithPropertiesStub(MemberModel::class, [
+            'id' => 1,
+            'email' => 'email@example.com',
+        ]);
+
+        $page = $this->createClassWithPropertiesStub(PageModel::class);
+
+        $container = $this->getContainerWithFrameworkTemplate($member, true, $page, '/foobar');
+        $container->set('contao.framework', $this->mockFrameworkWithTemplate($member));
+
+        $rateLimit = $this->createMock(RateLimit::class);
+        $rateLimit
+            ->expects($this->once())
+            ->method('isAccepted')
+            ->willReturn(true)
+        ;
+
+        $rateLimiter = $this->createMock(LimiterInterface::class);
+        $rateLimiter
+            ->expects($this->once())
+            ->method('consume')
+            ->willReturn($rateLimit)
+        ;
+
+        $rateLimiterFactory = $this->createMock(RateLimiterFactoryInterface::class);
+        $rateLimiterFactory
+            ->expects($this->once())
+            ->method('create')
+            ->willReturn($rateLimiter)
+        ;
+
+        $simpleTokenParser = $this->createMock(SimpleTokenParser::class);
+        $simpleTokenParser
+            ->expects($this->once())
+            ->method('parse')
+        ;
+
+        $controller = new LostPasswordController(
+            $this->createStub(TranslatorInterface::class),
+            $rateLimiterFactory,
+            $this->createStub(OptIn::class),
+            $simpleTokenParser,
+            $this->createStub(LoggerInterface::class),
+        );
+        $controller->setContainer($container);
+
+        $model = $this->createClassWithPropertiesStub(ContentModel::class, [
+            'reg_password' => 'Some text',
+        ]);
+
+        $request = new Request();
+
+        $response = $controller($request, $model, 'main');
+
+        $this->assertSame(Response::HTTP_FOUND, $response->getStatusCode());
     }
 
     /**
@@ -124,10 +211,17 @@ class LostPasswordControllerTest extends ContentElementTestCase
             ;
         }
 
+        $pageAdapter = $this->createAdapterStub(['findById']);
+        $pageAdapter
+            ->method('findById')
+            ->willReturn(null)
+        ;
+
         $framework = $this->createContaoFrameworkStub([
             MemberModel::class => $memberAdapter,
             Controller::class => $this->createAdapterStub(['loadDataContainer']),
             System::class => $systemAdapter,
+            PageModel::class => $pageAdapter,
         ]);
 
         $framework
@@ -138,7 +232,7 @@ class LostPasswordControllerTest extends ContentElementTestCase
         return $framework;
     }
 
-    private function getContainerWithFrameworkTemplate(MemberModel|null $member = null, bool|null $formIsValid = null): ContainerBuilder
+    private function getContainerWithFrameworkTemplate(MemberModel|null $member = null, bool|null $formIsValid = null, PageModel|null $page = null, string|null $redirectUrl = null): ContainerBuilder
     {
         $form = $this->createMock(FormInterface::class);
         $form
@@ -171,9 +265,24 @@ class LostPasswordControllerTest extends ContentElementTestCase
             ->willReturn($formField)
         ;
 
+        $pageFinder = $this->createMock(PageFinder::class);
+        $pageFinder
+            ->expects($page ? $this->once() : $this->never())
+            ->method('getCurrentPage')
+            ->willReturn($page)
+        ;
+
+        $contentUrlGenerator = $this->createMock(ContentUrlGenerator::class);
+        $contentUrlGenerator
+            ->expects($redirectUrl ? $this->atLeast(1) : $this->never())
+            ->method('generate')
+            ->willReturn((string) $redirectUrl)
+        ;
+
         $container = $this->getContainerWithContaoConfiguration();
         $container->set('contao.framework', $this->mockFrameworkWithTemplate($member));
-        $container->set('contao.routing.content_url_generator', $this->createStub(ContentUrlGenerator::class));
+        $container->set('contao.routing.page_finder', $pageFinder);
+        $container->set('contao.routing.content_url_generator', $contentUrlGenerator);
         $container->set('contao.cache.tag_manager', $this->createStub(CacheTagManager::class));
         $container->set('form.factory', $this->mockFormFactory(null !== $formIsValid ? $form : null));
         $container->set('security.password_hasher_factory', $this->createStub(PasswordHasherFactoryInterface::class));
