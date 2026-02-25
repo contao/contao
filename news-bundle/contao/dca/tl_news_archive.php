f49b3cb6d73a314c79901e43ad55d3691295b618
Lukas Bableck <lukas@bableck.dev>
2026-02-25 21:17:48 +0100

    Add option to change news JSON-LD type

--- news-bundle/contao/dca/tl_news_archive.php
+++ news-bundle/contao/dca/tl_news_archive.php
@@ -112,6 +112,14 @@ $GLOBALS['TL_DCA']['tl_news_archive'] = array
 			'eval'                    => array('mandatory'=>true, 'multiple'=>true),
 			'sql'                     => array('type'=>'blob', 'length'=>AbstractMySQLPlatform::LENGTH_LIMIT_BLOB, 'notnull'=>false),
 			'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
+		),
+		'jsonLdType' => array
+		(
+			'inputType'               => 'select',
+			'options'                 => array('Article', 'BlogPosting', 'NewsArticle'),
+			'default'                 => 'NewsArticle',
+			'eval'                    => array('tl_class'=>'w50'),
+			'sql'                     => array('type' => 'string', 'length' => 32, 'default' => 'NewsArticle')
 		)
 	)
 );
