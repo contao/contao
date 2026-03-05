501ae86d5edeec80b748e778ff070a129296c506
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-03-05 11:41:32 +0100

    Fixed deserialization and tests

--- core-bundle/src/DataContainer/ValueFormatter.php
+++ core-bundle/src/DataContainer/ValueFormatter.php
@@ -216,6 +216,8 @@ class ValueFormatter implements ResetInterface
 
     public function getLabel(string $table, string $field, mixed $value, mixed $dc): string
     {
+        $value = StringUtil::deserialize($value);
+
         // Translate UUIDs to paths
         if ('fileTree' === ($GLOBALS['TL_DCA'][$table]['fields'][$field]['inputType'] ?? null)) {
             $objFile = $this->framework->getAdapter(FilesModel::class)->findByUuid($value);
@@ -431,7 +433,7 @@ class ValueFormatter implements ResetInterface
             return StringUtil::deserialize($value, true);
         }
 
-        return StringUtil::deserialize($value);
+        return $value;
     }
 
     private function fetchForeignValue(string $table, string $field, mixed $id): mixed
