10a551602a9487dddf734d4037520075fd22aaa6
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-04-13 10:25:07 +0200

    Use `symfony/polyfill-php84` and update the usage of `Mysql::ATTR_MULTI_STATEMENTS`

--- manager-bundle/src/ContaoManager/Plugin.php
+++ manager-bundle/src/ContaoManager/Plugin.php
@@ -256,11 +256,10 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface, RoutingPlu
             return $extensionConfigs;
         }
 
-        $key = \defined('Pdo\Mysql::ATTR_MULTI_STATEMENTS') ? Mysql::ATTR_MULTI_STATEMENTS : \PDO::MYSQL_ATTR_MULTI_STATEMENTS;
         [$driver, $options] = $this->parseDbalDriverAndOptions($extensionConfigs, $container);
 
         // Do not add PDO options if custom options have been defined
-        if (isset($options[$key])) {
+        if (isset($options[Mysql::ATTR_MULTI_STATEMENTS])) {
             return $extensionConfigs;
         }
 
@@ -274,7 +273,7 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface, RoutingPlu
                 'connections' => [
                     'default' => [
                         'options' => [
-                            $key => false,
+                            Mysql::ATTR_MULTI_STATEMENTS => false,
                         ],
                     ],
                 ],
