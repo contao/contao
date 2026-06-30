923eb2340436dc2da908ab37b09b24467c912935
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-06-30 16:28:19 +0200

    Introduce the API Platform foundation (see #9939)
    Description
    -----------
    
    Just the simple foundation to require API Platform in the latest version and foundational configuration.
    
    Commits
    -------
    
    6f3da91b Introduce API Platform foundation
    d0618a68 Cleanup plugin registration

--- api-bundle/src/ContaoManager/Plugin.php
+++ api-bundle/src/ContaoManager/Plugin.php
@@ -12,22 +12,43 @@ declare(strict_types=1);
 
 namespace Contao\ApiBundle\ContaoManager;
 
+use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Contao\ApiBundle\ContaoApiBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * @internal
  */
-class Plugin implements BundlePluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface, RoutingPluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
+            BundleConfig::create(ApiPlatformBundle::class),
             BundleConfig::create(ContaoApiBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class]),
+                ->setLoadAfter([ApiPlatformBundle::class, ContaoCoreBundle::class]),
         ];
+    }
+
+    public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel): RouteCollection|null
+    {
+        $path = __DIR__.'/../../config/routes.yaml';
+
+        return $resolver->resolve($path)->load($path);
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
+    {
+        $loader->load(__DIR__.'/../../skeleton/config/config.yaml');
+        $loader->load(__DIR__.'/../../skeleton/config/services.yaml');
     }
 }
