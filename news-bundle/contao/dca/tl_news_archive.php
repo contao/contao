7be109d8d2584c6606bc3b2a03f214b13da18263
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-07-01 11:27:21 +0200

    Move the field to the "expert settings" palette

--- news-bundle/contao/dca/tl_news_archive.php
+++ news-bundle/contao/dca/tl_news_archive.php
@@ -63,7 +63,7 @@ $GLOBALS['TL_DCA']['tl_news_archive'] = array
 	'palettes' => array
 	(
 		'__selector__'                => array('protected'),
-		'default'                     => '{title_legend},title,jumpTo,jsonLdType;{protected_legend:hide},protected'
+		'default'                     => '{title_legend},title,jumpTo;{protected_legend:hide},protected;{expert_legend:hide},jsonLdType'
 	),
 
 	// Sub-palettes
@@ -118,7 +118,7 @@ $GLOBALS['TL_DCA']['tl_news_archive'] = array
 			'inputType'               => 'select',
 			'options'                 => array('Article', 'BlogPosting', 'NewsArticle'),
 			'eval'                    => array('tl_class'=>'w50'),
-			'sql'                     => array('type' => 'string', 'length' => 32, 'default' => 'NewsArticle')
+			'sql'                     => array('type' => 'string', 'length' => 32, 'default'=>'NewsArticle')
 		)
 	)
 );
