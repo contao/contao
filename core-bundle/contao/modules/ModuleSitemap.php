fe356e41147fb58bd714a4a3ee405c25adb2e076
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-07-06 17:17:19 +0200

    Don‘t apply `specialcharsUrl()` to wildcard URLs

--- core-bundle/contao/modules/ModuleSitemap.php
+++ core-bundle/contao/modules/ModuleSitemap.php
@@ -37,7 +37,7 @@ class ModuleSitemap extends Module
 			$objTemplate->title = $this->headline;
 			$objTemplate->id = $this->id;
 			$objTemplate->link = $this->name;
-			$objTemplate->href = StringUtil::specialcharsUrl(System::getContainer()->get('router')->generate('contao_backend', array('do'=>'themes', 'table'=>'tl_module', 'act'=>'edit', 'id'=>$this->id)));
+			$objTemplate->href = System::getContainer()->get('router')->generate('contao_backend', array('do'=>'themes', 'table'=>'tl_module', 'act'=>'edit', 'id'=>$this->id));
 
 			return $objTemplate->parse();
 		}
