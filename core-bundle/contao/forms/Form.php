87fa870b6a92e83beb8269bacbb612f7ff5cc28b
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-07-07 17:37:50 +0200

    Generate the form data XML file using `DOMDocument`

--- core-bundle/contao/forms/Form.php
+++ core-bundle/contao/forms/Form.php
@@ -513,15 +513,39 @@ class Form extends Hybrid
 			// Attach XML file
 			if ($this->format == 'xml')
 			{
-				// Encode the values (see #6053)
-				array_walk_recursive($fields, static function (&$value) { $value = htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE | ENT_XML1); });
+				$dom = new \DOMDocument('1.0', $this->charset);
+				$dom->xmlStandalone = true;
+				$dom->formatOutput = true;
 
-				// TODO: This template only exists for backwards compatibility and needs to be replaced with a DOM object in Contao 6
-				$objTemplate = new FrontendTemplate('form_xml');
-				$objTemplate->fields = $fields;
-				$objTemplate->charset = System::getContainer()->getParameter('kernel.charset');
+				$form = $dom->createElement('form');
+				$dom->appendChild($form);
 
-				$email->attachFileFromString($objTemplate->parse(), 'form.xml', 'application/xml');
+				foreach ($fields as $field) {
+					$fieldElement = $dom->createElement('field');
+
+					$nameElement = $dom->createElement('name');
+					$nameElement->textContent = $field['name'];
+					$fieldElement->appendChild($nameElement);
+
+					foreach ($field['values'] as $value) {
+						$valueElement = $dom->createElement('value');
+						$valueElement->textContent = $value;
+						$fieldElement->appendChild($valueElement);
+					}
+
+					$form->appendChild($fieldElement);
+				}
+
+				$dtd = <<<'DTD'
+					<!DOCTYPE form [
+					  <!ELEMENT form (field)+>
+					  <!ELEMENT field (name, value+)>
+					  <!ELEMENT name (#PCDATA)>
+					  <!ELEMENT value (#PCDATA)>
+					]>
+					DTD;
+
+				$email->attachFileFromString(str_replace('<form>', "$dtd\n<form>", $dom->saveXML()), 'form.xml', 'application/xml');
 			}
 
 			// Attach CSV file
