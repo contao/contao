df0d6d2b89b640f5227713c7ffb9dd4c50a04590
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-07-10 14:33:47 +0200

    Revert "Remove deprecated limit-height.js"
    This reverts commit 495c506cfa2f1ca8323bfc7d291a932fb43065e8.

--- core-bundle/assets/backend.js
+++ core-bundle/assets/backend.js
@@ -7,6 +7,7 @@ import TextareaAutogrow from 'stimulus-textarea-autogrow';
 
 import './scripts/mootao.js';
 import './scripts/core.js';
+import './scripts/limit-height.js';
 
 import './styles/backend.pcss';
 
