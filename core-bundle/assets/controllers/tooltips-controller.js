98f304b6b1d052deab0bd62b343b661609ef50de
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-07-15 11:02:47 +0200

    CS

--- core-bundle/assets/controllers/tooltips-controller.js
+++ core-bundle/assets/controllers/tooltips-controller.js
@@ -109,7 +109,7 @@ export default class extends Controller {
     #positionTooltip() {
         const anchor = this.#isTouch ? this.#current : Position.pointerAnchor(this.#pointer.x, this.#pointer.y);
 
-        Position.compute(anchor, this.popupTarget, this.#arrowEl(), 'bottom-start', this.#isTouch ? 6 : 20);
+        Position.compute(anchor, this.popupTarget, this.#arrowEl(), 'bottom-start', this.#isTouch ? 6 : 18);
     }
 
     #arrowEl() {
