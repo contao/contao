aba5c99ecd577c859de8de7e5baef62a679560ab
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2026-07-22 20:05:37 +0200

    Apply `article-image` to the article icon

--- core-bundle/contao/dca/tl_article.php
+++ core-bundle/contao/dca/tl_article.php
@@ -300,9 +300,10 @@ class tl_article extends Backend
 		}
 
 		$attributes = sprintf(
-			'data-icon="%s" data-icon-disabled="%s"',
+			'data-icon="%s" data-icon-disabled="%s" class="%s"',
 			$row['protected'] ? 'articles_2.svg' : 'articles.svg',
 			$row['protected'] ? 'articles_3.svg' : 'articles_1.svg',
+			'article-image',
 		);
 
 		$href = System::getContainer()->get('router')->generate('contao_backend_preview', array('page'=>$row['pid'], 'article'=>($row['alias'] ?: $row['id'])));
