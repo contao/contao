e3285ea4a4935cbf1d42b6830d46b66b935a120e
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-08-14 00:01:44 +0200

    Fix phpstan marking class as unused because of missing test in main

---
+++ core-bundle/tests/DataContainer/DynamicPtableTraitTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\Tests\DataContainer;
+
+use Contao\CoreBundle\DataContainer\DynamicPtableTrait;
+use Doctrine\DBAL\Connection;
+use PHPUnit\Framework\TestCase;
+
+class DynamicPtableTraitTest extends TestCase
+{
+    public function testFindsTheDynamicParent(): void
+    {
+        $connection = $this->createMock(Connection::class);
+        $connection
+            ->expects($this->once())
+            ->method('fetchAllAssociative')
+            ->willReturn([['id' => 5, 'pid' => 3, 'ptable' => 'tl_article']])
+        ;
+
+        $subject = new class {
+            use DynamicPtableTrait;
+
+            /**
+             * @return array{0: string, 1: int}
+             */
+            public function findParent(Connection $connection, string $table, int $id): array
+            {
+                return $this->getParentTableAndId($connection, $table, $id);
+            }
+        };
+
+        $this->assertSame(
+            ['tl_article', 3],
+            $subject->findParent($connection, 'tl_content', 5),
+        );
+    }
+}
