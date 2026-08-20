b0e25d3ae30eaa3695ebfc468cd61fd5ff4c78c4
Andreas Schempp <andreas.schempp@terminal42.ch>
2026-08-20 10:38:13 +0200

    Allow bundles to load filesystem config

---
+++ core-bundle/src/DependencyInjection/Filesystem/AbstractFilesystemBundle.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Contao\CoreBundle\DependencyInjection\Filesystem;
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * @experimental
+ */
+abstract class AbstractFilesystemBundle extends AbstractBundle implements ConfigureFilesystemInterface
+{
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if ('' === $this->extensionAlias) {
+            $this->extensionAlias = Container::underscore(preg_replace('/Bundle$/', '', $this->getName()));
+        }
+
+        return $this->extension ??= new FilesystemBundleExtension($this, $this->extensionAlias);
+    }
+}
