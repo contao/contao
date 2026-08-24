af7440965c09d89d00cf4f1189ac7f4d43cde6b1
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-08-24 22:28:26 +0330

    Set blank option label to "auto"
    Co-authored-by: Martin Auswöger <martin@auswoeger.com>

--- core-bundle/contao/dca/tl_image_size.php
+++ core-bundle/contao/dca/tl_image_size.php
@@ -182,7 +182,7 @@ $GLOBALS['TL_DCA']['tl_image_size'] = array
 		(
 			'inputType'               => 'select',
 			'options'                 => array('high', 'low'),
-			'eval'                    => array('tl_class'=>'w50 clr', 'includeBlankOption'=>true),
+			'eval'                    => array('tl_class'=>'w50 clr', 'includeBlankOption'=>true, 'blankOptionLabel'=>'auto'),
 			'sql'                     => array('type'=>'string', 'length'=>8, 'notnull'=>false)
 		),
 		'decoding' => array
