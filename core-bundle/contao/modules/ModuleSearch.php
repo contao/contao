9519c1049462da35054c114d07efdeecaef32d06
Martin Auswöger <martin@auswoeger.com>
2026-08-25 08:36:53 +0200

    Merge commit from fork

--- core-bundle/contao/modules/ModuleSearch.php
+++ core-bundle/contao/modules/ModuleSearch.php
@@ -235,8 +235,8 @@ class ModuleSearch extends Module
 			{
 				$objTemplate = new FrontendTemplate($this->searchTpl ?: 'search_default');
 				$objTemplate->setData($arrResult[$i]);
-				$objTemplate->href = $arrResult[$i]['url'];
-				$objTemplate->link = $arrResult[$i]['title'];
+				$objTemplate->href = StringUtil::specialchars($arrResult[$i]['url']);
+				$objTemplate->link = StringUtil::specialchars($arrResult[$i]['title']);
 				$objTemplate->url = StringUtil::specialchars(urldecode($arrResult[$i]['url']), true, true);
 				$objTemplate->title = StringUtil::specialchars(StringUtil::stripInsertTags($arrResult[$i]['title']));
 				$objTemplate->relevance = \sprintf($GLOBALS['TL_LANG']['MSC']['relevance'], number_format($arrResult[$i]['relevance'] / $arrResult[0]['relevance'] * 100, 2) . '%');
@@ -293,7 +293,7 @@ class ModuleSearch extends Module
 			}
 
 			$figureMeta = new Metadata(array_filter(array(
-				Metadata::VALUE_CAPTION => $v['https://schema.org/primaryImageOfPage']['caption'] ?? null,
+				Metadata::VALUE_CAPTION => Input::encodeInput($v['https://schema.org/primaryImageOfPage']['caption'] ?? '', InputEncodingMode::sanitizeHtml),
 				Metadata::VALUE_TITLE => $v['https://schema.org/primaryImageOfPage']['name'] ?? null,
 				Metadata::VALUE_ALT => $v['https://schema.org/primaryImageOfPage']['alternateName'] ?? null,
 			)));
