296492089a7b7c16d46f60fec2bd6efc9dcfe3d0
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2026-09-09 18:21:19 +0200

    update url highlight to v4

--- core-bundle/src/Twig/Runtime/AutolinkRuntime.php
+++ core-bundle/src/Twig/Runtime/AutolinkRuntime.php
@@ -13,19 +13,29 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Twig\Runtime;
 
 use Twig\Extension\RuntimeExtensionInterface;
+use VStelmakh\UrlHighlight\Format;
+use VStelmakh\UrlHighlight\Highlighter\CallbackHighlighter;
+use VStelmakh\UrlHighlight\Url;
 use VStelmakh\UrlHighlight\UrlHighlight;
 
 final class AutolinkRuntime implements RuntimeExtensionInterface
 {
+    private readonly CallbackHighlighter $highlighter;
+
     /**
      * @internal
      */
     public function __construct(private readonly UrlHighlight $urlHighlight)
     {
+        $this->highlighter = new CallbackHighlighter(fn (Url $url) => sprintf(
+            '<a href="%s" target="_blank" rel="noreferrer noopener">%s</a>',
+            htmlspecialchars($url->toHref('https')),
+            htmlspecialchars($url->full),
+        ));
     }
 
     public function linkUrls(string $text): string
     {
-        return $this->urlHighlight->highlightUrls($text);
+        return $this->urlHighlight->highlight($text, $this->highlighter, Format::HtmlEncoded);
     }
 }
