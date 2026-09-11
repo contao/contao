ff55ec616415f10224ce7fb2838ece7676fc38e6
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2026-09-11 22:10:06 +0200

    Narrow down the static cache to ignore
    Co-authored-by: Martin Auswöger <martin@auswoeger.com>

--- core-bundle/tests/PhpunitExtension/GlobalStateWatcher.php
+++ core-bundle/tests/PhpunitExtension/GlobalStateWatcher.php
@@ -223,7 +223,7 @@ final class GlobalStateWatcher implements Extension
                 'Symfony\Component\VarDumper\\',
                 'Symfony\Component\Yaml\\',
                 'Symfony\Polyfill\DeepClone\DeepClone',
-                'VStelmakh\UrlHighlight\\',
+                'VStelmakh\UrlHighlight\Matcher\Domains\TopLevelDomains',
                 'Webmozart\PathUtil\\',
             ] as $ignorePrefix) {
                 if (0 === strncmp($ignorePrefix, $class, \strlen($ignorePrefix))) {
